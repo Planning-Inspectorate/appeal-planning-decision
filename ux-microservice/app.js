3fbea05e6a86e920c56f74830eb62b9636b65090
Jon Morgan <morgz.dev74@gmail.com>
2020-10-19 11:47:58 +0100

    chore(setup): Add more templates for sample PINS

--- ux-microservice/app.js
+++ ux-microservice/app.js
@@ -12,7 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, "/public")));
 app.use(
   "/node_modules/govuk-frontend",
   express.static(path.join(__dirname, "/node_modules/govuk-frontend"))
