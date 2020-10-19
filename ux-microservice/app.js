b93a65d0551f47c0d61fe19519e2132c99f7a3cd
Jon Morgan <morgz.dev74@gmail.com>
2020-10-19 14:32:31 +0100

    chore(setup): Add lint rules to UX service

--- ux-microservice/app.js
+++ ux-microservice/app.js
@@ -1,39 +1,39 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var nunjucks = require("nunjucks");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const nunjucks = require('nunjucks');
 
-var indexRouter = require("./routes/index");
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/public", express.static(path.join(__dirname, "/public")));
+app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use(
-  "/node_modules/govuk-frontend",
-  express.static(path.join(__dirname, "/node_modules/govuk-frontend"))
+  '/node_modules/govuk-frontend',
+  express.static(path.join(__dirname, '/node_modules/govuk-frontend')),
 );
 
-app.use("/", indexRouter);
-app.set("views", __dirname + "/views");
-app.set("view engine", "njk");
+app.use('/', indexRouter);
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'njk');
 
-const isDev = app.get("env") === "development";
+const isDev = app.get('env') === 'development';
 
 const nunjucksConfig = {
   autoescape: true,
-  noCache: true,
+  noCache: isDev,
   watch: isDev,
   express: app,
 };
 
 const viewPaths = [
-  path.join(__dirname, "/node_modules/govuk-frontend"),
-  path.join(__dirname, "/views"),
+  path.join(__dirname, '/node_modules/govuk-frontend'),
+  path.join(__dirname, '/views'),
 ];
 
 nunjucks.configure(viewPaths, nunjucksConfig);
--- ux-microservice/routes/users.js
+++ ux-microservice/routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
+
+  next();
 });
 
 module.exports = router;
