e231381a74cd07288a2479cf72ea812a408b9332
Simon Emms <simon@simonemms.com>
2020-10-20 10:22:17 +0100

    feat(common): add container registry to terraform
    The "common" pattern  exists outside the environment structure to provide resources that can be
    accessed by any environment

---
+++ infrastructure/common/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "pins-uk-terraform-rg"
+    storage_account_name = "pinsf4terraform"
+    container_name = "tfstate"
+    key = "common.tfstate"
+  }
+}
+
+provider "azurerm" {
+  version = "~> 2.31.1"
+  features {}
+}
+
+provider "github" {
+  version = "~> 2.9"
+  organization = var.github_org_name
+  token = var.github_token
+}
