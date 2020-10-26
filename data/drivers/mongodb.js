47b4ed74b2b1332ca4c524bfcc8c55394c6f69aa
Simon Emms <simon@simonemms.com>
2020-10-26 12:42:28 +0000

    chore(setup): configure docker-compose with data ingestor

---
+++ data/drivers/mongodb.js
@@ -0,0 +1,42 @@
+/**
+ * mysql
+ */
+
+/* Node modules */
+
+/* Third-party modules */
+const { MongoClient } = require('mongodb')
+
+/* Files */
+
+module.exports = class MongoDB {
+  constructor() {
+    this.opts = {
+      url: process.env.MONGODB_URL,
+    };
+  }
+
+  async auth() {
+    this.db = new MongoClient(this.opts.url, {
+      useUnifiedTopology: true,
+    })
+    await this.db.connect();
+
+    /* Do a dummy query to check connection ok */
+    await this.db.db('admin').command({ ping: 1 })
+  }
+
+  async close() {
+    await this.db.close();
+  }
+
+  async insertBulk(collection, dataArr) {
+
+    await this.db.db().collection(collection)
+      .insertMany(dataArr)
+  }
+
+  async truncate(table) {
+    await this.db.db().dropDatabase();
+  }
+};
