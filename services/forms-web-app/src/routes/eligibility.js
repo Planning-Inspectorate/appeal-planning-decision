658d418dbbb65a3e256545db5f5f8adcc12b7fda
Jon Morgan <morgz.dev74@gmail.com>
2020-10-27 16:15:06 +0000

    fix(no-decision) - Create controllers for routes

--- services/forms-web-app/src/routes/eligibility.js
+++ services/forms-web-app/src/routes/eligibility.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const eligibilityController = require('../controllers/eligibility');
 
 const router = express.Router();
 
 /* GET eligibility no decision page. */
-router.get('/no-decision', (req, res) => {
-  res.render('eligibility/no-decision');
-});
+router.get('/no-decision', eligibilityController.getNoDecision);
 
 module.exports = router;
