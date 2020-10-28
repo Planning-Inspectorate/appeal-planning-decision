278919dcb4808fd45f66bccf17f4c3b698ae1183
Marek Muchlinski <marek.muchlinski@foundry4.com>
2020-10-28 09:16:34 +0000

    !!GitHub_Desktop<feature/eligibility-decision-date-ucd-744>

--- services/forms-web-app/src/routes/eligibility.js
+++ services/forms-web-app/src/routes/eligibility.js
@@ -7,4 +7,9 @@ router.get('/no-decision', (req, res) => {
   res.render('eligibility/no-decision');
 });
 
+/* GET eligibility no decision page. */
+router.get('/decision-date', (req, res) => {
+  res.render('eligibility/decision-date');
+});
+
 module.exports = router;
