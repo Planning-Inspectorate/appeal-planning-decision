1bf381dc179ba38d4d659d106fbb0522726a297b
Jon Morgan <morgz.dev74@gmail.com>
2020-10-29 13:30:03 +0000

    fix(eligibility-controller-unit-tests) - Added starter unit tests for eligibility controller

--- forms-web-app/tests/unit/controllers/eligibility.test.js
+++ forms-web-app/tests/unit/controllers/eligibility.test.js
@@ -1,5 +1,25 @@
+const eligibilityController = require('../../../src/controllers/eligibility');
+const { mockReq, mockRes } = require('../mocks');
+
+const req = mockReq();
+const res = mockRes();
+
 describe('Eligibility Controller Tests', () => {
-  it('Test the getNoDecision method returns the correct status', () => {
-    expect(true).toBe(true);
+  it('Test the getNoDecision method calls the correct template', () => {
+    eligibilityController.getNoDecision(req, res);
+
+    expect(res.render).toHaveBeenCalledWith('eligibility/no-decision');
+  });
+
+  it('Test the getDecisionDate method calls the correct template', () => {
+    eligibilityController.getDecisionDate(req, res);
+
+    expect(res.render).toHaveBeenCalledWith('eligibility/decision-date');
+  });
+
+  it('Test the getDecisionDateExpired method calls the correct template', () => {
+    eligibilityController.getDecisionDateExpired(req, res);
+
+    expect(res.render).toHaveBeenCalledWith('eligibility/decision-date-expired');
   });
 });
