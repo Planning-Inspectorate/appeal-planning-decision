dff99975bf2eab989b85d7f32f3b93b1bc2d3111
Jon Morgan <morgz.dev74@gmail.com>
2020-10-30 15:01:49 +0000

    feature(decision-date-validation) - Add validation and routing for decision date checks

--- forms-web-app/src/routes/eligibility.js
+++ forms-web-app/src/routes/eligibility.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const eligibilityController = require('../controllers/eligibility');
+const { decisionDateValidationRules, validator } = require('./validators/validator');
 
 const router = express.Router();
 
@@ -8,7 +10,13 @@ router.get('/no-decision', eligibilityController.getNoDecision);
 
 /* GET eligibility decision date input page. */
 router.get('/decision-date', eligibilityController.getDecisionDate);
-router.post('/decision-date', eligibilityController.postDecisionDate);
+
+router.post(
+  '/decision-date',
+  decisionDateValidationRules(),
+  validator,
+  eligibilityController.postDecisionDate
+);
 
 /* GET eligibility decision date out page. */
 router.get('/decision-date-expired', eligibilityController.getDecisionDateExpired);
