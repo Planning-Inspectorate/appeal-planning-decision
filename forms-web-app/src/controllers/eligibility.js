ca0560d5303cdddbdf35c88348247172925cc227
Marek Muchlinski <marek.muchlinski@foundry4.com>
2020-10-30 09:43:25 +0000

    feat(decision-date) : form validation added but not working

--- forms-web-app/src/controllers/eligibility.js
+++ forms-web-app/src/controllers/eligibility.js
@@ -6,6 +6,22 @@ exports.getDecisionDate = (req, res) => {
   res.render('eligibility/decision-date');
 };
 
+exports.postDecisionDate = (req, res) => {
+
+  var inputDate = new Date(body.decison-date-year, body.decision-date-month - 1, body.decision-date-day);
+  var currentDate = new Date();
+  var deadlineDate = new Date() - 84;
+
+  if (inputDate instanceof Date && isNaN(date.valueOf())){ throw new Error('Date is invalid')} 
+  else if (inputDate > currentDate) { throw new Error('Date cannot be in the future')}
+  else if (inputDate < deadlineDate) {res.render('eligibility/decision-date-expired')}
+  else { {res.render('eligibility/appeal-in-time')  }
+
+
+}
+ 
+};
+
 exports.getDecisionDateExpired = (req, res) => {
   res.render('eligibility/decision-date-expired');
 };
