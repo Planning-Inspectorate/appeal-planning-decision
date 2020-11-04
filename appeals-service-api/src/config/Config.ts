e4e7b633d5a82986c211850990a34d63503fdb40
Simon Emms <simon@simonemms.com>
2020-11-04 17:31:29 +0000

    feat(appeals-service): remove old appeals-service
    This is the start of the refactoring

--- appeals-service-api/src/config/Config.ts
+++
@@ -1,9 +0,0 @@
-export default class Config {
-  public static NAME: String = process.env.APD_API_NAME ?? 'Appeal Planning Decision API';
-
-  public static get PORT() {
-    const tmpPort = Number(process.env.APD_API_PORT);
-
-    return Number.isNaN(tmpPort) ? 4000 : tmpPort;
-  }
-}
