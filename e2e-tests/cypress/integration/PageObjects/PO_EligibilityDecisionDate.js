0c23b49af4ca682e6e5f9a084ff0bedf4ec9e588
Dan Watkinson <dan.watkinson@gmail.com>
2020-11-12 16:56:37 +0000

    test: added basic eligibility decision tests

--- e2e-tests/cypress/integration/PageObjects/PO_EligibilityDecisionDate.js
+++ e2e-tests/cypress/integration/PageObjects/PO_EligibilityDecisionDate.js
@@ -5,6 +5,7 @@ class PO_EligibilityDecisionDate {
     }
 
     validatePageTitle() {
+        //cy.title().should('eq', 'Local Planning authority details') -> Actual, delete the below line
         cy.title().should('eq', 'GOV.UK - The best place to find government services and information')
     }
 
@@ -14,6 +15,7 @@ class PO_EligibilityDecisionDate {
     }
 
     pageHeaderlink() {
+        //const headerLink = cy.get('a[href="Appeal a householder planning decision"]')
         const headerLink = cy.get('.govuk-header__content > .govuk-header__link')
         assert.exists(headerLink, 'Appeal a householder planning decision exists')
     }
@@ -27,10 +29,13 @@ class PO_EligibilityDecisionDate {
 
     validateText() {
         const captionTextStart = cy.get('.govuk-caption-l')
+        // assert.equal(captionTextStart, 'Before you start', 'actual and expected text match')
         assert.exists(captionTextStart, 'Before you start')
+        //const captionText = cy.get('.govuk-fieldset__heading')
 
         const descisionDateText = cy.get('.govuk-fieldset__heading')
         assert.exists(descisionDateText, "What's is the decision text exists")
+        // .should('eq', "What's the decision date on the letter from the local planning department?​")
         const dateText = cy.get('#decision-date-hint')
         assert.exists(dateText, 'For example hint exists')
 
@@ -91,11 +96,20 @@ class PO_EligibilityDecisionDate {
         expect(dateError2).to.exist
     }
 
-
     dateFields(Day, Month, Year) {
+      if (Day) {
         cy.get('#decision-date-day').type(Day)
-        Day.type(day)
-          cy.debug()
+      }
+      if (Month) {
+        cy.get('#decision-date-month').type(Month)
+      }
+      if (Year) {
+        cy.get('#decision-date-year').type(Year)
+      }
+    }
+
+    dateFieldsempty(Day, Month, Year) {
+        cy.get('#decision-date-day').type(Day)
         cy.get('#decision-date-month').type(Month)
         cy.get('#decision-date-year').type(Year)
     }
@@ -103,6 +117,55 @@ class PO_EligibilityDecisionDate {
     continueBtn() {
         const continueBtn = cy.get('.govuk-button').click()
     }
+
+
+//No decision date PO's
+
+navigateToNoDecisionPage() {
+    const eligDateURL = cy.visit("/eligibility/no-decision")
+}
+
+serviceText() {
+    cy.wait(2000)
+    const serviceTxt = cy.get('.govuk-heading-l')
+    assert.exists(serviceTxt, 'This service is only for householder content exists')
+    const contentTxt = cy.get('.govuk-body')
+    assert.exists(contentTxt, 'If you applied for householder planning permission content exists')
+}
+
+appealPlanningDecLink() {
+    const appealPlanningDecLnk = cy.get('.govuk-body > a')
+    assert.exists(appealPlanningDecLnk, 'https://acp.planninginspectorate.gov.uk/ is the link')
+    cy.wait(2000)
+}
+
+appealsCaseWorkPageURL() {
+    const caseWorkPageURL = cy.get('.govuk-body > a')
+    assert.exists(caseWorkPageURL, 'the URL points to the correct site')
+}
+appealsCaseworkPortalPage() {
+    cy.visit("https://acp.planninginspectorate.gov.uk/")
+    // cy.pause()
+
+}
+appealsCaseworkPortalPageLogiIn() {
+    const logInRegTxt = cy.get('#cphMainContent_LoginUser_LoginLegend')
+    cy.wait(2000)
+    assert.exists(logInRegTxt, 'Log in or Register page displayed')
+}
+
+navigateToLocalPlanDept(){
+    const localplandept = cy.visit('https://appeals-dev.planninginspectorate.gov.uk/eligibility/planning-department')
+}
+localPlanDeptText(){
+    const plandept = cy.get('.govuk-label')
+    assert.exists(plandept,'What is the name of the local planning department')
+}
+
+deadlinePassedPageText(){
+    const deadlinePassedText = cy.get('.govuk-heading-l')
+}
+
 
 }
 export default PO_EligibilityDecisionDate
