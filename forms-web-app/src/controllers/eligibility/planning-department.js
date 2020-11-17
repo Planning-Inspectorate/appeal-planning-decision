480d281f2aa8c5b6c0ed291660a92c95cc63e3eb
Simon Emms <simon@simonemms.com>
2020-11-17 09:17:19 +0000

    fix(lpa-list): get lpa list from appeals service api

--- forms-web-app/src/controllers/eligibility/planning-department.js
+++ forms-web-app/src/controllers/eligibility/planning-department.js
@@ -1,16 +1,14 @@
+const { getLPAList } = require('../../lib/appeals-api-wrapper');
+
 const PLANNING_DEPARTMENT = 'eligibility/planning-department';
 
-const LPAS = [
-  { id: 'E60000281', name: 'Adur LPA', inTrial: true, england: true, wales: false },
-  { id: 'E60000019', name: 'Allerdale LPA', inTrial: false, england: true, wales: false },
-  { id: 'E60000077', name: 'Amber Valley LPA', inTrial: false, england: true, wales: false },
-  { id: 'E60000282', name: 'Arun LPA', inTrial: false, england: true, wales: false },
-  { id: 'E60000106', name: 'Ashfield LPA', inTrial: false, england: true, wales: false },
-  { id: 'E60000253', name: 'Ashford LPA', inTrial: false, england: true, wales: false },
-];
-
 /* Get planning department  */
-exports.getPlanningDepartment = (req, res) => {
-  const data = JSON.stringify(LPAS.map(({ name }) => name));
-  res.render(PLANNING_DEPARTMENT, { data });
+exports.getPlanningDepartment = async (req, res) => {
+  const { data: lpaList } = await getLPAList();
+
+  const data = lpaList.map(({ name }) => name);
+
+  res.render(PLANNING_DEPARTMENT, {
+    data,
+  });
 };
--- forms-web-app/tests/unit/controllers/eligibility/planning-department.test.js
+++ forms-web-app/tests/unit/controllers/eligibility/planning-department.test.js
@@ -1,18 +1,33 @@
+jest.mock('../../../../src/lib/appeals-api-wrapper');
+
 const planningDepartmentController = require('../../../../src/controllers/eligibility/planning-department');
 const { mockReq, mockRes } = require('../../mocks');
+const { getLPAList } = require('../../../../src/lib/appeals-api-wrapper');
 
 const req = mockReq();
 const res = mockRes();
-const expectedData = {
-  data: '["Adur LPA","Allerdale LPA","Amber Valley LPA","Arun LPA","Ashfield LPA","Ashford LPA"]',
-};
 
 describe('controller/eligibility/planning-department', () => {
   describe('getPlanningDepartment', () => {
-    it('should call the correct template', () => {
-      planningDepartmentController.getPlanningDepartment(req, res);
+    it('should call the correct template', async () => {
+      const sampleLpa = [
+        {
+          name: 'lpa1',
+        },
+        {
+          name: 'lpa2',
+        },
+      ];
 
-      expect(res.render).toHaveBeenCalledWith('eligibility/planning-department', expectedData);
+      getLPAList.mockResolvedValue({
+        data: sampleLpa,
+      });
+
+      await planningDepartmentController.getPlanningDepartment(req, res);
+
+      const data = sampleLpa.map(({ name }) => name);
+
+      expect(res.render).toBeCalledWith('eligibility/planning-department', { data });
     });
   });
 });
