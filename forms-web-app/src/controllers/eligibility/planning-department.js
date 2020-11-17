370dcc84c184be5ac63f286fac3b4960de9f8ec6
mmuchlinski <marek.muchlinski@foundry4.com>
2020-11-17 08:24:17 +0000

    feat(lpa-list): add accessible autocomplete for lpa list

--- forms-web-app/src/controllers/eligibility/planning-department.js
+++ forms-web-app/src/controllers/eligibility/planning-department.js
@@ -1,3 +1,16 @@
+const PLANNING_DEPARTMENT = 'eligibility/planning-department';
+
+const LPAS = [
+  { id: 'E60000281', name: 'Adur LPA', inTrial: true, england: true, wales: false },
+  { id: 'E60000019', name: 'Allerdale LPA', inTrial: false, england: true, wales: false },
+  { id: 'E60000077', name: 'Amber Valley LPA', inTrial: false, england: true, wales: false },
+  { id: 'E60000282', name: 'Arun LPA', inTrial: false, england: true, wales: false },
+  { id: 'E60000106', name: 'Ashfield LPA', inTrial: false, england: true, wales: false },
+  { id: 'E60000253', name: 'Ashford LPA', inTrial: false, england: true, wales: false },
+];
+
+/* Get planning department  */
 exports.getPlanningDepartment = (req, res) => {
-  res.render('eligibility/planning-department');
+  const data = JSON.stringify(LPAS.map(({ name }) => name));
+  res.render(PLANNING_DEPARTMENT, { data });
 };
--- forms-web-app/tests/unit/controllers/eligibility/planning-department.test.js
+++ forms-web-app/tests/unit/controllers/eligibility/planning-department.test.js
@@ -3,13 +3,16 @@ const { mockReq, mockRes } = require('../../mocks');
 
 const req = mockReq();
 const res = mockRes();
+const expectedData = {
+  data: '["Adur LPA","Allerdale LPA","Amber Valley LPA","Arun LPA","Ashfield LPA","Ashford LPA"]',
+};
 
 describe('controller/eligibility/planning-department', () => {
   describe('getPlanningDepartment', () => {
     it('should call the correct template', () => {
       planningDepartmentController.getPlanningDepartment(req, res);
 
-      expect(res.render).toHaveBeenCalledWith('eligibility/planning-department');
+      expect(res.render).toHaveBeenCalledWith('eligibility/planning-department', expectedData);
     });
   });
 });
