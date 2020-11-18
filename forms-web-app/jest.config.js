7948b7d32d822196ed032c8cab27ce5801bd6eea
Christopher Moss <chris@a6software.co.uk>
2020-11-18 14:50:53 +0000

    build(jest): add minimum coverage threshhold and reporting

--- forms-web-app/jest.config.js
+++ forms-web-app/jest.config.js
@@ -4,4 +4,15 @@ module.exports = {
   clearMocks: true,
   testEnvironment: 'node',
   setupFiles: ['./tests/setupTests.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js'],
+  coverageReporters: ['json', 'html', 'text', 'text-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 76,
+      functions: 80,
+      lines: 54,
+      statements: -175,
+    },
+  },
 };
