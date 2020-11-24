3fbcddd742711a02705ca334c49a018fc81dcc8c
Dan Watkinson <dan.watkinson@gmail.com>
2020-11-24 15:17:11 +0000

    test: feature files should use concise language

--- e2e-tests/cypress/integration/eligibility-listed-building-status.feature
+++ e2e-tests/cypress/integration/eligibility-listed-building-status.feature
@@ -3,14 +3,14 @@ Feature: A prospective appellant supplies states whether or not their appeal cov
     so that I don't accidentally submit an appeal against a decision made about a listed building?
 
   Scenario: Listed Building statement is required
-    When the prospective appellant does not provide a Listed Building statement
-    Then the prospective appellant is informed that a Listed Building statement is required
+    When the user does not provide a Listed Building statement
+    Then the user is informed that a Listed Building statement is required
 
   Scenario: User selects yes option in the Is your appeal about a listed building page
-    When the prospective appellant states that their case concerns a Listed Building
-    Then the prospective appellant is informed that cases concerning a Listed Building cannot be processed via this service
-    And the prospective appellant is directed to the Appeal a Planning Decision service
+    When the user states that their case concerns a Listed Building
+    Then the user is informed that cases concerning a Listed Building cannot be processed via this service
+    And the user is directed to the Appeal a Planning Decision service
 
   Scenario: User selects no option in the Is your appeal about a listed building page
-    When the prospective appellant states that their case does not concern a Listed Building
-    Then the prospective appellant can proceed with a non-Listed Building
+    When the user states that their case does not concern a Listed Building
+    Then the user can proceed with a non-Listed Building
--- e2e-tests/cypress/integration/eligibility-local-planning-department/eligibility-local-planning-department.js
+++ e2e-tests/cypress/integration/eligibility-local-planning-department/eligibility-local-planning-department.js
@@ -1,30 +1,30 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps"
 
-When('the prospective appellant does not provide a Local Planning Department', () => {
+When('the user does not provide a Local Planning Department', () => {
   cy.provideLocalPlanningDepartment('');
 });
 
-Then('the prospective appellant is informed that a Local Planning Department is required', () => {
+Then('the user is informed that a Local Planning Department is required', () => {
   cy.confirmLocalPlanningDepartmentIsRequired();
 });
 
 
-When('the prospective appellant provides a Local Planning Department that is not participating in this service', () => {
+When('the user provides a Local Planning Department that is not participating in this service', () => {
   cy.provideLocalPlanningDepartment('Anything. Whatever you want to type.');
 });
 
-Then('the prospective appellant is informed that the selected Local Planning Department is not participating in the service', () => {
+Then('the user is informed that the selected Local Planning Department is not participating in the service', () => {
   cy.confirmLocalPlanningDepartmentIsNotParticipating();
 });
 
-Then('the prospective appellant is directed to the Appeal a Planning Decision service', () => {
+Then('the user is directed to the Appeal a Planning Decision service', () => {
   cy.confirmRedirectToExternalService();
 });
 
-When('the prospective appellant provides a Local Planning Department that is participating in this service', () => {
+When('the user provides a Local Planning Department that is participating in this service', () => {
   cy.provideLocalPlanningDepartment('Adur LPA');
 });
 
-Then('the prospective appellant can proceed with the provided Local Planning Department', () => {
+Then('the user can proceed with the provided Local Planning Department', () => {
   cy.confirmProviedLocalPlanningDepartmentWasAccepted();
 });
