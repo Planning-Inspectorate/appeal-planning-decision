5f1a728330db7f0fb9eac3a63ccd584ad77d32a2
Frank Phillips <frank@dodion.co.uk>
2020-11-30 09:52:55 +0000

    test(e2e-tests): updated tests to use 50MB file size limit for uploads
    This affected appeal statement and decision letter file uploads.
    The script to create large test files now uses 50MB as a default
    but will use value from FILE_UPLOAD_MAX_FILE_SIZE_BYTES env var if available.
    Also fixed typo for checking sensitive information checkbox in appeal statement submission.

--- e2e-tests/cypress/support/appeal-statement-submission/checkNoSensitiveInformation.js
+++ e2e-tests/cypress/support/appeal-statement-submission/checkNoSensitiveInformation.js
@@ -1,4 +1,4 @@
 module.exports = () => {
-  cy.get('#does-not-include-sensitive-information').uncheck();
+  cy.get('#does-not-include-sensitive-information').check();
   cy.wait(Cypress.env('demoDelay'));
 };
