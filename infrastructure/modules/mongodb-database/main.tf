94d1b1a7e8d8f4902d9491f63d02488cd01e3d1e
Simon Emms <simon@simonemms.com>
2020-11-30 14:58:30 +0000

    fix(mongodb): remove the sharding and indexing configuration from the mongo creation
    From call with MS, this is achievable using the default Mongoose model with no differences

--- infrastructure/modules/mongodb-database/main.tf
+++ infrastructure/modules/mongodb-database/main.tf
@@ -27,11 +27,6 @@ resource "azurerm_cosmosdb_mongo_collection" "mongodb" {
   name = var.collections[count.index].name
   default_ttl_seconds = var.collections[count.index].default_ttl_seconds
 
-  index {
-    keys = ["_id"]
-    unique = true
-  }
-
   dynamic "index" {
     for_each = var.collections[count.index].indexes
     content {
@@ -43,14 +38,4 @@ resource "azurerm_cosmosdb_mongo_collection" "mongodb" {
   account_name = var.account_name
   database_name = azurerm_cosmosdb_mongo_database.mongodb.name
   resource_group_name = var.resource_group_name
-
-  shard_key = "_id"
-  throughput = var.auto_scale == false ? var.throughput : null
-
-  dynamic "autoscale_settings" {
-    for_each = var.auto_scale ? [var.throughput * 10] : []
-    content {
-      max_throughput = autoscale_settings.value
-    }
-  }
 }
