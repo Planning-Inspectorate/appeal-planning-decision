e6d8804819a6ac899c09dc4620e5f00922b8b402
Simon Emms <simon@simonemms.com>
2020-12-09 17:00:45 +0000

    feat(horizon): configure the horizon api gateway connection

--- infrastructure/environments/local.tf
+++ infrastructure/environments/local.tf
@@ -1,6 +1,7 @@
 locals {
   app_name = "pins-env"
   current_ip = chomp(data.http.myip.body)
+  horizon_count = var.horizon_enabled ? 1 : 0
   location = substr(var.location, 0, 3) # short version of the location
   lock_delete = "CanNotDelete"
   lock_none = null # Doesn't add tag
