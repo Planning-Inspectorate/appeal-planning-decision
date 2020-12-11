301bbca5b38ab8165eb7bb86d24e53aa7633b27f
Frank Phillips <frank@dodion.co.uk>
2020-12-11 10:08:24 +0000

    feat(appeals-service-app): implemented business logic validation for appeal statement file upload

--- appeals-service-api/src/__tests__/appeals.spec.js
+++ appeals-service-api/src/__tests__/appeals.spec.js
@@ -8,7 +8,7 @@ const { appealDocument } = require('../models/appeal');
 jest.mock('../db/db');
 
 async function createAppeal() {
-  const appeal = appealDocument;
+  const appeal = JSON.parse(JSON.stringify(appealDocument));
   appeal.id = uuid.v4();
   await mongodb.get().collection('appeals').insertOne({ _id: appeal.id, uuid: appeal.id, appeal });
   return appeal;
@@ -34,11 +34,8 @@ describe('Appeals API', () => {
   });
 
   test('POST /api/v1/appeals - It responds with a newly created appeal', async () => {
-    const appeal = appealDocument;
-    const response = await request(app).post('/api/v1/appeals').send({
-      // lpaCode: 'E60000281',
-      // decisionDate,
-    });
+    const appeal = JSON.parse(JSON.stringify(appealDocument));
+    const response = await request(app).post('/api/v1/appeals').send({});
     appeal.id = response.body.id;
     expect(response.body).toEqual(appeal);
     expect(response.statusCode).toBe(201);
@@ -212,5 +209,61 @@ describe('Appeals API', () => {
       'The appeal state cannot be SUBMITTED if any sections are not COMPLETED'
     );
     expect(response.statusCode).toBe(400);
+  });
+
+  test('PUT /api/v1/appeals/{id} - It responds with an error - appeal statement upload file cannot have name without id', async () => {
+    const appeal = await createAppeal();
+    appeal.yourAppealSection.appealStatement.uploadedFile.name =
+      'my_uploaded_file_appeal_statement.pdf';
+    appeal.yourAppealSection.appealStatement.uploadedFile.id = null;
+    const response = await request(app).put(`/api/v1/appeals/${appeal.id}`).send(appeal);
+    expect(response.body.code).toEqual(400);
+    expect(response.body.errors).toContain(
+      'The appeal statement uploaded file must have an id for the file when it has a name'
+    );
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('PUT /api/v1/appeals/{id} - It responds with an error - appeal statement upload file cannot have id without name', async () => {
+    const appeal = await createAppeal();
+    appeal.yourAppealSection.appealStatement.uploadedFile.name = '';
+    appeal.yourAppealSection.appealStatement.uploadedFile.id =
+      '3fa85f64-5717-4562-b3fc-2c963f66afa6';
+    const response = await request(app).put(`/api/v1/appeals/${appeal.id}`).send(appeal);
+    expect(response.body.code).toEqual(400);
+    expect(response.body.errors).toContain(
+      'The appeal statement uploaded file must have a name for the file when it has an id'
+    );
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('PUT /api/v1/appeals/{id} - It responds with an error - appeal statement upload file cannot have sensitive information', async () => {
+    const appeal = await createAppeal();
+    appeal.yourAppealSection.appealStatement.uploadedFile.name =
+      'my_uploaded_file_appeal_statement.pdf';
+    appeal.yourAppealSection.appealStatement.uploadedFile.id =
+      '3fa85f64-5717-4562-b3fc-2c963f66afa6';
+    appeal.yourAppealSection.appealStatement.hasSensitiveInformation = true;
+    const response1 = await request(app).put(`/api/v1/appeals/${appeal.id}`).send(appeal);
+    expect(response1.body.code).toEqual(400);
+    expect(response1.body.errors).toContain(
+      'The appeal statement uploaded file cannot be accepted unless it is confirmed to have no sensitive information'
+    );
+    expect(response1.statusCode).toBe(400);
+  });
+
+  test('PUT /api/v1/appeals/{id} - It responds with an error - appeal statement upload file must include answer to sensitive information question', async () => {
+    const appeal = await createAppeal();
+    appeal.yourAppealSection.appealStatement.uploadedFile.name =
+      'my_uploaded_file_appeal_statement.pdf';
+    appeal.yourAppealSection.appealStatement.uploadedFile.id =
+      '3fa85f64-5717-4562-b3fc-2c963f66afa6';
+    appeal.yourAppealSection.appealStatement.hasSensitiveInformation = null;
+    const response1 = await request(app).put(`/api/v1/appeals/${appeal.id}`).send(appeal);
+    expect(response1.body.code).toEqual(400);
+    expect(response1.body.errors).toContain(
+      'The appeal statement uploaded file cannot be accepted unless it is confirmed to have no sensitive information'
+    );
+    expect(response1.statusCode).toBe(400);
   });
 });
