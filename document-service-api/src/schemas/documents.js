3af7f78bf548fd88c0edb00dac001d7b31d55df4
Simon Emms <simon@simonemms.com>
2020-12-11 10:02:04 +0000

    feat(document-upload): create document schema module

--- document-service-api/src/schemas/documents.js
+++ document-service-api/src/schemas/documents.js
@@ -3,7 +3,7 @@ const DocumentsMethod = require('./documentsMethods');
 
 const DocumentsSchema = new Schema(
   {
-    uuid: {
+    id: {
       type: String,
       required: true,
       immutable: true,
@@ -11,11 +11,62 @@ const DocumentsSchema = new Schema(
         unique: true,
       },
     },
+    applicationId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    uploadDate: {
+      type: Date,
+      required: true,
+      index: true,
+    },
+    mimeType: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      required: true,
+    },
+    // Location in Blob Storage
+    location: {
+      type: String,
+      required: true,
+    },
+    upload: {
+      processed: {
+        type: Boolean,
+        default: false,
+      },
+      processAttempts: {
+        type: Number,
+        default: 0,
+      },
+    },
   },
   {
     timestamps: true,
   }
 );
+
+DocumentsSchema.index({
+  applicationId: 1,
+  id: 1,
+});
+
+DocumentsSchema.index({
+  createdAt: 1,
+});
+
+DocumentsSchema.index({
+  'upload.processed': 1,
+  'upload.processAttempts': 1,
+});
 
 DocumentsSchema.loadClass(DocumentsMethod);
 
