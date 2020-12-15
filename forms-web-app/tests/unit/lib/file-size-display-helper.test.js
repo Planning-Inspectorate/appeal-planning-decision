c32309cee0f8f5b09b666bc2a4ff084d44ea052d
Christopher Moss <chris@a6software.co.uk>
2020-12-15 11:21:40 +0000

    fix(forms-web-app): file size display helper - 1000, not 1024

--- forms-web-app/tests/unit/lib/file-size-display-helper.test.js
+++ forms-web-app/tests/unit/lib/file-size-display-helper.test.js
@@ -11,6 +11,10 @@ describe('lib/file-size-display-helper', () => {
       expected: '1 Bytes',
     },
     {
+      given: 1000,
+      expected: '1 KB',
+    },
+    {
       given: 1024,
       expected: '1 KB',
     },
@@ -19,7 +23,15 @@ describe('lib/file-size-display-helper', () => {
       expected: '1 MB',
     },
     {
+      given: 1000 ** 2,
+      expected: '1 MB',
+    },
+    {
       given: 1024 ** 3,
+      expected: '1 GB',
+    },
+    {
+      given: 1000 ** 3,
       expected: '1 GB',
     },
     {
@@ -27,7 +39,15 @@ describe('lib/file-size-display-helper', () => {
       expected: '1 TB',
     },
     {
+      given: 1000 ** 4,
+      expected: '1 TB',
+    },
+    {
       given: 1024 ** 5,
+      expected: '1 PB',
+    },
+    {
+      given: 1000 ** 5,
       expected: '1 PB',
     },
     {
@@ -35,12 +55,32 @@ describe('lib/file-size-display-helper', () => {
       expected: '1 EB',
     },
     {
+      given: 1000 ** 6,
+      expected: '1 EB',
+    },
+    {
       given: 1024 ** 7,
+      expected: '1 ZB',
+    },
+    {
+      given: 1000 ** 7,
       expected: '1 ZB',
     },
     {
       given: 1024 ** 8,
       expected: '1 YB',
+    },
+    {
+      given: 1000 ** 8,
+      expected: '1 YB',
+    },
+    {
+      given: 50000000,
+      expected: '50 MB',
+    },
+    {
+      given: 52428800,
+      expected: '52 MB',
     },
   ].forEach(({ given, expected }) => {
     it(`should display the expected file size - ${expected}`, () => {
