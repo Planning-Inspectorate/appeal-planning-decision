7786b28543f5d349dc3481569acabe6d902b92ab
Frank Phillips <frank@dodion.co.uk>
2020-12-17 15:50:19 +0000

    test(e2e-tests): extend and refactor tests for Planning Application Number feature

--- e2e-tests/cypress/support/appellant-submission-planning-application-number/confirmPlanningApplicationNumberHasUpdated.js
+++ e2e-tests/cypress/support/appellant-submission-planning-application-number/confirmPlanningApplicationNumberHasUpdated.js
@@ -1,14 +1,5 @@
 module.exports = (applicationNumber) => {
-  //TODO validate this on the check-your-answers page once this is implemented
-  cy.url().should('include','/appellant-submission/upload-application');
-
-  cy.wait(Cypress.env('demoDelay'));
-
-  // revisit the application number page and prove the number is still there..
   cy.visit('/appellant-submission/application-number');
-
   cy.get('[data-cy="application-number"]').should('have.value', applicationNumber);
-
   cy.wait(Cypress.env('demoDelay'));
-
 };
--- forms-web-app/src/validators/appellant-submission/application-number.js
+++ forms-web-app/src/validators/appellant-submission/application-number.js
@@ -1,7 +1,12 @@
 const { body } = require('express-validator');
 
 const rulePlanningApplicationNumber = () =>
-  body('application-number').notEmpty().withMessage('Enter your planning application number');
+  body('application-number')
+    .notEmpty()
+    .withMessage('Enter your planning application number')
+    .bail()
+    .isLength({ min: 1, max: 30 })
+    .withMessage('Planning application number must be 30 characters or fewer');
 
 const rules = () => {
   return [rulePlanningApplicationNumber()];
