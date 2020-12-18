25f34c899c2f557d88fe3783bb6fb2e2bbaf127a
Yassine Ferkouch <yassine.ferkouch@foundry4.com>
2020-12-18 18:48:23 +0100

    test(e2e-tests): refactor e2e tests and steps for Who Are You task

--- e2e-tests/cypress/integration/appellant-submission-who-are-you/appellant-submission-who-are-you.js
+++ e2e-tests/cypress/integration/appellant-submission-who-are-you/appellant-submission-who-are-you.js
@@ -1,7 +1,27 @@
-import { When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
-When('the user has stated that they {string} the original appellant', (original) => {
+Given('the user {string} previously stated {string} the original appellant', (alreadySubmitted, originalAppellant) => {
   cy.goToWhoAreYouPage();
+
+  if (alreadySubmitted === 'had') {
+
+    if (originalAppellant === 'being') {
+      cy.answerYesOriginalAppellant();
+    } else {
+      cy.answerNoOriginalAppellant();
+    }
+    cy.clickSaveAndContinue();
+    cy.goToWhoAreYouPage();
+  }
+
+});
+
+When('the user does not state being or not the original appellant', () => {
+  cy.clickSaveAndContinue();
+});
+
+
+When('the user states that they {string} the original appellant', (original) => {
   if (original === 'are') {
     cy.answerYesOriginalAppellant();
   } else {
@@ -24,4 +44,12 @@ Then('the user will {string} asked who they are representing', (asked) => {
 
 Then('the user can see that their appeal has been updated with the {string} answer', (answer) => {
   cy.confirmAnswered(answer);
+});
+
+Then('the user is informed that he must answer', (answer) => {
+  cy.confirmAnswered(answer);
+});
+
+Then('the user is told {string}', (message) => {
+  cy.confirmWhoAreYouPageRejectedBecause(message);
 });
