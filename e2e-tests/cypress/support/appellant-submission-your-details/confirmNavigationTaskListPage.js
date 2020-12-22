69cf7eb9d8ad919592af7ae010731db18ceedd0f
Frank Phillips <frank@dodion.co.uk>
2020-12-22 21:59:23 +0000

    test(e2e-tests): refactored e2e - mainly wording and style of feature files

--- e2e-tests/cypress/support/appellant-submission-your-details/confirmNavigationTaskListPage.js
+++ e2e-tests/cypress/support/appellant-submission-your-details/confirmNavigationTaskListPage.js
@@ -1,4 +1,4 @@
 module.exports = () => {
-  cy.url().should('include', '/appellant-submission/applicant-name');
+  cy.url().should('include', '/appellant-submission/task-list');
   cy.wait(Cypress.env('demoDelay'));
 }
