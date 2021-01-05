94748d00c0ff2f73711164eb15a1c8858fe750c9
Yassine Ferkouch <yassine.ferkouch@foundry4.com>
2021-01-05 09:15:15 +0100

    test(e2e-tests): add of header step files

---
+++ e2e-tests/cypress/support/appeal-header-footer-links/confirmFeedbackLinkIsDisplayed.js
@@ -0,0 +1,7 @@
+module.exports = () => {
+  cy.get('[data-cy="Feedback"]')
+    .should('have.attr', 'href')
+    .and('include', 'https://www.smartsurvey.co.uk/s/7OGXZZ/');
+
+  cy.wait(Cypress.env('demoDelay'));
+};
