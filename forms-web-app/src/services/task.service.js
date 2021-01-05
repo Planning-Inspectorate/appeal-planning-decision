89f831dd1558adc4ef5abcc2fbec1e3b5596c016
Yassine Ferkouch <yassine.ferkouch@foundry4.com>
2021-01-05 16:55:38 +0100

    fix(forms-web-app): make site ownership mandatory to access to appeal's review

--- forms-web-app/src/services/task.service.js
+++ forms-web-app/src/services/task.service.js
@@ -83,6 +83,7 @@ function statusCheckYourAnswer(appeal) {
     statusDecisionLetter,
     statusAppealStatement,
     statusAppealSiteAddress,
+    statusSiteOwnership,
     statusSiteAccess,
     statusHealthAndSafety,
   ];
