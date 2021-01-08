1e97772870db773d55d955cb19f45f0b4bdef1a9
Yassine Ferkouch <yassine.ferkouch@foundry4.com>
2021-01-08 12:24:55 +0100

    test(e2e-tests): update glue code to check if lpa is stored

--- e2e-tests/cypress/support/eligibility-local-planning-department/provideLocalPlanningDepartment.js
+++ e2e-tests/cypress/support/eligibility-local-planning-department/provideLocalPlanningDepartment.js
@@ -1,8 +1,5 @@
 module.exports = (text) => {
-  cy.visit('/eligibility/planning-department');
   cy.get('input#planning-department-label').type(`{selectall}{backspace}${text}`);
 
   cy.wait(Cypress.env('demoDelay'));
-  const continueBtn = cy.get('.govuk-button').click();
-  cy.wait(Cypress.env('demoDelay'));
-}
+};
