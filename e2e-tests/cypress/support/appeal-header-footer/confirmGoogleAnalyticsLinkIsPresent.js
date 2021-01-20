95082197553b065373283a08661fce643f9eb17f
Yassine Ferkouch <yassine.ferkouch@foundry4.com>
2021-01-20 12:30:16 +0100

    test(e2e-tests): update google analytics id

--- e2e-tests/cypress/support/appeal-header-footer/confirmGoogleAnalyticsLinkIsPresent.js
+++ e2e-tests/cypress/support/appeal-header-footer/confirmGoogleAnalyticsLinkIsPresent.js
@@ -2,7 +2,7 @@ module.exports = () => {
   cy.get('head script[data-cy="Google Analytics"]').should(
     'have.attr',
     'src',
-    'https://www.googletagmanager.com/gtag/js?id=G-FFM8G1FWCZ',
+    'https://www.googletagmanager.com/gtag/js?id=G-RTYZW789M0',
   );
   cy.wait(Cypress.env('demoDelay'));
 };
