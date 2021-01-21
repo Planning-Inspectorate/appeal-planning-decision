31d9ce9f8c6dc77d0130925b460d042efe8aba63
Christopher Moss <chris@a6software.co.uk>
2021-01-21 13:18:58 +0000

    fix(forms-web-app): removes unused route /application-number
    This route lives under /appellant-submission and is a leftover from an earlier time.

--- packages/forms-web-app/src/routes/index.js
+++ packages/forms-web-app/src/routes/index.js
@@ -3,13 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const appellantSubmissionRouter = require('./appellant-submission');
-const applicationNumberRouter = require('./application-number');
 const eligibilityRouter = require('./eligibility');
 const homeRouter = require('./home');
 
 router.use('/', homeRouter);
 router.use('/appellant-submission', appellantSubmissionRouter);
-router.use('/application-number', applicationNumberRouter);
 router.use('/eligibility', eligibilityRouter);
 
 module.exports = router;
