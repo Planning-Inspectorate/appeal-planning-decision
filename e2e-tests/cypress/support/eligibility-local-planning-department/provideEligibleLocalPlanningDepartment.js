0b594f14bb58039c9710187fb5b0fdbfe193849c
Frank Phillips <frank@dodion.co.uk>
2021-01-21 11:35:36 +0000

    feat(forms-web-app): update LPD eligibility tests to be dynamic

---
+++ e2e-tests/cypress/support/eligibility-local-planning-department/provideEligibleLocalPlanningDepartment.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+  cy.get('[data-cy="eligible-departments"]').invoke('text')
+    .then((departments) => {
+      let firstEligibleLocalPlanningDepartment = '';
+      const eligiblePlanningDepartments = departments.toString().split(',');
+      if (eligiblePlanningDepartments.length > 0) {
+        firstEligibleLocalPlanningDepartment = eligiblePlanningDepartments[0];
+      }
+      cy.get('input#planning-department-label').type(`{selectall}{backspace}${firstEligibleLocalPlanningDepartment}`);
+      cy.wait(Cypress.env('demoDelay'));
+    })
+}
