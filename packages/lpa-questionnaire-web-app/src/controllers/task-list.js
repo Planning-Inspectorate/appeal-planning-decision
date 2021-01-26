77d47870691f9e85a93a1225e03341d1fa2ae9cd
swdpcomputing <swdpcomputing@outlook.com>
2021-01-26 15:40:22 +0000

    feat(lpa-task-list): populate task list and add placeholder

--- packages/lpa-questionnaire-web-app/src/controllers/task-list.js
+++ packages/lpa-questionnaire-web-app/src/controllers/task-list.js
@@ -1,56 +1,42 @@
-const { getTaskStatus, SECTIONS } = require('../services/task.service');
+const { HEADERS, SECTIONS } = require('../services/task.service');
 const { VIEW } = require('../lib/views');
-const countTasks = require('../lib/count-task');
 
-const HEADERS = {
-  aboutAppealSection: 'About the appeal',
-  submissionAccuracy: "Review accuracy of the appellant's submission",
-};
-
+/**
+ * @name buildTaskLists
+ * @description Builds array of section objects, each has an array of tasks
+ * @param questionnaire Questionnaire details from request
+ * @return {array} Array of section objects
+ */
 function buildTaskLists(questionnaire) {
-  const taskList = [];
-  const sections = SECTIONS;
-  Object.keys(sections).forEach((sectionName) => {
-    const section = sections[sectionName];
-
-    const task = {
+  return SECTIONS.map(({ sectionId, tasks }) => {
+    return {
       heading: {
-        text: HEADERS[sectionName],
+        text: HEADERS[sectionId],
       },
-      items: [],
+      tasks: tasks.map(({ taskId, href, rule }) => {
+        const status = rule(questionnaire);
+
+        return {
+          text: HEADERS[taskId],
+          href,
+          attributes: {
+            name: taskId,
+            [`${taskId}-status`]: status,
+          },
+          status,
+        };
+      }),
     };
-
-    Object.keys(section).forEach((subSectionName) => {
-      const subSection = section[subSectionName];
-
-      const status = getTaskStatus(questionnaire, sectionName, subSectionName);
-
-      task.items.push({
-        text: HEADERS[subSectionName],
-        href: subSection.href,
-        attributes: {
-          name: subSectionName,
-          [`${subSectionName}-status`]: status,
-        },
-        status,
-      });
-    });
-    taskList.push(task);
   });
-  return taskList;
 }
 
 exports.getTaskList = (req, res) => {
   const { questionnaire } = req.session;
   const sections = buildTaskLists(questionnaire);
-
   const applicationStatus = 'Application incomplete';
-
-  const sectionInfo = countTasks(sections);
 
   res.render(VIEW.TASK_LIST, {
     applicationStatus,
-    sectionInfo,
     sections,
   });
 };
