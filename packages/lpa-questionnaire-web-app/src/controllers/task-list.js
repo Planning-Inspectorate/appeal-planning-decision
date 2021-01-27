564fe83eb154abc909d15a0108b42f43c161b790
swdpcomputing <swdpcomputing@outlook.com>
2021-01-27 12:12:59 +0000

    refactor(lpa-tasklist): imporve SECTIONS structure and simplify tasklist generation

--- packages/lpa-questionnaire-web-app/src/controllers/task-list.js
+++ packages/lpa-questionnaire-web-app/src/controllers/task-list.js
@@ -1,49 +1,27 @@
-const { getTaskStatus, SECTIONS } = require('../services/task.service');
+const { HEADERS, SECTIONS } = require('../services/task.service');
 const { VIEW } = require('../lib/views');
-const countTasks = require('../lib/count-task');
 
-const HEADERS = {
-  aboutAppealSection: 'About the appeal',
-  submissionAccuracy: "Review accuracy of the appellant's submission",
-  extraConditions: 'Do you have any extra conditions?',
-  areaAppeals: 'Tell us about any appeals in the immediate area',
-  aboutAppealSiteSection: 'About the appeal site',
-  aboutSite: 'Tell us about the appeal site',
-  requiredDocumentsSection: 'Required documents',
-  plansDecision: 'Upload the plans used to reach the decision',
-  officersReport: "Upload the Planning Officer's report",
-  optionalDocumentsSection: 'Optional supporting documents',
-  interestedPartiesApplication: 'Telling interested parties about the application',
-  representationsInterestedParties: 'Representations from interested parties',
-  interestedPartiesAppeal: 'Notifying interested parties of the appeal',
-  siteNotices: 'Site notices',
-  planningHistory: 'Planning history',
-  statutoryDevelopment: 'Statutory development plan policy',
-  otherPolicies: 'Other relevant policies',
-  supplementaryPlanningDocuments: 'Supplementary planning document extracts',
-  developmentOrNeighbourhood: 'Development Plan Document or Neighbourhood Plan',
-  submitQuestionnaireSection: 'Before you submit',
-  checkYourAnswers: 'Check your answers',
-};
-
+/**
+ * @name buildTaskLists
+ * @description Builds array of section objects, each has an array of tasks
+ * @param questionnaire Questionnaire details from request
+ * @return {array} Array of section objects
+ */
 function buildTaskLists(questionnaire) {
-  return Object.keys(SECTIONS).map((sectionId) => {
-    const section = SECTIONS[sectionId];
-
+  return SECTIONS.map(({ sectionId, tasks }) => {
     return {
       heading: {
         text: HEADERS[sectionId],
       },
-      items: Object.keys(section).map((subSectionId) => {
-        const subSection = section[subSectionId];
-        const status = getTaskStatus(questionnaire, sectionId, subSectionId);
+      tasks: tasks.map(({ taskId, href, rule }) => {
+        const status = rule(questionnaire);
 
         return {
-          text: HEADERS[subSectionId],
-          href: subSection.href,
+          text: HEADERS[taskId],
+          href,
           attributes: {
-            name: subSectionId,
-            [`${subSectionId}-status`]: status,
+            name: taskId,
+            [`${taskId}-status`]: status,
           },
           status,
         };
@@ -55,14 +33,10 @@ function buildTaskLists(questionnaire) {
 exports.getTaskList = (req, res) => {
   const { questionnaire } = req.session;
   const sections = buildTaskLists(questionnaire);
-
   const applicationStatus = 'Application incomplete';
-
-  const sectionInfo = countTasks(sections);
 
   res.render(VIEW.TASK_LIST, {
     applicationStatus,
-    sectionInfo,
     sections,
   });
 };
