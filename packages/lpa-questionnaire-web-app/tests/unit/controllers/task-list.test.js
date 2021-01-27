799ec866a05cf4d8e5c9b4c0371d3fe571a2a884
David Lewis <dave@davehlewis.com>
2021-01-27 16:51:16 +0000

    test(lpa-task-list): update tests following task-list refactor work

--- packages/lpa-questionnaire-web-app/tests/unit/controllers/task-list.test.js
+++ packages/lpa-questionnaire-web-app/tests/unit/controllers/task-list.test.js
@@ -2,6 +2,32 @@ const taskListController = require('../../../src/controllers/task-list');
 const { VIEW } = require('../../../src/lib/views');
 const { mockReq, mockRes } = require('../mocks');
 
+jest.mock('../../../src/services/task.service', () => ({
+  __esModule: true,
+  SECTIONS: [
+    {
+      sectionId: 'mockSection',
+      tasks: [
+        {
+          taskId: 'mockTask1',
+          href: '/mock-task-1',
+          rule: () => 'NOT STARTED',
+        },
+        {
+          taskId: 'mockTask2',
+          href: '/mock-task-2',
+          rule: () => 'NOT STARTED',
+        },
+      ],
+    },
+  ],
+  HEADERS: {
+    mockSection: 'Mock Section',
+    mockTask1: 'Mock Task 1',
+    mockTask2: 'Mock Task 2',
+  },
+}));
+
 describe('controllers/task-list', () => {
   describe('getTaskList', () => {
     it('All the tasks except check answers should be in not started', () => {
@@ -15,16 +41,25 @@ describe('controllers/task-list', () => {
         sections: [
           {
             heading: {
-              text: 'About the appeal',
+              text: 'Mock Section',
             },
             tasks: [
               {
-                href: `#`,
-                text: "Review accuracy of the appellant's submission",
+                href: '/mock-task-1',
+                text: 'Mock Task 1',
                 status: 'NOT STARTED',
                 attributes: {
-                  'submissionAccuracy-status': 'NOT STARTED',
-                  name: 'submissionAccuracy',
+                  'mockTask1-status': 'NOT STARTED',
+                  name: 'mockTask1',
+                },
+              },
+              {
+                href: '/mock-task-2',
+                text: 'Mock Task 2',
+                status: 'NOT STARTED',
+                attributes: {
+                  'mockTask2-status': 'NOT STARTED',
+                  name: 'mockTask2',
                 },
               },
             ],
--- packages/lpa-questionnaire-web-app/tests/unit/services/task.service.test.js
+++ packages/lpa-questionnaire-web-app/tests/unit/services/task.service.test.js
@@ -1,53 +1,16 @@
-const { VIEW } = require('../../../src/lib/views');
 const {
+  NOT_STARTED,
   CANNOT_START_YET,
-  IN_PROGRESS,
 } = require('../../../src/services/task-status/task-statuses');
-
-const { getNextTask, getTaskStatus } = require('../../../src/services/task.service');
+const { SECTIONS } = require('../../../src/services/task.service');
 
 describe('services/task.service', () => {
-  describe('getNextTask', () => {
-    it('should return task list as all the remaining section tasks cannot be started yet', async () => {
-      const questionnaire = {
-        sectionStates: {
-          Section1: {
-            Task1: IN_PROGRESS,
-            Task2: CANNOT_START_YET,
-            Task3: CANNOT_START_YET,
-            Task4: CANNOT_START_YET,
-          },
-        },
-      };
-
-      const task = getNextTask(
-        questionnaire,
-        {
-          sectionName: 'Section1',
-          taskName: 'Task1',
-        },
-        questionnaire.sectionStates
-      );
-
-      expect(task.href).toEqual(`/${VIEW.TASK_LIST}`);
+  describe('SECTIONS', () => {
+    it('should return not started from statusTemp', () => {
+      expect(SECTIONS[0].tasks[0].rule()).toEqual(NOT_STARTED);
     });
-  });
-  describe('getTaskStatus', () => {
-    it('should return null if task does not exist', () => {
-      const questionnaire = {
-        sectionStates: {
-          Section1: {
-            Task1: IN_PROGRESS,
-            Task2: CANNOT_START_YET,
-            Task3: CANNOT_START_YET,
-            Task4: CANNOT_START_YET,
-          },
-        },
-      };
-
-      const status = getTaskStatus(questionnaire, 'Section2', 'Task1');
-
-      expect(status).toBeNull();
+    it('should return cannot start yet for statusCheckYourAnswer', () => {
+      expect(SECTIONS[SECTIONS.length - 1].tasks[0].rule()).toEqual(CANNOT_START_YET);
     });
   });
 });
