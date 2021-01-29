b3e9e79e53f813f3f45ce94ead84c9c8edc081f0
sbasu <shilpa.basu@difrent.co.uk>
2021-01-29 09:52:08 +0000

    test: updated page objects for the tests

--- lpa-submissions-e2e-tests/cypress/support/PageObjects/appeals-questionnaire-tasklist-pageobjects.js
+++ lpa-submissions-e2e-tests/cypress/support/PageObjects/appeals-questionnaire-tasklist-pageobjects.js
@@ -1,67 +1,67 @@
 class AppealsQuestionnaireTaskList {
   getSubmissionAccuracy(){
-    return cy.get('a[taskId="submissionAccuracy"]');
+    return cy.get('a[data-cy="submissionAccuracy"]');
   }
 
   getExtraConditions(){
-    return cy.get('a[taskId="extraConditions"]');
+    return cy.get('a[data-cy="extraConditions"]');
   }
 
 
   getAreaAppeals(){
-    return cy.get('a[taskId="areaAppeals"]');
+    return cy.get('a[data-cy="areaAppeals"]');
   }
 
   getAboutSite(){
-    return cy.get('a[taskId="aboutSite"]');
+    return cy.get('a[data-cy="aboutSite"]');
   }
 
   getPlansDecision(){
-    return cy.get('a[taskId="plansDecision"]');
+    return cy.get('a[data-cy="plansDecision"]');
   }
 
   getOfficersReport(){
-    return cy.get('a[taskId="officersReport"]');
+    return cy.get('a[data-cy="officersReport"]');
   }
 
   getInterestedPartiesApplication(){
-    return cy.get('a[taskId="interestedPartiesApplication"]');
+    return cy.get('a[data-cy="interestedPartiesApplication"]');
   }
 
   getRepresentationsInterestedParties(){
-    return cy.get('a[taskId="representationsInterestedParties"]');
+    return cy.get('a[data-cy="representationsInterestedParties"]');
   }
 
   getInterestedPartiesAppeal(){
-    return cy.get('a[taskId="interestedPartiesAppeal"]');
+    return cy.get('a[data-cy="interestedPartiesAppeal"]');
   }
 
   getSiteNotices(){
-    return cy.get('a[taskId="siteNotices"]');
+    return cy.get('a[data-cy="siteNotices"]');
   }
 
   getPlanningHistory(){
-    return cy.get('a[taskId="planningHistory"]');
+    return cy.get('a[data-cy="planningHistory"]');
   }
 
   getStatutoryDevelopment(){
-    return cy.get('a[taskId="statutoryDevelopment"]');
+    return cy.get('a[data-cy="statutoryDevelopment"]');
   }
 
   getOtherPolicies(){
-    return cy.get('a[taskId="otherPolicies"]');
+    return cy.get('a[data-cy="otherPolicies"]');
   }
 
   getSupplementaryPlanningDocuments(){
-    return cy.get('a[taskId="supplementaryPlanningDocuments"]');
+    return cy.get('a[data-cy="supplementaryPlanningDocuments"]');
   }
 
   getDevelopmentOrNeighbourhood(){
-    return cy.get('a[taskId="developmentOrNeighbourhood"]');
+    return cy.get('a[data-cy="developmentOrNeighbourhood"]');
   }
 
   getCheckYourAnswers(){
-    return cy.get('a[taskId="checkYourAnswers"]');
+    return cy.get('a[data-cy="checkYourAnswers"]');
   }
 }
 
