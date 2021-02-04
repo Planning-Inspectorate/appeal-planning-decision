0eb842955d2570ca7f9dee895cd820ccc8fc6f1b
Dan Watkinson <dan.watkinson@gmail.com>
2021-02-04 16:16:28 +0000

    feat: lint

--- packages/forms-web-app/src/routes/pdf/index.js
+++ packages/forms-web-app/src/routes/pdf/index.js
@@ -1,14 +1,13 @@
+const axios = require('axios');
+const FormData = require('form-data');
+const express = require('express');
 const { getExistingAppeal } = require('../../lib/appeals-api-wrapper');
 const { getDepartmentFromId } = require('../../services/department.service');
 const { VIEW } = require('../../lib/views');
-const logger = require('../../lib/logger');
-const axios = require('axios');
-const FormData = require('form-data');
 
-const express = require('express');
 const router = express.Router();
 
-router.get( '/source/:id', async (req, res, next) => {
+router.get('/source/:id', async (req, res) => {
   const appeal = await getExistingAppeal(req.params.id);
 
   let appealLPD = '';
@@ -24,25 +23,22 @@ router.get( '/source/:id', async (req, res, next) => {
     appealLPD,
     appeal,
   });
-
 });
 
-router.get( '/download/:id', async (req, res, next) => {
-
+router.get('/download/:id', async (req, res) => {
   // build the request
   const form = new FormData();
-    //-note... because we give this url to gotenberg, we have to give it the url that it can see
-    //         that means we have to use the hostname+port it can see from inside docker-compose-land
-    //         not the url we use from outside (ie. localhost:9000)
+  // -note... because we give this url to gotenberg, we have to give it the url that it can see
+  //         that means we have to use the hostname+port it can see from inside docker-compose-land
+  //         not the url we use from outside (ie. localhost:9000)
   form.append('remoteURL', `http://forms-web-app:3000/pdf/source/${req.params.id}`);
 
   // make the call
   const response = await axios.post('http://gotenberg:4000/convert/url', form, {
     headers: form.getHeaders(),
     responseType: 'stream',
-  })
+  });
 
   response.data.pipe(res);
-
 });
 module.exports = router;
