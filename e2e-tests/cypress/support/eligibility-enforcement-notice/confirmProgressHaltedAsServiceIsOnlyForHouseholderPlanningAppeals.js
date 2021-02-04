e7fed8316389a8d8cbc5c9064aa53c4a96f547cc
Christopher Moss <chris@a6software.co.uk>
2021-02-04 15:30:41 +0000

    test(e2e-tests): adds glue code for enforcement notice

---
+++ e2e-tests/cypress/support/eligibility-enforcement-notice/confirmProgressHaltedAsServiceIsOnlyForHouseholderPlanningAppeals.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+  // confirm we are in the right place
+  cy.url().should('include', '/eligibility/enforcement-notice-out');
+
+  cy.title().should(
+    'eq',
+    'This service is only for appeals without an enforcement notice - Eligibility - Appeal a householder planning decision - GOV.UK',
+  );
+  cy.get('[data-cy="enforcement-notice-out"]').should('exist');
+
+  // pause long enough to capture a nice video
+  cy.wait(Cypress.env('demoDelay'));
+};
