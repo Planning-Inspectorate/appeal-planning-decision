93f9baaed3645ca4ce13f8ad61b6fc39dcfb9e25
swdpcomputing <swdpcomputing@outlook.com>
2021-02-05 10:12:56 +0000

    feat(appeal-reply-service-api): create skeleton reply service

---
+++ packages/appeal-reply-service-api/src/db/db.js
@@ -0,0 +1,33 @@
+const mongoClient = require('mongodb').MongoClient;
+const config = require('../lib/config');
+const logger = require('../lib/logger');
+
+let mongodb;
+
+function connect(callback) {
+  logger.info('Connecting to MongoDB...');
+  mongoClient
+    .connect(config.db.mongodb.url, { useUnifiedTopology: true })
+    .then((client) => {
+      mongodb = client;
+      callback();
+      logger.info('MongoDB connected');
+    })
+    .catch((err) => {
+      logger.error('Error connecting to MongoDB', err);
+    });
+}
+
+function get() {
+  return mongodb.db(config.db.mongodb.dbName);
+}
+
+function close() {
+  mongodb.close();
+}
+
+module.exports = {
+  connect,
+  get,
+  close,
+};
---
+++ packages/appeal-reply-service-api/src/models/reply.js
@@ -0,0 +1,92 @@
+const replyDocument = {
+  id: null,
+  appealId: null,
+  state: 'DRAFT',
+  aboutAppealSection: {
+    submissionAccuracy: null,
+    extraConditions: {
+      hasExtraConditions: null,
+      extraConditions: '',
+    },
+    areaAppeals: {
+      adjacentAppeals: null,
+      appealReferenceNumbers: '',
+    },
+  },
+  aboutAppealSiteSection: {
+    cannotSeeLand: null,
+    wouldNeedToEnter: null,
+    wouldNeedNeighbourAccess: null,
+    wouldAffectListedBuilding: null,
+    isGreenBelt: null,
+    isConservationArea: null,
+  },
+  requiredDocumentsSection: {
+    plansDecision: {
+      uploadedFiles: [],
+    },
+    officersReport: {
+      uploadedFiles: [],
+    },
+  },
+  optionalDocumentsSection: {
+    interestedPartiesApplication: {
+      uploadedFiles: [],
+    },
+    representationsInterestedParties: {
+      uploadedFiles: [],
+    },
+    interestedPartiesAppeal: {
+      uploadedFiles: [],
+    },
+    siteNotices: null,
+    planningHistory: {
+      uploadedFiles: [],
+    },
+    statutoryDevelopment: {
+      uploadedFiles: [],
+    },
+    otherPolicies: {
+      uploadedFiles: [],
+    },
+    supplementaryPlanningDocuments: {
+      uploadedFiles: [],
+    },
+    developmentOrNeighbourhood: {
+      hasPlanSubmitted: null,
+      planChanges: '',
+    },
+  },
+  sectionStates: {
+    aboutAppealSection: {
+      submissionAccuracy: 'NOT STARTED',
+      extraConditions: 'NOT STARTED',
+      areaAppeals: 'NOT STARTED',
+    },
+    aboutAppealSiteSection: {
+      aboutSite: 'NOT STARTED',
+    },
+    requiredDocumentsSection: {
+      plansDecision: 'NOT STARTED',
+      officersReport: 'NOT STARTED',
+    },
+    optionalDocumentsSection: {
+      interestedPartiesApplication: 'NOT STARTED',
+      representationsInterestedParties: 'NOT STARTED',
+      interestedPartiesAppeal: 'NOT STARTED',
+      siteNotices: 'NOT STARTED',
+      planningHistory: 'NOT STARTED',
+      statutoryDevelopment: 'NOT STARTED',
+      otherPolicies: 'NOT STARTED',
+      supplementaryPlanningDocuments: 'NOT STARTED',
+      developmentOrNeighbourhood: 'NOT STARTED',
+    },
+    submitReplySection: {
+      checkYourAnswers: 'NOT STARTED',
+    },
+  },
+};
+
+module.exports = {
+  replyDocument,
+};
