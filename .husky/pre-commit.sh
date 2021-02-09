ba7c2b3f80c18c20f938a9cce1f4834b2c79993e
Simon Emms <simon.emms@foundry4.com>
2021-02-09 16:46:27 +0000

    chore(hooks): set pre-commit hooks not to error
    The hooks fail if there are no changes in the last package - as this is just forcing the
    linting to be done, we don't need this to fail

--- .husky/pre-commit.sh
+++ .husky/pre-commit.sh
@@ -7,5 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 for pkg in "${DIR}"/"${PACKAGES_DIR}"/*
 do
   cd "${pkg}" || exit 1
- "${DIR}/node_modules/.bin/lint-staged"
+ "${DIR}/node_modules/.bin/lint-staged" || true
 done
