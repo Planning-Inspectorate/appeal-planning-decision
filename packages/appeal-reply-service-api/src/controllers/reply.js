d2a964adcd9f57329c170940510cfc1e363b83bb
swdpcomputing <swdpcomputing@outlook.com>
2021-02-10 11:12:20 +0000

    feat(packages/appeal-reply-service-api): initial appeal-reply-service-api

---
+++ packages/appeal-reply-service-api/src/controllers/reply.js
@@ -0,0 +1,92 @@
+const uuid = require('uuid');
+const logger = require('../lib/logger');
+const mongodb = require('../db/db');
+const { blankModel } = require('../models/blankModel');
+
+const dbId = 'reply';
+
+module.exports = {
+  async create(req, res) {
+    const { appealId } = req.body;
+    if (appealId === '' || appealId === undefined) {
+      logger.error(`Problem creating reply - no appealID`);
+      res.status(400).send(`AppealId must be included`);
+      return;
+    }
+    const reply = blankModel;
+    reply.id = uuid.v4();
+    reply.appealId = appealId;
+    logger.debug(`Creating reply ${reply.id} ...`);
+    try {
+      await mongodb
+        .get()
+        .collection(dbId)
+        .insertOne({ _id: reply.id, uuid: reply.id, reply })
+        .then(() => {
+          mongodb
+            .get()
+            .collection(dbId)
+            .findOne({ _id: reply.id })
+            .then((doc) => {
+              logger.debug(`Reply ${reply.id} created`);
+              res.status(201).send(doc.reply);
+            });
+        });
+    } catch (err) {
+      logger.error(`Problem creating a reply ${reply.id}\n${err}`);
+      res.status(500).send(`Problem creating an reply`);
+    }
+  },
+
+  async get(req, res) {
+    const idParam = req.params.id;
+    logger.debug(`Retrieving reply ${idParam} ...`);
+    try {
+      await mongodb
+        .get()
+        .collection(dbId)
+        .findOne({ _id: idParam })
+        .then((doc) => {
+          logger.debug(`Reply ${idParam} retrieved`);
+          res.status(200).send(doc.reply);
+        })
+        .catch((err) => {
+          logger.warn(`Could not find reply ${idParam}\n${err}`);
+          res.status(404).send(null);
+        });
+    } catch (err) {
+      logger.error(`Problem retrieving reply ${idParam}\n${err}`);
+      res.status(500).send(`Problem finding reply ${idParam}`);
+    }
+  },
+
+  async update(req, res) {
+    const idParam = req.params.id;
+    logger.debug(`Updating reply ${idParam} ...`);
+
+    await mongodb
+      .get()
+      .collection(dbId)
+      .findOne({ _id: idParam })
+      .then(async (originalDoc) => {
+        logger.debug(`Original doc \n${originalDoc.reply}`);
+
+        await mongodb
+          .get()
+          .collection(dbId)
+          .updateOne({ _id: idParam }, { $set: { uuid: idParam, reply: req.body } })
+          .then(() => {
+            logger.debug(`Updated reply ${idParam}\n`);
+            res.status(200).send(req.body);
+          })
+          .catch((err) => {
+            logger.error(`Problem updating reply ${idParam}\n${err}`);
+            res.status(500).send(`Problem updating reply`);
+          });
+      })
+      .catch((err) => {
+        logger.warn(`Couldn't find reply ${idParam} to update\n${err}`);
+        res.status(404).send(`Couldn't find reply`);
+      });
+  },
+};
