39c100314cef94b4b36696c51da2f0b16feb1a8e
Yassine Ferkouch <yassine.ferkouch@foundry4.com>
2021-02-11 23:43:44 +0100

    feat(appeals-service-api): schema validation for the patch endpoint

--- packages/appeals-service-api/src/routes/appeals.js
+++ packages/appeals-service-api/src/routes/appeals.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const appealsController = require('../controllers/appeals');
 const validateDto = require('../middleware/validateDto');
-const appealDto = require('../dto/updateAppeal');
+const putAppeal = require('../dto/putAppeal');
+const patchAppeal = require('../dto/patchAppeal');
 
 const routes = new Router();
 
@@ -9,8 +10,8 @@ routes.get('/:id', appealsController.get);
 
 routes.post('/', appealsController.create);
 
-routes.put('/:id', validateDto(appealDto), appealsController.replace);
+routes.put('/:id', validateDto(putAppeal), appealsController.replace);
 
-routes.patch('/:id', appealsController.update);
+routes.patch('/:id', validateDto(patchAppeal), appealsController.update);
 
 module.exports = routes;
