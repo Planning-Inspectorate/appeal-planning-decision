d33babedc3e3525e0e0603c99ab86a19c0505267
David Lewis <dave@davehlewis.com>
2021-02-12 12:20:58 +0000

    fix(appeal-data-panel): data would not update when id changed

--- packages/lpa-questionnaire-web-app/src/middleware/fetch-appeal.js
+++ packages/lpa-questionnaire-web-app/src/middleware/fetch-appeal.js
@@ -9,14 +9,18 @@ const { getAppeal } = require('../lib/appeals-api-wrapper');
  * @returns {Promise<*>}
  */
 module.exports = async (req, res, next) => {
-  if (!req.session) {
+  const appealId = req.params && req.params.id;
+
+  if (!req.session || !appealId) {
     return next();
   }
 
-  if ((!req.session.appeal || !req.session.appeal.id) && req.params && req.params.id) {
+  const { appeal } = req.session;
+
+  if (!appeal || !appeal.id || appeal.id !== appealId) {
     try {
-      req.log.debug({ id: req.params.id }, 'Get existing appeal');
-      req.session.appeal = await getAppeal(req.params.id);
+      req.log.debug({ appealId }, 'Get existing appeal');
+      req.session.appeal = await getAppeal(appealId);
     } catch (err) {
       req.log.debug({ err }, 'Error retrieving appeal');
       req.session.appeal = null;
