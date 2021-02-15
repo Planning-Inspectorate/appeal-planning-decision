f6adbd1883fec02bd8b86eec8473a544de56d44c
David Lewis <dave@davehlewis.com>
2021-02-15 17:08:15 +0000

    fix(lpa-questionnaire): task-list to get appealReply

--- packages/lpa-questionnaire-web-app/src/services/task-status/other-appeals.js
+++ packages/lpa-questionnaire-web-app/src/services/task-status/other-appeals.js
@@ -3,11 +3,12 @@ const { COMPLETED, NOT_STARTED } = require('./task-statuses');
 module.exports = (appealReply) => {
   if (!appealReply) return null;
 
-  const { adjacentAppeals, appealReferenceNumbers } = appealReply?.aboutAppealSection?.otherAppeals;
+  const task = appealReply.aboutAppealSection?.otherAppeals;
   let completion;
 
-  if (typeof adjacentAppeals === 'boolean') {
-    completion = (!adjacentAppeals || (adjacentAppeals && appealReferenceNumbers)) && COMPLETED;
+  if (typeof task.adjacentAppeals === 'boolean') {
+    completion =
+      (!task.adjacentAppeals || (task.adjacentAppeals && task.appealReferenceNumbers)) && COMPLETED;
   }
 
   return completion || NOT_STARTED;
