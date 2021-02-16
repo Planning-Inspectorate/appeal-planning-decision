0e715d1913da5c06f7a321abe08c5f1b2a51aef2
Yassine Ferkouch <yassine.ferkouch@foundry4.com>
2021-02-16 02:47:23 +0100

    test(e2e-tests): update the submission confirmation glue code

--- e2e-tests/cypress/integration/common/appeal-submission-confirmation.js
+++ e2e-tests/cypress/integration/common/appeal-submission-confirmation.js
@@ -1,6 +1,49 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
 Given('an appeal exists', () => {
+  cy.goToWhoAreYouPage();
+  cy.answerYesOriginalAppellant();
+  cy.clickSaveAndContinue();
+
+  cy.provideDetailsName('Valid Name');
+  cy.provideDetailsEmail('valid@email.com');
+  cy.clickSaveAndContinue();
+
+  cy.promptUserToProvidePlanningApplicationNumber();
+  cy.providePlanningApplicationNumber('ValidNumber/12345');
+  cy.goToPlanningApplicationSubmission();
+  cy.uploadPlanningApplicationFile('appeal-statement-valid.doc');
+  cy.clickSaveAndContinue();
+
+  cy.goToDecisionLetterPage();
+  cy.uploadDecisionLetterFile('appeal-statement-valid.doc');
+  cy.clickSaveAndContinue();
+
+  cy.goToAppealStatementSubmission();
+  cy.checkNoSensitiveInformation();
+  cy.uploadAppealStatementFile('appeal-statement-valid.doc');
+  cy.clickSaveAndContinue();
+
+  cy.goToSiteAddressPage();
+  cy.provideAddressLine1('1 Taylor Road');
+  cy.provideAddressLine2('Clifton');
+  cy.provideTownOrCity('Bristol');
+  cy.provideCounty('South Glos');
+  cy.providePostcode('BS8 1TG');
+  cy.clickSaveAndContinue();
+
+  cy.goToWholeSiteOwnerPage();
+  cy.answerOwnsTheWholeAppeal();
+  cy.clickSaveAndContinue();
+
+  cy.goToAccessSitePage();
+  cy.answerCanSeeTheWholeAppeal();
+  cy.clickSaveAndContinue();
+
+  cy.goToHealthAndSafetyPage();
+  cy.answerSiteHasNoIssues();
+  cy.clickSaveAndContinue();
+
   cy.goToSubmissionPage();
 });
 
