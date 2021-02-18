fbabe0b6772d3f3825220efb55bebad52fbf6917
Frank Phillips <frank@dodion.co.uk>
2021-02-18 23:01:48 +0000

    feat(pdf-service-api): initial implementation of a service to generate pdf files from appeal data

---
+++ packages/pdf-service-api/src/__tests__/pdf.spec.js
@@ -0,0 +1,7 @@
+const logger = require('../lib/logger');
+
+describe('Pdf API', () => {
+  test('GET /api/v1/pdf/appeals/12345 - It responds with a pdf file', async () => {
+    logger.debug('Hitting the JEST tests...');
+  });
+});
