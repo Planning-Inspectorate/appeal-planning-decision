4c61031e9d90cd0c52906b231f3f41838d6442e0
Frank Phillips <frank@dodion.co.uk>
2021-02-18 23:01:48 +0000

    feat(pdf-service-api): pdf service to generate pdf file from uploaded html file

--- packages/pdf-service-api/src/__tests__/pdf.spec.js
+++
@@ -1,7 +0,0 @@
-const logger = require('../lib/logger');
-
-describe('Pdf API', () => {
-  test('GET /api/v1/pdf/appeals/12345 - It responds with a pdf file', async () => {
-    logger.debug('Hitting the JEST tests...');
-  });
-});
---
+++ packages/pdf-service-api/src/lib/pdfUtils.js
@@ -0,0 +1,18 @@
+const { pipe, gotenberg, convert, html, please, set, scale, to } = require('gotenberg-js-client');
+const config = require('./config');
+
+const toPDF = pipe(
+  gotenberg(`${config.gotenberg.url}`),
+  convert,
+  html,
+  set(scale(0.75)),
+  to({
+    marginTop: 0.2,
+    marginBottom: 0.2,
+    marginLeft: 0.2,
+    marginRight: 0.2,
+  }),
+  please
+);
+
+module.exports = toPDF;
