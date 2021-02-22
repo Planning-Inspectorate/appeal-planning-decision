147e65e32e8fe0d5e43222f0ea90e5ce9818736e
Marc Fielding <marcfielding@hotmail.com>
2021-02-22 15:01:51 +0000

    feat(lpa-questionnaire-web-app): fixed page display problems, ticket draft complete

--- packages/lpa-questionnaire-web-app/src/controllers/extra-conditions.js
+++ packages/lpa-questionnaire-web-app/src/controllers/extra-conditions.js
@@ -1,5 +1,9 @@
 const { VIEW } = require('../lib/views');
+const getAppealSideBarDetails = require('../lib/appeal-sidebar-details');
 
 exports.getExtraConditions = (req, res) => {
-  res.render(`/${req.params.id}/${VIEW.EXTRA_CONDITIONS}`);
+  res.render(VIEW.EXTRA_CONDITIONS, {
+    appeal: getAppealSideBarDetails(req.session.appeal),
+    backLink: req.session.backLink || `/${req.params.id}/${VIEW.TASK_LIST}`,
+  });
 };
