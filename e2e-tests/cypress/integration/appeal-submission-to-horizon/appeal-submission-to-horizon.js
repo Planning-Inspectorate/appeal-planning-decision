2037f1cf896e06d292b7b544809d96fc25f910e7
Dan Watkinson <dan.watkinson@gmail.com>
2021-02-22 16:02:44 +0000

    test: add cypress jest plugin for better expectations

--- e2e-tests/cypress/integration/appeal-submission-to-horizon/appeal-submission-to-horizon.js
+++ e2e-tests/cypress/integration/appeal-submission-to-horizon/appeal-submission-to-horizon.js
@@ -1,6 +1,51 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 const { APPEAL_DOCUMENT } = require('../../../../packages/forms-web-app/src/lib/empty-appeal');
 
+const matchWhatWeCanFrom = (hardCodedExpectations) => {
+  return {
+    ...hardCodedExpectations,
+    _id: expect.any(String),
+    uuid: expect.any(String),
+    appeal: {
+      ...hardCodedExpectations.appeal,
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+      id: expect.any(String),
+      decisionDate: expect.any(String),
+      yourAppealSection: {
+        ...hardCodedExpectations.appeal.yourAppealSection,
+        appealStatement: {
+          ...hardCodedExpectations.appeal.yourAppealSection.appealStatement,
+          uploadedFile: {
+            ...hardCodedExpectations.appeal.yourAppealSection.appealStatement.uploadedFile,
+            id: expect.any(String),
+            location: expect.any(String),
+          }
+        },
+      },
+      requiredDocumentsSection: {
+        ...hardCodedExpectations.appeal.requiredDocumentsSection,
+        originalApplication: {
+          ...hardCodedExpectations.appeal.requiredDocumentsSection.originalApplication,
+          uploadedFile: {
+            ...hardCodedExpectations.appeal.requiredDocumentsSection.originalApplication.uploadedFile,
+            id: expect.any(String),
+            location: expect.any(String),
+          }
+        },
+        decisionLetter: {
+          ...hardCodedExpectations.appeal.requiredDocumentsSection.decisionLetter,
+          uploadedFile: {
+            ...hardCodedExpectations.appeal.requiredDocumentsSection.decisionLetter.uploadedFile,
+            id: expect.any(String),
+            location: expect.any(String),
+          }
+        }
+      }
+    }
+  };
+}
+
 Given('a prospective appellant has provided valid appeal information', () => {
   cy.provideCompleteAppeal();
   cy.clickCheckYourAnswers();
@@ -23,40 +68,9 @@ Then('a case is created for a case officer', () => {
   cy.confirmAppealSubmitted();
 
   cy.task('getLastFromQueue').then((actualMessage) => {
-    const expected = require('./ucd-831-ac1.json');
+    const hardCodedExpectations = require('./ucd-831-ac1.json');
+    const reasonableExpectation = matchWhatWeCanFrom(hardCodedExpectations);
 
-    const expectedNoDynamicFields = {
-      ...expected,
-    };
-    delete expectedNoDynamicFields.appeal.createdAt;
-    delete expectedNoDynamicFields.appeal.updatedAt;
-    delete expectedNoDynamicFields.appeal.id;
-    delete expectedNoDynamicFields.appeal.decisionDate;
-    delete expectedNoDynamicFields.appeal.yourAppealSection.appealStatement.uploadedFile.id;
-    delete expectedNoDynamicFields.appeal.yourAppealSection.appealStatement.uploadedFile.location;
-    delete expectedNoDynamicFields.appeal.requiredDocumentsSection.originalApplication.uploadedFile.id;
-    delete expectedNoDynamicFields.appeal.requiredDocumentsSection.originalApplication.uploadedFile
-      .location;
-    delete expectedNoDynamicFields.appeal.requiredDocumentsSection.decisionLetter.uploadedFile.id;
-    delete expectedNoDynamicFields.appeal.requiredDocumentsSection.decisionLetter.uploadedFile.location;
-
-    const actualNoDynamicFields = {
-      ...actualMessage,
-    };
-
-    delete actualNoDynamicFields._id;
-    delete actualNoDynamicFields.uuid;
-    delete actualNoDynamicFields.appeal.createdAt;
-    delete actualNoDynamicFields.appeal.updatedAt;
-    delete actualNoDynamicFields.appeal.id;
-    delete actualNoDynamicFields.appeal.decisionDate;
-    delete actualNoDynamicFields.appeal.yourAppealSection.appealStatement.uploadedFile.id;
-    delete actualNoDynamicFields.appeal.yourAppealSection.appealStatement.uploadedFile.location;
-    delete actualNoDynamicFields.appeal.requiredDocumentsSection.originalApplication.uploadedFile.id;
-    delete actualNoDynamicFields.appeal.requiredDocumentsSection.originalApplication.uploadedFile.location;
-    delete actualNoDynamicFields.appeal.requiredDocumentsSection.decisionLetter.uploadedFile.id;
-    delete actualNoDynamicFields.appeal.requiredDocumentsSection.decisionLetter.uploadedFile.location;
-
-    expect(JSON.stringify(actualNoDynamicFields)).to.equal(JSON.stringify(expectedNoDynamicFields));
+    expect(actualMessage).toEqual(reasonableExpectation);
   });
 });
