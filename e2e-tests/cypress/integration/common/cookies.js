16ca0dc95e85438f106a620586c0f5b738746b05
Frank Phillips <frank@dodion.co.uk>
2021-02-24 11:53:44 +0000

    test(e2e-tests): update cookie step defs

--- e2e-tests/cypress/integration/common/cookies.js
+++ e2e-tests/cypress/integration/common/cookies.js
@@ -1,10 +1,31 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-import {
-  expectCookieIsNotDefined,
-  expectCookiePolicy,
-  expectExpressSessionCookieIsDefined,
-} from '../cookies/cookies';
 import { DEFAULT_COOKIE_POLICY } from '../../../../packages/forms-web-app/src/lib/cookies';
+import { expectCookieIsNotDefined, expectCookiePolicy, expectExpressSessionCookieIsDefined } from '../cookies/cookies';
+
+When('the user navigates through the service', () => {
+  cy.goToLandingPage();
+  cy.goToTaskListPage();
+});
+
+When('the user accepts not necessary cookies', () => {
+  // To do
+});
+
+When('the user rejects not necessary cookies', () => {
+  // To do
+});
+
+Then('the cookie banner remains until actioned', () => {
+  // To do
+});
+
+Then('the GA cookies are enabled', () => {
+  // To do
+});
+
+Then('the GA cookies remain disabled', () => {
+  // To do
+});
 
 Given('a user has not previously submitted cookie preferences', () => {
   cy.clearCookies();
