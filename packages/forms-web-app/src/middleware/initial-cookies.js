9caea15a6cd01d0a620b8281602a22d8908b96df
Christopher Moss <chris@a6software.co.uk>
2021-02-24 07:52:43 +0000

    docs(forms-web-app): adds docblock for initial-cookies middleware

--- packages/forms-web-app/src/middleware/initial-cookies.js
+++ packages/forms-web-app/src/middleware/initial-cookies.js
@@ -2,6 +2,17 @@ const { DEFAULT_COOKIE_POLICY } = require('../lib/cookies');
 
 const cookiePolicyKey = 'cookie_policy';
 
+/**
+ * Middleware function to give access to cookies via Nunjucks globals.
+ *
+ * This feels like a hack, and that there should be an easier way to get access to this information.
+ * The suggested approach from reading via github issues and google searches was to use
+ * `res.locals`, however this doesn't seem to work.
+ *
+ * Open to a nicer implementation.
+ *
+ * @param env
+ */
 module.exports = (env) => async (req, res, next) => {
   if (req.cookies && typeof req.cookies[cookiePolicyKey] === 'undefined') {
     res.cookie(cookiePolicyKey, JSON.stringify(DEFAULT_COOKIE_POLICY), {
