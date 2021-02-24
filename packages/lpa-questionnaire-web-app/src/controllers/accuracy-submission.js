19354e8fe015692b44cf429378fa42aa4359a8d8
David Lewis <dave@davehlewis.com>
2021-02-24 16:14:36 +0000

    feat(submission-accuracy): add data handling for question and hook up to api

--- packages/lpa-questionnaire-web-app/src/controllers/accuracy-submission.js
+++ packages/lpa-questionnaire-web-app/src/controllers/accuracy-submission.js
@@ -1,15 +1,40 @@
+const logger = require('../lib/logger');
 const { VIEW } = require('../lib/views');
 const getAppealSideBarDetails = require('../lib/appeal-sidebar-details');
+const { getTaskStatus } = require('../services/task.service');
+const { createOrUpdateAppealReply } = require('../lib/appeal-reply-api-wrapper');
+
+const sectionName = 'aboutAppealSection';
+const taskName = 'submissionAccuracy';
 
 exports.getAccuracySubmission = (req, res) => {
+  const { submissionAccuracy } = req.session.appealReply.aboutAppealSection;
+
+  const { accurateSubmission, inaccuracyReason } = submissionAccuracy;
+
+  let accurateSubmissionValue = accurateSubmission;
+
+  if (typeof accurateSubmission === 'boolean') {
+    accurateSubmissionValue = accurateSubmission ? 'yes' : 'no';
+  }
+
+  const values = {
+    'accurate-submission': accurateSubmissionValue,
+    'inaccuracy-reason': inaccuracyReason,
+  };
+
   res.render(VIEW.ACCURACY_SUBMISSION, {
     appeal: getAppealSideBarDetails(req.session.appeal),
     backLink: req.session.backLink || `/${req.params.id}/${VIEW.TASK_LIST}`,
+    values,
   });
 };
 
 exports.postAccuracySubmission = async (req, res) => {
-  const { body } = req;
+  const {
+    body,
+    session: { appealReply },
+  } = req;
   const { errors = {}, errorSummary = [] } = body;
 
   const values = {
@@ -25,6 +50,31 @@ exports.postAccuracySubmission = async (req, res) => {
       errorSummary,
       values,
     });
+    return;
+  }
+
+  const task = appealReply[sectionName][taskName];
+  task.accurateSubmission = body['accurate-submission'] === 'yes';
+  task.inaccuracyReason = body['accurate-submission'] === 'no' ? body['inaccuracy-reason'] : '';
+  appealReply.sectionStates[sectionName][taskName] = getTaskStatus(
+    appealReply,
+    sectionName,
+    taskName
+  );
+
+  try {
+    req.session.appealReply = await createOrUpdateAppealReply(appealReply);
+  } catch (e) {
+    logger.error(e);
+
+    res.render(VIEW.ACCURACY_SUBMISSION, {
+      appeal: getAppealSideBarDetails(req.session.appeal),
+      backLink: req.session.backLink || `/${req.params.id}/${VIEW.TASK_LIST}`,
+      errors,
+      errorSummary: [{ text: e.toString() }],
+      values,
+    });
+
     return;
   }
 
