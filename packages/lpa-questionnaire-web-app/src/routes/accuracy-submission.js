7087b25cd4a2c6f5811d96c3a7418b114103bd57
David Lewis <dave@davehlewis.com>
2021-02-24 11:00:04 +0000

    feat(questionnaire-accuracy-submission): add validator for accuracy submision

--- packages/lpa-questionnaire-web-app/src/routes/accuracy-submission.js
+++ packages/lpa-questionnaire-web-app/src/routes/accuracy-submission.js
@@ -2,6 +2,8 @@ const express = require('express');
 const accuracySubmissionController = require('../controllers/accuracy-submission');
 const fetchExistingAppealReplyMiddleware = require('../middleware/fetch-existing-appeal-reply');
 const fetchAppealMiddleware = require('../middleware/fetch-appeal');
+const { validationErrorHandler } = require('../validators/validation-error-handler');
+const { rules: accuracySubmissionValidationRules } = require('../validators/accuracy-submission');
 
 const router = express.Router();
 
@@ -9,6 +11,13 @@ router.get(
   '/:id/accuracy-submission',
   [fetchAppealMiddleware, fetchExistingAppealReplyMiddleware],
   accuracySubmissionController.getAccuracySubmission
+);
+
+router.post(
+  '/:id/accuracy-submission',
+  accuracySubmissionValidationRules(),
+  validationErrorHandler,
+  accuracySubmissionController.postAccuracySubmission
 );
 
 module.exports = router;
