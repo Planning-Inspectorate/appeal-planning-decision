1909599c31c879de17c4ac46f61d060805843dd3
Frank Phillips <frank@dodion.co.uk>
2021-02-24 11:53:44 +0000

    test(e2e-tests): update cookie step defs

--- e2e-tests/cypress/support/commands.js
+++ e2e-tests/cypress/support/commands.js
@@ -1015,6 +1015,36 @@ Cypress.Commands.add(
 );
 
 Cypress.Commands.add(
+  'provideAcceptNotNecessaryCookies',
+  require('./cookies/provideAcceptNotNecessaryCookies'),
+);
+
+Cypress.Commands.add(
+  'provideRejectNotNecessaryCookies',
+  require('./cookies/provideRejectNotNecessaryCookies'),
+);
+
+Cypress.Commands.add(
+  'confirmAcceptedCookieBannerVisible',
+  require('./cookies/confirmAcceptedCookieBannerVisible'),
+);
+
+Cypress.Commands.add(
+  'confirmRejectewokieBannerVisible',
+  require('./cookies/confirmRejectedCookieBannerVisible'),
+);
+
+Cypress.Commands.add(
+  'confirmNoDecisionCookieBannerVisible',
+  require('./cookies/confirmNoDecisionCookieBannerVisible'),
+);
+
+Cypress.Commands.add(
+  'confirmCookiePolicy',
+  require('./cookies/confirmCookiePolicy'),
+);
+
+Cypress.Commands.add(
   'goToCookiePreferencesPage',
   require('./cookies/goToCookiePreferences'),
 );
