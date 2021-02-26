905cea27fba6957e2796d2700e75704663c424c0
Christopher Moss <chris@a6software.co.uk>
2021-02-26 10:49:09 +0000

    test(e2e-tests): replace generic button clicks with clickSaveAndContinue

--- e2e-tests/cypress/support/eligibility-decision-date/provideDecisionDate.js
+++ e2e-tests/cypress/support/eligibility-decision-date/provideDecisionDate.js
@@ -1,5 +1,5 @@
 // implementation of the users desire to provide a decision date..
-module.exports = ({day, month, year}) => {
+module.exports = ({ day, month, year }) => {
   // go to the right page
   cy.visit('/eligibility/decision-date');
 
@@ -9,7 +9,7 @@ module.exports = ({day, month, year}) => {
   cy.get('#decision-date-year').type(`{selectall}{backspace}${year}`);
 
   // click the submit button, leaving enough time for us to capture nice videos..
-  cy.wait(Cypress.env('demoDelay'))
-  const continueBtn = cy.get('.govuk-button').click()
-  cy.wait(Cypress.env('demoDelay'))
-}
+  cy.wait(Cypress.env('demoDelay'));
+  cy.clickSaveAndContinue();
+  cy.wait(Cypress.env('demoDelay'));
+};
--- e2e-tests/cypress/support/eligibility-listed-building-status/stateCaseDoesNotInvolveAListedBuilding.js
+++ e2e-tests/cypress/support/eligibility-listed-building-status/stateCaseDoesNotInvolveAListedBuilding.js
@@ -1,10 +1,9 @@
 module.exports = () => {
   cy.visit('/eligibility/listed-building');
 
-  cy.get('#is-your-appeal-about-a-listed-building-2').click()
-
+  cy.get('#is-your-appeal-about-a-listed-building-2').click();
 
   cy.wait(Cypress.env('demoDelay'));
-  const continueBtn = cy.get('.govuk-button').click();
+  cy.clickSaveAndContinue();
   cy.wait(Cypress.env('demoDelay'));
-}
+};
