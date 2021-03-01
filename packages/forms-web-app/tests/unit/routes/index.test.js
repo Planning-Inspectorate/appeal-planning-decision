38d2ae2179f207ec3b80860b7bd6a30a9a0a1d4f
ray@rcmsoftwaredevelopment.co.uk <ray@rcmsoftwaredevelopment.co.uk>
2021-03-01 18:49:31 +0000

    feat(as-1224): update to view cookie preferences
    update to view cookie preferences

--- packages/forms-web-app/tests/unit/routes/index.test.js
+++ packages/forms-web-app/tests/unit/routes/index.test.js
@@ -2,6 +2,7 @@ const { use } = require('./router-mock');
 const appellantSubmissionRouter = require('../../../src/routes/appellant-submission');
 const eligibilityRouter = require('../../../src/routes/eligibility');
 const homeRouter = require('../../../src/routes/home');
+const cookieRouter = require('../../../src/routes/cookies');
 
 describe('routes/index', () => {
   beforeEach(() => {
@@ -15,8 +16,9 @@ describe('routes/index', () => {
 
   it('should define the expected routes', () => {
     expect(use).toHaveBeenCalledWith('/', homeRouter);
+    expect(use).toHaveBeenCalledWith('/cookies', cookieRouter);
     expect(use).toHaveBeenCalledWith('/appellant-submission', appellantSubmissionRouter);
     expect(use).toHaveBeenCalledWith('/eligibility', eligibilityRouter);
-    expect(use.mock.calls.length).toBe(3);
+    expect(use.mock.calls.length).toBe(4);
   });
 });
