e8ff82060b0f4f44eb2a72ec5b176b077df2a3e9
ray@rcmsoftwaredevelopment.co.uk <ray@rcmsoftwaredevelopment.co.uk>
2021-03-01 13:37:35 +0000

    feat(as-1224): cooke view preferences web page
    cookie view preferences web page

--- packages/forms-web-app/src/routes/home.js
+++ packages/forms-web-app/src/routes/home.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const indexController = require('../controllers');
+const cookiePreferencesController = require('../controllers/cookies/cookie-preferences');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', indexController.getIndex);
+router.get('/cookies', cookiePreferencesController.getCookiePreferences);
 
 module.exports = router;
