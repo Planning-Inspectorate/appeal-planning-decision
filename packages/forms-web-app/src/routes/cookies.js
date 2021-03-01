5fee721cc8881a6973a63ad68abd0d35c8c7cd8e
ray@rcmsoftwaredevelopment.co.uk <ray@rcmsoftwaredevelopment.co.uk>
2021-03-01 18:49:31 +0000

    feat(as-1224): update to view cookie preferences
    update to view cookie preferences

--- packages/forms-web-app/src/routes/cookies.js
+++ packages/forms-web-app/src/routes/cookies.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const indexController = require('../controllers');
+const cookiePreferencesController = require('../controllers/cookies/cookie-preferences');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', indexController.getIndex);
+router.get('/', cookiePreferencesController.getCookiePreferences);
 
 module.exports = router;
