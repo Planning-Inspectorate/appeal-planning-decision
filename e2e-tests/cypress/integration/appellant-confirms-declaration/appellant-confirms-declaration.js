47d01f174cb441a6084b1462e886b9b3e7ac9c41
Yassine Ferkouch <yassine.ferkouch@foundry4.com>
2021-03-03 01:28:29 +0100

    test(e2e-tests): test that the appeal pdf is stored in the document service after submission

--- e2e-tests/cypress/integration/appellant-confirms-declaration/appellant-confirms-declaration.js
+++ e2e-tests/cypress/integration/appellant-confirms-declaration/appellant-confirms-declaration.js
@@ -54,11 +54,30 @@ When('the declaration is not agreed', () => {
   cy.doNotAgreeToTheDeclaration();
 });
 When('the declaration is agreed', () => {
+  cy.task('listenToQueue');
   cy.agreeToTheDeclaration();
 });
 
 Then('the submission confirmation is presented', () => {
   cy.confirmAppealSubmitted();
+
+  cy.task('getLastFromQueue').then((document) => {
+    const applicationId = document.appeal.id;
+
+    cy.request('http://localhost:3001/api/v1/' + applicationId).then((resp) => {
+      expect(resp.status).to.eq(200);
+
+      const documents = resp.body;
+      let appealPdfDocument = null;
+      documents.forEach((doc) => {
+        if (doc.name === applicationId + '.pdf') {
+          appealPdfDocument = doc;
+        }
+      });
+
+      expect(appealPdfDocument).to.not.eq(null);
+    });
+  });
 });
 
 Then('no submission confirmation is presented', () => {
