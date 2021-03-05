5c0b0132730b6f3d00c3a627821940b0a54b0e3d
Dan Watkinson <dan.watkinson@gmail.com>
2021-03-05 15:35:12 +0000

    test: provide easy scripts for sending appeals to horizon via cypress

--- e2e-tests/cypress/plugins/queue.js
+++ e2e-tests/cypress/plugins/queue.js
@@ -1,6 +1,8 @@
 const container = require('rhea');
 
 module.exports = (config) => {
+  const queueValidationEnabled = config.env.QUEUE_VALIDATION_ENABLED;
+
   const username = config.env.HORIZON_HAS_PUBLISHER_USERNAME;
   const password = config.env.HORIZON_HAS_PUBLISHER_PASSWORD;
   const port = config.env.HORIZON_HAS_PUBLISHER_PORT;
@@ -15,46 +17,62 @@ module.exports = (config) => {
   return {
     listenToQueue: () => {
       return new Promise( (resolve, reject) => {
-        container.on('message', function (context) {
-          const messageFromBuffer = context.message.body.content.toString('utf-8');
-          const message = JSON.parse(messageFromBuffer);
-          received.push(message);
-        });
 
-        container.on('error', function (context) {
-          reject(error);
-        });
+        if (queueValidationEnabled) {
+          container.on('message', function (context) {
+            const messageFromBuffer = context.message.body.content.toString('utf-8');
+            const message = JSON.parse(messageFromBuffer);
+            received.push(message);
+          });
 
-        var connection = container.connect(opts);
+          container.on('error', function (context) {
+            reject(error);
+          });
 
-        connection.open_receiver(queue);
+          var connection = container.connect(opts);
 
-        resolve(null);
+          connection.open_receiver(queue);
+
+          resolve(null);
+        } else {
+          resolve(null);
+        }
+
       });
     },
 
     putOnQueue: (message)=> {
       return new Promise( (resolve, reject) => {
-        container.once('sendable', function (context) {
-          context.sender.send({body: message});
+
+        if (queueValidationEnabled) {
+          container.once('sendable', function (context) {
+            context.sender.send({body: message});
+            resolve(null);
+          });
+
+          container.on('error', function (context) {
+            reject(error);
+          });
+
+          var connection = container.connect(opts);
+
+          connection.open_sender(queue);
+        } else {
           resolve(null);
-        });
+        }
 
-        container.on('error', function (context) {
-          reject(error);
-        });
-
-        var connection = container.connect(opts);
-
-        connection.open_sender(queue);
       });
     },
 
     getLastFromQueue: ()=> {
-      if (!received || !received.length) {
-        return null; // at least surface the failure in the test..
+      if (queueValidationEnabled) {
+        if (!received || !received.length) {
+          return null; // at least surface the failure in the test..
+        }
+        return received[received.length - 1];
+      } else {
+        return "should not be validating against this as queue validation is disabled"
       }
-      return received[received.length - 1];
     }
   }
 }
