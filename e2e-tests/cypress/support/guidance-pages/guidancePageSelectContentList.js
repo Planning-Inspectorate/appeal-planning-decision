b2ba1e788fed5eb9eb5fe5e306c22accc031e908
David Lewis <dave@davehlewis.com>
2021-03-05 15:15:28 +0000

    test(appeals-e2e): fix incorrect lookup for nav

--- e2e-tests/cypress/support/guidance-pages/guidancePageSelectContentList.js
+++ e2e-tests/cypress/support/guidance-pages/guidancePageSelectContentList.js
@@ -1,7 +1,7 @@
 module.exports = (listItem) =>{
   const listIdentifier = listItem.toLowerCase().split(' ').join('-');
 
-  cy.get(`[data-cy="guidance-page-nav--${listIdentifier}]`).first().click();
+  cy.get(`[data-cy="guidance-page-nav--${listIdentifier}"]`).first().click();
 
   cy.wait(Cypress.env('demoDelay'));
 }
