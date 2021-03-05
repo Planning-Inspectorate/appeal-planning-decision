76efb802232dfc365c1a24dd6c40e248f2757b82
Christopher Moss <chris@a6software.co.uk>
2021-03-05 10:21:37 +0000

    test(e2e-tests): rebase to latest master - fixup test structure changes

--- e2e-tests/cypress/support/commands.js
+++ e2e-tests/cypress/support/commands.js
@@ -1,7 +1,7 @@
 require('./commands/header-footer-commands');
 require('./visit-without-javascript-enabled');
 require('./common/commands');
-require('./commands/cookies-commands');
+require('./cookies/cookies-commands');
 require('./commands/navigation-commands');
 require('./commands/eligibility-commands');
 require('./commands/submission-commands');
