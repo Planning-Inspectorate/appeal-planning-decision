69ba6224a1687bf76cd4682a70f602127a543788
David Lewis <dave@davehlewis.com>
2021-03-10 10:52:25 +0000

    test(forms-web-app-e2e): feature file and tests for stages of appeal page

--- e2e-tests/cypress/support/commands/guidance-page-commands.js
+++ e2e-tests/cypress/support/commands/guidance-page-commands.js
@@ -1,5 +1,6 @@
 Cypress.Commands.add('goToPageBeforeYouAppeal', require('../go-to-page/goToPageBeforeYouAppeal'));
 Cypress.Commands.add('goToPageWhenYouCanAppeal', require('../go-to-page/goToPageWhenYouCanAppeal'));
+Cypress.Commands.add('goToPageStagesOfAnAppeal', require('../go-to-page/goToPageStagesOfAnAppeal'));
 
 Cypress.Commands.add(
   'guidancePageNavigation',
