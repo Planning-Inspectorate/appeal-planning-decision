74854c8367d4166468d9b476ef116646bd5ab846
David Lewis <dave@davehlewis.com>
2021-03-10 12:25:13 +0000

    feat(forms-web-app): add stages of an appeal guidance page

--- packages/forms-web-app/src/routes/guidance-pages.js
+++ packages/forms-web-app/src/routes/guidance-pages.js
@@ -4,6 +4,7 @@ const {
   getWhenAppeal,
   getAfterAppeal,
   getStartAppeal,
+  getStagesAppeal,
 } = require('../controllers/guidance-pages');
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.get('/before-you-appeal', getBeforeAppeal);
 router.get('/when-you-can-appeal', getWhenAppeal);
 router.get('/after-you-appeal', getAfterAppeal);
 router.get('/start-your-appeal', getStartAppeal);
+router.get('/stages-of-an-appeal', getStagesAppeal);
 
 module.exports = router;
