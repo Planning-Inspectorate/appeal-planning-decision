36cb57518bc4c0273e12e45d26fa63f1596968fb
David Lewis <dave@davehlewis.com>
2021-03-10 14:53:00 +0000

    feat(forms-web-app): add start appeal page to guidance pages

--- packages/forms-web-app/src/routes/guidance-pages.js
+++ packages/forms-web-app/src/routes/guidance-pages.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { getBeforeAppeal, getWhenAppeal, getAfterAppeal } = require('../controllers/guidance-pages');
+const {
+  getBeforeAppeal,
+  getWhenAppeal,
+  getAfterAppeal,
+  getStartAppeal,
+} = require('../controllers/guidance-pages');
 
 const router = express.Router();
 
 router.get('/before-you-appeal', getBeforeAppeal);
 router.get('/when-you-can-appeal', getWhenAppeal);
 router.get('/after-you-appeal', getAfterAppeal);
+router.get('/start-your-appeal', getStartAppeal);
 
 module.exports = router;
