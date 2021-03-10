5cf9719d0c99f8566e2fbcc178062b2e54b076c8
Frank Phillips <frank@dodion.co.uk>
2021-03-10 11:19:53 +0000

    fix(forms-web-app): add env appropriate GA_ID value to each page and use it to initalise GA

--- packages/forms-web-app/webpack.prod.js
+++ packages/forms-web-app/webpack.prod.js
@@ -1,14 +1,12 @@
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const config = require('./src/config.js');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.GOOGLE_ANALYTICS_ID': JSON.stringify(config.server.googleAnalyticsId),
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
   ],
