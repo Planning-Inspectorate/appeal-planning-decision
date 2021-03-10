ab2e7ccddbdb15dd890d4cfeccb836ed87402956
Christopher Moss <chris@a6software.co.uk>
2021-03-10 15:28:36 +0000

    build(forms-web-app): removes webpack define config - not needed, misleading

--- packages/forms-web-app/webpack.dev.js
+++ packages/forms-web-app/webpack.dev.js
@@ -1,13 +1,7 @@
-const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-  ],
 });
