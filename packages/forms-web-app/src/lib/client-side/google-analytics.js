147cbb9e9108df15a1c855cf6446a250c24ebaba
Christopher Moss <chris@a6software.co.uk>
2021-03-11 10:36:33 +0000

    fix(forms-web-app): call GA when initialised

--- packages/forms-web-app/src/lib/client-side/google-analytics.js
+++ packages/forms-web-app/src/lib/client-side/google-analytics.js
@@ -10,6 +10,16 @@ const initialiseGoogleAnalytics = (document) => {
 
   const firstScriptElement = document.getElementsByTagName('script')[0];
   firstScriptElement.parentNode.insertBefore(gaScript, firstScriptElement);
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    // eslint-disable-next-line no-undef, prefer-rest-params
+    dataLayer.push(arguments);
+  }
+
+  gtag('js', new Date());
+  gtag('config', gaId);
 };
 
 module.exports = {
--- packages/forms-web-app/tests/unit/lib/client-side/google-analytics.test.js
+++ packages/forms-web-app/tests/unit/lib/client-side/google-analytics.test.js
@@ -6,18 +6,43 @@
 const { initialiseGoogleAnalytics } = require('../../../../src/lib/client-side/google-analytics');
 
 describe('lib/client-side/google-analytics', () => {
-  beforeEach(() => {
+  const FIXED_SYSTEM_TIME = '2020-11-18T00:00:00Z';
+
+  const fakeGaId = 'some-test-value';
+
+  const setupFakeDom = () => {
     const gaIdElement = document.createElement('p');
     gaIdElement.id = 'gaId';
-    gaIdElement.textContent = 'some-test-value';
+    gaIdElement.textContent = fakeGaId;
     document.body.append(gaIdElement);
     const script = document.createElement('script');
     document.body.appendChild(script);
+  };
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+
+    setupFakeDom();
+
+    // https://github.com/facebook/jest/issues/2234#issuecomment-730037781
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(Date.parse(FIXED_SYSTEM_TIME));
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
   });
 
   test('initialiseGoogleAnalytics', () => {
+    expect(window.dataLayer).toBe(undefined);
+
     initialiseGoogleAnalytics(document);
 
     expect(document.body).toMatchSnapshot();
+
+    expect(window.dataLayer).toHaveLength(2);
+    // https://github.com/facebook/jest/issues/8475#issuecomment-495729482
+    expect([...window.dataLayer[0]]).toMatchObject(['js', new Date()]);
+    expect([...window.dataLayer[1]]).toMatchObject(['config', fakeGaId]);
   });
 });
