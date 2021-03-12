ab91bd448783fe6383638cfaab778db89a18d424
Simon Emms <simon.emms@foundry4.com>
2021-03-12 17:02:24 +0000

    chore(openfaas): update base template with a logger
    This now uses the new structured log format as the default. This also bumps the watchdog to v0.8.2
    (latest)

--- template/node/index.js
+++ template/node/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pino = require('pino');
 
 const FunctionEvent = require('./lib/functionEvent');
 const FunctionContext = require('./lib/functionContext');
@@ -7,6 +8,14 @@ const handler = require('./function/handler');
 
 const app = express();
 app.disable('x-powered-by');
+
+const logger = pino({
+  level: process.env.LOGGER_LEVEL ?? 'info',
+  redact: (process.env.LOGGER_REDACT ?? '')
+    .split(',')
+    .map((item) => item.trim())
+    .filter((item) => item),
+});
 
 if (process.env.RAW_BODY === 'true') {
   app.use(bodyParser.raw({ type: '*/*' }));
@@ -22,19 +31,23 @@ const isArray = (input) => !!input && input.constructor === Array;
 const isObject = (input) => !!input && input.constructor === Object;
 
 const middleware = async (req, res) => {
-  const fnEvent = new FunctionEvent(req);
+  const fnEvent = new FunctionEvent(req, logger);
   const fnContext = new FunctionContext();
 
   let output;
   try {
+    fnEvent.log.trace('Function invoked');
     output = await handler(fnEvent, fnContext);
+    fnEvent.log.trace('Function called successfully');
   } catch (err) {
-    console.error(err);
+    fnEvent.log.error({ err }, 'Function errored');
     output = err.toString ? err.toString() : err;
     fnContext.httpStatus = 500;
   }
 
   const fnResult = isArray(output) || isObject(output) ? JSON.stringify(output) : output;
+
+  fnEvent.log.info(fnContext, 'Function response complete');
 
   res.status(fnContext.httpStatus).set(fnContext.httpHeaders).send(fnResult);
 };
@@ -48,5 +61,5 @@ app.delete('/*', middleware);
 const port = Number(process.env.http_port || 3000);
 
 app.listen(port, () => {
-  console.log(`OpenFaaS Node.js listening on port: ${port}`);
+  logger.info({ port }, 'OpenFaaS function listening');
 });
