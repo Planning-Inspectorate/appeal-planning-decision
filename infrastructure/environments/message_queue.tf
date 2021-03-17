9964b5ca1a18370efbea20a779ccfb1731a8ae69
Simon Emms <simon.emms@foundry4.com>
2021-03-17 14:46:56 +0000

    chore(monitoring): add alert for abandoned messages in queue

--- infrastructure/environments/message_queue.tf
+++ infrastructure/environments/message_queue.tf
@@ -26,3 +26,28 @@ resource "azurerm_servicebus_namespace" "message_queue" {
   zone_redundant = var.message_queue_zone_redundancy_enabled
 }
 
+resource "azurerm_monitor_metric_alert" "abandoned_message_alert" {
+  count = local.monitoring_alert_email_count
+
+  name = azurerm_servicebus_namespace.message_queue.name
+  resource_group_name = azurerm_resource_group.message_queue.name
+  scopes = [azurerm_servicebus_namespace.message_queue.id]
+  description = "Alert for abandoned messages on ${azurerm_servicebus_namespace.message_queue.name}"
+  severity = 1
+  # Only check once per hour for things in that window
+  frequency = "PT1H"
+  window_size = "PT1H"
+
+  criteria {
+    aggregation = "Total"
+    metric_name = "AbandonMessage"
+    metric_namespace = "Microsoft.ServiceBus/namespaces"
+    operator = "GreaterThanOrEqual"
+    threshold = 1
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.monitoring[count.index].id
+  }
+
+}
