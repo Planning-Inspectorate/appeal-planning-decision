c457cdf03846a0171197e912581447ce204abf63
David Lewis <dave@davehlewis.com>
2021-03-22 14:10:37 +0000

    test: add missing test file for file routes

---
+++ packages/lpa-questionnaire-web-app/tests/unit/routes/files.test.js
@@ -0,0 +1,30 @@
+const { post } = require('./router-mock');
+const filesController = require('../../../src/controllers/files');
+const reqFilesToReqBodyFilesMiddleware = require('../../../src/middleware/req-files-to-req-body-files');
+const filesValidationRules = require('../../../src/validators/files');
+const { validationErrorHandler } = require('../../../src/validators/validation-error-handler');
+
+jest.mock('../../../src/middleware/req-files-to-req-body-files');
+jest.mock('../../../src/validators/files');
+
+describe('routes/placeholder', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line global-require
+    require('../../../src/routes/files');
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should define the expected routes', () => {
+    expect(post).toHaveBeenCalledWith(
+      '/upload',
+      [reqFilesToReqBodyFilesMiddleware('documents'), filesValidationRules()],
+      validationErrorHandler,
+      filesController.uploadFile
+    );
+
+    expect(post).toHaveBeenCalledWith('/delete', filesController.deleteFile);
+  });
+});
