25323d958c459f75d33e59bef9eb15d8d888f685
Christopher Moss <chris@a6software.co.uk>
2021-03-25 08:15:57 +0000

    fix(forms-web-app): adds AXE to CI pipeline flow, fix accessibility issues found by AXE

--- e2e-tests/cypress/support/go-to-page/goToAppealStatementSubmission.js
+++ e2e-tests/cypress/support/go-to-page/goToAppealStatementSubmission.js
@@ -1,4 +1,6 @@
 module.exports = () => {
-  cy.visit('/appellant-submission/appeal-statement', {failOnStatusCode: false});
+  cy.visit('/appellant-submission/appeal-statement', { failOnStatusCode: false });
   cy.wait(Cypress.env('demoDelay'));
+
+  cy.checkPageA11y();
 };
