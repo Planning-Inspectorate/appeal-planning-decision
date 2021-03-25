0c0bfa94fa95ad875d20885568e1d47e6b681aa2
Yassine Ferkouch <yas.ferkouch@gmail.com>
2021-03-25 23:53:30 +0100

    test(e2e-tests):  test

--- e2e-tests/reporter.js
+++ e2e-tests/reporter.js
@@ -4,30 +4,30 @@ const report = require('multiple-cucumber-html-reporter');
 const fs = require('fs');
 const length = fs.readdirSync('./cypress/cucumber-json').length;
 
-if (length>0) {
-report.generate({
-	jsonDir: './cypress/cucumber-json',
-	reportPath: './cypress/cucumber-report',
-	// metadata:{
-  //       browser: {
-  //           name: 'chrome',
-  //           version: '60'
-  //       },
-  //       device: 'Local test machine',
-  //       platform: {
-  //           name: 'ubuntu',
-  //           version: '16.04'
-  //       }
-  //   },
-  //   customData: {
-  //       title: 'Run info',
-  //       data: [
-  //           {label: 'Project', value: 'Custom project'},
-  //           {label: 'Release', value: '1.2.3'},
-  //           {label: 'Cycle', value: 'B11221.34321'},
-  //           {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
-  //           {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
-  //       ]
-  //   }
-});
-};
+if (length > 0) {
+  report.generate({
+    jsonDir: './cypress/cucumber-json',
+    reportPath: './cypress/cucumber-report',
+    // metadata:{
+    //       browser: {
+    //           name: 'chrome',
+    //           version: '60'
+    //       },
+    //       device: 'Local test machine',
+    //       platform: {
+    //           name: 'ubuntu',
+    //           version: '16.04'
+    //       }
+    //   },
+    //   customData: {
+    //       title: 'Run info',
+    //       data: [
+    //           {label: 'Project', value: 'Custom project'},
+    //           {label: 'Release', value: '1.2.3'},
+    //           {label: 'Cycle', value: 'B11221.34321'},
+    //           {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
+    //           {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
+    //       ]
+    //   }
+  });
+}
