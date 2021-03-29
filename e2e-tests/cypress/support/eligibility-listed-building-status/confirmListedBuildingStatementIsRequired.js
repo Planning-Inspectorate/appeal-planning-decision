46d75305b158b36bdd0269c012efcf72023da2f7
Christopher Moss <chris@a6software.co.uk>
2021-03-29 12:02:49 +0100

    fix(forms-web-app): adds 'error' to page title - listed building

--- e2e-tests/cypress/support/eligibility-listed-building-status/confirmListedBuildingStatementIsRequired.js
+++ e2e-tests/cypress/support/eligibility-listed-building-status/confirmListedBuildingStatementIsRequired.js
@@ -1,7 +1,13 @@
 module.exports = () => {
-  cy.get(".govuk-error-summary__list").invoke('text').then((text) => {
-    expect(text).to.contain("Select yes if your appeal is about a listed building");
-  });
+  cy.get('.govuk-error-summary__list')
+    .invoke('text')
+    .then((text) => {
+      expect(text).to.contain('Select yes if your appeal is about a listed building');
+    });
+
+  cy.url().should('include', '/eligibility/listed-building');
+
+  cy.title().should('match', /^Error: /);
 
   cy.wait(Cypress.env('demoDelay'));
-}
+};
