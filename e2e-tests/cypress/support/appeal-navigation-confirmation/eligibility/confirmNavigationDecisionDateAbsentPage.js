81670aae812a32891e0e3b0d1acc9046379ca68c
Christopher Moss <chris@a6software.co.uk>
2021-03-29 11:44:09 +0100

    fix(forms-web-app): adds 'error' to page title - decision date

--- e2e-tests/cypress/support/appeal-navigation-confirmation/eligibility/confirmNavigationDecisionDateAbsentPage.js
+++ e2e-tests/cypress/support/appeal-navigation-confirmation/eligibility/confirmNavigationDecisionDateAbsentPage.js
@@ -1,4 +1,4 @@
 module.exports = () => {
   cy.url().should('include', '/eligibility/no-decision');
   cy.wait(Cypress.env('demoDelay'));
-}
+};
--- e2e-tests/cypress/support/eligibility-decision-date/confirmProvidedDecisionDateError.js
+++ e2e-tests/cypress/support/eligibility-decision-date/confirmProvidedDecisionDateError.js
@@ -1,7 +1,13 @@
 module.exports = (error) => {
   cy.url().should('include', '/eligibility/decision-date');
-  cy.get(".govuk-error-summary__list").invoke('text').then((text) => {
-    expect(text).to.contain(error);
-  });
-  cy.wait(Cypress.env('demoDelay'))
-}
+
+  cy.title().should('match', /^Error: /);
+
+  cy.get('.govuk-error-summary__list')
+    .invoke('text')
+    .then((text) => {
+      expect(text).to.contain(error);
+    });
+
+  cy.wait(Cypress.env('demoDelay'));
+};
