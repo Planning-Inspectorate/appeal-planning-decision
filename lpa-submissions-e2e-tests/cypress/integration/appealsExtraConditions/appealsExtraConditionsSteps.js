ac045d3352c2dcab9d89a0256eefa40c941268ae
David Lewis <dave@davehlewis.com>
2021-03-30 11:20:30 +0100

    test(lpa-questionnaire): add save and continue button attribute

--- lpa-submissions-e2e-tests/cypress/integration/appealsExtraConditions/appealsExtraConditionsSteps.js
+++ lpa-submissions-e2e-tests/cypress/integration/appealsExtraConditions/appealsExtraConditionsSteps.js
@@ -42,8 +42,8 @@ When(`user selects Save and Continue`, () => {
 
 Then(`user is shown an error message {string}`, (errorMessage) => {
   errorMessage === 'Select yes if you have extra conditions'
-    ? cy.validateErrorMessage(errorMessage, 'extra-conditions-error', 'has-extra-conditions')
-    : cy.validateErrorMessage(errorMessage, 'extra-conditions-text-error', 'extra-conditions-text');
+    ? cy.validateErrorMessage(errorMessage, '[data-cy="extra-conditions-error"]', 'has-extra-conditions')
+    : cy.validateErrorMessage(errorMessage, '[data-cy="extra-conditions-text-error"]', 'extra-conditions-text');
 });
 
 Then(`the user remains on extra conditions page`, () => {
--- lpa-submissions-e2e-tests/cypress/support/upload-plan-to-reach-decision/fileDragAndDrop.js
+++
@@ -1,7 +0,0 @@
-/// <reference types = "Cypress"/>
-import UploadThePlanToReachDecision from '../PageObjects/UploadThePlanToReachDecisionPageObjects';
-const fileDragAndDrop = new UploadThePlanToReachDecision();
-module.exports = (fileName) => {
-    fileDragAndDrop.fileDragAndDrop().attachFile(fileName, { subjectType: 'drag-n-drop' });
-
-};
