71288cd6a5ae456fc4fca6e291f86caf51e61635
swdpcomputing <swdpcomputing@outlook.com>
2021-03-31 12:58:27 +0100

    feat(lpa-submissions-e2e-tests): refactored appeals development plan e2e tests

--- lpa-submissions-e2e-tests/cypress/support/appeals-development-plan/inputDevelopmentPlanDetails.js
+++
@@ -1,6 +0,0 @@
-/// <reference types = "Cypress"/>
-import DevelopmentPlanPage from '../PageObjects/appeals-development-plan-pageobjects';
-const developmentPlanDetails = new DevelopmentPlanPage();
-module.exports = () => {
-  developmentPlanDetails.developmentPlanDetailsTextBox();
-};
