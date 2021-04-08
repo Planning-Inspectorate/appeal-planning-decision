f1fe306976237e60a350649d8b6124fdd76fa075
swdpcomputing <swdpcomputing@outlook.com>
2021-04-08 16:09:07 +0100

    feat(lpa-questionnaire-web-app): created view for submission

--- packages/lpa-questionnaire-web-app/src/routes/index.js
+++ packages/lpa-questionnaire-web-app/src/routes/index.js
@@ -8,6 +8,7 @@ const accuracySubmissionRouter = require('./accuracy-submission');
 const otherAppealsRouter = require('./other-appeals');
 const extraConditionsRouter = require('./extra-conditions');
 const developmentPlanRouter = require('./development-plan');
+const questionnaireSubmittedRouter = require('./questionnaire-submitted');
 
 router.use(homeRouter);
 router.use(taskListRouter);
@@ -15,5 +16,6 @@ router.use(accuracySubmissionRouter);
 router.use(otherAppealsRouter);
 router.use(extraConditionsRouter);
 router.use(developmentPlanRouter);
+router.use(questionnaireSubmittedRouter);
 
 module.exports = router;
