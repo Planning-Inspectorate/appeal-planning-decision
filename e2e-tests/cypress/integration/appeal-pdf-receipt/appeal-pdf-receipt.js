af50ef6bbaf2897a2edc472b3546afa872f8a989
Paul Bradley <paul.bradley@foundry4.com>
2021-04-12 11:28:40 +0100

    test: check PDF source has question row as expected

--- e2e-tests/cypress/integration/appeal-pdf-receipt/appeal-pdf-receipt.js
+++ e2e-tests/cypress/integration/appeal-pdf-receipt/appeal-pdf-receipt.js
@@ -1,14 +1,22 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-const OWNERS_INFORMED = require('../../fixtures/expected-appeal-without-certificate-a-other-owner-informed.json');
+import { APPEAL_NOT_OWNER_OTHERS_INFORMED } from '../common/standard-appeal';
 
 Given('an agent or appellant has submitted an appeal and they do not wholly own the site', () => {
-  cy.provideCompleteAppeal(OWNERS_INFORMED);
+  cy.provideCompleteAppeal(APPEAL_NOT_OWNER_OTHERS_INFORMED);
+  cy.clickCheckYourAnswers();
+  cy.clickSaveAndContinue();
+  cy.agreeToTheDeclaration();
+  cy.confirmAppealSubmitted();
+  cy.navigateToSubmissionInformationPage();
 });
 
 When('the pdf is viewed', () => {
-  cy.clickPDFLink();
+  // Get the PDF when we have it available ... for now just test the html source
 });
 
 Then('the answer for other owner notification is displayed as submitted', () => {
-  cy.confirmPDFContent();
+  cy.confirmSubmissionInformationDisplayItems({
+    'site-ownership': 'No',
+    'other-owner-notification': 'Yes, I have already told the other owners',
+  });
 });
