b1167b64b0d62b388220911748c35fc2ea127da6
ravikorrai <ravi.kumar@difrent.co.uk>
2021-04-14 09:54:18 +0100

    test(lpa-submission-e2e-tests): feature and steps for pdf generation

--- lpa-submissions-e2e-tests/cypress/support/common/completeQuestionnaire.js
+++ lpa-submissions-e2e-tests/cypress/support/common/completeQuestionnaire.js
@@ -17,19 +17,23 @@ const uploadFiles = (fileName) => {
 const stepCompletion = () => {
   // Accuracy of submission
   cy.goToPage('accuracy-submission');
-  input('accurate-submission-no').check()
-  textArea('inaccuracy-reason').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.');
+  input('accurate-submission-no').check();
+  textArea('inaccuracy-reason').type(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.',
+  );
   cy.clickSaveAndContinue();
 
   // Extra conditions
   cy.goToPage('extra-conditions');
-  input('has-extra-conditions-yes').check()
-  textArea('extra-conditions-text').type('Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis.\n\nSed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.');
+  input('has-extra-conditions-yes').check();
+  textArea('extra-conditions-text').type(
+    'Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis.\n\nSed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.',
+  );
   cy.clickSaveAndContinue();
 
   // Other appeals
   cy.goToPage('other-appeals');
-  input('adjacent-appeals-yes').check()
+  input('adjacent-appeals-yes').check();
   textBox('appeal-reference-numbers').type('abc-123, def-456');
   cy.clickSaveAndContinue();
 
@@ -60,28 +64,31 @@ const stepCompletion = () => {
 
   // Development Plan
   cy.goToPage('development-plan');
-  input('has-plan-submitted-yes').check()
+  input('has-plan-submitted-yes').check();
   textArea('plan-changes-text').type('mock plan changes');
   cy.clickSaveAndContinue();
-}
+};
 
 const apiCompletion = () => {
   // Visit task list page to invoke session, which generates an appeal reply ID
   cy.goToTaskListPage();
 
-  let id
-  cy.getAppealReplyId().then(replyId => id = replyId);
-
-  cy.fixture('completedAppealReply.json').then((reply) => {
-    cy.request(
-      'PUT',
-      `${Cypress.env('APPEAL_REPLY_SERVICE_BASE_URL')}/api/v1/reply/${id}`,
-      reply
-    ).then(response => {
-      expect(response.body.aboutAppealSection.submissionAccuracy).to.have.property('accurateSubmission', false);
+  cy.getAppealReplyId().then((replyId) => {
+    cy.fixture('completedAppealReply.json').then((reply) => {
+      reply.id = replyId;
+      cy.request(
+        'PUT',
+        `${Cypress.env('APPEAL_REPLY_SERVICE_BASE_URL')}/reply/${replyId}`,
+        reply,
+      ).then((response) => {
+        expect(response.body.aboutAppealSection.submissionAccuracy).to.have.property(
+          'accurateSubmission',
+          false,
+        );
+      });
     });
   });
-}
+};
 
 module.exports = () => {
   if (!Cypress.env('ASSUME_LIMITED_ACCESS')) {
