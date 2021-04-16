7c1f467526a76f86e83fa471d4a772f10da01aa0
Christopher Moss <chris@a6software.co.uk>
2021-04-16 07:56:59 +0100

    fix(e2e-tests): adds explicit scenario for testing listed building status back link

--- e2e-tests/cypress/support/eligibility-listed-building-status/confirmUserCanProceedWithNonListedBuilding.js
+++ e2e-tests/cypress/support/eligibility-listed-building-status/confirmUserCanProceedWithNonListedBuilding.js
@@ -6,13 +6,4 @@ module.exports = () => {
     });
 
   cy.wait(Cypress.env('demoDelay'));
-
-  // prove that the entered data is retained during navigation
-  // -> use the provided 'back' button
-  cy.clickBackLink();
-
-  // -> confirm the expected state of the radio-buttons
-  cy.get('#is-your-appeal-about-a-listed-building-2').should('be.checked');
-  cy.get('#is-your-appeal-about-a-listed-building').should('not.be.checked');
-  cy.wait(Cypress.env('demoDelay'));
 };
