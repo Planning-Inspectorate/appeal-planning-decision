ef196794a7c39643a29f0fcfb5a494d272ef7f62
David Lewis <dave@davehlewis.com>
2021-04-21 15:40:27 +0100

    test(questionnaire-web-app): update tests to work in deployed environments

--- lpa-submissions-e2e-tests/cypress/support/common/completeQuestionnaire.js
+++ lpa-submissions-e2e-tests/cypress/support/common/completeQuestionnaire.js
@@ -1,4 +1,56 @@
-module.exports = () => {
+import { input, textBox, textArea } from '../PageObjects/common-page-objects';
+
+const uploadFiles = (fileName) => {
+  // start watching the POST requests
+  cy.server({ method: 'POST' });
+  cy.route({
+    method: 'POST',
+    url: 'upload',
+  }).as('upload');
+
+  cy.get('input#documents').attachFile([fileName]);
+
+  cy.wait('@upload', { requestTimeout: 3000 });
+  cy.server({ enable: false });
+};
+
+const stepCompletion = () => {
+  // Accuracy of submission
+  cy.goToPage('accuracy-submission');
+  input('accurate-submission-no').check()
+  textArea('inaccuracy-reason').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.');
+  cy.clickSaveAndContinue();
+
+  // Extra conditions
+  cy.goToPage('extra-conditions');
+  input('has-extra-conditions-yes').check()
+  textArea('extra-conditions-text').type('Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis.\n\nSed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.');
+  cy.clickSaveAndContinue();
+
+  // Other appeals
+  cy.goToPage('other-appeals');
+  input('adjacent-appeals-yes').check()
+  textBox('appeal-reference-numbers').type('abc-123, def-456');
+  cy.clickSaveAndContinue();
+
+  // Plans
+  cy.goToPage('plans');
+  uploadFiles('upload-file-valid.pdf');
+  cy.clickSaveAndContinue();
+
+  // Officers Report
+  cy.goToPage('officers-report');
+  uploadFiles('upload-file-valid.pdf');
+  cy.clickSaveAndContinue();
+
+  // Development Plan
+  cy.goToPage('development-plan');
+  input('has-plan-submitted-yes').check()
+  textArea('plan-changes-text').type('mock plan changes');
+  cy.clickSaveAndContinue();
+}
+
+const apiCompletion = () => {
   // Visit task list page to invoke session, which generates an appeal reply ID
   cy.goToTaskListPage();
 
@@ -14,4 +66,12 @@ module.exports = () => {
       expect(response.body.aboutAppealSection.submissionAccuracy).to.have.property('accurateSubmission', false);
     });
   });
+}
+
+module.exports = () => {
+  if (!Cypress.env('ASSUME_LIMITED_ACCESS')) {
+    apiCompletion();
+  } else {
+    stepCompletion();
+  }
 };
