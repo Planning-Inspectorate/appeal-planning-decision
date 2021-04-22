912f2ca9a0cbea5a5a3a673f68dbaf0966913258
ray@rcmsoftwaredevelopment.co.uk <ray@rcmsoftwaredevelopment.co.uk>
2021-04-22 12:01:49 +0100

    feat(as-1873): clear down appeal-id session once the appeal has been submitted
    clear down appeal-id session once the appeal has been submitted

--- packages/forms-web-app/src/controllers/appellant-submission/confirmation.js
+++ packages/forms-web-app/src/controllers/appellant-submission/confirmation.js
@@ -2,8 +2,11 @@ const { VIEW } = require('../../lib/views');
 
 exports.getConfirmation = (req, res) => {
   const appellantEmail = req.session && req.session.appeal && req.session.appeal['appellant-email'];
+  const appealId = req.session.appeal.id;
+  req.session.appeal = null;
+
   res.render(VIEW.APPELLANT_SUBMISSION.CONFIRMATION, {
     appellantEmail,
-    appealId: req.session.appeal.id,
+    appealId,
   });
 };
