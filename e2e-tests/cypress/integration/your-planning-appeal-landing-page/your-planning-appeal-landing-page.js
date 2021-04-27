fecb2dafa66c05a6097c348613d67f5bc02381fd
pbradley-f4 <paul.bradley@foundry4.com>
2021-04-27 17:23:42 +0100

    test: add submission date check and tweak test methods

--- e2e-tests/cypress/integration/your-planning-appeal-landing-page/your-planning-appeal-landing-page.js
+++ e2e-tests/cypress/integration/your-planning-appeal-landing-page/your-planning-appeal-landing-page.js
@@ -1,14 +1,14 @@
 import { When, Then } from 'cypress-cucumber-preprocessor/steps';
 import { STANDARD_APPEAL } from '../common/standard-appeal';
 
-Given('an agent or appellant has submitted an appeal ', () => {
+Given('an agent or appellant has submitted an appeal', () => {
   cy.provideCompleteAppeal(STANDARD_APPEAL);
   cy.clickCheckYourAnswers();
   cy.clickSaveAndContinue();
   cy.agreeToTheDeclaration();
 });
 
-When('your planning appeal page is viewed with a correct appealId', () => {
+When('your planning appeal page is viewed with a valid appealId', () => {
   cy.visit('/appellant-submission/confirmation');
   cy.get('[data-cy="submission-information-appeal-id"]')
     .invoke('val')
@@ -22,13 +22,17 @@ When('your planning appeal page is viewed with an incorrect appealId', () => {
 });
 
 Then('the user sees the appropriate general data along with data for step 1', () => {
-  cy.get('[data-cy="appelant-name"]').should(
-    'equal',
+  cy.get('[data-cy="appellant-name"]').should(
+    'have.text',
     STANDARD_APPEAL.aboutYouSection.yourDetails.name,
   );
-  cy.get('[data-cy="appelant-address"]').should(
-    'equal',
+  cy.get('[data-cy="appellant-address"]').should(
+    'have.text',
     `${STANDARD_APPEAL.appealSiteSection.siteAddress.addressLine1}, ${STANDARD_APPEAL.appealSiteSection.siteAddress.addressLine2}, ${STANDARD_APPEAL.appealSiteSection.siteAddress.town}, ${STANDARD_APPEAL.appealSiteSection.siteAddress.county}, ${STANDARD_APPEAL.appealSiteSection.siteAddress.postcode}`,
+  );
+  cy.get('[data-cy="appeal-submission-date"]').should(
+    'have.text',
+    Cypress.moment().format('D MMMM YYYY'),
   );
 });
 
