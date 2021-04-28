fc556854c1bfd6d394ed8d43f18a3075c4c35f59
pbradley-f4 <paul.bradley@foundry4.com>
2021-04-28 12:33:34 +0100

    feat: update copy and add lpd as required

--- packages/forms-web-app/src/controllers/your-planning-appeal.js
+++ packages/forms-web-app/src/controllers/your-planning-appeal.js
@@ -1,13 +1,16 @@
 const logger = require('../lib/logger');
 const { VIEW } = require('../lib/views');
+const { getDepartmentFromId } = require('../services/department.service');
 const appealModel = require('../models/appealModel');
 
 exports.getYourPlanningAppeal = async (req, res, next) => {
   const { appealId } = req.params;
   let appeal;
+  let lpd;
 
   try {
     appeal = await appealModel.getAppeal(appealId);
+    lpd = await getDepartmentFromId(appeal.lpaCode);
   } catch (error) {
     if (error.response.status === 404) return next();
 
@@ -17,5 +20,6 @@ exports.getYourPlanningAppeal = async (req, res, next) => {
 
   res.render(VIEW.YOUR_PLANNING_APPEAL, {
     appeal,
+    lpd,
   });
 };
--- packages/forms-web-app/tests/unit/controllers/your-planning-appeal.test.js
+++ packages/forms-web-app/tests/unit/controllers/your-planning-appeal.test.js
@@ -1,11 +1,21 @@
 const yourPlanningAppealController = require('../../../src/controllers/your-planning-appeal');
 const { getAppeal } = require('../../../src/models/appealModel');
+const { getDepartmentFromId } = require('../../../src/services/department.service');
 const { mockReq, mockRes } = require('../mocks');
 const { VIEW } = require('../../../src/lib/views');
 
 jest.mock('../../../src/models/appealModel');
+jest.mock('../../../src/services/department.service');
 
 describe('controllers/your-planning-appeal', () => {
+  const appeal = {
+    id: '1234',
+  };
+  const lpd = {
+    name: 'Test Council',
+  };
+  const error404 = { response: { status: 404 } };
+  const error500 = { response: { status: 500 } };
   let req;
   let res;
   let next;
@@ -22,31 +32,44 @@ describe('controllers/your-planning-appeal', () => {
 
   describe('getYourPlanningAppeal', () => {
     it('should render the correct view with the appeal data', async () => {
-      const appeal = {
-        id: '1234',
-      };
+      getDepartmentFromId.mockResolvedValue(lpd);
       getAppeal.mockResolvedValue(appeal);
       await yourPlanningAppealController.getYourPlanningAppeal(req, res, next);
 
       expect(res.render).toHaveBeenCalledWith(VIEW.YOUR_PLANNING_APPEAL, {
         appeal,
+        lpd,
       });
     });
 
     it('should call next() when appeal API returns 404', async () => {
-      const error = { response: { status: 404 } };
-      getAppeal.mockRejectedValue(error);
+      getAppeal.mockRejectedValue(error404);
       await yourPlanningAppealController.getYourPlanningAppeal(req, res, next);
 
       expect(next).toHaveBeenCalledWith();
     });
 
     it('should call next(error) when appeal API returns error', async () => {
-      const error = { response: { status: 500 } };
-      getAppeal.mockRejectedValue(error);
+      getAppeal.mockRejectedValue(error500);
       await yourPlanningAppealController.getYourPlanningAppeal(req, res, next);
 
-      expect(next).toHaveBeenCalledWith(error);
+      expect(next).toHaveBeenCalledWith(error500);
+    });
+
+    it('should call next() when LPD Service returns 404', async () => {
+      getAppeal.mockResolvedValue(appeal);
+      getDepartmentFromId.mockRejectedValue(error404);
+      await yourPlanningAppealController.getYourPlanningAppeal(req, res, next);
+
+      expect(next).toHaveBeenCalledWith();
+    });
+
+    it('should call next(error) when LPD Service returns error', async () => {
+      getAppeal.mockResolvedValue(appeal);
+      getDepartmentFromId.mockRejectedValue(error500);
+      await yourPlanningAppealController.getYourPlanningAppeal(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(error500);
     });
   });
 });
