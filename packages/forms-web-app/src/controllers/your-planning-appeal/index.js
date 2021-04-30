daa65f017dd811e82ff37f03ff581a4f47981390
Christopher Moss <chris@a6software.co.uk>
2021-04-30 11:10:45 +0100

    WIP

--- packages/forms-web-app/src/controllers/your-planning-appeal/index.js
+++ packages/forms-web-app/src/controllers/your-planning-appeal/index.js
@@ -2,7 +2,7 @@ const logger = require('../../lib/logger');
 const { VIEW } = require('../../lib/views');
 
 exports.getYourPlanningAppeal = async (req, res, next) => {
-  const { appeal, appealLPD } = req.session;
+  const { appeal, appealLPD } = res.locals;
 
   return res.render(VIEW.YOUR_PLANNING_APPEAL.INDEX, {
     appeal,
