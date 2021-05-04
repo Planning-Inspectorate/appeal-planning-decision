eb2fe3b4d7c297c072001b3577deea0d4194d4ba
swdpcomputing <swdpcomputing@outlook.com>
2021-05-04 14:07:26 +0100

    feat(horizon-householder-appeal-publish): created horizon reply publisher

---
+++ packages/horizon-householder-appeal-publish/tests/handler-reply-mocked.spec.js
@@ -0,0 +1,59 @@
+jest.mock('axios');
+jest.mock('../src/publishDocuments');
+const axios = require('axios');
+
+process.env.GATEWAY_URL = 'openfaas-gateway';
+
+const { handlerReply } = require('../handler-reply');
+const { publishDocuments } = require('../src/publishDocuments');
+
+describe('handlerReply', () => {
+  const envvars = process.env;
+  const horizonCaseId = 'mock-case-id';
+  let context;
+  let logMock;
+
+  beforeEach(() => {
+    process.env = { ...envvars };
+
+    logMock = {
+      debug: jest.fn(),
+      error: jest.fn(),
+      info: jest.fn(),
+    };
+
+    context = {
+      httpStatus: 200,
+    };
+  });
+
+  afterEach(() => {
+    process.env = envvars;
+    axios.post.mockReset();
+  });
+
+  it('should simulate a failed Horizon call', async () => {
+    const lpaCode = 'mock-lpa-code';
+
+    const event = {
+      log: logMock,
+      body: {
+        reply: {
+          lpaCode,
+        },
+      },
+    };
+
+    const mockError = new Error('Error sending to Horizon');
+    mockError.request = true; // To elicit correct response in catchErrorHandling()
+    publishDocuments.mockRejectedValue(mockError);
+
+    expect(await handlerReply(event, horizonCaseId, context)).toEqual({
+      message: 'Error sending to Horizon',
+    });
+
+    expect(context).toEqual({
+      httpStatus: 400,
+    });
+  });
+});
