1bd6a17297b1d2f187427530d7abaf50c73b5ed0
pbradley-f4 <paul.bradley@foundry4.com>
2021-05-08 20:51:40 +0100

    fix(forms-web-app): view-my-appeal (AS-1606) - ensure correct name displayed

---
+++ packages/forms-web-app/tests/fixtures/lpd.js
@@ -0,0 +1,12 @@
+const lpd = {
+  id: 'E123456789',
+  name: 'Fixture Borough Council',
+  inTrial: true,
+  email: 'lpd@planninginspectorate.gov.uk',
+  domain: 'planninginspectorate.gov.uk',
+  england: true,
+  wales: false,
+  horizonId: null,
+};
+
+module.exports = { lpd };
--- packages/forms-web-app/tests/unit/views/nunjucks-render-helper.js
+++ packages/forms-web-app/tests/unit/views/nunjucks-render-helper.js
@@ -1,8 +1,9 @@
 const nunjucks = require('nunjucks');
 const path = require('path');
-
+const dateFilter = require('nunjucks-date-filter');
 const filterByKey = require('../../../src/lib/filter-by-key');
 const addKeyValuePair = require('../../../src/lib/add-key-value-pair');
+const appealSiteAddressToArray = require('../../../src/lib/appeal-site-address-to-array');
 const render = require('../../../src/lib/render-template-filter');
 
 const viewPaths = [
@@ -19,6 +20,8 @@ const env = nunjucks.configure(viewPaths, nunjucksConfig);
 
 env.addFilter('filterByKey', filterByKey);
 env.addFilter('addKeyValuePair', addKeyValuePair);
+env.addFilter('appealSiteAddressToArray', appealSiteAddressToArray);
+env.addFilter('date', dateFilter);
 env.addFilter('render', render);
 
 module.exports = env;
