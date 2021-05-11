5c56250f31211a208bb390cfac5382f97b8d8449
swdpcomputing <swdpcomputing@outlook.com>
2021-05-11 13:51:45 +0100

    test(lpa-questionnaire-web-app): added e2e tests for add email to confirmation screen

--- lpa-submissions-e2e-tests/cypress/integration/appealsInformationSubmitted/appealsInformationSubmittedSteps.js
+++ lpa-submissions-e2e-tests/cypress/integration/appealsInformationSubmitted/appealsInformationSubmittedSteps.js
@@ -1,8 +1,15 @@
 import { Given, _, Then } from 'cypress-cucumber-preprocessor/steps';
 
+const getAppeal = require('../../../../packages/lpa-questionnaire-web-app/src/lib/appeals-api-wrapper');
+const getLPAList = require('../../../../packages/forms-web-app/src/lib/appeals-api-wrapper');
+
 const informationSubmittedPageId = 'information-submitted';
 const informationSubmittedPageTitle =
   'Information submitted - Appeal questionnaire - Appeal a householder planning decision - GOV.UK';
+
+const getLPAEmail = (lpaId) => {
+  return getLPAList().data.filter((obj) => obj.id === lpaId).email;
+};
 
 Given(`the Information Submitted page is requested`, () => {
   cy.goToPage(informationSubmittedPageId);
@@ -11,4 +18,12 @@ Given(`the Information Submitted page is requested`, () => {
 Then(`the Information Submitted page will be shown`, () => {
   cy.verifyPageTitle(informationSubmittedPageTitle);
   cy.checkPageA11y(informationSubmittedPageId);
+});
+
+Then(`the LPA email address is displayed on the Information Submitted page`, () => {
+  cy.visibleWithText(`${getLPAEmail(getAppeal().lpaCode)}`, 'lpaEmailString');
+});
+
+Then(`the LPA email address is not displayed on the Information Submitted page`, () => {
+  cy.visibleWithText('', 'lpaEmailString');
 });
