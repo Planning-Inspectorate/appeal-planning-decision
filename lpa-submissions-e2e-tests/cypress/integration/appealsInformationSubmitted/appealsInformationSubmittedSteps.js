675de4807b321c1a44485732f98cb616ba5cad98
swdpcomputing <swdpcomputing@outlook.com>
2021-05-11 13:51:45 +0100

    test(lpa-questionnaire-web-app): added e2e tests for add email to confirmation screen

--- lpa-submissions-e2e-tests/cypress/integration/appealsInformationSubmitted/appealsInformationSubmittedSteps.js
+++ lpa-submissions-e2e-tests/cypress/integration/appealsInformationSubmitted/appealsInformationSubmittedSteps.js
@@ -1,14 +1,21 @@
-import { Given, _, Then } from 'cypress-cucumber-preprocessor/steps';
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
-const informationSubmittedPageId = 'information-submitted';
+const informationSubmittedUrl = 'information-submitted';
 const informationSubmittedPageTitle =
   'Information submitted - Appeal questionnaire - Appeal a householder planning decision - GOV.UK';
 
 Given(`the Information Submitted page is requested`, () => {
-  cy.goToPage(informationSubmittedPageId);
+  cy.goToPage(informationSubmittedUrl);
 });
 
 Then(`the Information Submitted page will be shown`, () => {
+  cy.verifyPage(informationSubmittedUrl);
   cy.verifyPageTitle(informationSubmittedPageTitle);
-  cy.checkPageA11y(informationSubmittedPageId);
+});
+
+Then(`the LPA email address is displayed on the Information Submitted page`, () => {
+  cy.visibleWithText(
+    `We’ve sent a confirmation email to abby.bale@planninginspectorate.gov.uk.`,
+    '[data-cy=lpaEmailString]',
+  );
 });
