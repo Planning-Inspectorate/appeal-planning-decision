2ac9be5efd75f5c8d71b0b4ae5542fa1157a6b0f
ray@rcmsoftwaredevelopment.co.uk <ray@rcmsoftwaredevelopment.co.uk>
2021-05-17 11:17:53 +0100

    feat(as-2013): jest code

--- packages/forms-web-app/src/validators/custom/mime-binary-type.js
+++ packages/forms-web-app/src/validators/custom/mime-binary-type.js
@@ -3,13 +3,21 @@ const fs = require('fs');
 const validMimeType = require('./mime-type');
 
 module.exports = async (fileInformation, allowableMimeTypes, errorMessage) => {
-  if (typeof fileInformation !== 'undefined') {
-    if (typeof fileInformation.tempFilePath !== 'undefined') {
-      const fileStream = fs.createReadStream(fileInformation.tempFilePath);
-      const fileStreamType = await fileContentType.fromStream(fileStream);
 
-      validMimeType(fileStreamType?.mime || null, allowableMimeTypes, errorMessage);
+  console.log('Start');
+  console.log(fileInformation);
+  if (typeof fileInformation !== 'undefined') {
+    console.log('Start2');
+    if (typeof fileInformation.tempFilePath !== 'undefined') {
+      console.log('Inner');
+      const fileStream = fs.createReadStream(fileInformation.tempFilePath);
+      console.log('Inner2');
+      //console.log(fileStream);
+      const fileStreamType = await fileContentType.fromStream(fileStream);
+console.log(fileStreamType);
+      validMimeType(fileStreamType?.mime, allowableMimeTypes, errorMessage);
     }
   }
+  console.log('end');
   return true;
 };
