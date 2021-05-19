04e44aab1453390547825299120da22ba6bb82be
swdpcomputing <swdpcomputing@outlook.com>
2021-05-19 12:37:44 +0100

    feat(lpa-questionnaire-web-app): add other relevant policies page

---
+++ packages/lpa-questionnaire-web-app/src/routes/other-policies.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { VIEW } = require('../lib/views');
+const uploadQuestionController = require('../controllers/upload-question');
+const fetchExistingAppealReplyMiddleware = require('../middleware/fetch-existing-appeal-reply');
+const fetchAppealMiddleware = require('../middleware/fetch-appeal');
+const reqFilesToReqBodyFilesMiddleware = require('../middleware/req-files-to-req-body-files');
+const clearUploadedFilesMiddleware = require('../middleware/clear-uploaded-files');
+const uploadValidationRules = require('../validators/upload-tasks');
+const { validationErrorHandler } = require('../validators/validation-error-handler');
+
+const router = express.Router();
+
+const getConfig = (_, res, next) => {
+  res.locals.routeInfo = {
+    sectionName: 'optionalDocumentsSection',
+    taskName: 'otherPolicies',
+    view: VIEW.OTHER_POLICIES,
+    name: 'Other relevant policies',
+  };
+
+  next();
+};
+
+router.get(
+  '/:id/other-policies',
+  [fetchAppealMiddleware, fetchExistingAppealReplyMiddleware, clearUploadedFilesMiddleware],
+  getConfig,
+  uploadQuestionController.getUpload
+);
+
+router.post(
+  '/:id/other-policies',
+  [reqFilesToReqBodyFilesMiddleware('documents'), uploadValidationRules()],
+  validationErrorHandler,
+  getConfig,
+  uploadQuestionController.postUpload
+);
+
+module.exports = {
+  router,
+  getConfig,
+};
