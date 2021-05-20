f77be3452eac6bd323239f53eedb89b9dd780175
David Lewis <dave@davehlewis.com>
2021-05-20 13:49:52 +0100

    test(questionnaire-e2e): feature file and tests for appealID matching

---
+++ lpa-submissions-e2e-tests/cypress/integration/matchAppealId/matchAppealId.step.js
@@ -0,0 +1,36 @@
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { input } from '../../support/PageObjects/common-page-objects';
+
+Given('an appeal has been created', () => {});
+Given('a questionnaire has been created', () => {});
+
+Given('answers have been saved to the questionnaire', () => {
+  cy.completeQuestionnaire();
+});
+
+When('the questionnaire is revisited in a new session', () => {
+  // First go to a page to create an initial session
+  cy.goToTaskListPage();
+  cy.verifyCompletedStatus('submissionAccuracy');
+
+  // Clear cookies to remove tie to session
+  cy.clearCookies();
+
+  // Reloading the page will generate a new session
+  cy.reload();
+});
+
+When('changes are made to the questions', () => {
+  cy.goToPage('accuracy-submission');
+  input('accurate-submission-yes').check();
+  cy.clickSaveAndContinue();
+});
+
+Then('previously entered data will be visible', () => {
+  cy.verifyCompletedStatus('submissionAccuracy');
+});
+
+Then('the changes over write the previously saved answers', () => {
+  cy.goToPage('accuracy-submission');
+  input('accurate-submission-yes').should('be.checked');
+});
