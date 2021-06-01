88247f4f8400dd8507c4c7f9fa13083071e9a323
Christopher Moss <chris@a6software.co.uk>
2021-06-01 15:15:57 +0100

    feat: extract footer from FWA, move to common, import for FWA & LPAQ

--- packages/forms-web-app/tests/unit/views/nunjucks-render-helper.js
+++ packages/forms-web-app/tests/unit/views/nunjucks-render-helper.js
@@ -9,6 +9,7 @@ const render = require('../../../src/lib/render-template-filter');
 const viewPaths = [
   path.join(__dirname, '../../..', 'node_modules', 'govuk-frontend'),
   path.join(__dirname, '../../..', 'node_modules', '@ministryofjustice', 'frontend'),
+  path.join(__dirname, '../../..', 'node_modules', '@pins', 'common', 'src', 'frontend'),
   path.join(__dirname, '../../..', 'src', 'views'),
 ];
 
