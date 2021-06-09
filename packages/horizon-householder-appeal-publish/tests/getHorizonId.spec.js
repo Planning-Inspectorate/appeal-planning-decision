b4f6d32f47ca1e087e4790f6b4fa70300618d78c
swdpcomputing <swdpcomputing@outlook.com>
2021-06-09 10:17:24 +0100

    test(horizon-householder-appeal-publish): tests for getHorizonId and switch back to mock-horizonId

---
+++ packages/horizon-householder-appeal-publish/tests/getHorizonId.spec.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const logger = require('../src/lib/logger');
+const { getHorizonId } = require('../src/getHorizonId');
+
+jest.mock('axios');
+
+jest.mock('../src/lib/logger', () => ({
+  error: jest.fn(),
+}));
+
+describe('getHorizonId', () => {
+  const appealId = 'mock-appeal-id';
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should return a correct horizonId from an appealId', async () => {
+    const expectedHorizonId = '3000001';
+    const mockAppealRes = { data: { appeal: { horizonId: expectedHorizonId } } };
+    axios.get.mockResolvedValue(mockAppealRes);
+    expect(await getHorizonId(appealId)).toEqual(expectedHorizonId);
+  });
+
+  it('should log an error', async () => {
+    axios.get.mockRejectedValue('error');
+    const horizonId = await getHorizonId(appealId);
+    expect(logger.error).toHaveBeenCalledWith({ err: 'error' }, 'Unable to retrieve appeal data.');
+    expect(horizonId).toEqual(0);
+  });
+});
