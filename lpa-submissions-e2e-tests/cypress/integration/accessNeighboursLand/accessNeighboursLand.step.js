3c0dbfb97d57ed5f57dce12bd4e162bac20e1b19
swdpcomputing <swdpcomputing@outlook.com>
2021-06-18 16:34:34 +0100

    test(lpa-submissions-e2e-tests): add feature file and step definitions

---
+++ lpa-submissions-e2e-tests/cypress/integration/accessNeighboursLand/accessNeighboursLand.step.js
@@ -0,0 +1,19 @@
+import { Before } from 'cypress-cucumber-preprocessor/steps';
+
+const page = {
+  id: 'accessNeighboursLand',
+  heading: "Would the inspector need access to a neighbour's land?",
+  section: 'About the Appeal Site',
+  title:
+    "Would the inspector need access to a neighbour's land? - Appeal questionnaire - Appeal a householder planning decision - GOV.UK",
+  url: 'neighbours-land',
+  emptyError: 'Select yes if the inspector needs access to a neighbour’s land',
+  textEmptyError: "Enter the reasons the Inspector would need to enter a neighbour's land",
+  textChildOf: 'Yes',
+  textMock:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. accessNeighboursLand.',
+};
+
+Before(() => {
+  cy.wrap(page).as('page');
+});
