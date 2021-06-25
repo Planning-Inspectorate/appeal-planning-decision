b96013ad2d6ba223c2fd5fc4508b175b4372de38
swdpcomputing <swdpcomputing@outlook.com>
2021-06-25 13:27:49 +0100

    test(lpa-submissions-e2e-tests): add feature file and step definitions

---
+++ lpa-submissions-e2e-tests/cypress/integration/settingListedBuilding/settingListedBuilding.step.js
@@ -0,0 +1,19 @@
+import { Before } from 'cypress-cucumber-preprocessor/steps';
+
+const page = {
+  id: 'listedBuilding',
+  heading: "Would the development affect the setting of a listed building?",
+  section: 'About the Appeal Site',
+  title:
+    "Would the development affect the setting of a listed building? - Appeal Questionnaire - Appeal a householder planning decision - GOV.UK",
+  url: 'listed-building',
+  emptyError: 'Select yes if the development affects the setting of a listed building',
+  textEmptyError: "Enter the details for the listed building",
+  textChildOf: 'Yes',
+  textMock:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. listedBuilding.',
+};
+
+Before(() => {
+  cy.wrap(page).as('page');
+});
