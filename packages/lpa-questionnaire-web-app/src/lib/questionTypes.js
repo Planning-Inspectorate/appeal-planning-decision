6af1514fc098d08d5cc8520531ba81873de69d5d
swdpcomputing <swdpcomputing@outlook.com>
2021-06-30 09:08:05 +0100

    fix(lpa-questionnaire-web-app): fixed typo

--- packages/lpa-questionnaire-web-app/src/lib/questionTypes.js
+++ packages/lpa-questionnaire-web-app/src/lib/questionTypes.js
@@ -97,7 +97,7 @@ exports.booleanQuestions = [
     title:
       'Is the appeal site in or near a conservation area? - Appeal Questionnaire - Appeal a householder planning decision - GOV.UK',
     url: 'conservation-area',
-    emptyError: 'Select yes it the appeal site is in or near a conservation area',
+    emptyError: 'Select yes if the appeal site is in or near a conservation area',
   },
   {
     id: 'originalPlanningApplicationPublicised',
