98e6d2197b9866e1330b31046842133da9bfa96a
Christopher Moss <chris@a6software.co.uk>
2021-06-30 12:41:21 +0100

    fix: fixups from PR feedback, and adds auth middleware based on active session id

--- packages/forms-web-app/src/routes/document-service-proxy.js
+++ packages/forms-web-app/src/routes/document-service-proxy.js
@@ -3,7 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const documentServiceProxyController = require('../controllers/document-service-proxy');
+const ensureAppealMatchesSessionMiddleware = require('../middleware/ensure-appeal-matches-session');
 
-router.get('/:appealId/:documentId', documentServiceProxyController.getDocument);
+router.get(
+  '/:appealId/:documentId',
+  [ensureAppealMatchesSessionMiddleware],
+  documentServiceProxyController.getDocument
+);
 
 module.exports = router;
