1b74a9d76452683fa5bda0b9ee7239d89154e8c5
swdpcomputing <swdpcomputing@outlook.com>
2021-06-30 15:03:35 +0100

    fix(lpa-questionnaire-web-app): added error message for nothing selected

--- packages/lpa-questionnaire-web-app/src/lib/questionTypes.js
+++ packages/lpa-questionnaire-web-app/src/lib/questionTypes.js
@@ -106,5 +106,6 @@ exports.booleanQuestions = [
     title:
       'Original planning application publicity - Appeal questionnaire - Appeal a householder planning decision - GOV.UK',
     url: 'application-publicity',
+    emptyError: 'Did you publicise the original planning application?',
   },
 ];
