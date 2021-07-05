e6f072a78a95ffc7b64fd30aac8d9f687f3efb65
swdpcomputing <swdpcomputing@outlook.com>
2021-07-05 10:43:09 +0100

    fix(lpa-questionnaire-web-app): fix task-list crash when adding non-existing data

--- packages/lpa-questionnaire-web-app/src/services/task-status/boolean.js
+++ packages/lpa-questionnaire-web-app/src/services/task-status/boolean.js
@@ -6,13 +6,16 @@ module.exports = (appealReply, taskId) => {
 
   const task = appealReply[taskId];
   const { dataId, text } = booleanQuestions.find((q) => q.id === taskId);
-
   let completed;
   if (text) {
     const id = dataId || 'value';
-    completed =
-      (typeof task[id] === 'boolean' && task[id] !== text.parentValue) ||
-      (task[id] === text.parentValue && task[text.id]);
+    if (typeof task === 'undefined') {
+      completed = false;
+    } else {
+      completed =
+        (typeof task[id] === 'boolean' && task[id] !== text.parentValue) ||
+        (task[id] === text.parentValue && task[text.id]);
+    }
   } else {
     completed = typeof task === 'boolean';
   }
