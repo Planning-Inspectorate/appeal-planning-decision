1bb1126865b9abf50767a6431e2e9290fb70c305
rcmsoftwaredevelopment <33132862+rcmsoftwaredevelopment@users.noreply.github.com>
2021-07-06 16:05:57 +0100

    feat(as-2831): implement clamav
    implement clamav

---
+++ packages/forms-web-app/src/validators/custom/clamav.js
@@ -0,0 +1,28 @@
+/* istanbul ignore file */
+const NodeClam = require('clamscan');
+const fs = require('fs');
+
+module.exports = async (fileInformation, errorMessage) => {
+  if (typeof fileInformation?.tempFilePath !== 'undefined') {
+    const clamscan = await new NodeClam().init({
+      debug_mode: true,
+      clamdscan: {
+        host: 'clamav-server',
+        port: 3310,
+        bypass_test: true,
+      },
+    });
+
+    const fileStream = fs.createReadStream(fileInformation.tempFilePath);
+
+    // eslint-disable-next-line camelcase
+    const { is_infected } = await clamscan.scan_stream(fileStream);
+
+    // eslint-disable-next-line camelcase
+    if (is_infected) {
+      throw new Error(errorMessage);
+    }
+  }
+
+  return true;
+};
