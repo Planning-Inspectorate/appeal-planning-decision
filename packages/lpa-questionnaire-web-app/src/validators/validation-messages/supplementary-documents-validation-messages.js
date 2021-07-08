dc4a76ea428b67c72f96833f5725c572fd46815e
swdpcomputing <swdpcomputing@outlook.com>
2021-07-08 11:48:09 +0100

    fix(lpa-questionnaire-web-app): fix error messages when adopted equals yes and date is fully blank

--- packages/lpa-questionnaire-web-app/src/validators/validation-messages/supplementary-documents-validation-messages.js
+++ packages/lpa-questionnaire-web-app/src/validators/validation-messages/supplementary-documents-validation-messages.js
@@ -1,5 +1,13 @@
-module.exports = {
+const sectionErrorTexts = {
   upload: 'Upload a relevant supplementary planning document',
   name: 'Enter a name for the supplementary planning document',
   adopted: 'Select whether this supplementary planning document has been adopted',
 };
+
+const dateErrorTexts = {
+  enterDate: 'Tell us the date the supplementary planning document was adopted',
+  monthYear: 'Date of adoption must include a month and year',
+  month: 'Date of adoption must include a year',
+};
+
+module.exports = { sectionErrorTexts, dateErrorTexts };
