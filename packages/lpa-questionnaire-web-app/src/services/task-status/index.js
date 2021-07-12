c6530e3c70c7979c563a0aefefc084cc575f8df5
swdpcomputing <swdpcomputing@outlook.com>
2021-07-12 09:36:56 +0100

    refactor(lpa-questionnaire-web-app): change file upload completion functions to template

--- packages/lpa-questionnaire-web-app/src/services/task-status/index.js
+++ packages/lpa-questionnaire-web-app/src/services/task-status/index.js
@@ -1,33 +1,19 @@
 const accuracySubmissionCompletion = require('./accuracy-submission');
 const extraConditionsCompletion = require('./extra-conditions');
 const otherAppealsCompletion = require('./other-appeals');
-const interestedPartiesCompletion = require('./interested-parties');
-const representationsCompletion = require('./representations');
-const notifyingPartiesCompletion = require('./notifying-parties');
 const developmentPlanCompletion = require('./development-plan');
 const uploadPlansCompletion = require('./upload-plans');
 const officersReportCompletion = require('./officers-report');
-const siteNoticesCompletion = require('./site-notices');
-const conservationAreaMapCompletion = require('./conservation-area-map');
-const planningHistoryCompletion = require('./planning-history');
-const otherPoliciesCompletion = require('./other-policies');
-const statutoryDevelopmentCompletion = require('./statutory-development');
 const booleanCompletion = require('./boolean');
+const fileUploadCompletion = require('./file-upload');
 
 module.exports = {
   accuracySubmissionCompletion,
   extraConditionsCompletion,
   otherAppealsCompletion,
-  interestedPartiesCompletion,
-  representationsCompletion,
-  notifyingPartiesCompletion,
   developmentPlanCompletion,
   uploadPlansCompletion,
   officersReportCompletion,
-  siteNoticesCompletion,
-  conservationAreaMapCompletion,
-  planningHistoryCompletion,
-  otherPoliciesCompletion,
-  statutoryDevelopmentCompletion,
   booleanCompletion,
+  fileUploadCompletion,
 };
--- packages/lpa-questionnaire-web-app/src/services/task-status/site-notices.js
+++
@@ -1,9 +0,0 @@
-const { COMPLETED, NOT_STARTED } = require('./task-statuses');
-
-module.exports = (appealReply) => {
-  if (!appealReply) return null;
-
-  const { uploadedFiles = [] } = appealReply.optionalDocumentsSection?.siteNotices;
-
-  return uploadedFiles.length ? COMPLETED : NOT_STARTED;
-};
