6074da785b2f30b2afc31de702ad02116cf5c09e
rcmsoftwaredevelopment <33132862+rcmsoftwaredevelopment@users.noreply.github.com>
2021-07-16 12:07:30 +0100

    fix(as-2831): clamav

--- packages/forms-web-app/src/validators/appellant-submission/upload-decision-schema.js
+++ packages/forms-web-app/src/validators/appellant-submission/upload-decision-schema.js
@@ -2,7 +2,6 @@ const config = require('../../config');
 const validateFileSize = require('../custom/file-size');
 const validMimeType = require('../custom/mime-type');
 const validBinaryMimeType = require('../custom/mime-binary-type');
-const validAV = require('../custom/clamav');
 const {
   MIME_TYPE_DOC,
   MIME_BINARY_TYPE_DOC,
@@ -48,9 +47,6 @@ module.exports = {
           ],
           'The selected file must be a DOC, DOCX, PDF, TIF, JPG or PNG'
         );
-
-        // check file for Virus
-        await validAV(req.files['decision-upload'], 'The selected file contains a virus');
 
         // check binary mime type of file
         await validBinaryMimeType(
