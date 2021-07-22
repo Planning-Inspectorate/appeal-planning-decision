400afb47dc0ad81c4273f4f2544d65adceac68ce
swdpcomputing <swdpcomputing@outlook.com>
2021-07-22 16:12:57 +0100

    feat(lpa-questionnaire-web-app): add uploaded supplementary documents page

--- packages/lpa-questionnaire-web-app/src/routes/supplementary-documents.js
+++ packages/lpa-questionnaire-web-app/src/routes/supplementary-documents.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const supplementaryDocumentsController = require('../controllers/supplementary-documents');
+const addSupplementaryDocumentController = require('../controllers/supplementary-documents/add-supplementary-document');
+const uploadedDocumentsController = require('../controllers/supplementary-documents/uploaded-documents');
 const fetchExistingAppealReplyMiddleware = require('../middleware/fetch-existing-appeal-reply');
 const fetchAppealMiddleware = require('../middleware/fetch-appeal');
 const reqFilesToReqBodyFilesMiddleware = require('../middleware/req-files-to-req-body-files');
@@ -10,18 +11,24 @@ const supplementaryDocumentsValidationRules = require('../validators/supplementa
 const router = express.Router();
 
 router.get(
-  '/:id/supplementary-documents/add-document',
+  '/:id/supplementary-documents',
   [fetchAppealMiddleware, fetchExistingAppealReplyMiddleware],
-  supplementaryDocumentsController.getAddDocument
+  addSupplementaryDocumentController.getAddDocument
 );
 
 router.post(
-  '/:id/supplementary-documents/add-document',
+  '/:id/supplementary-documents',
   reqFilesToReqBodyFilesMiddleware('documents'),
   combineDateInputsMiddleware,
   supplementaryDocumentsValidationRules(),
   validationErrorHandler,
-  supplementaryDocumentsController.postAddDocument
+  addSupplementaryDocumentController.postAddDocument
+);
+
+router.get(
+  '/:id/supplementary-documents/uploaded-documents',
+  [fetchAppealMiddleware, fetchExistingAppealReplyMiddleware],
+  uploadedDocumentsController.getUploadedDocuments
 );
 
 module.exports = router;
