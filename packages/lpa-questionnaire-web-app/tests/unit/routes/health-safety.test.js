f8040f77c9a5d44bd9a253a8d9ce589c93ad2b64
swdpcomputing <swdpcomputing@outlook.com>
2021-07-26 10:31:23 +0100

    test(lpa-questionnaire-web-app): add unit tests for health and safety page

---
+++ packages/lpa-questionnaire-web-app/tests/unit/routes/health-safety.test.js
@@ -0,0 +1,33 @@
+const { get, post } = require('./router-mock');
+const healthSafetyController = require('../../../src/controllers/health-safety');
+const fetchExistingAppealReplyMiddleware = require('../../../src/middleware/fetch-existing-appeal-reply');
+const fetchAppealMiddleware = require('../../../src/middleware/fetch-appeal');
+const { validationErrorHandler } = require('../../../src/validators/validation-error-handler');
+const { rules: healthSafetyValidationRules } = require('../../../src/validators/health-safety');
+
+jest.mock('../../../src/validators/health-safety');
+
+describe('routes/health-safety', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line global-require
+    require('../../../src/routes/health-safety');
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should define the expected routes', () => {
+    expect(get).toHaveBeenCalledWith(
+      `/:id/health-safety`,
+      [fetchAppealMiddleware, fetchExistingAppealReplyMiddleware],
+      healthSafetyController.getHealthSafety
+    );
+    expect(post).toHaveBeenCalledWith(
+      '/:id/health-safety',
+      healthSafetyValidationRules(),
+      validationErrorHandler,
+      healthSafetyController.postHealthSafety
+    );
+  });
+});
