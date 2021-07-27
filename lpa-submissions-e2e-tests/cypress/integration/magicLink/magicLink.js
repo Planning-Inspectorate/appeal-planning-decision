cd4a38c22be53d18fd76b0f21ce964bd6f63bec4
adrianamiclos <adriana.miclos@manifesto.co.uk>
2021-07-27 14:10:15 +0100

    test(lpa-submissions-e2e-tests): add inital e2e tests for magic link functionality

---
+++ lpa-submissions-e2e-tests/cypress/integration/magicLink/magicLink.js
@@ -0,0 +1,172 @@
+import { And, Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
+import verifyPage from '../../support/common/verifyPage';
+import verifyNotificationBanner from '../../support/common/verifyNotificationBanner';
+import validateErrorMessage from '../../support/common/validateErrorMessage';
+import goToPage from '../../support/common/goToPage';
+import clickLink from '../../support/common/clickLink';
+import clickSubmit from '../../support/common/clickSubmitButton';
+import hasLink from '../../support/common/hasLink';
+import inputEmailAddress from '../../support/magic-link/inputEmailAddress';
+import authenticateLPA from '../../support/magic-link/authenticateLPA';
+import getMagicLink from '../../support/magic-link/getMagicLink';
+import getMagicLinkEmail from '../../support/magic-link/getMagicLinkEmail';
+
+const enterEmailAddressLink = 'authentication/your-email';
+const confirmEmailAddressLink = 'authentication/confirm-email';
+const questionnairePage = 'task-list';
+const lpaName = 'System Test Borough Council';
+
+Given('LPA wants to access a questionnaire', () => {
+  // nothing to do here
+});
+
+Given('the LPA is on the confirm your email page', () => {
+  goToPage(confirmEmailAddressLink);
+});
+
+Given('LPA user receives a magic link for accessing the questionnaire', () => {
+  goToPage(enterEmailAddressLink);
+  inputEmailAddress();
+  clickSubmit();
+});
+
+And('is trying to access the LPA questionnaire', () => {
+  // nothing to do here
+});
+
+Given('access to the questionnaire is requested', () => {
+  goToPage(enterEmailAddressLink);
+});
+
+Given('LPA user wants to have the magic link resent to them', () => {
+  goToPage(confirmEmailAddressLink);
+});
+
+Given('the session has timed out', () => {
+  // TODO check if the session id cookie name is correct and if this works as expected
+  const expiryDate = new Date();
+  expiryDate.setMinutes(expiryDate.getMinutes() - 20);
+
+  cy.getCookie('connect.sid').then((cookie) => {
+    cy.clearCookies();
+    cy.setCookie(cookie.name, cookie.value, {
+      expiry: expiryDate.valueOf(),
+      domain: cookie.domain,
+    });
+  });
+});
+
+Given('LPA Planning Officer wants to complete a questionnaire', () => {
+  // nothing to do here
+});
+
+Given('the LPA Planning Officer has not been authenticated', () => {
+  cy.clearCookies();
+});
+
+Given('the LPA Planning Officer is authenticated', () => {
+  authenticateLPA();
+});
+
+When('they click on the link in the start email', () => {
+  goToPage(questionnairePage);
+});
+
+When('the email address does not match the domain of the LPA from the appeal', () => {
+  inputEmailAddress('test@test.gov.uk');
+  clickSubmit();
+});
+
+When('an email address is not provided', () => {
+  inputEmailAddress('');
+  clickSubmit();
+});
+
+When('the email address provided in not in the correct format', () => {
+  inputEmailAddress('invalidEmailAddress');
+  clickSubmit();
+});
+
+When('a valid email address is provided matching the domain of the LPA', () => {
+  inputEmailAddress();
+  clickSubmit();
+});
+
+When('they select to email the customer support team', () => {
+  // nothing to do here
+});
+
+When('they select a valid link', () => {
+  getMagicLink().then((magicLink) => goToPage(magicLink));
+});
+
+When('they select an expired magic link', () => {
+  const expiredToken = 'expiredToken';
+  const expiredMagicLink = `${questionnairePage}?token=${expiredToken}`;
+  goToPage(expiredMagicLink);
+});
+
+When('they select ‘resend the email’', () => {
+  clickLink('resend-email-link');
+});
+
+When('the LPA tries to access the questionnaire', () => {
+  goToPage(questionnairePage);
+});
+
+Then('enter email address page will be displayed', () => {
+  verifyPage(enterEmailAddressLink);
+});
+
+Then('a magic link is sent to that email address via Notify', () => {
+  getMagicLinkEmail().then((response) => {
+    // TODO assert email body
+    // expect(response.body).toEqual([]);
+  });
+});
+
+Then('a mailto link will be provided', () => {
+  hasLink(
+    '[data-cy="customer-support-mailto-link"]',
+    'mailto:enquiries@planninginspectorate.gov.uk',
+  );
+});
+
+Then('the questionnaire page is presented', () => {
+  verifyPage(questionnairePage);
+});
+
+And('a session expired notification banner is displayed on the page', () => {
+  verifyPage(`${enterEmailAddressLink}/session-expired`);
+
+  const notificationBody =
+    '<p class="govuk-notification-banner__heading">Your session has timed out</p>' +
+    `<p class="govuk-body">To return to the questionnaire, you need to enter your ${lpaName} email address.</p>`;
+  verifyNotificationBanner('session-expired-notification', 'Session expired', notificationBody);
+});
+
+And('a link expired notification banner is displayed on the page', () => {
+  verifyPage(`${enterEmailAddressLink}/link-expired`);
+
+  const notificationBody =
+    '<p class="govuk-notification-banner__heading">Your link has expired</p>' +
+    `<p class="govuk-body">To get a new link, you need to enter your ${lpaName} email address.< /p>`;
+  verifyNotificationBanner('link-expired-notification', 'Link expired', notificationBody);
+});
+
+Then('progress is made to the confirm your email address page', () => {
+  verifyPage(confirmEmailAddressLink);
+});
+
+And('a magic link is not sent to the user', () => {
+  getMagicLink().then((magicLink) => expect(magicLink).to.be.undefined);
+});
+
+Then('progress is halted with an error message to enter an email address', () => {
+  verifyPage(enterEmailAddressLink);
+  validateErrorMessage(
+    'Enter an email address in the correct format, like name@example.com',
+    '#email',
+    'email',
+  );
+});
--- lpa-submissions-e2e-tests/cypress/integration/sendEmailConfirmationToNotify/sendEmailConfirmationToNotify.steps.js
+++ lpa-submissions-e2e-tests/cypress/integration/sendEmailConfirmationToNotify/sendEmailConfirmationToNotify.steps.js
@@ -1,13 +1,16 @@
 import { Then } from 'cypress-cucumber-preprocessor/steps';
+import getNotificationEmail from '../../support/common/getNotificationEmail';
 
 Then('a confirmation email is sent to the LPA', () => {
   cy.verifyPage('information-submitted');
   cy.get('@appeal').then((appeal) => {
-    cy.request('GET', `${Cypress.env('EMAIL_NOTIFICATION_URL')}`).then((response) => {
+    getNotificationEmail().then((response) => {
       const lastEmailNotificationOnTheStack = response.body.length - 1;
       const emailNotification = response.body[lastEmailNotificationOnTheStack];
       expect(emailNotification.template_id).to.eq('937b4147-8420-42da-859d-d4a65bdf99bc');
-      expect(emailNotification.email_address).to.eq('AppealPlanningDecisionTest@planninginspectorate.gov.uk');
+      expect(emailNotification.email_address).to.eq(
+        'AppealPlanningDecisionTest@planninginspectorate.gov.uk',
+      );
 
       expect(Object.keys(emailNotification.personalisation).length).to.eq(4);
       expect(emailNotification.personalisation['Planning appeal number']).to.eq(appeal.horizonId);
@@ -28,9 +31,7 @@ Then('a confirmation email is sent to the LPA', () => {
         false,
       );
 
-      expect(emailNotification.reference).to.eq(
-        `${appeal.id}.SubmissionConfirmation`,
-      );
+      expect(emailNotification.reference).to.eq(`${appeal.id}.SubmissionConfirmation`);
       expect(emailNotification.email_reply_to_id).to.eq('f1e6c8c5-786e-41ca-9086-10b67f31bc86');
       expect(response.status).to.eq(200);
     });
