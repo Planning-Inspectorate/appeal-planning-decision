39f90fc415c934d5d33669f5101d03192f79b0cf
Anthony Walsh <anthony.walsh@planninginspectorate.gov.uk>
2021-07-27 13:06:49 +0100

    feat: add GTM functionality and update tests
    Adding interaction with GTM to the cookie banner and default settins on every page. Updating tests
    accordingly.

---
+++ packages/forms-web-app/src/lib/client-side/google-tag-manager.js
@@ -0,0 +1,29 @@
+/* eslint-env browser */
+
+function applyConsent(consent) {
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    // eslint-disable-next-line no-undef, prefer-rest-params
+    dataLayer.push(arguments);
+  }
+
+  if (consent === 'denied' || consent === 'granted') {
+    gtag('consent', 'update', {
+      analytics_storage: consent, // We only care about analytics_storage
+    });
+  }
+}
+
+const denyConsent = () => {
+  applyConsent('denied');
+};
+
+const grantConsent = () => {
+  applyConsent('granted');
+};
+
+module.exports = {
+  grantConsent,
+  denyConsent,
+};
