e38b41c45df43086d828bad3083c2b4311c409b5
adrianamiclos <adriana.miclos@manifesto.co.uk>
2021-07-29 14:38:11 +0100

    fix(lpa-submissions-e2e-tests): very small updates for the magic link steps. Fix inputEmailAddress
    Very small updates for the magic link steps. Fix inputEmailAddress test functionality.

--- lpa-submissions-e2e-tests/cypress/support/magic-link/inputEmailAddress.js
+++ lpa-submissions-e2e-tests/cypress/support/magic-link/inputEmailAddress.js
@@ -1,7 +1,9 @@
 import uuid from 'uuid';
+import { input } from '../PageObjects/common-page-objects';
 
 module.exports = (emailAddress) => {
   const email =
     emailAddress !== undefined ? emailAddress : `${uuid.v4()}.user@planninginspectorate.gov.uk`;
   cy.wrap(email).as('lpaEmail');
+  input('lpa-authentication-email').type(email);
 };
