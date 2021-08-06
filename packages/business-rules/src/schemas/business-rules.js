786394cc8b1a2a2667ab624595011f79e27bd58f
David Shepherd <david@x-designs.co.uk>
2021-08-06 15:22:18 +0100

    feat(as-2002): update business rules to throw a more specific error
    Previously an Error object was thrown and this change creates a BusinessRuleError object which
    extends Error and should make it clearer in the logs that the error was thrown by the business rules

--- packages/business-rules/src/schemas/business-rules.js
+++ packages/business-rules/src/schemas/business-rules.js
@@ -1,17 +1,18 @@
 const householderAppeal = require('./householder-appeal');
 const isValid = require('../validation/appeal/type/is-valid');
 const { APPEAL_ID } = require('../constants');
+const BusinessRulesError = require('../lib/business-rules-error');
 
 const businessRules = (appealType, data, config) => {
   if (!isValid(appealType)) {
-    throw new Error(`${appealType} is not a valid appeal type`);
+    throw new BusinessRulesError(`${appealType} is not a valid appeal type`);
   }
 
   switch (appealType) {
     case APPEAL_ID.HOUSEHOLDER:
       return householderAppeal.validate(data, config);
     default:
-      throw new Error(`No business rules schema found for ${appealType}`);
+      throw new BusinessRulesError(`No business rules schema found for appeal type ${appealType}`);
   }
 };
 
