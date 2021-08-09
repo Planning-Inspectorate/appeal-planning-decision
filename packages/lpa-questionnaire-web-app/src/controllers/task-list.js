8e0dcdde17b0fede52a77d803a5f5813fe9a3266
Ademola Afolabi <79277486+demzzydev@users.noreply.github.com>
2021-08-09 16:08:45 +0100

    fix(as-3113): fix backlink issue with tasklist

--- packages/lpa-questionnaire-web-app/src/controllers/task-list.js
+++ packages/lpa-questionnaire-web-app/src/controllers/task-list.js
@@ -41,7 +41,7 @@ exports.getTaskList = (req, res) => {
   const applicationStatus = 'Application incomplete';
 
   // Set backLink property in session
-  req.session.backLink = `/${req.params.id}/${VIEW.TASK_LIST}`;
+  req.session.backLink = `/appeal-questionnaire/${req.params.id}/${VIEW.TASK_LIST}`;
 
   renderView(res, VIEW.TASK_LIST, {
     prefix: 'appeal-questionnaire',
