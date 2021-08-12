a252d89b27fa3cfc79f3e8abd4bfc23b319e9dd2
Ray Mitchell <raymitchell@Rays-MacBook-Air.local>
2021-08-12 13:28:07 +0100

    fix(as-3101): fix tests

--- packages/forms-web-app/src/lib/client-side/index.js
+++ packages/forms-web-app/src/lib/client-side/index.js
@@ -2,6 +2,6 @@
 
 const { cookieConsentHandler } = require('./cookie/cookie-consent');
 const { initialiseOptionalJavaScripts } = require('./javascript-requiring-consent');
-const config = require('../../config');
 
 cookieConsentHandler(document);
+initialiseOptionalJavaScripts(document);
