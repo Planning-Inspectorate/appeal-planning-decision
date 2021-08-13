d45c60be64be31b4f54b854e915de408923b6b2a
Ray Mitchell <raymitchell@Rays-MacBook-Air.local>
2021-08-13 10:47:38 +0100

    fix(as-3101): fix cypress js

--- packages/forms-web-app/src/lib/client-side/index.js
+++ packages/forms-web-app/src/lib/client-side/index.js
@@ -2,7 +2,6 @@
 
 const { cookieConsentHandler } = require('./cookie/cookie-consent');
 const { initialiseOptionalJavaScripts } = require('./javascript-requiring-consent');
-const config = require('../../config');
 
 cookieConsentHandler(document);
-initialiseOptionalJavaScripts(document, config);
+initialiseOptionalJavaScripts(document);
