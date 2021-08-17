a254cf130cf3758d3a3990ad64796c8924740c99
adrianamiclos <adriana.miclos@manifesto.co.uk>
2021-08-17 14:16:41 +0100

    feat(lpa-questionnaire-web-app): add screens required for the user authentication

---
+++ packages/lpa-questionnaire-web-app/src/controllers/authentication.js
@@ -0,0 +1,32 @@
+const { VIEW } = require('../lib/views');
+
+function showEnterEmailAddress(req, res) {
+  const isSessionExpired = req.params.error === 'session-expired';
+  const isLinkedExpired = req.params.error === 'link-expired';
+
+  return res.render(VIEW.AUTHENTICATION.ENTER_EMAIL_ADDRESS, {
+    isSessionExpired,
+    isLinkedExpired,
+    lpaName: 'testLPA',
+    enterEmailLink: `/${req.params.id}/${VIEW.AUTHENTICATION.ENTER_EMAIL_ADDRESS}`,
+  });
+}
+
+function processEmailAddress(req, res) {
+  req.session.email = req.body?.email;
+  res.redirect(`/${req.params.id}/${VIEW.AUTHENTICATION.EMAIL_ADDRESS_CONFIRMATION}`);
+}
+
+function showEmailConfirmation(req, res) {
+  return res.render(VIEW.AUTHENTICATION.EMAIL_ADDRESS_CONFIRMATION, {
+    tokenExpirationTime: '15 minutes',
+    email: req.session?.email,
+    enterEmailLink: `/${req.params.id}/${VIEW.AUTHENTICATION.ENTER_EMAIL_ADDRESS}`,
+  });
+}
+
+module.exports = {
+  processEmailAddress,
+  showEnterEmailAddress,
+  showEmailConfirmation,
+};
