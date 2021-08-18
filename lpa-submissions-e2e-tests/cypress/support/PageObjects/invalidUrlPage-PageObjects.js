fed3ef92056b632ef498297da209f15c01e40ddb
shilpabasu22 <shilpabasu22@gmail.com>
2021-08-18 17:33:27 +0100

    test: tests for contact us page added

---
+++ lpa-submissions-e2e-tests/cypress/support/PageObjects/invalidUrlPage-PageObjects.js
@@ -0,0 +1,4 @@
+export const getContactUsLink = () => cy.get('a[href="appeal-questionnaire/contact-us"]');
+
+export const getFindAboutCallChargesLink = () => cy.get('a[href="https://www.gov.uk/call-charges"]');
+export const getEmailLink = () => cy.get('[data-cy="Enquiries"]');
