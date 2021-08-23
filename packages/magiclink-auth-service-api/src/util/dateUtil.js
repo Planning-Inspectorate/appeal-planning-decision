ecdbef95b6a59416b59d57394383506b441ac1cc
adrianamiclos <adriana.miclos@manifesto.co.uk>
2021-08-23 11:45:47 +0100

    docs(magiclink-auth-service-api): add js docs

--- packages/magiclink-auth-service-api/src/util/dateUtil.js
+++ packages/magiclink-auth-service-api/src/util/dateUtil.js
@@ -1,3 +1,9 @@
+/**
+ * Creates a Date object by adding the given input number of milliseconds to the current Date.
+ *
+ * @param timeInMillis milliseconds to be added to the current date
+ * @returns {Date}
+ */
 function addMillisToCurrentDate(timeInMillis) {
   return new Date(Date.now() + timeInMillis);
 }
