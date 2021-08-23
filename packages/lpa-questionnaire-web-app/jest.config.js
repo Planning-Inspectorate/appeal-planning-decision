aae2e9e300840965999ee7d42baec05b9f8e7b06
Anthony Walsh <anthony.walsh@planninginspectorate.gov.uk>
2021-08-23 10:51:37 +0100

    Adding build pipeline for lpa-questionnaire

--- packages/lpa-questionnaire-web-app/jest.config.js
+++ packages/lpa-questionnaire-web-app/jest.config.js
@@ -3,11 +3,11 @@ process.env.TZ = 'GMT';
 module.exports = {
   clearMocks: true,
   testEnvironment: 'node',
-  setupFiles: ['./tests/setupTests.js'],
+  setupFiles: ['./__tests__/setupTests.js'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js'],
-  coverageReporters: ['json', 'html', 'text', 'text-summary'],
   coveragePathIgnorePatterns: ['node_modules', '<rootDir>/src/app.js', '<rootDir>/src/server.js'],
+  testMatch: ['**/?(*.)+(spec|test).js?(x)'],
   coverageThreshold: {
     global: {
       branches: 100,
