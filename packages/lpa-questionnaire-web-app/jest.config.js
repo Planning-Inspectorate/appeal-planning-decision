d7f83ae5cf28c97e7f2f6433accfd2948ad2179c
Anthony Walsh <anthony.walsh@planninginspectorate.gov.uk>
2021-08-23 13:58:07 +0100

    Attempt moving setup

--- packages/lpa-questionnaire-web-app/jest.config.js
+++ packages/lpa-questionnaire-web-app/jest.config.js
@@ -3,7 +3,7 @@ process.env.TZ = 'GMT';
 module.exports = {
   clearMocks: true,
   testEnvironment: 'node',
-  setupFiles: ['./__tests__/setupTests.js'],
+  setupFilesAfterEnv: ['./__tests__/setupTests.js'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js'],
   coveragePathIgnorePatterns: ['node_modules', '<rootDir>/src/app.js', '<rootDir>/src/server.js'],
