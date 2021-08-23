8ac55898b01ea0a023188772af9d77f72d8713a5
Anthony Walsh <anthony.walsh@planninginspectorate.gov.uk>
2021-08-23 15:48:17 +0100

    Move reset mocks to package.json

--- packages/lpa-questionnaire-web-app/jest.config.js
+++ packages/lpa-questionnaire-web-app/jest.config.js
@@ -2,7 +2,6 @@ process.env.TZ = 'GMT';
 
 module.exports = {
   clearMocks: true,
-  resetMocks: false,
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./__tests__/setupTests.js'],
   collectCoverage: true,
