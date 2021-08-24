7d640cb521ace9c61f7e54922b1e4af25997eff3
Anthony Walsh <anthony.walsh@planninginspectorate.gov.uk>
2021-08-24 17:23:04 +0100

    Adding yaml and moving things in support of pdf service pipeline

--- packages/pdf-service-api/jest.config.js
+++ packages/pdf-service-api/jest.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  watchPathIgnorePatterns: ['globalConfig'],
+  clearMocks: true,
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    '<rootDir>/src/app.js',
+    '<rootDir>/src/server.js',
+    '<rootDir>/src/main.js',
+  ],
+  testMatch: ['**/?(*.)+(spec|test).js?(x)'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
