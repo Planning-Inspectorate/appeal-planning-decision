657528a090217f4be7e712a70445d43c34deff0d
Anthony Walsh <anthony.walsh@planninginspectorate.gov.uk>
2021-08-25 10:29:50 +0100

    Removing github/husky, tagging better for images, correct image repos

--- .github/ISSUE_TEMPLATE.md
+++
@@ -1,5 +0,0 @@
----
-# ISSUES RAISED IN GITHUB WILL NOT BE ACTIONED
-
-Please raise on the Jira board instead
----
--- .github/actions/kubectl-helm/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-result="$(bash -c "$1")"
-
-status=$?
-echo ::set-output name=result::$result
-echo "$result"
-if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
