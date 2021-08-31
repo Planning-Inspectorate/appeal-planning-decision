61cc9406b1bc40c0365c708c23449d79a6945b25
adrianamiclos <adriana.miclos@manifesto.co.uk>
2021-08-31 14:33:42 +0100

    test(lpa-questionnaire-web-app): add missing unit tests

--- packages/lpa-questionnaire-web-app/src/services/authentication/error/InvalidJWTError.js
+++
@@ -1,7 +0,0 @@
-module.exports = class InvalidJWTError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    Error.captureStackTrace(this, this.constructor);
-  }
-};
