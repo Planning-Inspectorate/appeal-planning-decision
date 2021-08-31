936b304b3a2939630d3d9c7a01e684c16042aa1f
Anthony Walsh <anthony.walsh@planninginspectorate.gov.uk>
2021-08-31 11:39:58 +0100

    Convert line endings to unix

--- lpa-submissions-e2e-tests/run-lpa-tests.sh
+++ lpa-submissions-e2e-tests/run-lpa-tests.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 endpoint="dev-lpaquestionnaireserviceapi.azurewebsites.net"
 cd "$1" || exit
-
 if nc -dvzw1 $endpoint 443 2>/dev/null; then
     echo "Successfully connected to $endpoint";
     echo "Running tests....";
