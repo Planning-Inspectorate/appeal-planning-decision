49302b2b0a16a6c41ce22b0d1c1a156314c9f29f
Anthony Walsh <anthony.walsh@planninginspectorate.gov.uk>
2021-09-01 10:36:18 +0100

    Remove temp testing tests

--- test-lite/cypress/support/appeal-header-footer/confirmTermsAndConditionsLinkDisplayed.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => {
-  cy.get('[data-cy="Terms and conditions"]')
-    .should('have.attr', 'href')
-    .and('include', 'https://www.gov.uk/guidance/appeals-casework-portal-terms-and-conditions');
-
-  cy.wait(Cypress.env('demoDelay'));
-};
--- test-lite/cypress/support/appeal-navigation-confirmation/appellant-submission/confirmNavigationPlanningApplicationNumberPage.js
+++
@@ -1,4 +0,0 @@
-module.exports = () => {
-  cy.url().should('include', '/appellant-submission/application-number');
-  cy.wait(Cypress.env('demoDelay'));
-}
