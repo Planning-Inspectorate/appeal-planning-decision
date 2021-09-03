bc442869414e76a5ea16c7988bc6117f1f4c1627
Ademola Afolabi <79277486+demzzydev@users.noreply.github.com>
2021-09-03 14:50:12 +0100

    build(as-3074): add relevant build details for microservices

--- packages/forms-web-app/jest.config.js
+++ packages/forms-web-app/jest.config.js
@@ -3,22 +3,22 @@ process.env.TZ = 'GMT';
 module.exports = {
   clearMocks: true,
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./tests/setupTests.js'],
+  setupFilesAfterEnv: ['./__tests__/setupTests.js'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js'],
-  coverageReporters: ['json', 'html', 'text', 'text-summary'],
+  collectCoverageFrom: ['./src/**/*.js'],
   coveragePathIgnorePatterns: [
     'node_modules',
     '<rootDir>/src/app.js',
     '<rootDir>/src/server.js',
     '<rootDir>/src/public/javascripts/main.js',
   ],
+  testMatch: ['**/?(*.)+(spec|test).js'],
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };
