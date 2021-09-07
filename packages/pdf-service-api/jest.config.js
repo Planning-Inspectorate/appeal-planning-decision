790891811486075298555356e3ca8c27f8be5b2d
Ray Mitchell <ray@Rays-Air-2.broadband>
2021-09-07 04:31:41 +0100

    fix(as-3074): azure pipelines pdf service api

--- packages/pdf-service-api/jest.config.js
+++ packages/pdf-service-api/jest.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  watchPathIgnorePatterns: ['globalConfig'],
+  clearMocks: true,
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    '<rootDir>/src/app.js',
+    '<rootDir>/src/server.js',
+    '<rootDir>/src/main.js',
+  ],
+  testMatch: ['**/?(*.)+(spec|test).js'],
+  moduleFileExtensions: ['js', 'json'],
+  coverageThreshold: {
+    global: {
+      branches: 20,
+      functions: 20,
+      lines: 20,
+      statements: 20,
+    },
+  },
 };
