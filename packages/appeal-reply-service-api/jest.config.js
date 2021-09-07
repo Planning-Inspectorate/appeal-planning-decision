7b3cd84062aa7ca3f222987de62d800c6c732ca4
Ray Mitchell <ray@Rays-Air-2.broadband>
2021-09-07 08:19:42 +0100

    fix(as-3074): azure pipelines appeal reply service api

--- packages/appeal-reply-service-api/jest.config.js
+++ packages/appeal-reply-service-api/jest.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   preset: '@shelf/jest-mongodb',
   watchPathIgnorePatterns: ['globalConfig'],
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**/*.js'],
+  coveragePathIgnorePatterns: ['node_modules', '<rootDir>/src/app.js', '<rootDir>/src/server.js'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageThreshold: {
+    global: {
+      branches: 30,
+      functions: 30,
+      lines: 30,
+      statements: 30,
+    },
+  },
+  moduleFileExtensions: ['js', 'json'],
+  testMatch: ['**/?(*.)+(spec|test).js'],
+  testEnvironment: 'node',
 };
