8bfc2c8378cd68029e378d146b907dc4e7bc4921
adrianamiclos <adriana.miclos@manifesto.co.uk>
2021-09-09 11:03:25 +0100

    fix(magiclink-auth-service-api): add optional magicLinkURL variable required for local environment
    Add magicLinkURL config value that is used for local testing.

--- packages/magiclink-auth-service-api/src/config.js
+++ packages/magiclink-auth-service-api/src/config.js
@@ -16,4 +16,12 @@ module.exports = {
   jwtSigningKey: process.env.JWT_SIGNING_KEY,
   magicLinkValidityTimeMillis: Number(process.env.MAGIC_LINK_VALIDITY_TIME_MILLIS),
   cookieValidityTimeMillis: Number(process.env.COOKIE_VALIDITY_TIME_MILLIS),
+  /**
+   * Optional variable used for constructing the magic link URL that is sent via email.
+   * If the env variable is not defined, then the magic link URL is constructed dynamically using the API protocol and host.
+   *
+   * The main purpose of this configuration is to help with local testing. When the service runs inside the docker container
+   * the API host name is not 'localhost' but the name of the container which is accessible only from the docker compose network.
+   */
+  magicLinkURL: process.env.MAGIC_LINK_API_URL,
 };
