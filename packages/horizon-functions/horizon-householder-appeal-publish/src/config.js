31b62c489582c754f97c30f923aa86b038b4cbd6
Ray Mitchell <ray@Rays-Air-2.broadband>
2021-09-14 14:30:00 +0100

    fix(as-3074): rebase horizon fns

---
+++ packages/horizon-functions/horizon-householder-appeal-publish/src/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  appealsService: {
+    url: process.env.APPEALS_SERVICE_URL,
+  },
+  documentService: {
+    url: process.env.DOCUMENT_SERVICE_URL,
+  },
+  horizon: {
+    url: process.env.HORIZON_URL,
+  },
+  openfaas: {
+    gatewayUrl: process.env.GATEWAY_URL,
+  },
+  logger: {
+    level: process.env.LOGGER_LEVEL || 'info',
+  },
+};
---
+++ packages/horizon-functions/horizon-householder-appeal-publish/src/createContacts.js
@@ -0,0 +1,89 @@
+const rabbitmq = require('./lib/rabbitmq');
+
+/**
+ * Create Contacts
+ *
+ * Takes the body data, parses it into agent/appellant, creates the
+ * contact inside Horizon and returns the data that is in the format
+ * that can be used by the CreateCase endpoint.
+ *
+ * @param {*} log
+ * @param {*} body
+ * @returns {Promise}
+ */
+const createContacts = async (log, body) => {
+  const contacts = [];
+  let logMessage;
+
+  if (body.appeal.aboutYouSection.yourDetails.isOriginalApplicant) {
+    /* User is original applicant - just add appellant */
+    logMessage = 'User is original applicant';
+    contacts.push({
+      type: 'Appellant',
+      email: body.appeal.aboutYouSection.yourDetails.email,
+      name: body.appeal.aboutYouSection.yourDetails.name,
+    });
+  } else {
+    /* User is agent - add both agent and OP */
+    logMessage = 'User is agent';
+    contacts.push(
+      {
+        type: 'Agent',
+        email: body.appeal.aboutYouSection.yourDetails.email,
+        name: body.appeal.aboutYouSection.yourDetails.name,
+      },
+      {
+        /* Email not collected here */
+        type: 'Appellant',
+        name: body.appeal.aboutYouSection.yourDetails.appealingOnBehalfOf,
+      }
+    );
+  }
+
+  log.info(contacts, logMessage);
+
+  return Promise.all(
+    contacts.map(async ({ name, email, type }) => {
+      /* Create the user in Horizon */
+      const [firstName, ...lastName] = name.split(' ');
+      let contactId;
+
+      log.info('Inserting contact into Horizon');
+
+      await rabbitmq.publisher('create-contact', {
+        firstName,
+        lastName: lastName.join(' '),
+        email,
+      });
+
+      return {
+        /* Add user contact details */
+        key: 'Case Involvement:Case Involvement',
+        value: [
+          {
+            key: 'Case Involvement:Case Involvement:ContactID',
+            value: contactId,
+          },
+          {
+            key: 'Case Involvement:Case Involvement:Contact Details',
+            value: name,
+          },
+          {
+            key: 'Case Involvement:Case Involvement:Involvement Start Date',
+            value: new Date(),
+          },
+          {
+            key: 'Case Involvement:Case Involvement:Communication Preference',
+            value: 'e-mail',
+          },
+          {
+            key: 'Case Involvement:Case Involvement:Type Of Involvement',
+            value: type,
+          },
+        ],
+      };
+    })
+  );
+};
+
+module.exports = { createContacts };
