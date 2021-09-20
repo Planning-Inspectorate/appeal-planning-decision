7603d1908e458973b56346ae49da6048990fa38d
Demzy <79277486+demzzydev@users.noreply.github.com>
2021-09-20 15:25:57 +0100

    refactor(as-3123): change route name for better consistency

--- packages/lpa-questionnaire-web-app/src/routes/contact-us.js
+++ packages/lpa-questionnaire-web-app/src/routes/contact-us.js
@@ -3,6 +3,6 @@ const contactUsController = require('../controllers/contact-us');
 
 const router = express.Router();
 
-router.get('/appeal-questionnaire/contact-us', contactUsController.renderContactUs);
+router.get('/appeal-questionnaire/contact-us', contactUsController.getContactUs);
 
 module.exports = router;
