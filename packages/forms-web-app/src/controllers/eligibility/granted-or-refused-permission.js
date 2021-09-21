3b8810ea10d235e921833ee6877984519b096ffe
nssdesikan <nssdesikan@rediffmail.com>
2021-09-21 15:51:12 +0100

    refactor(update existing files): address further PR comments
    Address PR comments related to Linting and Structure of Constants

--- packages/forms-web-app/src/controllers/eligibility/granted-or-refused-permission.js
+++ packages/forms-web-app/src/controllers/eligibility/granted-or-refused-permission.js
@@ -1,7 +1,7 @@
 const logger = require('../../lib/logger');
 const { VIEW } = require('../../lib/views');
 const { createOrUpdateAppeal } = require('../../lib/appeals-api-wrapper');
-const { constants } = require('../../constants');
+const { eligibility } = require('../../constants');
 const {
   validHouseholderPlanningPermissionStatusOptions,
 } = require('../../validators/eligibility/granted-or-refused-permission');
@@ -66,8 +66,7 @@ exports.postGrantedOrRefusedPermission = async (req, res) => {
 
   if (validHouseholderPlanningPermissionStatusOptions.includes(planningPermissionStatus)) {
     isPlanningPermissionRefused =
-      planningPermissionStatus.toLowerCase() ===
-      constants.eligibility.planningPermissionStatus.REFUSED;
+      planningPermissionStatus.toLowerCase() === eligibility.planningPermissionStatus.REFUSED;
   }
 
   try {
