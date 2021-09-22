db7dd639457f1ec53399116c42ab695afaed68dd
Ray Mitchell <ray@Rays-Air-2.broadband>
2021-09-22 19:44:30 +0100

    fix(as-30002): pdf

--- packages/pdf-service-api/src/controllers/pdf.js
+++ packages/pdf-service-api/src/controllers/pdf.js
@@ -1,5 +1,6 @@
+const puppeteer = require('puppeteer');
 const logger = require('../lib/logger');
-const toPDF = require('../lib/pdfUtils');
+// const toPDF = require('../lib/pdfUtils');
 
 module.exports = {
   async generatePdf(req, res) {
@@ -7,11 +8,23 @@ module.exports = {
     try {
       const { htmlFile } = req.files;
       const buffer = htmlFile.data;
-      const pdf = await toPDF(buffer.toString());
-      logger.debug(`Sending pdf file ${htmlFile.name}.pdf generated from html file ${htmlFile.name}`);
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', `attachment; filename="${htmlFile.name}.pdf"`);
-      pdf.pipe(res);
+
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      await page.setContent(buffer);
+
+      const pdfBuffer = await page.pdf();
+
+      await page.close();
+      await browser.close();
+
+      res.pipe(pdfBuffer);
+      // const pdf = await toPDF(buffer.toString());
+      // logger.debug(`Sending pdf file ${htmlFile.name}.pdf generated from html file ${htmlFile.name}`);
+      // res.setHeader('Content-Type', 'application/pdf');
+      // res.setHeader('Content-Disposition', `attachment; filename="${htmlFile.name}.pdf"`);
+      // pdf.pipe(res);
     } catch (err) {
       logger.error(`Problem processing html file with error response:\n ${err}`);
       res.status(400).send('Provided html file was invalid');
