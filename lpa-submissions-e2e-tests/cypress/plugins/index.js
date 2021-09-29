6038fa0e5c4daa3cb9620cd84e4474ee71cd5ed5
nssdesikan <nssdesikan@rediffmail.com>
2021-09-29 13:40:54 +0100

    feat(as-2979): update files to address HTML validation issue

--- lpa-submissions-e2e-tests/cypress/plugins/index.js
+++ lpa-submissions-e2e-tests/cypress/plugins/index.js
@@ -1,6 +1,7 @@
 const cucumber = require('cypress-cucumber-preprocessor').default;
 const { downloadFile } = require('cypress-downloadfile/lib/addPlugin');
 const pdf = require('pdf-parse');
+const htmlvalidate = require('cypress-html-validate/dist/plugin');
 
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -24,6 +25,11 @@ const parsePdf = async (pdfBuffer) => {
 
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber());
+
+  htmlvalidate.install(on, null, {
+    exclude: ["title", "link","script",".govuk-header",".govuk-footer"],
+    include: [],
+  });
 
   on('task', {
     log(message) {
