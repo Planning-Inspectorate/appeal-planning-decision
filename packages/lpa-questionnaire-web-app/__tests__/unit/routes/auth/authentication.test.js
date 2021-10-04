4224644ab572e76bfb5816b68726f9454af9edae
nssdesikan <nssdesikan@rediffmail.com>
2021-10-04 15:40:59 +0100

    Changes to complete missing features and validatio
    feat(as-3487): validate yor email page, refactor
    
    Validate Your Email page for user input, refactor the code to make the code unit testable, add unit
    tests
    
    feat(as-3487): change page layout, set redirecturl to req.session
    
    refactor(as-3487): update test script for code change

--- packages/lpa-questionnaire-web-app/__tests__/unit/routes/auth/authentication.test.js
+++ packages/lpa-questionnaire-web-app/__tests__/unit/routes/auth/authentication.test.js
@@ -1,8 +1,12 @@
 const { get, post } = require('../router-mock');
 const authenticationController = require('../../../../src/controllers/authentication');
 const fetchLPA = require('../../../../src/middleware/fetch-lpa');
+const { rules: yourEmailValidatorRules } = require('../../../../src/validators/email');
+const { validationErrorHandler } = require('../../../../src/validators/validation-error-handler');
 
-describe('routes/extra-conditions', () => {
+jest.mock('../../../../src/validators/email');
+
+describe('routes/authentication', () => {
   beforeEach(() => {
     // eslint-disable-next-line global-require
     require('../../../../src/routes/auth/authentication');
@@ -16,6 +20,8 @@ describe('routes/extra-conditions', () => {
     );
     expect(post).toHaveBeenCalledWith(
       '/:lpaCode/authentication/your-email',
+      yourEmailValidatorRules(),
+      validationErrorHandler,
       fetchLPA,
       authenticationController.processEmailAddress
     );
