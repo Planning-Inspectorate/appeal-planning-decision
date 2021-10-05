873e117bf6aded85a68d663223eb2fac5060e67e
nssdesikan <nssdesikan@rediffmail.com>
2021-10-05 16:50:46 +0100

    feat(as-3487): changes to make Magic Link code PR ready

--- packages/lpa-questionnaire-web-app/src/routes/auth/authentication.js
+++ packages/lpa-questionnaire-web-app/src/routes/auth/authentication.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const authenticationController = require('../../controllers/authentication');
 const fetchLPA = require('../../middleware/fetch-lpa');
+const { validationErrorHandler } = require('../../validators/validation-error-handler');
+const { rules: yourEmailValidatorRules } = require('../../validators/email');
 
 const router = express.Router();
 
 router.get(
-  '/:lpaCode/authentication/your-email/:error(session-expired|link-expired)?',
+  '/appeal-questionnaire/:lpaCode/authentication/your-email/:error(session-expired|link-expired)?',
   fetchLPA,
   authenticationController.showEnterEmailAddress
 );
 router.post(
-  '/:lpaCode/authentication/your-email',
+  '/appeal-questionnaire/:lpaCode/authentication/your-email',
+  yourEmailValidatorRules(),
+  validationErrorHandler,
   fetchLPA,
   authenticationController.processEmailAddress
 );
 router.get(
-  '/:lpaCode/authentication/confirm-email',
+  '/appeal-questionnaire/:lpaCode/authentication/confirm-email',
   fetchLPA,
   authenticationController.showEmailConfirmation
 );
