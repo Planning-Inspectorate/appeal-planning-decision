f64042bddb12e4ad00b09c873608a59ecb624eb9
nssdesikan <nssdesikan@rediffmail.com>
2021-10-18 18:15:20 +0100

    refactor(as-3540): addressed PR comments

--- lpa-submissions-e2e-tests/cypress/integration/alreadySubmitted/alreadySubmittedStep.js
+++ lpa-submissions-e2e-tests/cypress/integration/alreadySubmitted/alreadySubmittedStep.js
@@ -1,6 +1,6 @@
 import { When, Then } from 'cypress-cucumber-preprocessor/steps';
 import { getPage } from '../../support/alreadySubmitted/getPage';
-import{ findoutAboutCallCharges, pinsEnquiriesEmailLink} from '../../support/PageObjects/common-page-objects';
+import{ findoutAboutCallCharges, enquiriesEmailLink} from '../../support/PageObjects/common-page-objects';
 
 const page = {
   heading: 'Questionnaire already submitted',
@@ -8,22 +8,22 @@ const page = {
   url: 'already-submitted',
 }
 
-When('LPA Planning Officer is trying to access {string} of already submitted questionnaire', (page) => {
+When('the LPA Planning Officer is trying to access {string} of already submitted questionnaire', (page) => {
   cy.goToPage(getPage(page));
 });
 
-Then('LPA Planning Officer is presented with already submitted page', () => {
+Then('the LPA Planning Officer is presented with already submitted page', () => {
   cy.verifyPage(page.url);
   cy.verifyPageTitle(page.title);
   cy.verifyPageHeading(page.heading);
 });
 
-Then('Already Submitted page should have PINS Enquiries Email link', () => {
-  pinsEnquiriesEmailLink()
+Then('the Already Submitted page should have Enquiries Email link', () => {
+  enquiriesEmailLink()
     .should('have.attr','href','mailto:enquiries@planninginspectorate.gov.uk');
 });
 
-Then('Already Submitted page should have link to Call charges', () => {
+Then('the Already Submitted page should have link to Call charges', () => {
   findoutAboutCallCharges()
     .should('have.attr','href','https://www.gov.uk/call-charges')
     .should('have.attr','target','_blank')
