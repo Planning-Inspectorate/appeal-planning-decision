3a279276eb0d171c6f197795ec13ac024c81eba8
David Shepherd <david@x-designs.co.uk>
2021-11-04 08:39:33 +0000

    feat(as-3122): add document types list to the common package

---
+++ packages/common/src/document-types.js
@@ -0,0 +1,72 @@
+const documentTypes = {
+  originalApplication: {
+    name: 'originalApplication',
+    multiple: false,
+  },
+  decisionLetter: {
+    name: 'decisionLetter',
+    multiple: false,
+  },
+  appealStatement: {
+    name: 'appealStatement',
+    multiple: false,
+  },
+  otherDocuments: {
+    name: 'otherDocuments',
+    multiple: true,
+  },
+  appealPdf: {
+    name: 'appealPdf',
+    multiple: false,
+  },
+  decisionPlans: {
+    name: 'decisionPlans',
+    multiple: false,
+  },
+  officersReport: {
+    name: 'officersReport',
+    multiple: false,
+  },
+  interestedParties: {
+    name: 'interestedParties',
+    multiple: false,
+  },
+  representations: {
+    name: 'representations',
+    multiple: false,
+  },
+  notifyingParties: {
+    name: 'notifyingParties',
+    multiple: false,
+  },
+  siteNotices: {
+    name: 'siteNotices',
+    multiple: false,
+  },
+  conservationAreaMap: {
+    name: 'conservationAreaMap',
+    multiple: false,
+  },
+  planningHistory: {
+    name: 'planningHistory',
+    multiple: false,
+  },
+  otherPolicies: {
+    name: 'otherPolicies',
+    multiple: false,
+  },
+  statutoryDevelopment: {
+    name: 'statutoryDevelopment',
+    multiple: false,
+  },
+  supplementaryDocuments: {
+    name: 'supplementaryDocuments',
+    multiple: false,
+  },
+  questionnairePdf: {
+    name: 'questionnairePdf',
+    multiple: false,
+  },
+};
+
+module.exports = documentTypes;
--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -2,10 +2,12 @@ const functional = require('./functional');
 const healthcheck = require('./health');
 const prometheus = require('./prometheus');
 const utils = require('./utils');
+const documentTypes = require('./document-types');
 
 module.exports = {
   functional,
   healthcheck,
   prometheus,
   utils,
+  documentTypes,
 };
