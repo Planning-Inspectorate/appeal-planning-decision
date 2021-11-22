d1ff604cc8eb3c0a5f0454cc635e09e70796e182
shilpabasu22 <shilpabasu22@gmail.com>
2021-11-22 12:18:59 +0000

    Revert "test: added new tests for local planning department page"
    This reverts commit 92b128c793bd187aa01504c1d349b796b360db4a.

--- e2e-tests/cypress/support/before-you-start-local-planning-depart/enter-local-planning-depart.js
+++
@@ -1,5 +0,0 @@
-import { getLocalPlanningDepart } from '../page-objects/local-planning-depart';
-
-export const enterLocalPlanningDepart = (departmentName) =>{
-  getLocalPlanningDepart().type(departmentName);
-}
