ed15776416779807789f56e93b070a7267464a2c
shilpabasu22 <shilpabasu22@gmail.com>
2021-11-22 20:51:21 +0000

    test: updated the local planning department tests

--- e2e-tests/cypress/support/go-to-page/goToLocalPlanningDepartment.js
+++
@@ -1,9 +0,0 @@
-import { acceptCookiesBanner } from '../common/accept-cookies-banner';
-
-export const goToLocalPlanningDepartment = () =>{
-  cy.visit('/before-you-start/local-planning-depart');
-  acceptCookiesBanner();
-  cy.checkPageA11y({
-    exclude: ['.govuk-radios__input'],
-  });
-}
