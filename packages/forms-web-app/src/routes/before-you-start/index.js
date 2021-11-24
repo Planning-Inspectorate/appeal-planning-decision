cefcc7a4b8d03c9a8a09c05ee17fa5edf8522ee3
Demzy <79277486+demzzydev@users.noreply.github.com>
2021-11-24 10:00:34 +0000

    feat(as-3842): add validator for any-of-following controller

--- packages/forms-web-app/src/routes/before-you-start/index.js
+++ packages/forms-web-app/src/routes/before-you-start/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const anyOfFollowingRouter = require('./any-of-following');
+const {
+  rules: anyOfFollowingRules,
+} = require('../../validators/before-you-start/any-of-following');
 
 const router = express.Router();
 
-router.use('/any-of-following', anyOfFollowingRouter);
+router.use('/any-of-following', anyOfFollowingRules(), anyOfFollowingRouter);
 
 module.exports = router;
--- packages/forms-web-app/src/validators/before-you-start/any-of-following.js
+++ packages/forms-web-app/src/validators/before-you-start/any-of-following.js
@@ -1,15 +1,9 @@
-const { body } = require('express-validator');
+const { body, checkSchema } = require('express-validator');
+const anyOfFollowingSchema = require('./any-of-following-schema');
 
-const anyOfFollowingOptions = [
-  'none_of_these',
-  'major_retail_and_services',
-  'major_travelling_and_caravan_pitches',
-  'major_general_industry_storage_warehousing',
-  'major_dwellings',
-  'a_listed_building',
-];
-
-const rules = () => body('options').notEmpty().isIn(anyOfFollowingOptions);
+const rules = () => {
+  return [checkSchema(anyOfFollowingSchema), body('option').notEmpty()];
+};
 
 module.exports = {
   rules,
