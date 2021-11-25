29d16a0afb9b233eeccb7fb5e27ab1a9d02c3420
swdpcomputing <swdpcomputing@outlook.com>
2021-11-25 10:14:24 +0000

    feat(forms-web-app): create appellents selects type of planning application page

--- packages/forms-web-app/src/routes/before-you-start/index.js
+++ packages/forms-web-app/src/routes/before-you-start/index.js
@@ -3,6 +3,7 @@ const localPlanningDepartmentRouter = require('./local-planning-department');
 const enforcementNotice = require('./enforcement-notice');
 
 const router = express.Router();
+
 router.use(localPlanningDepartmentRouter);
 router.use(enforcementNotice);
 
