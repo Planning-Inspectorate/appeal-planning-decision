cdcc1b1bab7c005fd1dc5b5164e829357ef2e41a
shilpabasu22 <shilpabasu22@gmail.com>
2021-11-29 11:04:15 +0000

    test: added tests for application type

---
+++ e2e-tests/cypress/support/page-objects/planning-application-type-po.js
@@ -0,0 +1,9 @@
+export const getFullPlanningRadio = () => cy.get('input[data-cy="answer-full-planning"]');
+export const getHouseHolderPlanningRadio = () => cy.get('input[data-cy="answer-householder-planning"]');
+export const getOutlinePlanningRadio = () => cy.get('input[data-cy="answer-outline-planning"]');
+export const getPriorApprovalPlanningRadio = () => cy.get('input[data-cy="answer-prior-approval"]');
+export const getReservedMattersPlanningRadio = () => cy.get('input[data-cy="answer-reserved-matters"]');
+export const getRemovalOrVariationOfConditionsRadio = () => cy.get('input[data-cy="answer-removal-or-variation-of-conditions"]');
+export const getSomethingElseRadio = () => cy.get('input[data-cy="answer-something-else"]');
+export const getNoApplicationMadeRadio = () => cy.get('input[data-cy="answer-i-have-not-made-a-planning-application"]');
+
