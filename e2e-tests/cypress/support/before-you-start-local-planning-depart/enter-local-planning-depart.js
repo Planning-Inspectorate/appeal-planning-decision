ab2a3ea2970ec4e2c2368761657df816aa099b21
shilpabasu22 <shilpabasu22@gmail.com>
2021-12-03 15:00:50 +0000

    test: updated tests for application type and local planning department

--- e2e-tests/cypress/support/before-you-start-local-planning-depart/enter-local-planning-depart.js
+++ e2e-tests/cypress/support/before-you-start-local-planning-depart/enter-local-planning-depart.js
@@ -1,5 +1,5 @@
 import { getLocalPlanningDepart } from '../page-objects/local-planning-department-po';
 
 export const enterLocalPlanningDepart = (departmentName) =>{
-  getLocalPlanningDepart().type(departmentName);
+  getLocalPlanningDepart().select(departmentName);
 }
--- e2e-tests/cypress/support/go-to-page/goToPage.js
+++ e2e-tests/cypress/support/go-to-page/goToPage.js
@@ -1,8 +1,7 @@
-import { acceptCookiesBanner } from '../common/accept-cookies-banner';
 
 export const goToPage = (url) =>{
   cy.visit(url);
-  acceptCookiesBanner();
+
   cy.checkPageA11y({
     exclude: ['.govuk-radios__input'],
   });
