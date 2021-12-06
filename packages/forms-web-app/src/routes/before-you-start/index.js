feb9f09ed54fe3c3f856986bd541ab7fd62ab18d
nssdesikan <nssdesikan@rediffmail.com>
2021-12-06 15:13:10 +0000

    feat(as-4052): enforment Notice page development

--- packages/forms-web-app/src/routes/before-you-start/index.js
+++ packages/forms-web-app/src/routes/before-you-start/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const localPlanningDepartmentRouter = require('./local-planning-department');
+const enforcementNotice = require('./enforcement-notice');
 
 const router = express.Router();
 router.use(localPlanningDepartmentRouter);
+router.use(enforcementNotice);
 
 module.exports = router;
