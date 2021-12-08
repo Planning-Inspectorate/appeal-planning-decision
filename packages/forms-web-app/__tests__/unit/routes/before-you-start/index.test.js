bc6ec2ee0d7a666ff1bfdf5f5c60d3d96d39a1bc
swdpcomputing <swdpcomputing@outlook.com>
2021-12-08 13:41:35 +0000

    feat(before-you-start): add use a different service page

--- packages/forms-web-app/__tests__/unit/routes/before-you-start/index.test.js
+++ packages/forms-web-app/__tests__/unit/routes/before-you-start/index.test.js
@@ -1,6 +1,8 @@
 const { use } = require('../router-mock');
 const anyOfFollowingRouter = require('../../../../src/routes/before-you-start/any-of-following');
-const localPlanningDepartmentRouter = require('../../../../src/routes/before-you-start/any-of-following');
+const localPlanningDepartmentRouter = require('../../../../src/routes/before-you-start/local-planning-department');
+const typeOfPlanningRouter = require('../../../../src/routes/before-you-start/type-of-planning-application');
+const useADifferentServiceRouter = require('../../../../src/routes/before-you-start/use-a-different-service');
 
 describe('routes/before-you-start/index', () => {
   beforeEach(() => {
@@ -16,8 +18,10 @@ describe('routes/before-you-start/index', () => {
 
   it('should define the expected routes', () => {
     expect(use).toHaveBeenCalledWith(localPlanningDepartmentRouter);
+    expect(use).toHaveBeenCalledWith(typeOfPlanningRouter);
+    expect(use).toHaveBeenCalledWith(useADifferentServiceRouter);
     expect(use).toHaveBeenCalledWith('/any-of-following', anyOfFollowingRouter);
 
-    expect(use.mock.calls.length).toBe(3);
+    expect(use.mock.calls.length).toBe(4);
   });
 });
--- packages/forms-web-app/src/routes/before-you-start/index.js
+++ packages/forms-web-app/src/routes/before-you-start/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const anyOfFollowingRouter = require('./any-of-following');
 const localPlanningDepartmentRouter = require('./local-planning-department');
 const typeOfPlanningApplicationRouter = require('./type-of-planning-application');
+const useADifferentServiceRouter = require('./use-a-different-service');
 
 const router = express.Router();
 
 router.use(localPlanningDepartmentRouter);
 router.use(typeOfPlanningApplicationRouter);
 router.use('/any-of-following', anyOfFollowingRouter);
+router.use(useADifferentServiceRouter);
 
 module.exports = router;
