e553d830d62ea2193fa44007f61208975487f5c4
nssdesikan <nssdesikan@rediffmail.com>
2021-12-09 08:34:27 +0000

    feat(as-4052): fix failing tests

--- packages/forms-web-app/src/routes/before-you-start/index.js
+++ packages/forms-web-app/src/routes/before-you-start/index.js
@@ -5,10 +5,6 @@ const typeOfPlanningApplicationRouter = require('./type-of-planning-application'
 const enforcementNoticeRouter = require('./enforcement-notice');
 const anyOfFollowingRouter = require('./any-of-following');
 const useADifferentServiceRouter = require('./use-a-different-service');
-const enforcementNotice = require('./enforcement-notice');
-const anyOfFollowingRouter = require('./any-of-following');
-const useADifferentServiceRouter = require('./use-a-different-service');
-const enforcementNotice = require('./enforcement-notice');
 
 const router = express.Router();
 
@@ -17,8 +13,5 @@ router.use(typeOfPlanningApplicationRouter);
 router.use('/any-of-following', anyOfFollowingRouter);
 router.use(enforcementNoticeRouter);
 router.use(useADifferentServiceRouter);
-router.use(enforcementNotice);
-router.use(useADifferentServiceRouter);
-router.use(enforcementNotice);
 
 module.exports = router;
