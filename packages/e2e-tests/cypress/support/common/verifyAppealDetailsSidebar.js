94a98271197d6e7daddc11fc0ab3ae07cfbe44f1
shilpabasu22 <shilpabasu22@gmail.com>
2021-12-10 21:14:34 +0000

    refactor: updated folder structure for lpa questionnaire

--- packages/e2e-tests/cypress/support/common/verifyAppealDetailsSidebar.js
+++ packages/e2e-tests/cypress/support/common/verifyAppealDetailsSidebar.js
@@ -1,13 +1,14 @@
-import AppealDetails from '../householder-planning/lpa-questionnaire/PageObjects/AppealDetailsPageObjects';
-
-const appealDetails = new AppealDetails();
+import {
+  getPlanningApplicationAddress, getPlanningApplicationAppellant,
+  getPlanningApplicationNumber,
+} from '../householder-planning/lpa-questionnaire/PageObjects/AppealDetailsPageObjects';
 
 export const verifyAppealDetailsSidebar = ({
   applicationNumber = 'ABC/123',
   applicationAddress = '999 Letsby Avenue, Sheffield, South Yorkshire, S9 1XY',
   apellantName = 'Bob Smith',
 }) => {
-  appealDetails.getPlanningApplicationNumber().contains(applicationNumber);
-  appealDetails.getPlanningApplicationAddress().contains(applicationAddress);
-  appealDetails.getPlanningApplicationAppellant().contains(apellantName);
+  getPlanningApplicationNumber().contains(applicationNumber);
+  getPlanningApplicationAddress().contains(applicationAddress);
+  getPlanningApplicationAppellant().contains(apellantName);
 };
