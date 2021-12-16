6183bc4e0b11a800cb51603f6fa5f2e1d6976839
shepda <david@x-designs.co.uk>
2021-12-16 16:36:24 +0000

    feat(forms-web-app): fix linting errors

--- packages/forms-web-app/src/validators/appellant-submission/appeal-statement-schema.js
+++ packages/forms-web-app/src/validators/appellant-submission/appeal-statement-schema.js
@@ -1,4 +1,3 @@
-const validAV = require('pins-clamav');
 const { validMimeType, validateMimeBinaryType } = require('pins-mime-validation');
 const config = require('../../config');
 const validateFileSize = require('../custom/file-size');
