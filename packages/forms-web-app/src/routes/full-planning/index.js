48cd62463666d76db5efa71295d06483f8a08c6d
Demzy <79277486+demzzydev@users.noreply.github.com>
2021-12-16 11:48:10 +0000

    feat(as-4190): add out of time shutter page + controller + unit tests

--- packages/forms-web-app/src/routes/full-planning/index.js
+++ packages/forms-web-app/src/routes/full-planning/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 const express = require('express');
 
 const localPlanningDepartmentRouter = require('./local-planning-department');
@@ -5,6 +6,7 @@ const typeOfPlanningApplicationRouter = require('./type-of-planning-application'
 const anyOfFollowingRouter = require('./any-of-following');
 const grantedOrRefusedRouter = require('./granted-or-refused');
 const useADifferentServiceRouter = require('./use-a-different-service');
+const outOfTimeRouter = require('./out-of-time');
 
 const router = express.Router();
 
@@ -13,5 +15,6 @@ router.use(typeOfPlanningApplicationRouter);
 router.use(anyOfFollowingRouter);
 router.use(grantedOrRefusedRouter);
 router.use(useADifferentServiceRouter);
+router.use(outOfTimeRouter);
 
 module.exports = router;
