c82fcdfb0803a02bfe427bfb2261fc4213cf6fc6
Karuna Kanumuri <karuna.kanumuri@foundry4.com>
2021-12-23 13:56:57 +0000

    feat: refactor code for house holder appeals
    refactor code

--- packages/e2e-tests/cypress/support/householder-planning/appeals-service/your-planning-appeal-your-appeal-details/appellantViewsYourAppealDetailsInvalid.js
+++
@@ -1,9 +0,0 @@
-import { goToAppealsPage } from '../../../common/go-to-page/goToAppealsPage';
-
-const { yourAppealDetailsPageHeadingSelector } = require('./selectors');
-
-const defaultOptions = { script: true, failOnStatusCode: true };
-
-export const appellantViewsYourAppealDetailsInvalid = ({ script, failOnStatusCode } = defaultOptions) => {
-  goToAppealsPage('your-planning-appeal/your-appeal-details', { script, failOnStatusCode });
-};
