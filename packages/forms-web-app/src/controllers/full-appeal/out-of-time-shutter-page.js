b24b73a88d32925d094e4e423c260d4954daadf7
nssdesikan <nssdesikan@rediffmail.com>
2021-12-27 23:50:37 +0000

    feat(as-4201): refactored code and updated commitlint sha

--- packages/forms-web-app/src/controllers/full-appeal/out-of-time-shutter-page.js
+++ packages/forms-web-app/src/controllers/full-appeal/out-of-time-shutter-page.js
@@ -1,11 +1,8 @@
 const { VIEW } = require('../../lib/views');
 
 const getOutOfTimeShutterPage = async (req, res) => {
-  const {
-    appeal: {
-      eligibility: { appealDeadline },
-    },
-  } = req.session;
+  const { appeal } = req.session;
+  const appealDeadline = appeal?.eligibility?.appealDeadline;
 
   return res.render(VIEW.OUT_OF_TIME_SHUTTER_PAGE, {
     appealDeadline,
