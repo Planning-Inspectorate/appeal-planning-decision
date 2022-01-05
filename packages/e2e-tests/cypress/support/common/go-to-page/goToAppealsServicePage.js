86a419a9345406a0f30c5b5e9e22a61eb0fe1817
shilpabasu22 <shilpabasu22@gmail.com>
2022-01-05 19:44:22 +0000

    test: removed go to appeals page duplicate code

--- packages/e2e-tests/cypress/support/common/go-to-page/goToAppealsServicePage.js
+++
@@ -1,8 +0,0 @@
-export const goToAppealsServicePage = (url) =>{
-    cy.visit(`${Cypress.env('APPEALS_BASE_URL')}/${url}`);
-
- // cy.htmlvalidate();
-  /* cy.checkPageA11y({
-     exclude: ['.govuk-radios__input'],
-   });*/
-}
--- packages/e2e-tests/cypress/support/householder-planning/appeals-service/go-to-page/goToAppealsPage.js
+++
@@ -1,5 +0,0 @@
-export const goToAppealsPage = (url) =>{
-    cy.wrap(`${Cypress.env('APPEALS_BASE_URL')}${url}`).then((url)=>{
-    cy.visit(url);
-  });
-}
