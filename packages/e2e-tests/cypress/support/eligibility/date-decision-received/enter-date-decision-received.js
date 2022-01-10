5598cb2a8f23b1aedc8b34b864c1eda4a7576061
shilpabasu22 <shilpabasu22@gmail.com>
2022-01-10 17:40:22 +0000

    test: added E2E tests for decision date

---
+++ packages/e2e-tests/cypress/support/eligibility/date-decision-received/enter-date-decision-received.js
@@ -0,0 +1,11 @@
+import {
+  getDateDecisionReceivedDay,
+  getDateDecisionReceivedMonth,
+  getDateDecisionReceivedYear
+} from "../page-objects/date-decision-received-po";
+
+export const enterDateDecisionReceived = ({ day, month, year} ) => {
+  if(day) getDateDecisionReceivedDay().clear().type(day);
+  if(month) getDateDecisionReceivedMonth().clear().type(month);
+  if(year) getDateDecisionReceivedYear().clear().type(year);
+}
