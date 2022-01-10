9f1214dad9e542ffdcd85773d0fcdfcc590b2e0a
shepda <david@x-designs.co.uk>
2022-01-10 10:28:16 +0000

    feat(as-4197): update full appeals application form page design

--- packages/forms-web-app/src/routes/full-appeal/submit-appeal/application-form.js
+++ packages/forms-web-app/src/routes/full-appeal/submit-appeal/application-form.js
@@ -3,12 +3,13 @@ const {
   getApplicationForm,
   postApplicationForm,
 } = require('../../../controllers/full-appeal/submit-appeal/application-form');
+const fetchExistingAppealMiddleware = require('../../../middleware/fetch-existing-appeal');
 const { validationErrorHandler } = require('../../../validators/validation-error-handler');
 const { rules: fileUploadValidationRules } = require('../../../validators/common/file-upload');
 
 const router = express.Router();
 
-router.get('/submit-appeal/application-form', getApplicationForm);
+router.get('/submit-appeal/application-form', [fetchExistingAppealMiddleware], getApplicationForm);
 router.post(
   '/submit-appeal/application-form',
   fileUploadValidationRules('Select your planning application form'),
