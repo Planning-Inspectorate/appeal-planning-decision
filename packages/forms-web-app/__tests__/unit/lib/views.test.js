425970d55a04012dcfbbf4631c7f6f8b7a0eb582
swdpcomputing <swdpcomputing@outlook.com>
2022-01-10 12:43:47 +0000

    feat(householder-planning): add householder listed building page

--- packages/forms-web-app/__tests__/unit/lib/views.test.js
+++ packages/forms-web-app/__tests__/unit/lib/views.test.js
@@ -68,10 +68,6 @@ describe('lib/views', () => {
         YOU_CANNOT_APPEAL: 'full-appeal/you-cannot-appeal',
       },
 
-      HOUSEHOLDER_PLANNING: {
-        LISTED_BUILDING: 'householder-planning/listed-building',
-      },
-
       YOUR_PLANNING_APPEAL: {
         INDEX: 'your-planning-appeal/index',
         YOUR_APPEAL_DETAILS: 'your-planning-appeal/your-appeal-details',
