2c5d0a39381de33e86f6355a047f1a2991e6e319
shilpabasu22 <shilpabasu22@gmail.com>
2022-01-10 17:40:22 +0000

    test: added E2E tests for decision date

--- packages/e2e-tests/cypress/support/eligibility/date-decision-due/enter-date-decision-due.js
+++ packages/e2e-tests/cypress/support/eligibility/date-decision-due/enter-date-decision-due.js
@@ -16,6 +16,4 @@ export const verifyHighlights = (highlights) => {
   highlightsList.forEach(input => {
     cy.get(`#decision-date-${input}`).should('have.class','govuk-input--error');
   });
-
-  cy.wait(Cypress.env('demoDelay'))
 }
