1b07d6a1b5fc625683e60ace98cdac7ef93be1c6
Ray Mitchell <ray@Rays-Air-2.broadband>
2022-01-14 10:56:41 +0000

    fix(as-4237): remove magic links

--- packages/magiclink-auth-service-api/__tests__/setup-jest.js
+++
@@ -1,6 +0,0 @@
-process.env.SERVER_PORT = '3009';
-process.env.MAGIC_LINK_TEMPLATE_ID = 'mockTemplateId';
-process.env.DATA_ENCRYPTION_KEY = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
-process.env.JWT_SIGNING_KEY = 'mockSigningKey';
-process.env.MAGIC_LINK_VALIDITY_TIME_MILLIS = '900000';
-process.env.COOKIE_VALIDITY_TIME_MILLIS = '28800000';
--- packages/magiclink-auth-service-api/__tests__/unit/routes/api-docs.test.js
+++
@@ -1,8 +0,0 @@
-const { use, serve } = require('./router-mock');
-require('../../../src/routes/api-docs');
-
-describe('routes/api-docs', () => {
-  it('should define the expected routes', () => {
-    expect(use).toHaveBeenCalledWith('/', serve);
-  });
-});
