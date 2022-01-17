ee5fb876779c54cc18e59046db8409f3f145dd92
Demzy <79277486+demzzydev@users.noreply.github.com>
2022-01-17 11:44:32 +0000

    refactor(as-4343): update pr with feedback

--- packages/forms-web-app/src/lib/full-appeal/map-planning-application.js
+++ packages/forms-web-app/src/lib/full-appeal/map-planning-application.js
@@ -1,12 +1,14 @@
+const { APPEAL_ID } = require('@pins/business-rules/src/constants');
+
 module.exports = (application) => {
   switch (application) {
     case 'full-appeal':
-      return '1005';
+      return APPEAL_ID.PLANNING_SECTION_78;
 
     case 'householder':
-      return '1001';
+      return APPEAL_ID.HOUSEHOLDER;
 
     default:
-      return '1001';
+      return undefined;
   }
 };
