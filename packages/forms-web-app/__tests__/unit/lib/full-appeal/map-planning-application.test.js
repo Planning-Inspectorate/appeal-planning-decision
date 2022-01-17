311d696f80ee5e8d58e5a036ea0b2d78a2b8ebe7
Demzy <79277486+demzzydev@users.noreply.github.com>
2022-01-17 13:21:12 +0000

    test(as-4343): add unit test

---
+++ packages/forms-web-app/__tests__/unit/lib/full-appeal/map-planning-application.test.js
@@ -0,0 +1,18 @@
+const { APPEAL_ID } = require('@pins/business-rules/src/constants');
+const mapping = require('../../../../src/lib/full-appeal/map-planning-application');
+
+describe('/lib/full-appeal/map-planning-application', () => {
+  it('should map full appeal planning application', () => {
+    const planningApplication = 'full-appeal';
+    const appealType = mapping(planningApplication);
+
+    expect(appealType).toEqual(APPEAL_ID.PLANNING_SECTION_78);
+  });
+
+  it('should map householder planning application', () => {
+    const planningApplication = 'householder';
+    const appealType = mapping(planningApplication);
+
+    expect(appealType).toEqual(APPEAL_ID.HOUSEHOLDER);
+  });
+});
