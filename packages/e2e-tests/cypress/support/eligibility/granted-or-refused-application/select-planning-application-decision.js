e5d7ab97fcdfc0bc8d236d378359aa8584904474
shilpabasu22 <shilpabasu22@gmail.com>
2022-01-18 12:18:12 +0000

    test: updated tests for decision status

--- packages/e2e-tests/cypress/support/eligibility/granted-or-refused-application/select-planning-application-decision.js
+++ packages/e2e-tests/cypress/support/eligibility/granted-or-refused-application/select-planning-application-decision.js
@@ -1,19 +1,19 @@
-import {
-  getGrantedRadio,
-  getRefusedRadio,
-  getNoDecisionReceivedRadio,
-} from '../page-objects/granted-or-refused-application-po';
+import {getGrantedRadio, getRefusedRadio, getNoDecisionReceivedRadio
+} from '../page-objects/granted-or-refused-application-po'
 
-export const selectPlanningApplicationDecision = (decision) => {
-  switch (decision) {
-    case 'Granted':
-      getGrantedRadio().check();
-      break;
-    case 'Refused':
-      getRefusedRadio().check();
-      break;
-    case 'I have not received a decision':
-      getNoDecisionReceivedRadio().check();
-      break;
-  }
-};
+export const selectPlanningApplicationDecision = (decision)=>{
+    switch(decision){
+        case 'Granted':
+            getGrantedRadio().check();
+            break;
+        case 'Refused':
+            getRefusedRadio().check();
+            break;
+        case 'I have Not Received a Decision':
+            getNoDecisionReceivedRadio().check();
+            break;
+        case 'I have not received a decision':
+            getNoDecisionReceivedRadio().check();
+            break;
+    }
+}
