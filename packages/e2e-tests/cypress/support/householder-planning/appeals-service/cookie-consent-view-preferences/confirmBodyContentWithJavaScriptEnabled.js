99a4709d15a893beddba832e02b0be0fc7b17e0b
Karuna Kanumuri <karuna.kanumuri@foundry4.com>
2022-01-20 14:09:42 +0000

    refactor: fix the failing cypress tests

--- packages/e2e-tests/cypress/support/householder-planning/appeals-service/cookie-consent-view-preferences/confirmBodyContentWithJavaScriptEnabled.js
+++ packages/e2e-tests/cypress/support/householder-planning/appeals-service/cookie-consent-view-preferences/confirmBodyContentWithJavaScriptEnabled.js
@@ -2,7 +2,7 @@ export const confirmBodyContentWithJavaScriptEnabled = () => {
   cy.get('[data-cy="cookies-without-js-content"]').should('not.be.visible');
 
   const expectedText =
-    'We use Google Analytics to measure how you use the appeal a householder planning decision service so we can improve it based on user needs.';
+    'We use Google Analytics to measure how you use the appeal a planning decision service so we can improve it based on user needs.';
 
   cy.get(`[data-cy="cookies-with-js-content"]`).should('be.visible').contains(expectedText);
 };
