b3d27c23d8659b121b4ee95c92fc76e9499df258
shepda <david@x-designs.co.uk>
2022-01-20 09:45:01 +0000

    feat(as-4375): update full appeals application form to remove validation for already uploaded files

--- packages/forms-web-app/__tests__/unit/controllers/full-appeal/submit-appeal/application-form.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/full-appeal/submit-appeal/application-form.test.js
@@ -9,7 +9,11 @@ const { createDocument } = require('../../../../../src/lib/documents-api-wrapper
 const { getTaskStatus } = require('../../../../../src/services/task.service');
 const TASK_STATUS = require('../../../../../src/services/task-status/task-statuses');
 const { mockReq, mockRes } = require('../../../mocks');
-const { VIEW } = require('../../../../../src/lib/full-appeal/views');
+const {
+  VIEW: {
+    FULL_APPEAL: { APPLICATION_FORM, APPLICATION_NUMBER },
+  },
+} = require('../../../../../src/lib/full-appeal/views');
 const file = require('../../../../fixtures/file-upload');
 
 jest.mock('../../../../../src/lib/appeals-api-wrapper');
@@ -21,8 +25,8 @@ describe('controllers/full-appeal/submit-appeal/application-form', () => {
   let res;
   let appeal;
 
-  const sectionName = 'requiredDocumentsSection';
-  const taskName = 'originalApplication';
+  const sectionName = 'planningApplicationDocumentsSection';
+  const taskName = documentTypes.originalApplication.name;
   const appealId = 'da368e66-de7b-44c4-a403-36e5bf5b000b';
   const errors = { 'file-upload': 'Select a file upload' };
   const errorSummary = [{ text: 'There was an error', href: '#' }];
@@ -43,6 +47,8 @@ describe('controllers/full-appeal/submit-appeal/application-form', () => {
       session: {
         appeal,
       },
+      sectionName,
+      taskName,
     };
     res = mockRes();
 
@@ -52,7 +58,7 @@ describe('controllers/full-appeal/submit-appeal/application-form', () => {
   describe('getApplicationForm', () => {
     it('should call the correct template', () => {
       getApplicationForm(req, res);
-      expect(res.render).toHaveBeenCalledWith(VIEW.FULL_APPEAL.APPLICATION_FORM, {
+      expect(res.render).toHaveBeenCalledWith(APPLICATION_FORM, {
         appealId,
         uploadedFile: file,
       });
@@ -75,30 +81,30 @@ describe('controllers/full-appeal/submit-appeal/application-form', () => {
       await postApplicationForm(req, res);
 
       expect(res.redirect).not.toHaveBeenCalled();
-      expect(res.render).toHaveBeenCalledWith(VIEW.FULL_APPEAL.APPLICATION_FORM, {
+      expect(res.render).toHaveBeenCalledWith(APPLICATION_FORM, {
         appealId,
         uploadedFile: file,
-        errors,
         errorSummary,
+        errors,
       });
     });
 
     it('should re-render the template with errors if an error is thrown', async () => {
       const error = new Error('Internal Server Error');
 
-      createDocument.mockImplementation(() => Promise.reject(error));
+      createOrUpdateAppeal.mockImplementation(() => Promise.reject(error));
 
       await postApplicationForm(req, res);
 
       expect(res.redirect).not.toHaveBeenCalled();
-      expect(res.render).toHaveBeenCalledWith(VIEW.FULL_APPEAL.APPLICATION_FORM, {
+      expect(res.render).toHaveBeenCalledWith(APPLICATION_FORM, {
         appealId,
         uploadedFile: file,
         errorSummary: [{ text: error.toString(), href: '#' }],
       });
     });
 
-    it('should redirect to the correct page if valid', async () => {
+    it('should redirect to the correct page if valid and a file is being uploaded', async () => {
       const submittedAppeal = {
         ...appeal,
         state: 'SUBMITTED',
@@ -118,15 +124,57 @@ describe('controllers/full-appeal/submit-appeal/application-form', () => {
 
       await postApplicationForm(req, res);
 
-      expect(createDocument).toHaveBeenCalledWith(
-        appeal,
-        file,
-        null,
-        documentTypes.originalApplication.name
-      );
+      expect(createDocument).toHaveBeenCalledWith(appeal, file, null, taskName);
       expect(getTaskStatus).toHaveBeenCalledWith(appeal, sectionName, taskName);
       expect(createOrUpdateAppeal).toHaveBeenCalledWith(appeal);
-      expect(res.redirect).toHaveBeenCalledWith(`/${VIEW.FULL_APPEAL.APPLICATION_NUMBER}`);
+      expect(res.redirect).toHaveBeenCalledWith(`/${APPLICATION_NUMBER}`);
+      expect(req.session.appeal).toEqual(submittedAppeal);
+    });
+
+    it('should redirect to the correct page if valid and a file is not being uploaded', async () => {
+      const submittedAppeal = {
+        ...appeal,
+        state: 'SUBMITTED',
+      };
+
+      getTaskStatus.mockReturnValue(TASK_STATUS.NOT_STARTED);
+      createOrUpdateAppeal.mockReturnValue(submittedAppeal);
+
+      await postApplicationForm(req, res);
+
+      expect(createDocument).not.toHaveBeenCalled();
+      expect(getTaskStatus).toHaveBeenCalledWith(appeal, sectionName, taskName);
+      expect(createOrUpdateAppeal).toHaveBeenCalledWith(appeal);
+      expect(res.redirect).toHaveBeenCalledWith(`/${APPLICATION_NUMBER}`);
+      expect(req.session.appeal).toEqual(submittedAppeal);
+    });
+
+    it('should redirect to the correct page if valid if appeal.planningApplicationDocumentsSection.originalApplication does not exist', async () => {
+      delete appeal.planningApplicationDocumentsSection.originalApplication;
+
+      const submittedAppeal = {
+        ...appeal,
+        state: 'SUBMITTED',
+      };
+
+      createDocument.mockReturnValue(file);
+      getTaskStatus.mockReturnValue(TASK_STATUS.NOT_STARTED);
+      createOrUpdateAppeal.mockReturnValue(submittedAppeal);
+
+      req = {
+        ...req,
+        body: {},
+        files: {
+          'file-upload': file,
+        },
+      };
+
+      await postApplicationForm(req, res);
+
+      expect(createDocument).toHaveBeenCalledWith(appeal, file, null, taskName);
+      expect(getTaskStatus).toHaveBeenCalledWith(appeal, sectionName, taskName);
+      expect(createOrUpdateAppeal).toHaveBeenCalledWith(appeal);
+      expect(res.redirect).toHaveBeenCalledWith(`/${APPLICATION_NUMBER}`);
       expect(req.session.appeal).toEqual(submittedAppeal);
     });
   });
