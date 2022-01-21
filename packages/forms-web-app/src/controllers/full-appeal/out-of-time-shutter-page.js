200b4b6b1bdf060ac9a100ace408fea216cc29bc
Demzy <79277486+demzzydev@users.noreply.github.com>
2022-01-21 11:53:10 +0000

    fix(as-4325): add formatting for the appeal deadline

--- packages/forms-web-app/src/controllers/full-appeal/out-of-time-shutter-page.js
+++ packages/forms-web-app/src/controllers/full-appeal/out-of-time-shutter-page.js
@@ -1,11 +1,14 @@
+const { format } = require('date-fns');
 const { VIEW } = require('../../lib/views');
 
 const getOutOfTimeShutterPage = async (req, res) => {
   const { appeal } = req.session;
+
   const appealDeadline = appeal?.eligibility?.appealDeadline;
+  const date = new Date(appealDeadline);
 
   return res.render(VIEW.OUT_OF_TIME_SHUTTER_PAGE, {
-    appealDeadline,
+    appealDeadline: format(date, 'dd MMMM yyyy'),
   });
 };
 
