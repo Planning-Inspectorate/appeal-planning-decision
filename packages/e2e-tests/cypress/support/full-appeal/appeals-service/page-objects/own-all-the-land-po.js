89d75b349d9ab0a976563f761fc2b30730ce6142
Karuna Kanumuri <karuna.kanumuri@foundry4.com>
2022-01-24 14:02:20 +0000

    feat: new feature to add e2e tests for own all the land

---
+++ packages/e2e-tests/cypress/support/full-appeal/appeals-service/page-objects/own-all-the-land-po.js
@@ -0,0 +1,4 @@
+export const ownAllLandYes = () => cy.get('[data-cy="answer-yes"]');
+export const ownAllLandNo = () => cy.get('[data-cy="answer-no"]');
+export const errorMessageOwnAllLand = () => cy.get('#own-all-the-land-error');
+
--- packages/e2e-tests/cypress/support/full-appeal/appeals-service/page-objects/task-list-page-po.js
+++ packages/e2e-tests/cypress/support/full-appeal/appeals-service/page-objects/task-list-page-po.js
@@ -1,7 +1,10 @@
 export const applicationStatus = () => cy.get('[data-cy=application-status]');
 export const applicationStatusDetailed = () => cy.get('[data-cy=application-status-detailed]');
 export const contactDetailsLink = () => cy.get('[data-cy=contactDetailsSection]');
-export const planningApplicationDocumentsLink = () => cy.get('[data-cy="planningApplicationDocumentsSection"]');
-export const appealDocumentsSectionLink = () => cy.get('[data-cy="appealDocumentsSection"]');
+export const aboutAppealSiteSectionLink = () => cy.get('[data-cy=aboutAppealSiteSection]')
+export const planningApplicationDocumentsLink = () => cy.get('[data-cy=planningApplicationDocumentsSection]');
+export const appealDocumentsSectionLink = () => cy.get('[data-cy=appealDocumentsSection]');
+export const pageCaptionText = () => cy.get('.govuk-caption-l');
+
 
 
