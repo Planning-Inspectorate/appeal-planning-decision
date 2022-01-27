b8eb3c840e366c6a37044ad1a6b0ca5324c618d9
Demzy <79277486+demzzydev@users.noreply.github.com>
2022-01-27 14:56:18 +0000

    test(as-4338): update unit test

--- packages/clamav-rest-server/__tests__/unit/lib/clamd.test.js
+++ packages/clamav-rest-server/__tests__/unit/lib/clamd.test.js
@@ -1,37 +1,22 @@
-jest.mock('../../../src/lib/clamd');
-
-const fs = require('fs');
 const clamd = require('../../../src/lib/clamd');
 
 describe('lib/clamd', () => {
   beforeEach(() => {});
 
-  it('should send invalid file', async (done) => {
+  it('should send invalid file', async () => {
     await expect(() => clamd.sendFile(undefined).rejects.toThrowError('invalid or empty file'));
-    done();
   });
 
-  it("should send variable that's not a buffer", async (done) => {
+  it("should send variable that's not a buffer", async () => {
     await expect(() =>
       clamd.sendFile('hello').rejects.toThrowError('invalid file type, requires a buffer')
     );
-
-    done();
   });
 
-  it('should send valid file', async () => {
-    const fileBuffer = fs.readFileSync('__tests__/fixtures/eicar.com.txt');
-    jest.spyOn(clamd, 'createClient');
-
-    clamd.createClient.mockResolvedValue(() => ({
-      scanStream: jest.fn(),
-    }));
-
-    clamd.sendFile.mockResolvedValue({
-      isInfected: true,
-    });
+  it('should send valid file with valid input', async () => {
+    const fileBuffer = Buffer.from([]);
 
     const result = await clamd.sendFile(fileBuffer);
-    expect(result.isInfected).toEqual(true);
+    expect(result.isInfected).toBeDefined();
   });
 });
