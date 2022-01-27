91f95959f0ca0b6f4cd30dfbcdc77464025420c7
Chris Cundill <chrisc@diligentsoft.co>
2022-01-27 09:38:11 +0000

    fix(amqp-interaction): isolated different queues into separate rhea amqp containers

--- packages/appeals-service-api/src/lib/queue.js
+++ packages/appeals-service-api/src/lib/queue.js
@@ -1,46 +1,47 @@
-const container = require('rhea');
-const containerSQL = require('rhea');
+const rhea = require('rhea');
 const config = require('./config');
 const logger = require('./logger');
 
 const options = config.messageQueue.horizonHASPublisher.connection;
+const horizonContainer = rhea.create_container({ id: 'horizon-appeal-publish-container' });
+const sqlContainer = rhea.create_container({ id: 'sql-appeal-publish-container-1' });
 
 function addAppeal(message) {
-  container.connect(options).open_sender(config.messageQueue.horizonHASPublisher.queue);
+  horizonContainer.connect(options).open_sender(config.messageQueue.horizonHASPublisher.queue);
 
-  container.once('sendable', (context) => {
+  horizonContainer.once('sendable', (context) => {
     context.sender.send({
-      body: container.message.data_section(Buffer.from(JSON.stringify(message), 'utf-8')),
+      body: horizonContainer.message.data_section(Buffer.from(JSON.stringify(message), 'utf-8')),
       content_type: 'application/json',
     });
     logger.info({ message }, 'Appeal message placed on queue');
   });
 
-  container.on('accepted', (context) => {
+  horizonContainer.on('accepted', (context) => {
     context.connection.close();
     logger.info(`Queue closed on message accepted`);
   });
 
-  container.on('error', (err) => {
+  horizonContainer.on('error', (err) => {
     logger.error({ err }, 'There was a problem with the queue');
   });
 
-  containerSQL.connect(options).open_sender(config.messageQueue.sqlHASAppealsPublisher.queue);
+  sqlContainer.connect(options).open_sender(config.messageQueue.sqlHASAppealsPublisher.queue);
 
-  containerSQL.once('sendable', (context) => {
+  sqlContainer.once('sendable', (context) => {
     context.sender.send({
-      body: container.message.data_section(Buffer.from(JSON.stringify(message), 'utf-8')),
+      body: sqlContainer.message.data_section(Buffer.from(JSON.stringify(message), 'utf-8')),
       content_type: 'application/json',
     });
     logger.info({ message }, 'Appeal message placed on SQL queue');
   });
 
-  containerSQL.on('accepted', (context) => {
+  sqlContainer.on('accepted', (context) => {
     context.connection.close();
     logger.info(`SQL ueue closed on message accepted`);
   });
 
-  containerSQL.on('error', (err) => {
+  sqlContainer.on('error', (err) => {
     logger.error({ err }, 'There was a problem with the SQL queue');
   });
 }
--- packages/appeals-service-api/src/lib/sql-appeals-queue.js
+++ packages/appeals-service-api/src/lib/sql-appeals-queue.js
@@ -1,28 +1,29 @@
-const container = require('rhea');
+const rhea = require('rhea');
 const config = require('./config');
 const logger = require('./logger');
 
 const options = config.messageQueue.sqlHASAppealsPublisher.connection;
+const sqlContainer = rhea.create_container({ id: 'sql-appeal-publish-container-2' });
 
 function addAppeal(message) {
-  container.connect(options).open_sender(config.messageQueue.sqlHASAppealsPublisher.queue);
+  sqlContainer.connect(options).open_sender(config.messageQueue.sqlHASAppealsPublisher.queue);
 
   logger.info({ message }, 'SQL HAS queue initialisation');
 
-  container.once('sendable', (context) => {
+  sqlContainer.once('sendable', (context) => {
     context.sender.send({
-      body: container.message.data_section(Buffer.from(JSON.stringify(message), 'utf-8')),
+      body: sqlContainer.message.data_section(Buffer.from(JSON.stringify(message), 'utf-8')),
       content_type: 'application/json',
     });
     logger.info({ message }, 'Appeal message placed on SQL HAS queue');
   });
 
-  container.on('accepted', (context) => {
+  sqlContainer.on('accepted', (context) => {
     context.connection.close();
     logger.info(`SQL HAS Queue closed on message accepted`);
   });
 
-  container.on('error', (err) => {
+  sqlContainer.on('error', (err) => {
     logger.error({ err }, 'There was a problem with the SQL HAS queue');
   });
 }
