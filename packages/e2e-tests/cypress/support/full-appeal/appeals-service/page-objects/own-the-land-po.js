bde55be674456b5a8379e5934e33ce6b81e3af04
Karuna Kanumuri <karuna.kanumuri@foundry4.com>
2022-01-27 14:34:24 +0000

    feat: add cypress tests for tenant of agricultural holding page

--- packages/e2e-tests/cypress/support/full-appeal/appeals-service/page-objects/own-the-land-po.js
+++ packages/e2e-tests/cypress/support/full-appeal/appeals-service/page-objects/own-the-land-po.js
@@ -6,4 +6,5 @@ export const errorMessageOwnSomeLand = () => cy.get('#own-some-of-the-land-error
 export const errorMessageKnowTheOwners = () => cy.get('#know-the-owners-error');
 export const errorMessageAgriculturalHolding = () => cy.get('#agricultural-holding-error');
 export const hintTextAgriculturalHolding = () => cy.get('#agricultural-holding-hint');
+export const errorMessageAreYouATenant = () => cy.get('#are-you-a-tenant-error');
 
