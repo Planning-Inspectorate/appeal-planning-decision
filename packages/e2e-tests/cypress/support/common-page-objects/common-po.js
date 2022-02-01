c0cac1781010873f783954bd2a1bf98f4e094b25
Karuna Kanumuri <karuna.kanumuri@foundry4.com>
2022-02-01 18:30:28 +0000

    feat: cypress tests for Declaration and Submission pages

--- packages/e2e-tests/cypress/support/common-page-objects/common-po.js
+++ packages/e2e-tests/cypress/support/common-page-objects/common-po.js
@@ -9,3 +9,8 @@ export const continueButton = () => cy.get('[data-cy=button-continue]');
 export const getAppealDetailsSidebar = () => cy.get('table[data-cy="appealDetails"]');
 export const getFileUploadButton = () => cy.get('#file-upload');
 export const getSaveAndContinueButton = () => cy.get('[data-cy="button-save-and-continue"]');
+export const confirmAndSubmitAppealButton = () => cy.get('[data-cy=accept-and-send]');
+export const privacyNoticeLink = () => cy.findAllByText('Privacy Notice').should('have.attr','href');
+export const termAndConditionsLink = () => cy.findAllByText('Terms and Conditions').should('have.attr','href');
+export const declarationWarningText = () => cy.get('.govuk-warning-text__text');
+export const appealSubmittedHeading = () => cy.get('.govuk-panel__title');
