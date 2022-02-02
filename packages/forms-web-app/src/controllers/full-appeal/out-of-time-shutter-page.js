54c0e1c52ba34e0304e193f3ada5cd3a76cdf976
Demzy <79277486+demzzydev@users.noreply.github.com>
2022-02-02 15:27:55 +0000

    feat(as-4325): update pr based on comments

--- packages/forms-web-app/src/controllers/full-appeal/out-of-time-shutter-page.js
+++ packages/forms-web-app/src/controllers/full-appeal/out-of-time-shutter-page.js
@@ -1,14 +1,11 @@
-const { format } = require('date-fns');
 const { VIEW } = require('../../lib/views');
 
 const getOutOfTimeShutterPage = async (req, res) => {
   const { appeal } = req.session;
   const { appealDeadline, appealPeriod } = appeal?.eligibility;
 
-  const date = new Date(appealDeadline);
-
   return res.render(VIEW.OUT_OF_TIME_SHUTTER_PAGE, {
-    appealDeadline: format(date, 'dd MMMM yyyy'),
+    appealDeadline,
     appealPeriod,
   });
 };
