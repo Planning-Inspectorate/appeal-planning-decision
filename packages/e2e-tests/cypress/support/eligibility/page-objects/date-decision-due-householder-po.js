4823916c74a09bf3cdd503eb0ed6349218318179
shepda <david@x-designs.co.uk>
2022-02-11 12:31:28 +0000

    feat(as-4390): update decision date field ids in the e2e tests

--- packages/e2e-tests/cypress/support/eligibility/page-objects/date-decision-due-householder-po.js
+++ packages/e2e-tests/cypress/support/eligibility/page-objects/date-decision-due-householder-po.js
@@ -2,6 +2,6 @@
 // export const getDateDecisionDueHouseholderMonth = () => cy.get('#date-decision-due-householder-month');
 // export const getDateDecisionDueHouseholderYear = () => cy.get('#date-decision-due-householder-year');
 export const getPlanningApplicationDecisionHouseholderError = () => cy.get('#date-decision-due-householder-error');
-export const getDateDecisionDueHouseholderDay = () => cy.get('#decision-date-householder-day');
-export const getDateDecisionDueHouseholderMonth = () => cy.get('#decision-date-householder-month');
-export const getDateDecisionDueHouseholderYear = () => cy.get('#decision-date-householder-year');
+export const getDateDecisionDueHouseholderDay = () => cy.get('#date-decision-due-householder-day');
+export const getDateDecisionDueHouseholderMonth = () => cy.get('#date-decision-due-householder-month');
+export const getDateDecisionDueHouseholderYear = () => cy.get('#date-decision-due-householder-year');
