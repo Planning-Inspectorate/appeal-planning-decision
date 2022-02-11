bb91cae28519791371f4e3e94bf48280a817a4b7
shepda <david@x-designs.co.uk>
2022-02-11 21:11:42 +0000

    feat(as-4390): update full appeal e2e tests

--- packages/e2e-tests/cypress/support/eligibility/page-objects/date-decision-due-householder-po.js
+++ packages/e2e-tests/cypress/support/eligibility/page-objects/date-decision-due-householder-po.js
@@ -1,6 +1,6 @@
-// export const getDateDecisionDueHouseholderDay = () => cy.get('#date-decision-due-householder-day');
-// export const getDateDecisionDueHouseholderMonth = () => cy.get('#date-decision-due-householder-month');
-// export const getDateDecisionDueHouseholderYear = () => cy.get('#date-decision-due-householder-year');
+export const getDecisionDateHouseholderDay = () => cy.get('#decision-date-householder-day');
+export const getDecisionDateHouseholderMonth = () => cy.get('#decision-date-householder-month');
+export const getDecisionDateHouseholderYear = () => cy.get('#decision-date-householder-year');
 export const getPlanningApplicationDecisionHouseholderError = () => cy.get('#date-decision-due-householder-error');
 export const getDateDecisionDueHouseholderDay = () => cy.get('#date-decision-due-householder-day');
 export const getDateDecisionDueHouseholderMonth = () => cy.get('#date-decision-due-householder-month');
