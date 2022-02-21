84794f05ab04382b299d19ab20ee2aebdf0dc546
Karuna Kanumuri <karuna.kanumuri@foundry4.com>
2022-02-21 19:41:58 +0000

    feat: add new section decide your appeal to task list

--- packages/e2e-tests/cypress/integration/full-appeal/appeals-service/task-list/appeal-form-task-list.feature
+++ packages/e2e-tests/cypress/integration/full-appeal/appeals-service/task-list/appeal-form-task-list.feature
@@ -1,11 +1,18 @@
-@wip
 Feature: As an Appellant I want, at a glance, to see what information
          I need to submit and once started to see my progress
          So that the Planning Inspectorate has what it needs to consider my appeal
 
+  Background:
+    Given appellant has completed full appeal eligibility journey
+
   @smoke
-  Scenario: Appellant has been successful through the Eligibility and they are now able to continue to start their appeal
+  Scenario: 1 - Appellant has been successful through the Eligibility and they are now able to continue to start their appeal
     Given Appellant has been successful on their eligibility
     When they are on the 'Appeal a Planning Decision' page
     Then they are presented with the list of tasks that they are required to complete in order to submit their appeal
-    And when a section has been completed they are able to see what has been completed or incompleted
+   # And when a section has been completed they are able to see what has been completed or incompleted
+
+  Scenario: 2 - Appellant or Agent is on the Appeal a Planning Decision Task List
+    Given that user is on the Appeal a Planning Decision task list
+    When the user click on 'Tell us how you would prefer us to decide your appeal'
+    Then the user is taken to the 'How would you prefer us to decide your appeal? page
--- packages/e2e-tests/cypress/integration/full-appeal/appeals-service/task-list/appeal-form-task-list/appeal-form-task-list.js
+++ packages/e2e-tests/cypress/integration/full-appeal/appeals-service/task-list/appeal-form-task-list/appeal-form-task-list.js
@@ -10,40 +10,50 @@ import {
   statusProvideYourContactDetails,
   statusTellAboutTheAppealSite,
   statusUploadDocsFromPlanningApplication,
-  statusUploadDocsForYourAppeal, statusCheckYourAnswers,
+  statusUploadDocsForYourAppeal, statusCheckYourAnswers, linkDecideYourAppeal,
 } from '../../../../../support/full-appeal/appeals-service/page-objects/appeal-form-task-list-po';
 import { verifyPageTitle } from '../../../../../support/common/verify-page-title';
-import { goToAppealsPage } from '../../../../../support/common/go-to-page/goToAppealsPage';
-import { acceptCookiesBanner } from '../../../../../support/common/accept-cookies-banner';
 import { goToFullAppealSubmitAppealTaskList } from '../../../../../support/full-appeal/appeals-service/goToFullAppealSubmitAppealTaskList';
 import { verifyPageHeading } from '../../../../../support/common/verify-page-heading';
 
 const pageHeading = 'Appeal a planning decision';
 const url = 'full-appeal/submit-appeal/task-list';
+const decideYourAppealUrl = 'full-appeal/submit-appeal/how-decide-appeal';
 const pageTitle = 'Appeal a planning decision - Appeal a planning decision - GOV.UK';
 
 Given('Appellant has been successful on their eligibility',()=> {
-  goToFullAppealSubmitAppealTaskList('before-you-start/local-planning-depart','Full planning');
+  cy.url().should('contain', url);
 })
 When("they are on the 'Appeal a Planning Decision' page",()=> {
   cy.url().should('contain', url);
   verifyPageTitle(pageTitle);
   verifyPageHeading(pageHeading);
-})
+});
 Then('they are presented with the list of tasks that they are required to complete in order to submit their appeal',()=> {
   linkProvideYourContactDetails().should('exist');
   statusProvideYourContactDetails().should('exist');
   linkTellAboutTheAppealSite().should('exist');
   statusTellAboutTheAppealSite().should('exist');
+  linkDecideYourAppeal().should('exist');
   linkUploadDocsFromPlanningApplication().should('exist');
   statusUploadDocsFromPlanningApplication().should('exist');
   linkUploadDocsForYourAppeal().should('exist');
-  statusUploadDocsForYourAppeal().should('exist');
+  // the below step needs tp be executed when the status is displayed
+  //statusUploadDocsForYourAppeal().should('exist');
   linkCheckYourAnswers().should('exist');
   statusCheckYourAnswers().should('exist');
-})
+});
 Then('when a section has been completed they are able to see what has been completed or incompleted',()=> {
   applicationStatusText().should('exist');
   applicationStatusDetailedText().should('exist');
-})
+});
+Given("that user is on the Appeal a Planning Decision task list", () => {
+  cy.url().should('contain', 'full-appeal/submit-appeal/task-list');
+});
+When("the user click on 'Tell us how you would prefer us to decide your appeal'", () => {
+  linkDecideYourAppeal().click();
+});
+Then("the user is taken to the 'How would you prefer us to decide your appeal? page", () => {
+  cy.url().should('include', decideYourAppealUrl);
+});
 
