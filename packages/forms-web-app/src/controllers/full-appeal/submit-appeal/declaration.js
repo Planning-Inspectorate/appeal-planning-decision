bf59ec8561b5dd096a5eec9e930ff49809cdb673
Birol <softnutuk@gmail.com>
2022-02-23 17:53:44 +0000

    fix(full appeal): dummy data removed from declaration.js

--- packages/forms-web-app/src/controllers/full-appeal/submit-appeal/declaration.js
+++ packages/forms-web-app/src/controllers/full-appeal/submit-appeal/declaration.js
@@ -1,4 +1,3 @@
-const fullAppeal = require('@pins/business-rules/test/data/full-appeal');
 const uuid = require('uuid');
 const { storePdfAppeal } = require('../../../services/pdf.service');
 const { VIEW } = require('../../../lib/full-appeal/views');
@@ -10,34 +9,10 @@ const {
 } = VIEW;
 
 exports.getDeclaration = (req, res) => {
-  req.session.appeal.appealType = fullAppeal.appealType;
   res.render(currentPage);
 };
 
 exports.postDeclaration = async (req, res) => {
-  const {
-    appealType,
-    lpaCode,
-    decisionDate,
-    eligibility,
-    contactDetailsSection,
-    appealSiteSection,
-    appealDecisionSection,
-    planningApplicationDocumentsSection,
-    appealDocumentsSection,
-    sectionStates,
-  } = fullAppeal;
-  // req.session.appeal.appealType = appealType;
-  req.session.appeal.lpaCode = lpaCode;
-  // req.session.appeal.decisionDate = decisionDate;
-  // req.session.appeal.eligibility = eligibility;
-  // req.session.appeal.contactDetailsSection = contactDetailsSection;
-  // req.session.appeal.appealSiteSection = appealSiteSection;
-  // req.session.appeal.appealDecisionSection = appealDecisionSection;
-  // req.session.appeal.planningApplicationDocumentsSection = planningApplicationDocumentsSection;
-  // req.session.appeal.appealDocumentsSection = appealDocumentsSection;
-  // req.session.appeal.sectionStates = sectionStates;
-
   const { body } = req;
   const { errors = {} } = body;
   const { appeal } = req.session;
