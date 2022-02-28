f75b80874ab000a9f826264a31553093e47a0f03
Ray Mitchell <ray@Rays-Air-2.broadband>
2022-02-28 22:05:31 +0000

    fix(as-2424): cypress issues

--- packages/e2e-tests/reporter.js
+++ packages/e2e-tests/reporter.js
@@ -1,10 +1,14 @@
 const report = require('multiple-cucumber-html-reporter');
 const fs = require('fs');
 
-if (fs.readdirSync('./cypress/cucumber-json').length > 0) {
-  report.generate({
-    ignoreBadJsonFile: true,
-    jsonDir: './cypress/cucumber-json',
-    reportPath: './cypress/cucumber-report'
-  });
+if (fs.existsSync('./cypress')) {
+  if (fs.existsSync('./cypress/cucumber-json')) {
+    if (fs.readdirSync('./cypress/cucumber-json').length > 0) {
+      report.generate({
+        ignoreBadJsonFile: true,
+        jsonDir: './cypress/cucumber-json',
+        reportPath: './cypress/cucumber-report'
+      });
+    }
+  }
 }
