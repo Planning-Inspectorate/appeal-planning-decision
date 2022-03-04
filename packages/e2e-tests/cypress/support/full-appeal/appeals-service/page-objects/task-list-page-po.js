f41a32668a1dfbac7dc5e0b275c74d4ee9a44e33
Birol <softnutuk@gmail.com>
2022-03-04 15:09:20 +0000

    fix(task-list): aboutAppealSiteSection to appealSiteSection in e2e tests

--- packages/e2e-tests/cypress/support/full-appeal/appeals-service/page-objects/task-list-page-po.js
+++ packages/e2e-tests/cypress/support/full-appeal/appeals-service/page-objects/task-list-page-po.js
@@ -1,7 +1,7 @@
 export const applicationStatus = () => cy.get('[data-cy=application-status]');
 export const applicationStatusDetailed = () => cy.get('[data-cy=application-status-detailed]');
 export const contactDetailsLink = () => cy.get('[data-cy=contactDetailsSection]');
-export const aboutAppealSiteSectionLink = () => cy.get('[data-cy=aboutAppealSiteSection]')
+export const aboutAppealSiteSectionLink = () => cy.get('[data-cy=appealSiteSection]')
 export const planningApplicationDocumentsLink = () => cy.get('[data-cy=planningApplicationDocumentsSection]');
 export const appealDocumentsSectionLink = () => cy.get('[data-cy=appealDocumentsSection]');
 export const checkYourAnswersLink = () => cy.get('[data-cy=submitYourAppealSection]');
