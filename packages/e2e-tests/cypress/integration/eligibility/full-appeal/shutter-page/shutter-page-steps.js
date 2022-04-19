42634261c7c1d553b4dee139fbe380fc7bd7ced6
Ed Packard <edward.packard@kinandcarta.com>
2022-04-19 11:44:49 +0100

    test(update e2e test): add new page object for lpd shutter page acp link

--- packages/e2e-tests/cypress/integration/eligibility/full-appeal/shutter-page/shutter-page-steps.js
+++ packages/e2e-tests/cypress/integration/eligibility/full-appeal/shutter-page/shutter-page-steps.js
@@ -1,7 +1,7 @@
 import {Given} from 'cypress-cucumber-preprocessor/steps';
 import { verifyPageTitle } from '../../../../support/common/verify-page-title';
 import { verifyPageHeading } from '../../../../support/common/verify-page-heading';
-import { getACPLink } from '../../../../support/eligibility/page-objects/shutter-page-po';
+import { getACPLink } from '../../../../support/eligibility/page-objects/lpd-shutter-page-po';
 import { goToAppealsPage } from '../../../../support/common/go-to-page/goToAppealsPage';
 import { getLocalPlanningDepart } from '../../../../support/eligibility/page-objects/local-planning-department-po';
 import { getSaveAndContinueButton } from '../../../../support/common-page-objects/common-po';
