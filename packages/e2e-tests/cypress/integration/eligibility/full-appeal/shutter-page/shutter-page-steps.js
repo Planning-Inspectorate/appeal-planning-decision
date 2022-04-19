8740c393a8f3fa96773fd55e1ea2e6c7850270cb
Ed Packard <edward.packard@kinandcarta.com>
2022-04-19 14:38:14 +0100

    test(amend e2e test): update e2e test to reflect feature amendments

--- packages/e2e-tests/cypress/integration/eligibility/full-appeal/shutter-page/shutter-page-steps.js
+++ packages/e2e-tests/cypress/integration/eligibility/full-appeal/shutter-page/shutter-page-steps.js
@@ -1,27 +1,27 @@
 import {Given} from 'cypress-cucumber-preprocessor/steps';
 import { verifyPageTitle } from '../../../../support/common/verify-page-title';
 import { verifyPageHeading } from '../../../../support/common/verify-page-heading';
-import { getACPLink } from '../../../../support/eligibility/page-objects/shutter-page-po';
+// import { getACPLink } from '../../../../support/eligibility/page-objects/shutter-page-po';
 import { goToAppealsPage } from '../../../../support/common/go-to-page/goToAppealsPage';
 import { getLocalPlanningDepart } from '../../../../support/eligibility/page-objects/local-planning-department-po';
 import { getSaveAndContinueButton } from '../../../../support/common-page-objects/common-po';
 import {acceptCookiesBanner} from "../../../../support/common/accept-cookies-banner";
-const url = 'before-you-start/use-a-different-service';
-const pageHeading = 'You need to use a different service';
-const pageTitle = 'You need to use a different service - Before you start - Appeal a planning decision - GOV.UK';
-Given('an appellant is on the shutter page',()=>{
+const url = 'before-you-start/use-existing-service-local-planning-department';
+const pageHeading = 'You need to use the existing service';
+const pageTitle =
+  'You need to use the existing service - Before you start - Appeal a planning decision - GOV.UK';
+Given('an appellant is on the shutter page', () => {
   goToAppealsPage('before-you-start/local-planning-depart');
   acceptCookiesBanner();
   getLocalPlanningDepart().select('Amber Valley');
   getSaveAndContinueButton().click();
   cy.url().should('contain', url);
 });
-Given('an appellant is presented with a link to use a different service',()=>{
-getACPLink().should('be.visible')
-  .should('have.attr','href');
+Given('an appellant is presented with a link to use a different service', () => {
+  cy.get('button').should('be.visible').should('have.attr', 'href');
 });
-When('an appellant clicks on the you can appeal using our appeals casework portal link',()=>{
-  getACPLink().click();
+When('an appellant clicks on the you can appeal using our appeals casework portal link', () => {
+  cy.get('button').click();
 });
 
 Then('an appellant is able to navigate to casework appeal portal',()=>{
