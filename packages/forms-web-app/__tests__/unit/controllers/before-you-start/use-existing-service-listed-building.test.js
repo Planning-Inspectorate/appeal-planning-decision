4a799e62ab16c65bc1159ac1661a94001eff35a4
Prabu Manickam <prabu.manickam@planninginspectorate.gov.uk>
2022-04-21 01:16:38 +0100

    feat(comment): added shetter page
    added existing service

---
+++ packages/forms-web-app/__tests__/unit/controllers/before-you-start/use-existing-service-listed-building.test.js
@@ -0,0 +1,16 @@
+const useExistingServiceController = require('../../../../src/controllers/before-you-start/use-existing-service-listed-building');
+const { VIEW } = require('../../../../src/lib/views');
+const { mockReq, mockRes } = require('../../mocks');
+
+const req = mockReq();
+const res = mockRes();
+
+describe('controllers/before-you-start/use-existing-service-listed-building', () => {
+  describe('getNoDecision', () => {
+    it('should call the correct template', () => {
+      useExistingServiceController.getAppealStatement(req, res);
+
+      expect(res.render).toHaveBeenCalledWith(VIEW.BEFORE_YOU_START.USE_EXISTING_SERVICE_LISTED_BUILDING);
+    });
+  });
+});
