365dc5e2798f74aeff6b4dfb05f95ff230c8550c
Ed Packard <edward.packard@kinandcarta.com>
2022-04-21 12:49:40 +0100

    feat(bys first page): add first page for before-you-start route

---
+++ packages/forms-web-app/src/routes/before-you-start.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const beforeYouStartController = require('../controllers/before-you-start');
+
+const router = express.Router();
+
+router.get('/', beforeYouStartController.getBeforeYouStartFirstPage);
+
+module.exports = router;
