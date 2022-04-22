aff4c585382367b74249246aac10970c9db58e50
Guilherme Ribeiro <Gui>
2022-04-22 16:38:31 +0100

    fix: updated url name from another PR

--- packages/forms-web-app/src/controllers/before-you-start.js
+++ packages/forms-web-app/src/controllers/before-you-start.js
@@ -2,6 +2,6 @@ const { VIEW } = require('../lib/views');
 
 exports.getBeforeYouStartFirstPage = async (_, res) => {
   res.render(VIEW.BEFORE_YOU_START.FIRST_PAGE, {
-    nextPage: '/before-you-start/local-planning-depart',
+    nextPage: '/before-you-start/local-planning-department',
   });
 };
