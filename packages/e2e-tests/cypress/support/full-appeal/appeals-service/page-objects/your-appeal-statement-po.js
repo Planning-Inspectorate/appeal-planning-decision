76a37ea3aae4694ff7f4512ec60859e8bcb8ca3a
Ed Packard <edward.packard@kinandcarta.com>
2022-04-28 11:26:05 +0100

    fix(as-4736): amend sensitive information wording

--- packages/e2e-tests/cypress/support/full-appeal/appeals-service/page-objects/your-appeal-statement-po.js
+++ packages/e2e-tests/cypress/support/full-appeal/appeals-service/page-objects/your-appeal-statement-po.js
@@ -1,9 +1,7 @@
-export const sensitiveInfoLabel = () => cy.findAllByText('What is sensitive information?');
+export const sensitiveInfoLabel = () =>
+  cy.findAllByText('See a full list of what counts as sensitive information');
 export const filesYouCanUpload = () => cy.findAllByText('Files you can upload');
 export const checkboxConfirmSensitiveInfo = () => cy.get('#does-not-include-sensitive-information');
 export const sensitiveInfoText = () => cy.get('.govuk-details__text');
 export const appealStatementBodyText = () => cy.get('.govuk-body');
 export const checkboxErrorMessage = () => cy.get('#does-not-include-sensitive-information-error');
-
-
-
