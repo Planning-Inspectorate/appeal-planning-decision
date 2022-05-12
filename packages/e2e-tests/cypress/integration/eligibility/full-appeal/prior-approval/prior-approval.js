3b444f6f352ec9a9d8f9cd80f095da02ea737f4e
Constantine Akritidis <constantine.akritidis@planninginspectorate.gov.uk>
2022-05-12 11:43:07 +0100

    test(fixed failing e2e tests): aS-4833 fixed failing e2e tests regarding the correct CYA page

--- packages/e2e-tests/cypress/integration/eligibility/full-appeal/prior-approval/prior-approval.js
+++ packages/e2e-tests/cypress/integration/eligibility/full-appeal/prior-approval/prior-approval.js
@@ -119,7 +119,7 @@ Then('appellant is navigated to Appeal a householder planning decision', () => {
   getSaveAndContinueButton().click();
   cy.url().should('contain', 'before-you-start/can-use-service');
   getSaveAndContinueButton().click();
-  cy.url().should('contain', 'full-appeal/submit-appeal/task-list');
+  cy.url().should('contain', 'appellant-submission/task-list');
 });
 
 Then('appellant is navigated to Appeal a planning decision for {string}', (decision) => {
@@ -147,7 +147,7 @@ Then('appellant is navigated to Appeal a planning decision for {string}', (decis
   getSaveAndContinueButton().click();
   cy.url().should('contain', 'before-you-start/can-use-service');
   getSaveAndContinueButton().click();
-  cy.url().should('contain', 'full-appeal/submit-appeal/task-list');
+  cy.url().should('contain', 'appellant-submission/task-list');
 });
 
 Then(
