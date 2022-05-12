d4241fd00846c0df0d40c13dc230372bcc617a83
Constantine Akritidis <constantine.akritidis@planninginspectorate.gov.uk>
2022-05-12 12:09:30 +0100

    test(as-4832 (fixed failing unit test)): as-4832 (fixed failing unit test)

--- packages/forms-web-app/__tests__/unit/controllers/before-you-start/can-use-service.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/before-you-start/can-use-service.test.js
@@ -31,35 +31,79 @@ describe('controllers/before-you-start/can-use-service', () => {
     getDepartmentFromId.mockImplementation(() => Promise.resolve({ name: 'Bradford' }));
   });
 
-  it('Test getCanUseService method calls the HP Check Your Answers page when typeOfPlanningApplication is householder-planning', async () => {
-    req = mockReq(householderAppeal);
+  describe('Testing controller for Full Planning Appeals Check Your Answers page', () => {
+    it('Test getCanUseService method calls the HP Check Your Answers page when typeOfPlanningApplication is householder-planning - date of decision', async () => {
+      req = mockReq(householderAppeal);
 
-    await canUseServiceController.getCanUseService(req, res);
+      await canUseServiceController.getCanUseService(req, res);
 
-    expect(res.render).toBeCalledWith(canUseServiceHouseholder, {
-      appealLPD: 'Bradford',
-      applicationDecision: 'Granted',
-      applicationType: 'Householder Planning',
-      claimingCosts: 'No',
-      deadlineDate: { date: 27, day: 'Wednesday', month: 'July', year: 2022 },
-      decisionDate: '04 May 2022',
-      enforcementNotice: 'No',
-      isListedBuilding: 'No',
+      expect(res.render).toBeCalledWith(canUseServiceHouseholder, {
+        appealLPD: 'Bradford',
+        applicationDecision: 'Granted',
+        applicationType: 'Householder Planning',
+        claimingCosts: 'No',
+        deadlineDate: { date: 27, day: 'Wednesday', month: 'July', year: 2022 },
+        decisionDate: '04 May 2022',
+        dateOfDecisionLabel: 'Date of Decision',
+        enforcementNotice: 'No',
+        isListedBuilding: 'No',
+      });
+    });
+
+    it('Test getCanUseService method calls the HP Check Your Answers page when typeOfPlanningApplication is householder-planning - date decision due', async () => {
+      const householderAppealNoDecisionReceived = { ...householderAppeal };
+      householderAppealNoDecisionReceived.eligibility.applicationDecision = 'nodecisionreceived';
+      req = mockReq(householderAppealNoDecisionReceived);
+
+      await canUseServiceController.getCanUseService(req, res);
+
+      expect(res.render).toBeCalledWith(canUseServiceHouseholder, {
+        appealLPD: 'Bradford',
+        applicationDecision: 'No Decision Received',
+        applicationType: 'Householder Planning',
+        claimingCosts: 'No',
+        deadlineDate: { date: 27, day: 'Wednesday', month: 'July', year: 2022 },
+        decisionDate: '04 May 2022',
+        dateOfDecisionLabel: 'Date decision due',
+        enforcementNotice: 'No',
+        isListedBuilding: 'No',
+      });
     });
   });
 
-  it('Test getCanUseService method calls the Full Appeal Check Your Answers page when typeOfPlanningApplication is full-appeal', async () => {
-    req = mockReq(fullAppeal);
+  describe('Testing controller for Full Planning Appeals Check Your Answers page', () => {
+    it('Test getCanUseService method calls the Full Appeal Check Your Answers page when typeOfPlanningApplication is full-appeal - date of decision', async () => {
+      req = mockReq(fullAppeal);
 
-    await canUseServiceController.getCanUseService(req, res);
+      await canUseServiceController.getCanUseService(req, res);
 
-    expect(res.render).toBeCalledWith(canUseServiceFullAppealUrl, {
-      appealLPD: 'Bradford',
-      applicationDecision: 'Granted',
-      applicationType: 'Full Appeal',
-      deadlineDate: { date: 4, day: 'Friday', month: 'November', year: 2022 },
-      decisionDate: '04 May 2022',
-      enforcementNotice: 'No',
+      expect(res.render).toBeCalledWith(canUseServiceFullAppealUrl, {
+        appealLPD: 'Bradford',
+        applicationDecision: 'Granted',
+        applicationType: 'Full Appeal',
+        deadlineDate: { date: 4, day: 'Friday', month: 'November', year: 2022 },
+        decisionDate: '04 May 2022',
+        dateOfDecisionLabel: 'Date of Decision',
+        enforcementNotice: 'No',
+      });
+    });
+
+    it('Test getCanUseService method calls the Full Appeal Check Your Answers page when typeOfPlanningApplication is full-appeal - date decision due', async () => {
+      const fullAppealNoDecisionReceived = { ...fullAppeal };
+      fullAppealNoDecisionReceived.eligibility.applicationDecision = 'nodecisionreceived';
+      req = mockReq(fullAppealNoDecisionReceived);
+
+      await canUseServiceController.getCanUseService(req, res);
+
+      expect(res.render).toBeCalledWith(canUseServiceFullAppealUrl, {
+        appealLPD: 'Bradford',
+        applicationDecision: 'No Decision Received',
+        applicationType: 'Full Appeal',
+        deadlineDate: { date: 4, day: 'Friday', month: 'November', year: 2022 },
+        decisionDate: '04 May 2022',
+        dateOfDecisionLabel: 'Date decision due',
+        enforcementNotice: 'No',
+      });
     });
   });
 });
