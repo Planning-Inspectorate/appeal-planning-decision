327db8f026857aae113aabd98358eac82477e47e
Ed Packard <edward.packard@kinandcarta.com>
2022-05-19 16:33:31 +0100

    feat(as-4807): update route and controller

--- packages/forms-web-app/src/controllers/full-appeal/submit-appeal/certificates.js
+++ packages/forms-web-app/src/controllers/full-appeal/submit-appeal/certificates.js
@@ -1,7 +1,78 @@
+const { documentTypes } = require('@pins/common');
 const { VIEW } = require('../../../lib/full-appeal/views');
+const { createDocument } = require('../../../lib/documents-api-wrapper');
+const { createOrUpdateAppeal } = require('../../../lib/appeals-api-wrapper');
+const { COMPLETED } = require('../../../services/task-status/task-statuses');
 
-const getCertificates = async (_, res) => {
-  res.render(VIEW.FULL_APPEAL.CERTIFICATES);
+const logger = require('../../../lib/logger');
+
+const sectionName = 'planningApplicationDocumentsSection';
+const taskName = documentTypes.ownershipCertificate.name;
+
+const getCertificates = async (req, res) => {
+  const {
+    session: {
+      appeal: {
+        id: appealId,
+        [sectionName]: {
+          [taskName]: { uploadedFile },
+        },
+      },
+    },
+  } = req;
+  res.render(VIEW.FULL_APPEAL.CERTIFICATES, { appealId, uploadedFile });
 };
 
-module.exports = { getCertificates };
+const postCertificates = async (req, res) => {
+  const {
+    body: { errors = {}, errorSummary = [] },
+    files,
+    session: {
+      appeal,
+      appeal: {
+        id: appealId,
+        [sectionName]: {
+          [taskName]: { uploadedFile },
+        },
+      },
+    },
+  } = req;
+
+  if (Object.keys(errors).length > 0) {
+    return res.render(VIEW.FULL_APPEAL.CERTIFICATES, {
+      appealId,
+      uploadedFile,
+      errorSummary,
+      errors,
+    });
+  }
+
+  try {
+    if (files) {
+      const document = await createDocument(appeal, files['file-upload'], null, taskName);
+
+      appeal[sectionName][taskName].uploadedFile = {
+        id: document.id,
+        name: files['file-upload'].name,
+        fileName: files['file-upload'].name,
+        originalFileName: files['file-upload'].name,
+        location: document.location,
+        size: document.size,
+      };
+    }
+
+    appeal.sectionStates[sectionName][taskName] = COMPLETED;
+    req.session.appeal = await createOrUpdateAppeal(appeal);
+  } catch (err) {
+    logger.error(err);
+    return res.render(VIEW.FULL_APPEAL.CERTIFICATES, {
+      appealId,
+      uploadedFile,
+      errorSummary: [{ text: err.toString(), href: '#' }],
+    });
+  }
+
+  return res.redirect(`/${VIEW.FULL_APPEAL.APPLICATION_NUMBER}`);
+};
+
+module.exports = { getCertificates, postCertificates };
--- packages/forms-web-app/src/routes/full-appeal/submit-appeal/certificates.js
+++ packages/forms-web-app/src/routes/full-appeal/submit-appeal/certificates.js
@@ -1,8 +1,29 @@
 const express = require('express');
-const router = express.Router();
-
 const certificatesController = require('../../../controllers/full-appeal/submit-appeal/certificates');
+const fetchExistingAppealMiddleware = require('../../../middleware/fetch-existing-appeal');
 
-router.get('/submit-appeal/certificates', certificatesController.getCertificates);
+const { documentTypes } = require('@pins/common');
+const { validationErrorHandler } = require('../../../validators/validation-error-handler');
+const { rules: fileUploadValidationRules } = require('../../../validators/common/file-upload');
+const setSectionAndTaskNames = require('../../../middleware/set-section-and-task-names');
+
+const router = express.Router();
+const sectionName = 'planningApplicationDocumentsSection';
+const taskName = documentTypes.ownershipCertificate.name;
+
+router.get(
+  '/submit-appeal/certificates',
+  [fetchExistingAppealMiddleware],
+  setSectionAndTaskNames(sectionName, taskName),
+  certificatesController.getCertificates
+);
+
+router.post(
+  '/submit-appeal/certificates',
+  setSectionAndTaskNames(sectionName, taskName),
+  fileUploadValidationRules('Select your ownership certificate and agricultural land declaration'),
+  validationErrorHandler,
+  certificatesController.postCertificates
+);
 
 module.exports = router;
