c876e943b961895034b9f9bad7d4cdd169fa583b
Ed Packard <edward.packard@kinandcarta.com>
2022-05-20 14:45:46 +0100

    feat(as-4807): refactor error message handling for oversize files

--- packages/forms-web-app/src/validators/custom/file-size.js
+++ packages/forms-web-app/src/validators/custom/file-size.js
@@ -1,8 +1,8 @@
 const fileSizeDisplayHelper = require('../../lib/file-size-display-helper');
 
-module.exports = (givenFileSize, maxFileSize, fileName = 'The selected file', errorMsg) => {
+module.exports = (givenFileSize, maxFileSize, fileName = 'The selected file', errorMsg = null) => {
   if (givenFileSize > maxFileSize) {
-    if (errorMsg) {
+    if (errorMsg != null) {
       throw new Error(errorMsg);
     } else {
       throw new Error(`${fileName} must be smaller than ${fileSizeDisplayHelper(maxFileSize)}`);
