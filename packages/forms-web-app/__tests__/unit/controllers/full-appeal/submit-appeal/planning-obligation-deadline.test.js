681501c9c9491eb6b78829bcf4ee5e7afa5cf691
Guilherme Ribeiro <Gui>
2022-05-30 09:30:12 +0100

    feat(planning obligation deadline): added planning obligation deadline

--- packages/forms-web-app/__tests__/unit/controllers/full-appeal/submit-appeal/planning-obligation-deadline.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/full-appeal/submit-appeal/planning-obligation-deadline.test.js
@@ -1,12 +1,22 @@
 const appeal = require('@pins/business-rules/test/data/full-appeal');
 const v8 = require('v8');
+const { PLANNING_OBLIGATION_STATUS_OPTION } = require('@pins/business-rules/src/constants');
 const {
   getPlanningObligationDeadline,
+  postPlanningObligationDeadline,
 } = require('../../../../../src/controllers/full-appeal/submit-appeal/planning-obligation-deadline');
 
-const { VIEW } = require('../../../../../src/lib/full-appeal/views');
+const {
+  VIEW: {
+    FULL_APPEAL: { PLANNING_OBLIGATION_DEADLINE, NEW_DOCUMENTS },
+  },
+} = require('../../../../../src/lib/full-appeal/views');
 
 const { mockReq, mockRes } = require('../../../mocks');
+const { createOrUpdateAppeal } = require('../../../../../src/lib/appeals-api-wrapper');
+
+jest.mock('../../../../../src/lib/appeals-api-wrapper');
+jest.mock('../../../../../src/services/task.service');
 
 describe('controllers/full-appeal/submit-appeal/planning-obligation-deadline', () => {
   let req;
@@ -27,8 +37,62 @@ describe('controllers/full-appeal/submit-appeal/planning-obligation-deadline', (
   describe('getPlanningObligationDeadline', () => {
     it('calls correct template', async () => {
       await getPlanningObligationDeadline(req, res);
-      expect(res.render).toBeCalledWith(VIEW.FULL_APPEAL.PLANNING_OBLIGATION_DEADLINE, {
+      expect(res.render).toBeCalledWith(PLANNING_OBLIGATION_DEADLINE, {
         planningObligationDeadline: undefined,
+      });
+    });
+  });
+
+  describe('postlanningObligationDeadline', () => {
+    it('postlanningObligationDeadline method calls the correct template if clicked continue', async () => {
+      req = {
+        ...req,
+        body: {
+          'planning-obligation-deadline': 'ok',
+        },
+      };
+      createOrUpdateAppeal.mockImplementation(() => Promise.resolve(appeal));
+      await postPlanningObligationDeadline(req, res);
+
+      expect(res.redirect).toBeCalledWith(`/${NEW_DOCUMENTS}`);
+    });
+
+    it('postlanningObligationDeadline method calls the correct template if it contains errors', async () => {
+      req = {
+        ...req,
+        body: {
+          errors: { error: 'error' },
+          errorSummary: ['error'],
+        },
+      };
+
+      await postPlanningObligationDeadline(req, res);
+
+      expect(res.render).toBeCalledWith(PLANNING_OBLIGATION_DEADLINE, {
+        appeal,
+        errors: { error: 'error' },
+        errorSummary: ['error'],
+      });
+    });
+
+    it('postlanningObligationDeadline method calls the correct template if the service throws errors', async () => {
+      req = {
+        ...req,
+        body: {
+          'planning-obligation-deadline': 'ok',
+        },
+      };
+
+      createOrUpdateAppeal.mockImplementation(() => Promise.reject(new Error()));
+      await postPlanningObligationDeadline(req, res);
+
+      expect(res.render).toBeCalledWith(PLANNING_OBLIGATION_DEADLINE, {
+        planningObligationDeadline: {
+          plansPlanningObligation: true,
+          planningObligationStatus: PLANNING_OBLIGATION_STATUS_OPTION.NOT_STARTED,
+        },
+        errors: {},
+        errorSummary: [{ text: 'Error', href: '#' }],
       });
     });
   });
