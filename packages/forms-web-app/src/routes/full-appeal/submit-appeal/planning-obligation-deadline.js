725613578b8f7c8440dac18e36312c4aeee7d023
Guilherme Ribeiro <Gui>
2022-05-30 11:36:35 +0100

    fix(planning obligation deadline router): removed unused code

--- packages/forms-web-app/src/routes/full-appeal/submit-appeal/planning-obligation-deadline.js
+++ packages/forms-web-app/src/routes/full-appeal/submit-appeal/planning-obligation-deadline.js
@@ -6,16 +6,11 @@ const {
   postPlanningObligationDeadline,
 } = require('../../../controllers/full-appeal/submit-appeal/planning-obligation-deadline');
 const { validationErrorHandler } = require('../../../validators/validation-error-handler');
-const setSectionAndTaskNames = require('../../../middleware/set-section-and-task-names');
-
-const sectionName = 'appealDocumentsSection';
-const taskName = 'planningObligationDeadline';
 
 router.get('/submit-appeal/planning-obligation-deadline', getPlanningObligationDeadline);
 
 router.post(
   '/submit-appeal/planning-obligation-deadline',
-  //setSectionAndTaskNames(sectionName, taskName),
   validationErrorHandler,
   postPlanningObligationDeadline
 );
