3fb608c973ebea4c88bce4946dbfeed38d3c1a9c
Ed Packard <edward.packard@kinandcarta.com>
2022-06-07 11:06:43 +0100

    feat(as-4874): add backlink to page

--- packages/forms-web-app/src/controllers/submit-appeal/enter-appeal-details.js
+++ packages/forms-web-app/src/controllers/submit-appeal/enter-appeal-details.js
@@ -1,5 +1,7 @@
 const { VIEW } = require('../../lib/submit-appeal/views');
 
 exports.getEnterAppealDetails = async (_, res) => {
-  res.render(VIEW.SUBMIT_APPEAL.ENTER_APPEAL_DETAILS, {});
+  res.render(VIEW.SUBMIT_APPEAL.ENTER_APPEAL_DETAILS, {
+    backLink: 'https://www.gov.uk/appeal-planning-decision/make-an-appeal',
+  });
 };
