29c9cf32ef264bafc3f24af25813d957bf6a45c5
Ed Packard <edward.packard@kinandcarta.com>
2022-06-07 11:06:43 +0100

    feat(as-4874): add backlink to page

--- packages/forms-web-app/__tests__/unit/controllers/submit-appeal/enter-appeal-details.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/submit-appeal/enter-appeal-details.test.js
@@ -2,6 +2,7 @@ const getEnterAppealDetailsController = require('../../../../src/controllers/sub
 
 const { VIEW } = require('../../../../src/lib/submit-appeal/views');
 const { mockReq, mockRes } = require('../../mocks');
+const url = 'https://www.gov.uk/appeal-planning-decision/make-an-appeal';
 
 describe('controllers/submit-appeal/enter-appeal-details', () => {
   const req = mockReq();
@@ -10,6 +11,6 @@ describe('controllers/submit-appeal/enter-appeal-details', () => {
   it('getEnterAppealDetails method calls the correct template', async () => {
     await getEnterAppealDetailsController.getEnterAppealDetails(req, res);
 
-    expect(res.render).toBeCalledWith(VIEW.SUBMIT_APPEAL.ENTER_APPEAL_DETAILS, {});
+    expect(res.render).toBeCalledWith(VIEW.SUBMIT_APPEAL.ENTER_APPEAL_DETAILS, { backLink: url });
   });
 });
