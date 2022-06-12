61ddcf831d57c2b14686bca8d2a62730fc770463
Guilherme Ribeiro <Gui>
2022-06-12 21:37:13 +0100

    feat(s&r api): save and return API

--- packages/appeals-service-api/__tests__/unit/services/save-and-return.service.test.js
+++ packages/appeals-service-api/__tests__/unit/services/save-and-return.service.test.js
@@ -1,7 +1,56 @@
-const { createToken } = require('../../../src/services/save-and-return.service');
+const {
+  createToken,
+  saveAndReturnGetService,
+  saveAndReturnNotify,
+} = require('../../../src/services/save-and-return.service');
+const mongodb = require('../../../src/db/db');
 
-describe('save-and-return service', () => {
-  it('should create token', () => {
-    expect(createToken().toString()).toMatch(/\d{5}/);
+jest.mock('../../../src/db/db');
+
+describe('save-and-return services', () => {
+  describe('save-and-return token service', () => {
+    it('should create token', () => {
+      expect(createToken().toString()).toMatch(/\d{5}/);
+    });
+
+    describe('saveAndReturnGetService', () => {
+      it('should retrieve saved appeal by appealId', async () => {
+        const saved = {
+          value: {
+            appealId: '123445',
+            token: null,
+            email: 'asd@asd.com',
+            createdAt: new Date(),
+            expirerAt: null,
+          },
+        };
+        mongodb.get = jest.fn(() => ({
+          collection: jest.fn(() => ({
+            findOne: jest.fn().mockResolvedValue(saved),
+          })),
+        }));
+
+        const savedRes = await saveAndReturnGetService('12345');
+
+        expect(savedRes).toEqual(saved.value);
+      });
+
+      it('should throw error', () => {
+        mongodb.get = jest.fn(() => ({
+          collection: jest.fn(() => ({
+            findOne: jest.fn().mockRejectedValue(new Error('Some error')),
+          })),
+        }));
+
+        expect(() => saveAndReturnGetService()).rejects.toThrowError('Some error');
+      });
+    });
+  });
+
+  describe(' save and return notify client', () => {
+    it('should send notification with the template', () => {
+      // TODO
+      saveAndReturnNotify();
+    });
   });
 });
--- packages/appeals-service-api/src/services/save-and-return.service.js
+++ packages/appeals-service-api/src/services/save-and-return.service.js
@@ -1,10 +1,10 @@
+const { NotifyClient } = require('notifications-node-client').NotifyClient;
+const config = require('@pins/common/src/config');
 const logger = require('../lib/logger');
 const mongodb = require('../db/db');
 
 module.exports = {
   async saveAndReturnCreateService(saved) {
-    logger.debug('XXXXXXXXXXXXXXXXXXXXXXX');
-    logger.debug(saved);
     try {
       await mongodb
         .get()
@@ -25,12 +25,36 @@ module.exports = {
             });
         });
     } catch (err) {
-      logger.error(saved, `Problem creating a reply`);
+      logger.error(saved, `Error when creating in the db`);
     }
   },
 
-  async saveAndReturnNotify(param) {
-    console.log(param);
+  async saveAndReturnGetService(email) {
+    let saved;
+    await mongodb
+      .get()
+      .collection('saveAndReturn')
+      .findOne({ email })
+      .then((doc) => {
+        saved = doc.value;
+      })
+      .catch((err) => {
+        console.log(err);
+        throw new Error(err);
+      });
+    return saved;
+  },
+
+  async saveAndReturnNotify(saved) {
+    // TODO - using the existing builder vs using tthe API directly
+    const notifyClient = new NotifyClient(config.services.notify.apiKey);
+    const options = {
+      personalisation: '',
+      reference: '',
+      emailReplyToId: '',
+    };
+    await notifyClient.sendEmail(this.templateId, this.destinationEmail, options);
+    return true;
   },
 
   createToken() {
