fd57894272643aa239e26abeb33ad974d27ee061
Constantine Akritidis <constantine.akritidis@planninginspectorate.gov.uk>
2022-06-13 11:21:27 +0300

    feat(as-4916 updated decision date logic and the respective pages): as-4916 updated deadline logic

--- packages/forms-web-app/src/lib/calculate-deadline.js
+++ packages/forms-web-app/src/lib/calculate-deadline.js
@@ -18,7 +18,7 @@ const calculateDeadline = {
   householderApplication: (decisionDate) => {
     const numOfWeeksToDeadline = 12;
     const tempDate = new Date(decisionDate);
-    tempDate.setDate(tempDate.getDate() + numOfWeeksToDeadline * 7);
+    tempDate.setDate(tempDate.getDate() + numOfWeeksToDeadline * 7 - 1);
     return {
       date: tempDate.getDate(),
       day: weekdays[tempDate.getDay()],
@@ -30,6 +30,7 @@ const calculateDeadline = {
     const numOfMonthsToDeadline = 6;
     const tempDate = new Date(decisionDate);
     tempDate.setMonth(tempDate.getMonth() + numOfMonthsToDeadline);
+    tempDate.setDate(tempDate.getDate() - 1);
     return {
       date: tempDate.getDate(),
       day: weekdays[tempDate.getDay()],
