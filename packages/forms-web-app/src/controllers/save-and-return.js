ce3d4de9a46c57cbd84e7e5b6d72bc737558f317
Ed Packard <edward.packard@kinandcarta.com>
2022-06-15 15:24:37 +0100

    feat(as-4951): new route and controller for save appeal

---
+++ packages/forms-web-app/src/controllers/save-and-return.js
@@ -0,0 +1,7 @@
+const { saveAppeal } = require('../lib/appeals-api-wrapper');
+const { VIEW } = require('../lib/submit-appeal/views');
+
+exports.postSaveAndReturn = async (req, res) => {
+  await saveAppeal(req.session.appeal);
+  res.redirect(VIEW.SUBMIT_APPEAL.APPLICATION_SAVED);
+};
