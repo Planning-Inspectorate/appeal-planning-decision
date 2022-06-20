52499f1b193d5d67f72a0aff81fec616230563df
Constantine Akritidis <constantine.akritidis@planninginspectorate.gov.uk>
2022-06-20 10:21:14 +0100

    fix(as-4978 fixed a bug related to calculating the deadline date): as-4978 fixed deadline date bug

--- packages/forms-web-app/src/lib/calculate-deadline.js
+++ packages/forms-web-app/src/lib/calculate-deadline.js
@@ -1,42 +1,29 @@
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
+const { rules } = require('@pins/business-rules');
+const { parseISO } = require('date-fns');
+const formatDate = require('./format-date-check-your-answers');
 
 const calculateDeadline = {
   householderApplication: (decisionDate) => {
     const numOfWeeksToDeadline = 12;
     const tempDate = new Date(decisionDate);
     tempDate.setDate(tempDate.getDate() + numOfWeeksToDeadline * 7 - 1);
-    return {
-      date: tempDate.getDate(),
-      day: weekdays[tempDate.getDay()],
-      month: months[tempDate.getMonth()],
-      year: tempDate.getFullYear(),
-    };
+    return formatDate(tempDate);
   },
   fullAppealApplication: (decisionDate) => {
     const numOfMonthsToDeadline = 6;
     const tempDate = new Date(decisionDate);
     tempDate.setMonth(tempDate.getMonth() + numOfMonthsToDeadline);
     tempDate.setDate(tempDate.getDate() - 1);
-    return {
-      date: tempDate.getDate(),
-      day: weekdays[tempDate.getDay()],
-      month: months[tempDate.getMonth()],
-      year: tempDate.getFullYear(),
-    };
+    return formatDate(tempDate);
+  },
+  businessRulesDeadline: (decisionDate, appealType, applicationDecision) => {
+    const deadlineDate = rules.appeal.deadlineDate(
+      parseISO(decisionDate),
+      appealType,
+      applicationDecision
+    );
+
+    return formatDate(deadlineDate);
   },
 };
 
