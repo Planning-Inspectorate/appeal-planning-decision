3fe1d30de6ffb78a940c8ea9db9096007c805f35
Chris Howarth <chris.howarth@kinandcarta.com>
2022-06-24 15:21:47 +0100

    feat(as-4697): adding the list of documents page for full appeals

--- packages/forms-web-app/src/lib/full-appeal/views.js
+++ packages/forms-web-app/src/lib/full-appeal/views.js
@@ -56,6 +56,8 @@ const VIEW = {
     EMAIL_ADDRESS: 'full-appeal/submit-appeal/email-address',
     CONFIRM_EMAIL_ADDRESS: 'full-appeal/submit-appeal/confirm-email-address',
     EMAIL_CONFIRMED: 'full-appeal/submit-appeal/email-confirmed',
+    'full-appeal/removal-or-variation-of-conditions/can-use-service',
+    LIST_OF_DOCUMENTS: 'full-appeal/submit-appeal/list-of-documents',
   },
 };
 
