2219641e7248f61cc77460bd5a55e0e70f11ff20
Constantine Akritidis <constantine.akritidis@planninginspectorate.gov.uk>
2022-06-27 15:40:00 +0100

    test(as-4680 added new unit tests and fixed failing ones): added new UTs and fixed failing ones

---
+++ packages/forms-web-app/__tests__/unit/controllers/full-appeal/submit-appeal/email-address.test.js
@@ -0,0 +1,101 @@
+const appeal = require('@pins/business-rules/test/data/full-appeal');
+const emailAddressController = require('../../../../../src/controllers/full-appeal/submit-appeal/email-address');
+const { mockReq, mockRes } = require('../../../mocks');
+const { createOrUpdateAppeal } = require('../../../../../src/lib/appeals-api-wrapper');
+const logger = require('../../../../../src/lib/logger');
+const {
+  VIEW: {
+    FULL_APPEAL: { EMAIL_ADDRESS, CONFIRM_EMAIL_ADDRESS },
+  },
+} = require('../../../../../src/lib/full-appeal/views');
+
+jest.mock('../../../../../src/lib/appeals-api-wrapper');
+jest.mock('../../../../../src/services/task.service');
+jest.mock('../../../../../src/lib/logger');
+
+const taskName = 'email';
+const email = 'testemail@example.com';
+
+describe('controllers/full-appeal/submit-appeal/email-address', () => {
+  let req;
+  let res;
+
+  beforeEach(() => {
+    req = mockReq(appeal);
+    res = mockRes();
+
+    appeal.email = email;
+
+    jest.resetAllMocks();
+  });
+
+  describe('getEmailAddress', () => {
+    it('should call the correct template', () => {
+      emailAddressController.getEmailAddress(req, res);
+      expect(res.render).toHaveBeenCalledWith(EMAIL_ADDRESS, {
+        email,
+      });
+    });
+  });
+
+  describe('postEmailAddress', () => {
+    it('should re-render the template with errors if submission validation fails', async () => {
+      const mockRequest = {
+        ...req,
+        body: {
+          errors: { a: 'b' },
+          errorSummary: [{ text: 'There were errors here', href: '#' }],
+        },
+      };
+      await emailAddressController.postEmailAddress(mockRequest, res);
+
+      expect(res.redirect).not.toHaveBeenCalled();
+      expect(res.render).toHaveBeenCalledWith(EMAIL_ADDRESS, {
+        email,
+        errorSummary: [{ text: 'There were errors here', href: '#' }],
+        errors: { a: 'b' },
+      });
+    });
+
+    it('should log an error if the api call fails, and remain on the same page', async () => {
+      const error = new Error('API is down');
+
+      createOrUpdateAppeal.mockImplementation(() => Promise.reject(error));
+      const mockRequest = {
+        ...req,
+        body: {},
+      };
+      await emailAddressController.postEmailAddress(mockRequest, res);
+
+      expect(logger.error).toHaveBeenCalledWith(error);
+
+      expect(res.redirect).not.toHaveBeenCalled();
+
+      expect(res.render).toHaveBeenCalledWith(EMAIL_ADDRESS, {
+        email,
+        errors: {},
+        errorSummary: [{ text: error.toString(), href: '#' }],
+      });
+    });
+
+    it('should redirect to `/full-appeal/submit-appeal/confirm-email-address` if valid', async () => {
+      const fakeEmail = 'test@test.com';
+
+      const mockRequest = {
+        ...req,
+        body: {
+          'email-address': fakeEmail,
+        },
+      };
+
+      await emailAddressController.postEmailAddress(mockRequest, res);
+
+      expect(createOrUpdateAppeal).toHaveBeenCalledWith({
+        ...appeal,
+        [taskName]: fakeEmail,
+      });
+
+      expect(res.redirect).toHaveBeenCalledWith(`/${CONFIRM_EMAIL_ADDRESS}`);
+    });
+  });
+});
