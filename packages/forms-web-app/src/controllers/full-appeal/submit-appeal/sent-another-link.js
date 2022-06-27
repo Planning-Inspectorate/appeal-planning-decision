69d1aff1ca7b5802ed2faded30a359027cff1c07
Chris Howarth <chris.howarth@kinandcarta.com>
2022-06-27 16:40:04 +0100

    feat(as-4686): adding sent another link page

---
+++ packages/forms-web-app/src/controllers/full-appeal/submit-appeal/sent-another-link.js
@@ -0,0 +1,15 @@
+const {
+  VIEW: {
+    FULL_APPEAL: { SENT_ANOTHER_LINK: currentPage },
+  },
+  } = require('../../../lib/full-appeal/views');
+
+  const getSentAnotherLink = (req, res) => {
+    res.render(currentPage, {
+    });
+  };
+
+    
+  module.exports = {
+    getSentAnotherLink,
+  };
---
+++ packages/forms-web-app/src/routes/full-appeal/submit-appeal/sent-another-link.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const fetchExistingAppealMiddleware = require('../../../middleware/fetch-existing-appeal');
+const {
+  getSentAnotherLink,
+} = require('../../../controllers/full-appeal/submit-appeal//sent-another-link');
+const router = express.Router();
+
+router.get(
+  '/submit-appeal/sent-another-link',
+  [fetchExistingAppealMiddleware],
+  getSentAnotherLink
+);
+
+
+module.exports = router;
