c8a7ce17f8e87f812470c3eb5cd95df11095b5ae
Chris Howarth <chris.howarth@kinandcarta.com>
2022-06-27 17:17:48 +0100

    feat(as-4809): appeal aready submitted initial template

---
+++ packages/forms-web-app/src/routes/full-appeal/submit-appeal/appeal-already-submitted.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const fetchExistingAppealMiddleware = require('../../../middleware/fetch-existing-appeal');
+const {
+    getAppealAlreadySubmitted,
+} = require('../../../controllers/full-appeal/submit-appeal/appeal-already-submitted');
+const router = express.Router();
+
+router.get(
+  '/submit-appeal/appeal-already-submitted',
+  [fetchExistingAppealMiddleware],
+  getAppealAlreadySubmitted
+);
+
+
+module.exports = router;
