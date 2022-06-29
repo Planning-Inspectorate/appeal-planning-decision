c5fe0c4822b5b4a28de1304132de07321b83fe09
Ed Packard <edward.packard@kinandcarta.com>
2022-06-29 15:49:08 +0100

    feat(as-4764): fix create email function in confirm email controller

--- packages/forms-web-app/__tests__/unit/controllers/full-appeal/submit-appeal/confirm-email-address.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/full-appeal/submit-appeal/confirm-email-address.test.js
@@ -1,6 +1,8 @@
 const {
   getConfirmEmailAddress,
 } = require('../../../../../src/controllers/full-appeal/submit-appeal/confirm-email-address');
+const { createConfirmEmail } = require('../../../../../src/lib/appeals-api-wrapper');
+const appeal = require('@pins/business-rules/test/data/full-appeal');
 
 const {
   VIEW: {
@@ -10,12 +12,14 @@ const {
 
 const { mockReq, mockRes } = require('../../../mocks');
 
+jest.mock('../../../../../src/lib/appeals-api-wrapper');
+
 describe('controllers/full-appeal/submit-appeal/confirm-email-address', () => {
   let req;
   let res;
 
   beforeEach(() => {
-    req = mockReq();
+    req = mockReq(appeal);
     res = mockRes();
 
     jest.resetAllMocks();
@@ -23,8 +27,10 @@ describe('controllers/full-appeal/submit-appeal/confirm-email-address', () => {
 
   describe('getConfirmEmailAddress', () => {
     it('calls correct template', async () => {
+      req.session.appeal = appeal;
       req.session.appeal.emailAddress = 'test@example.com';
       await getConfirmEmailAddress(req, res);
+      expect(createConfirmEmail).toBeCalledWith(appeal);
       expect(res.render).toBeCalledWith(CONFIRM_EMAIL_ADDRESS, {
         emailAddress: 'test@example.com',
         backLink: `/${EMAIL_ADDRESS}`,
