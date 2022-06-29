aef83e244f9586f6f3fcfebba806a0579da0505e
Ed Packard <edward.packard@kinandcarta.com>
2022-06-29 15:11:05 +0100

    feat(as-4764): add function call to confirm email controller

--- packages/forms-web-app/src/controllers/full-appeal/submit-appeal/confirm-email-address.js
+++ packages/forms-web-app/src/controllers/full-appeal/submit-appeal/confirm-email-address.js
@@ -1,3 +1,4 @@
+const { createConfirmEmail } = require('../../../lib/appeals-api-wrapper');
 const {
   VIEW: {
     FULL_APPEAL: { CONFIRM_EMAIL_ADDRESS, EMAIL_ADDRESS },
@@ -5,6 +6,7 @@ const {
 } = require('../../../lib/full-appeal/views');
 
 const getConfirmEmailAddress = async (req, res) => {
+  createConfirmEmail(req.body.appeal);
   res.render(CONFIRM_EMAIL_ADDRESS, {
     emailAddress: req.session.appeal.emailAddress,
     backLink: `/${EMAIL_ADDRESS}`,
