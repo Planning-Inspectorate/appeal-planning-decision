42efd122880439ec1ce66d31b1dff87b3260438c
Ed Packard <edward.packard@kinandcarta.com>
2022-06-29 15:50:46 +0100

    fix(as-4764): fix test error

--- packages/forms-web-app/__tests__/unit/lib/full-appeal/views.test.js
+++ packages/forms-web-app/__tests__/unit/lib/full-appeal/views.test.js
@@ -60,7 +60,6 @@ describe('/lib/full-appeal/views', () => {
         EMAIL_ADDRESS: 'full-appeal/submit-appeal/email-address',
         CONFIRM_EMAIL_ADDRESS: 'full-appeal/submit-appeal/confirm-email-address',
         EMAIL_CONFIRMED: 'full-appeal/submit-appeal/email-confirmed',
-        'full-appeal/removal-or-variation-of-conditions/can-use-service',
         LIST_OF_DOCUMENTS: 'full-appeal/submit-appeal/list-of-documents',
       },
     });
