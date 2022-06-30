2db0db7291abd5c93a4cde6674ef1391ef9f2dee
Andy Langridge <andy.langridge@kinandcarta.com>
2022-06-30 13:11:03 +0100

    style: run prettier

--- packages/forms-web-app/src/controllers/full-appeal/submit-appeal/sent-another-link.js
+++ packages/forms-web-app/src/controllers/full-appeal/submit-appeal/sent-another-link.js
@@ -1,15 +1,13 @@
 const {
-  VIEW: {
-    FULL_APPEAL: { SENT_ANOTHER_LINK: currentPage },
-  },
-  } = require('../../../lib/full-appeal/views');
+	VIEW: {
+		FULL_APPEAL: { SENT_ANOTHER_LINK: currentPage }
+	}
+} = require('../../../lib/full-appeal/views');
 
-  const getSentAnotherLink = (req, res) => {
-    res.render(currentPage, {
-    });
-  };
-    
-  module.exports = {
-    getSentAnotherLink,
-  };
-  
+const getSentAnotherLink = (req, res) => {
+	res.render(currentPage, {});
+};
+
+module.exports = {
+	getSentAnotherLink
+};
