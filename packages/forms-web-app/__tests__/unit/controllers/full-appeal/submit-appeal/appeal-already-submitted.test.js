19a352150e3b061401834c07034590e0c55ef9e7
Chris Howarth <chris.howarth@kinandcarta.com>
2022-07-01 15:57:10 +0100

    feat(as-4809): unit tests for appeal already submitted page

---
+++ packages/forms-web-app/__tests__/unit/controllers/full-appeal/submit-appeal/appeal-already-submitted.test.js
@@ -0,0 +1,30 @@
+const {
+    getAppealAlreadySubmitted,
+} = require('../../../../../src/controllers/full-appeal/submit-appeal/appeal-already-submitted');
+
+const {
+  VIEW: {
+    FULL_APPEAL: { APPEAL_ALREADY_SUBMITTED: currentPage },
+  },
+} = require('../../../../../src/lib/full-appeal/views');
+
+const { mockReq, mockRes } = require('../../../mocks');
+
+describe('controllers/full-appeal/submit-appeal/appeal-already-submitted', () => {
+  let req;
+  let res;
+
+  beforeEach(() => {
+    req = mockReq();
+    res = mockRes();
+
+    jest.resetAllMocks();
+  });
+
+  describe('getAppealAlreadySubmitted', () => {
+    it('calls correct template', async () => {
+      await getAppealAlreadySubmitted(req, res);
+      expect(res.render).toBeCalledWith(currentPage, {});
+    });
+  });
+});
