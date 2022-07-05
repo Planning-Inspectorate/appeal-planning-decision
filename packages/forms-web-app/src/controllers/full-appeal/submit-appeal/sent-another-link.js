ac284f2271c318165d4c27358e26ade647e1f477
Chris Howarth <chris.howarth@kinandcarta.com>
2022-07-05 13:50:45 +0100

    fix(as-5044): typo correction and adding missing data

--- packages/forms-web-app/src/controllers/full-appeal/submit-appeal/sent-another-link.js
+++ packages/forms-web-app/src/controllers/full-appeal/submit-appeal/sent-another-link.js
@@ -5,7 +5,9 @@ const {
 } = require('../../../lib/full-appeal/views');
 
 const getSentAnotherLink = (req, res) => {
-	res.render(currentPage, {});
+	res.render(currentPage, {
+		appeal: req.session.appeal
+	});
 };
 
 module.exports = {
