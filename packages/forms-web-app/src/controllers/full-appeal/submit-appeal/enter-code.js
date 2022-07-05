ac17477dcad07cc537eca958fd5a4c7b99ee6174
Ed Packard <edward.packard@kinandcarta.com>
2022-07-05 09:26:52 +0100

    feat(as-4828): add request new code page

--- packages/forms-web-app/src/controllers/full-appeal/submit-appeal/enter-code.js
+++ packages/forms-web-app/src/controllers/full-appeal/submit-appeal/enter-code.js
@@ -5,14 +5,18 @@ const {
 } = require('../../../lib/appeals-api-wrapper');
 const {
 	VIEW: {
-		FULL_APPEAL: { TASK_LIST, ENTER_CODE }
+		FULL_APPEAL: { TASK_LIST, ENTER_CODE, REQUEST_NEW_CODE }
 	}
 } = require('../../../lib/full-appeal/views');
 
 const getEnterCode = async (req, res) => {
+	const url = `/${REQUEST_NEW_CODE}`;
 	const { token } = req.params;
 	await sendToken(token);
-	res.render(ENTER_CODE);
+
+	res.render(ENTER_CODE, {
+		requestNewCodeLink: url
+	});
 };
 
 const postEnterCode = async (req, res) => {
