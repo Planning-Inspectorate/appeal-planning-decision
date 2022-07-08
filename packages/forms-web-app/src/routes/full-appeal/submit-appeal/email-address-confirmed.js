0f74cd6ae71832f1e9f9121085c03e91d57493a3
Constantine Akritidis <constantine.akritidis@planninginspectorate.gov.uk>
2022-07-08 13:46:19 +0300

    fix(as-5057 fixed code bug): fixed code bug

--- packages/forms-web-app/src/routes/full-appeal/submit-appeal/email-address-confirmed.js
+++ packages/forms-web-app/src/routes/full-appeal/submit-appeal/email-address-confirmed.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fetchExistingAppealMiddleware = require('../../../middleware/fetch-existing-appeal');
 const {
 	getEmailConfirmed
-} = require('../../../controllers/full-appeal/submit-appeal/email-confirmed');
+} = require('../../../controllers/full-appeal/submit-appeal/email-address-confirmed');
 
 const router = express.Router();
 
