7c62e137e82bc317f6616c9a65e303429cab17d2
Ed Packard <edward.packard@kinandcarta.com>
2022-07-28 12:06:35 +0100

    fix(as-5131): fix resend email link to correct url

--- packages/forms-web-app/src/controllers/appeal-householder-decision/confirm-email-address.js
+++ packages/forms-web-app/src/controllers/appeal-householder-decision/confirm-email-address.js
@@ -3,7 +3,8 @@ const { createConfirmEmail } = require('../../lib/appeals-api-wrapper');
 const getConfirmEmailAddress = async (req, res) => {
 	await createConfirmEmail(req.session.appeal);
 	res.render('appeal-householder-decision/confirm-email-address', {
-		emailAddress: req.session.appeal.email
+		emailAddress: req.session.appeal.email,
+		resendLink: '/appeal-householder-decision/sent-another-link'
 	});
 };
 
