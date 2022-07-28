f32789a6d3d33f792b1706569d98a9a85313163f
Ozan Kaya <ext_ozan.kaya@cazoo.co.uk>
2022-07-28 16:21:44 +0100

    chore: clamav rest server problems solved

--- packages/clamav-rest-server/src/lib/clamd.js
+++ packages/clamav-rest-server/src/lib/clamd.js
@@ -8,17 +8,46 @@ const sendFile = async (file) => {
 		throw new Error('invalid or empty file');
 	}
 	logger.info('sending file');
-	const client = new NodeClam().init({
+	const ClamScan = new NodeClam().init({
 		removeInfected: true,
 		clamdscan: {
 			host: config.services.clamav.host || 'localhost',
 			port: config.services.clamav.port || 3310,
-			bypass_test: true
+			active: true,
+			bypassRest: false
 		},
 		preference: 'clamdscan'
 	});
-	const readableStream = Readable.from(file.toString());
-	return client.scanStream(readableStream);
+
+	// Get instance by resolving ClamScan promise object
+	ClamScan.then(async (client) => {
+		try {
+			// You can re-use the `clamscan` object as many times as you want
+			const version = await client.getVersion();
+			console.log(`ClamAV Version: ${version}`);
+
+			// const {isInfected, file, viruses} = await client.isInfected('/some/file.zip');
+			// if (isInfected) console.log(`${file} is infected with ${viruses}!`);
+
+			const fileStream = Readable();
+			fileStream.push(file.data);
+			fileStream.push(null);
+
+			return client.scanStream(fileStream);
+
+			// return client.scanStream(Readable.from(file.toString()));
+		} catch (err) {
+			// Handle any errors raised by the code in the try block
+			console.log(err);
+			logger.error(err, 'error calling clamav client');
+			throw err;
+		}
+	}).catch((err) => {
+		// Handle errors that may have occurred during initialization
+		console.log(err);
+		logger.error(err, 'error initializing clamav client');
+		throw err;
+	});
 };
 
 module.exports = {
