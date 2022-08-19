5f2c7459c9a9590d1bf9f55e44ddd94461f0a7d4
Ed Packard <edward.packard@kinandcarta.com>
2022-08-19 12:45:05 +0100

    fix(as-5344): extract common appeal validations from fp insert schema

---
+++ packages/business-rules/src/schemas/components/insert/appeal-details/appeal-details.js
@@ -0,0 +1,28 @@
+const dateValidation = require('../../date-validation');
+const appealTypeValidation = require('./appeal-type/appeal-type-validation');
+const emailValidation = require('./email/email-validation');
+const horizonIdValidation = require('./horizon-id/horizon-id-validation');
+const idValidation = require('./id/id-validation');
+const lpaCodeValidation = require('./lpa-code/lpa-code-validation');
+const planningApplicationNumberValidation = require('./planning-application-number/planning-application-number-validation');
+const stateValidation = require('./state/state-validation');
+const typeOfPlanningApplicationValidation = require('./type-of-planning-application/type-of-planning-application-validation');
+
+const appealDetails = (valAction) => {
+	return {
+		id: idValidation(),
+		horizonId: horizonIdValidation(),
+		lpaCode: lpaCodeValidation(valAction),
+		planningApplicationNumber: planningApplicationNumberValidation(),
+		decisionDate: dateValidation(),
+		createdAt: dateValidation(true),
+		updatedAt: dateValidation(true),
+		submissionDate: dateValidation(),
+		state: stateValidation(),
+		appealType: appealTypeValidation(),
+		typeOfPlanningApplication: typeOfPlanningApplicationValidation(),
+		email: emailValidation()
+	};
+};
+
+module.exports = appealDetails;
