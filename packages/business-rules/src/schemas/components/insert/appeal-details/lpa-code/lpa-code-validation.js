ce4aa424a3cd0c822449e14899a484c1b01b23fb
Ed Packard <edward.packard@kinandcarta.com>
2022-08-19 12:45:05 +0100

    fix(as-5344): extract common appeal validations from fp insert schema

---
+++ packages/business-rules/src/schemas/components/insert/appeal-details/lpa-code/lpa-code-validation.js
@@ -0,0 +1,9 @@
+const stringMaxCharsValidation = require('../../../string-validators/string-max-chars-validation');
+
+stringMaxCharsValidation;
+
+const lpaCodeValidation = () => {
+	return stringMaxCharsValidation(20);
+};
+
+module.exports = lpaCodeValidation;
