d09d027b71c4ff689fdd210269c172647582ac55
Ed Packard <edward.packard@kinandcarta.com>
2022-08-19 12:45:05 +0100

    fix(as-5344): extract common appeal validations from fp insert schema

---
+++ packages/business-rules/src/schemas/components/insert/appeal-details/horizon-id/horizon-id-validation.js
@@ -0,0 +1,8 @@
+const stringMaxCharsValidation = require('../../../string-validators/string-max-chars-validation');
+
+stringMaxCharsValidation;
+const horizonIdValidation = () => {
+	return stringMaxCharsValidation(20);
+};
+
+module.exports = horizonIdValidation;
