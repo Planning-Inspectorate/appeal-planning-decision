4d0e3f1cca6f46837298f99205d5ae4ef192cc82
Chris Howarth <chris.howarth@kinandcarta.com>
2022-08-30 17:01:52 +0100

    fix(as-5330): full appeal unit test cleanup

--- packages/forms-web-app/__tests__/unit/controllers/full-appeal/use-a-different-service.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/full-appeal/use-a-different-service.test.js
@@ -2,7 +2,11 @@ const {
 	getUseADifferentService
 } = require('../../../../src/controllers/full-appeal/use-a-different-service');
 
-const { VIEW } = require('../../../../src/lib/views');
+const {
+	VIEW: {
+		FULL_APPEAL: { USE_A_DIFFERENT_SERVICE }
+	}
+} = require('../../../../src/lib/views');
 const { mockReq, mockRes } = require('../../mocks');
 
 describe('controllers/full-appeal/use-a-different-service', () => {
@@ -12,7 +16,7 @@ describe('controllers/full-appeal/use-a-different-service', () => {
 	it('Test the getPlanningDepartment method calls the correct template', async () => {
 		await getUseADifferentService(req, res);
 
-		expect(res.render).toBeCalledWith(VIEW.FULL_APPEAL.USE_A_DIFFERENT_SERVICE, {
+		expect(res.render).toBeCalledWith(USE_A_DIFFERENT_SERVICE, {
 			acpLink: 'https://acp.planninginspectorate.gov.uk/'
 		});
 	});
