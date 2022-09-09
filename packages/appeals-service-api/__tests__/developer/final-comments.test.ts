bdc8208da4c92bfcc7d16c782a78bf2cfedfabb9
Martyn Lloyd-Kelly <martynlloyd-kelly@Martyns-MacBook-Pro.local>
2022-09-09 16:44:32 +0100

    feat(AS-5408): working on happy path test

--- packages/appeals-service-api/__tests__/developer/final-comments.test.ts
+++ packages/appeals-service-api/__tests__/developer/final-comments.test.ts
@@ -1,4 +1,5 @@
 import { env } from 'node:process';
+const axios = require('axios');
 const {
 	createAMQPTestQueue,
 	sendMessageToAMQPTestQueue,
@@ -8,6 +9,7 @@ const {
 
 if (env.FINAL_COMMENT_FEATURE_ACTIVE) {
 	jest.setTimeout(60000);
+	jest.mock('axios');
 
 	beforeAll(async () => {
 		await createAMQPTestQueue();
@@ -18,76 +20,69 @@ if (env.FINAL_COMMENT_FEATURE_ACTIVE) {
 	});
 
 	describe('AS-5408', () => {
-		it('works', async () => {
-			sendMessageToAMQPTestQueue(`message1`);
-			let message = await getMessageFromAMQPTestQueue();
-			expect(message).toBe(`message1`);
+		// it('works', async () => {
+		// 	sendMessageToAMQPTestQueue(`message1`);
+		// 	let message = await getMessageFromAMQPTestQueue();
+		// 	expect(message).toBe(`message1`);
+		// });
+
+		// it('works again', async () => {
+		// 	sendMessageToAMQPTestQueue(`message2`);
+		// 	let message = await getMessageFromAMQPTestQueue();
+		// 	expect(message).toBe(`message2`);
+		// });
+
+		it('should be possible to upload final comments to an appeal during the final comments window', async () => {
+			// Given: the current date is 15th September 2022
+			jest.useFakeTimers().setSystemTime(new Date(2022, 9, 15));
+
+			// And: an appeal whose final comments window is 16th September 2022
+			axios.patch.mockResolvedValue(createHorizonResponse('2022-09-16T00:00:00+00:00')); // TODO: we should also specify the URL so we know the implementation is calling the expected URL
+
+			// When: the appellent uploads final comments within the final comments window
+			let finalComments = '¯_(ツ)_/¯';
+			// const response = request.patch('appeal/{id}/final_comments').withBody({"finalComments": finalComments});
+
+			// Then: we should get a 2xx in the response
+			// expect(response.status()).toBe("202") // Accepted for processing later
+
+			// And: the application with final comments should appear on the Horizon input queue for processing
+			// let expectedApplication = {}
+			let message = getMessageFromAMQPTestQueue();
+			expect(message).toBe(expectedApplication);
 		});
-
-		it('works again', async () => {
-			sendMessageToAMQPTestQueue(`message2`);
-			let message = await getMessageFromAMQPTestQueue();
-			expect(message).toBe(`message2`);
-		});
-
-		// it('should be possible to upload final comments to an appeal if this is attempted during the final comments window', async () => {
-
-		// 	// Given: a request date of the 15th September 2022
-		// 	const requestDate = new Date(2022, 9, 15);
-
-		// 	// And: an appeal whose final comments window spans 14th September 2022 to 16th September 2022
-		// 	const finalCommentsWindowStart = new Date(2022, 9, 14);
-		// 	const finalCommentsWindowEnd = new Date(2022, 9, 16);
-		// 	horizonMock.get('case/' + application.id).shouldReturn(horizonApplication);
-
-		// 	// When: the appellent uploads final comments within the final comments window
-		// 	application.finalComments = // ¯\_(ツ)_/¯
-		// 	const response = request.post('application/{id}/final_comments');
-
-		// 	// Then: we should get a 2xx in the response
-
-		// 	// And: the application with final comments should appear on the Horizon input queue for processing
-		// 	queue.getFirst() == application;
-		// });
-
-		// it('should not be possible to upload final comments to an appeal if this is attempted before the final comments window', async () => {
-		// 	//Given
-		// 	// I am appellant who has submitted an application post /application
-		// 	// Aquire relevant appeals object from horizon
-		// 	requestDate = // some date
-		// 		horizonApplication.finalCommentsWindowStart = // request date + 1 day
-		// 		horizonApplication.finalCommentsWindowEnd = // request date + 2 day
-		// 			horizonMock.get('case/' + application.id).shouldReturn(horizonApplication); // Make work good
-
-		// 	//When: the appellent uploads final comments within the final comments window
-		// 	application.finalComments = // ¯\_(ツ)_/¯
-		// 		response = request.post('application/{id}/final_comments');
-
-		// 	//Then: we should get a 4xx in the response
-		// 	response.status().shouldEqual('4xx');
-
-		// 	// And: the application with final comments should not appear on the Horizon input queue for processing
-		// 	queue.isEmpty();
-		// });
-
-		// it('should not be possible to upload final comments to an appeal if this is attempted after the final comments window', async () => {
-		// 	//Given
-		// 	// I am appellant who has submitted an application post /application
-		// 	// Aquire relevant appeals object from horizon
-		// 	requestDate = // some date
-		// 		horizonApplication.finalCommentsWindowStart = // request date - 2 day
-		// 		horizonApplication.finalCommentsWindowEnd = // request date - 1 day
-		// 			horizonMock.get('case/' + application.id).shouldReturn(horizonApplication); // Make work good
-
-		// 	//When: the appellent uploads final comments within the final comments window
-		// 	application.finalComments = // ¯\_(ツ)_/¯
-		// 		response = request.post('application/{id}/final_comments');
-
-		// 	//Then: we should get a 4xx in the response
-		// 	response.status().shouldEqual('4xx');
-
-		// 	// And: the application with final comments should not appear on the Horizon input queue for processing
-		// 	queue.isEmpty();
-		// });
 	});
+}
+
+function createHorizonResponse(finalCommentsDueDate) {
+	return {
+		Envelope: {
+			Body: {
+				GetCaseResponse: {
+					GetCaseResult: {
+						Metadata: {
+							Attributes: {
+								AttributeValue: {
+									Name: {
+										value: 'Case Document Dates:Final Comments Due Date'
+									},
+									Value: {
+										value: finalCommentsDueDate
+									}
+								},
+								AttributeValue: {
+									Name: {
+										value: 'Curb your enthusiasm'
+									},
+									Value: {
+										value: 'frolic'
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	};
 }
