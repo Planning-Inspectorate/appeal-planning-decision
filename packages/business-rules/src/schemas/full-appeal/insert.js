0890585097a6fb43d43592ecd2326a2d3a8ef526
Guilherme Ribeiro <Gui>
2022-09-12 10:12:28 +0100

    fix: fixed unit tests and eslint

--- packages/business-rules/src/schemas/full-appeal/insert.js
+++ packages/business-rules/src/schemas/full-appeal/insert.js
@@ -1,7 +1,5 @@
-const SchemaModel = require('../schema-model');
 const appealValidationSchema = require('./appeal-schema');
 
-SchemaModel.setValidationAction('insert');
 const insert = appealValidationSchema();
 
 module.exports = insert;
