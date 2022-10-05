c6aa2210cc113d01bd19bcbbf6efea2a283b33b9
Martyn Lloyd-Kelly <martynlloyd-kelly@Martyns-MacBook-Pro.local>
2022-10-05 13:04:25 +0100

    feat(AS-5031): req obj available to feature flag module now

--- packages/document-service-api/config/featureFlag.js
+++ packages/document-service-api/config/featureFlag.js
@@ -1,80 +1,46 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
+const { AppConfigurationClient } = require('@azure/app-configuration');
+const getAsyncLocalStorage = require('./asyncLocalStorage');
 
-/**
- * @summary Feature flags are settings that follow specific JSON schema for the value.
- */
- const {
-    AppConfigurationClient,
-    ConfigurationSetting,
-    featureFlagContentType,
-    FeatureFlagValue,
-    parseFeatureFlag
-  } = require("@azure/app-configuration");
+const connectionString =
+	'Endpoint=https://pins-asc-appeals-service-dev-ukw-001.azconfig.io;Id=xbZ0-ln-s0:DSk9gGngBpWQ2wivO6nB;Secret=tMBrLU5G3geDGvVUeteY3tQXZDY7SBbCAnSwYzLg1cg=';
+const appConfigClient = new AppConfigurationClient(connectionString);
 
-  const AsyncLocalStorage = require('node:async_hooks')
-  const asyncLocalStorage = new AsyncLocalStorage();
-  
-  // Load the .env file if it exists
-  // import * as dotenv from "dotenv";
-  // dotenv.config();
+const isFeatureActive = async (featureFlagName) => {
+	// TODO: grab a user/user-group identifier from this and pass it along to the
+	// request sent to the feature flag server so it can target the user/user-group
+	// when assessing if thefeature is active.
+	const request = getAsyncLocalStorage().getStore().get('request');
+	console.log(request);
 
-  // Set the following environment variable or edit the value on the following line.
-    const connectionString = "Endpoint=https://pins-asc-appeals-service-dev-ukw-001.azconfig.io;Id=xbZ0-ln-s0:DSk9gGngBpWQ2wivO6nB;Secret=tMBrLU5G3geDGvVUeteY3tQXZDY7SBbCAnSwYzLg1cg=";
-    const appConfigClient = new AppConfigurationClient(connectionString);
+	let flagName = featureFlagName.toString().trim();
 
-    const isFeatureActive = async (featureFlagName) => {
+	if (!flagName) {
+		console.error('A feature flag key must be supplied.');
+	} else {
+		try {
+			const result = await appConfigClient.getConfigurationSetting({
+				key: `.appconfig.featureflag/${flagName}`
+			});
 
-        const store = asyncLocalStorage.getStore();
-        console.log(store);
-        store.get("request")
-        // console.log('request')
-        // console.log(request)
-        // console.log('/request')
-        
-        // const useFeatureFlag = (featureFlagName = '') => {
-            let flagName = featureFlagName.toString().trim()
+			if (result && typeof result === 'object') {
+				console.debug('Feature: ' + JSON.parse(result.value).id, JSON.parse(result.value).enabled);
+				return JSON.parse(result.value).enabled;
+			}
+		} catch (error) {
+			console.error(error);
+			return false;
+		}
+	}
 
-        if (!flagName) {
-            console.error('A feature flag key must be supplied.');
-        } else {
-            try {
-                const result = await appConfigClient.getConfigurationSetting({
-                    key: `.appconfig.featureflag/${flagName}`,
-                });
+	return false;
+};
 
-                if (result && typeof result === 'object') {
-                    console.debug('Feature: ' + JSON.parse(result.value).id, JSON.parse(result.value).enabled);
-                    return JSON.parse(result.value).enabled;
-                }
+module.exports = isFeatureActive;
 
-                
-            } catch (error) {
-                console.error(error);
-                return false;
-            }
-            
-        }
-        
-        return false;
-    };
-
-    module.exports = isFeatureActive;
-
-//   /**
-//    * typeguard - for targeting client filter
-//    */
-//   function isTargetingClientFilter(
-//     clientFilter: any
-//   ): clientFilter is {
-//     parameters: {
-//       Audience: {
-//         Groups: Array<{ Name: string; RolloutPercentage: number }>;
-//         Users: Array<string>;
-//         DefaultRolloutPercentage: number;
-//       };
-//     };
-//   } {
+// /**
+//  * typeguard - for targeting client filter
+//  */
+// function isTargetingClientFilter(clientFilter) {
 //     return (
 //       clientFilter.name === "Microsoft.Targeting" &&
 //       clientFilter.parameters &&
@@ -85,3 +51,7 @@
 //     );
 //   }
 
+//   main().catch((err) => {
+//     console.error("Failed to run sample:", err);
+//     process.exit(1);
+//   });
