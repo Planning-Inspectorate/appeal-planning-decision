00538cc0fd4c40c4a2bbb6d63e9e5003d6e0390e
Chris Howarth <chris.howarth@local.amazerealise.com>
2022-10-18 15:54:21 +0100

    fix(as-5031): business rules additions to allow for additional doc date

--- packages/business-rules/src/schemas/components/update/document.js
+++ packages/business-rules/src/schemas/components/update/document.js
@@ -4,7 +4,9 @@ const document = () => {
 	return pinsYup.object().shape({
 		name: pinsYup.string().max(255).ensure().required(),
 		originalFileName: pinsYup.string().max(255).ensure().required(),
-		id: pinsYup.string().uuid().required()
+		id: pinsYup.string().uuid().required(),
+		horizonDocumentType: pinsYup.string().notRequired(),
+		horizonDocumentGroupType: pinsYup.string().notRequired()
 	});
 };
 
