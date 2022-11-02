68e45106de7b96d8cda625369697a585af05f4d4
Martyn Lloyd-Kelly <martynlloyd-kelly@Martyns-MacBook-Pro.local>
2022-11-02 11:16:55 +0000

    fix(as-5459): add Horizon-specifics, and correct day on date section

--- packages/platform-feature-tests/cypress/e2e/submit-full-appeal.cy.js
+++ packages/platform-feature-tests/cypress/e2e/submit-full-appeal.cy.js
@@ -2,7 +2,12 @@ const { submitAppealFlow } = require('../support/flows/appeal');
 
 describe('Appeal uploads', () => {
 	[
-		['no decision', 'hearing', 'in draft'],
+		[
+			'no decision',
+			'hearing',
+			'in draft',
+			{ expectedFolderHierarchy: ['', ''], expectedFileName: 'appeal-statement-valid.pdf' }
+		],
 		['refused', 'written', 'finalised']
 	].forEach((context) => {
 		it(`sends a householder planning application successfully to Horizon where the original application status is "${context[0]}", the decision type requested is "${context[1]}" and the planning obligation status is "${context[2]}"`, () => {
@@ -12,7 +17,36 @@ describe('Appeal uploads', () => {
 				statusOfPlanningObligation: context[2]
 			});
 
-			// TODO: code out what we think should be done in order for Horizon to check outcome
+			// TODO: this code below needs testing with Horizon when we get automated test access!
+
+			/*
+			cy.visit('https://horizontest.planninginspectorate.gov.uk/otcs/cs.exe?func=llworkspace')
+			// TODO: figure out how to interact with Drop down for username and password that is part of browser here
+			
+			// Horizon main screen
+			cy.get('#rowCell0').get('.browseItemName>a').eq(0).click()
+
+			cy.get('#rowCell17').get('.browseItemName>a').eq(0).click() // Clicks on appeal type (not householder)
+
+			cy.get('#browseViewCoreTable').get('.browseListHeaderName>a').eq(0).click() // Puts latest appeals at top of list
+			cy.get('#rowCell0').get('.browseItemName>a').eq(0).click()
+			
+			cy.get('#browseViewCoreTable').get('.browseListHeaderCenterText>a') // Puts latest appeals to top
+			cy.get('#rowCell0').get('.browseItemName>a').eq(0).click()
+
+			// For loop for each doc type
+			let docTypeFolderMappings = context[3];
+
+			for (docTypeFolderMapping in docTypeFolderMappings) {
+				
+				for (expectedFolderName in docTypeFolderMapping.expectedFolderHierarchy) {
+					cy.get('#browseViewCoreTable').get('.browseItemName').get(`a:contains(${expectedFolderName})`).click();
+				}
+
+				cy.get('#browseViewCoreTable').get('browseItemName').get(`a:contains(${expectedFileName})`).should('be.visible') // This is where we check if the file is in the right place
+				cy.get('#trail>a').eq(4).click()// Then we need to go bacck to the top level of the folder structure
+			}
+			*/
 		});
 	});
 });
