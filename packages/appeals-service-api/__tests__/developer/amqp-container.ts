57769a98d958986536bacaeae2da460effa1d0bb
Ed Packard <edward.packard@kinandcarta.com>
2022-11-10 10:55:56 +0000

    test(as-5559): wip

--- packages/appeals-service-api/__tests__/developer/amqp-container.ts
+++ packages/appeals-service-api/__tests__/developer/amqp-container.ts
@@ -10,18 +10,15 @@
  */
 import { GenericContainer, Wait, StartedTestContainer } from 'testcontainers/';
 import { AMQPClient, AMQPChannel, AMQPQueue } from '@cloudamqp/amqp-client';
-
-export class AMQPContainer {
-	startedContainer: StartedTestContainer;
-	amqpConnection: AMQPClient;
-	channel: AMQPChannel;
-	queue: AMQPQueue;
+import { AMQPTestConfiguration } from './amqp-test-configuration';
+export class AMQPTestMessageQueue {
+	private amqpTestConfiguration: AMQPTestConfiguration;
 
 	static async create(queueName: string) {
 		// We used the rabbitmq image rather than rabbitmq:management image since we don't
 		// need any management of queues via a browser as part of tests.
 		let container = await new GenericContainer('rabbitmq')
-			.withExposedPorts(5672, 15672)
+			.withExposedPorts(5672)
 			.withWaitStrategy(Wait.forLogMessage('Server startup complete'))
 			.start();
 
@@ -34,7 +31,6 @@ export class AMQPContainer {
 		let port = container.getMappedPort(5672);
 		process.env.HORIZON_HAS_PUBLISHER_PORT = port.toString();
 		process.env.HORIZON_HAS_PUBLISHER_QUEUE = 'test';
-		console.log(container.getMappedPort(15672));
 
 		// We need to get the exact port number since Testcontainers assigns a random port
 		// (by design) on the host machine to map to the one you expose in the container
@@ -43,44 +39,27 @@ export class AMQPContainer {
 		let channel = await connection.channel();
 		let queue = await channel.queue(queueName);
 
-		return {
-			container: container,
-			connection: connection,
-			channel: channel,
-			queue: queue
-		};
+		return new AMQPTestConfiguration(container, connection as AMQPClient, channel, queue);
 	}
 
-	constructor(config: {
-		container: StartedTestContainer;
-		connection: AMQPClient;
-		channel: AMQPChannel;
-		queue: AMQPQueue;
-	}) {
-		this.startedContainer = config.container;
-		this.amqpConnection = config.connection;
-		this.channel = config.channel;
-		this.queue = config.queue;
+	constructor(config: AMQPTestConfiguration) {
+		this.amqpTestConfiguration = config;
 	}
 
 	async sendMessageToQueue(msg: string) {
-		console.log(`Sending message "${msg}" to queue "${this.queue}"`);
-		this.queue.publish(msg);
+		this.amqpTestConfiguration.getQueue().publish(msg);
 	}
 
 	async getMessageFromQueue(): Promise<string> {
 		let message;
-		await this.queue.get().then((msg) => (message = msg?.bodyToString()));
+		await this.amqpTestConfiguration
+			.getQueue()
+			.get()
+			.then((msg) => (message = msg?.bodyToString()));
 		return message;
 	}
 
 	async teardown() {
-		try {
-			await this.channel.close();
-			await this.amqpConnection.close();
-			await this.startedContainer.stop();
-		} catch (e) {
-			console.log(e);
-		}
+		await this.amqpTestConfiguration.teardown();
 	}
 }
