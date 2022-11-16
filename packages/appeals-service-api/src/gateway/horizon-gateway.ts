0df1fd8ba037b4943c699a8c8022672c34c4f4b7
Martyn Lloyd-Kelly <martynlloyd-kelly@Martyns-MacBook-Pro.local>
2022-11-16 14:16:48 +0000

    feat(as-5459): wip: trying to set-up mock server for Horizon gateway

--- packages/appeals-service-api/src/gateway/horizon-gateway.ts
+++ packages/appeals-service-api/src/gateway/horizon-gateway.ts
@@ -1,7 +1,7 @@
-const axios = require('axios').default
 const jp = require('jsonpath');
-const config = require('../configuration/config')
-
+const config = require('../configuration/config');
+const axios = require('axios');
+const fetch = require('node-fetch')
 export class HorizonGateway {
 
     async getFinalCommentsDueDate(caseReference: string): Promise<Date | undefined> {
@@ -14,9 +14,17 @@ export class HorizonGateway {
             }
         }    
         
-        console.log(config.services.horizon.url);
-        const caseDetails = await axios.post(config.services.horizon.url, requestBody);
-        const finalCommentsDueDate = jp.query(caseDetails.body, '$..Attributes[*].Name[?(@.Name.value=="Case Document Dates:Final Comments Due Date")]');
+        console.log(config.services.horizon.url, requestBody)
+        // const caseDetails = await got.post(config.services.horizon.url, { json: requestBody}).json();
+        let caseDetails = await fetch(config.services.horizon.url, {
+            method: "POST",
+            body: JSON.stringify(requestBody),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            }
+        })
+        console.log(caseDetails);
+        const finalCommentsDueDate = jp.query(caseDetails, '$..Attributes[*].Name[?(@.Name.value=="Case Document Dates:Final Comments Due Date")]');
         console.log(finalCommentsDueDate);
         if (finalCommentsDueDate == false) {
             return undefined;
