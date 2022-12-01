2b61c9e135100c684876b0b3d63f598b46557192
Ed Packard <edward.packard@kinandcarta.com>
2022-12-01 10:20:30 +0000

    fix(as-5031): update logging

--- packages/appeals-service-api/src/services/appeal.service.js
+++ packages/appeals-service-api/src/services/appeal.service.js
@@ -52,7 +52,7 @@ async function getAppeal(id) {
 	}
 
 	logger.info(`Appeal ${id} retrieved`);
-	logger.info('DOCUMENT APPEAL:', document.appeal);
+	logger.info(`DOCUMENT APPEAL: ${document.appeal}`);
 	return document.appeal;
 }
 
