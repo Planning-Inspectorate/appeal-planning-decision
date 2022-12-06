f608aa1c542b67ed797a51e094e11babd70c950f
Chris Howarth <chris.howarth@local.amazerealise.com>
2022-12-06 13:51:25 +0000

    fix(as-5031): removing invalid test

--- packages/forms-web-app/__tests__/unit/lib/appeals-api-wrapper.test.js
+++ packages/forms-web-app/__tests__/unit/lib/appeals-api-wrapper.test.js
@@ -4,7 +4,6 @@ const fetch = require('node-fetch');
 const uuid = require('uuid');
 const {
 	createOrUpdateAppeal,
-	submitAppeal,
 	getExistingAppeal,
 	getLPAList,
 	saveAppeal,
@@ -106,23 +105,6 @@ describe('lib/appeals-api-wrapper', () => {
 			} catch (e) {
 				expect(e.toString()).toEqual('Error: something went wrong');
 			}
-		});
-	});
-
-	describe('submitAppeal', () => {
-		it(`should call the expected URL`, async () => {
-			fetch.mockResponseOnce(JSON.stringify({ shouldBe: 'valid' }));
-			const appealsApiResponse = await submitAppeal({ id: '123-abc' });
-
-			expect(fetch).toHaveBeenCalledWith(`${config.appeals.url}/api/v1/appeals/123-abc`, {
-				body: '{"id":"123-abc"}',
-				headers: {
-					'Content-Type': 'application/json',
-					'X-Correlation-ID': uuid.v4()
-				},
-				method: 'PATCH'
-			});
-			expect(appealsApiResponse).toEqual({ shouldBe: 'valid' });
 		});
 	});
 
