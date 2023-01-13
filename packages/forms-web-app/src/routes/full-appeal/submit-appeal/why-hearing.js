aafd7b6fae6b2c4460b0749b640e886399aeafe1
james.hill1@wrenkitchens.com <james.hill1@wrenkitchens.com>
2023-01-13 12:46:45 +0000

    fix(as-5691): update textfield validation from 255 chars to 1000
    increase character limit on why-hearing and why-inquiry textfield character limit to 1000 characters
    
    as-5691

--- packages/forms-web-app/src/routes/full-appeal/submit-appeal/why-hearing.js
+++ packages/forms-web-app/src/routes/full-appeal/submit-appeal/why-hearing.js
@@ -8,6 +8,7 @@ const { validationErrorHandler } = require('../../../validators/validation-error
 const { rules: textfieldValidationRules } = require('../../../validators/common/textfield');
 
 const router = express.Router();
+const textFieldMaxCharacters = 1000;
 
 router.get('/submit-appeal/why-hearing', [fetchExistingAppealMiddleware], getWhyHearing);
 router.post(
@@ -15,7 +16,8 @@ router.post(
 	textfieldValidationRules({
 		fieldName: 'why-hearing',
 		emptyError: 'Enter why you would prefer a hearing',
-		tooLongError: 'Hearing information must be $maxLength characters or less'
+		tooLongError: `Inquiry information must be ${textFieldMaxCharacters} characters or less`,
+		maxLength: textFieldMaxCharacters
 	}),
 	validationErrorHandler,
 	postWhyHearing
