cc23d83b413e6d5c92fad2eb5477459df9d4ee69
James Hill <james.hill1@wrenkitchens.com>
2023-01-26 17:44:44 +0000

    fix(as-5335): began writing new javascript to support multi file upload component

--- packages/forms-web-app/src/lib/client-side/index.js
+++ packages/forms-web-app/src/lib/client-side/index.js
@@ -2,6 +2,8 @@
 
 const { cookieConsentHandler } = require('./cookie/cookie-consent');
 const { initialiseOptionalJavaScripts } = require('./javascript-requiring-consent');
+const { initialiseMultiFileUpload } = require('./components/multi-file-upload.js');
 
 cookieConsentHandler(document);
 initialiseOptionalJavaScripts(document);
+initialiseMultiFileUpload(document);
