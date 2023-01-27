23c25a488c215eeebeb7dfc9cfe09b4e42adf0ad
Will Adams <willadams@will-adams.home>
2023-01-27 10:47:11 +0000

    adding missing files

--- packages/platform-feature-tests/cypress.config.js
+++ packages/platform-feature-tests/cypress.config.js
@@ -1,8 +1,24 @@
 const { defineConfig } = require('cypress');
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
 	e2e: {
+		async setupNodeEvents(on, config){
+			await preprocessor.addCucumberPreprocessorPlugin (on, config);
+
+			on(
+				"file:preprocessor",
+				createBundler({
+					plugins: [createEsbuildPlugin.default(config)],
+				})
+			);
+
+			
+		},
+		specPattern: 'platform-feature-tests/e2e/**/*.feature',
 		appeals_beta_base_url: 'https://appeals-service-test.planninginspectorate.gov.uk', //TODO: make this an env var?
 		horizon_base_url: 'http://10.0.7.4:8000/horizon'
 	}
-});
+}); 
