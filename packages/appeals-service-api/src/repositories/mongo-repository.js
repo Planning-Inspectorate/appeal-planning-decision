dd04ff022f304daa977d24c0a7cf82fd30275867
Alex Moulin <alex.moulin@planninginspectorate.gov.uk>
2023-02-02 11:05:03 +0000

    fix: wip: mongodb now updating

--- packages/appeals-service-api/src/repositories/mongo-repository.js
+++ packages/appeals-service-api/src/repositories/mongo-repository.js
@@ -6,9 +6,8 @@ const logger = require('../lib/logger');
  * This is intended to be used as an [abstract class]{@link https://en.wikipedia.org/wiki/Abstract_type}.
  */
 class MongoRepository {
-
 	/**
-	 * 
+	 *
 	 * @param {string} collectionName The name of the Mongo collection that this repository is intended to handle.
 	 */
 	constructor(collectionName) {
@@ -16,10 +15,10 @@ class MongoRepository {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param {any} query This should be JSON that will enable a query on the collection that this
 	 * repository is intended to handle. See {@link https://www.mongodb.com/docs/manual/tutorial/query-documents/}
-	 * for more details. 
+	 * for more details.
 	 * @returns {Promise<Model>} The document found as a result of executing the `query` specified on the
 	 * collection specified via the constructor.
 	 */
@@ -28,7 +27,7 @@ class MongoRepository {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param {Model} model The model to insert into the collection specified by the constructor
 	 * @returns {any} The JSON to insert.
 	 */
@@ -41,51 +40,64 @@ class MongoRepository {
 	}
 
 	/**
-	 * 
-	 * @param {AggregateDifference[]} aggregateDifferences 
-	 * @returns 
+	 *
+	 * @param {AggregateDifference[]} aggregateDifferences
+	 * @returns
 	 */
 	async updateMany(aggregateDifferences) {
-		logger.info(aggregateDifferences, "Differences to process via update")
+		logger.info(aggregateDifferences, 'Differences to process via update');
 		const collection = mongodb.get().collection(this.collectionName);
-		const updateOneOperations = aggregateDifferences.flatMap(aggregateDifference => {
-			
-			return aggregateDifference.getEntityDifferences().map(entityDifference => {
-				logger.info(entityDifference, "entity difference");
-				const result = { updateOne : {
-					filter : {
-						$and: [
-							{ _id: new ObjectId(aggregateDifference.getAggregateId()) },
-							{
-								documents: {
-									$elemMatch: {
-										id: entityDifference.getId()
-									}
-								}
-							}
-						] 
-					},
-					update : { $set: { horizon_id: entityDifference.getBackOfficeId() }},
-					upsert : true,
-				}}
+		// const updateOneOperations = aggregateDifferences.map(aggregateDifference => {
+		// 		const result = { updateOne : {
+		// 			filter : {
+		// 				_id: new ObjectId(aggregateDifference.id)
+		// 			},
+		// 			update : {
+		// 				$set:	{
+		// 					"organisations": aggregateDifference.organisations,
+		// 					"contacts" : aggregateDifference.contacts,
+		// 					"documents" : aggregateDifference.documents
+		// 				}
+		// 			},
+		// 			upsert : true,
+		// 		}};
+		// 		logger.info(result, "Update created!")
+		// 		return result;
+		// 	});
 
-				logger.info(result, "Update created!")
-				return result;
-			})
+		const updateOneOperations = aggregateDifferences.map((aggregateDifference) => {
+			return {
+				updateOne: {
+					filter: {
+						_id: new ObjectId(aggregateDifference._id)
+					},
+					update: {
+						$set: {
+							organisations: aggregateDifference.organisations,
+							contacts: aggregateDifference.contacts,
+							appeal: aggregateDifference.appeal,
+							documents: aggregateDifference.documents
+						}
+					},
+					upsert: true
+				}
+			};
 		});
-		logger.info(updateOneOperations, "Updating docs");
-		return await collection.bulkWrite(updateOneOperations)
+		logger.info(updateOneOperations, 'Updating docs');
+		return await collection.bulkWrite(updateOneOperations);
 	}
 
 	/**
-	 * 
-	 * @param {string[]} ids 
-	 * @returns 
+	 *
+	 * @param {string[]} ids
+	 * @returns
 	 */
 	async deleteMany(ids) {
-		const idsForFilter = ids.map(id => { return { _id: id } })
-		return await mongodb.get().collection(this.collectionName).deleteMany({$or: idsForFilter});
+		const idsForFilter = ids.map((id) => {
+			return { _id: id };
+		});
+		return await mongodb.get().collection(this.collectionName).deleteMany({ $or: idsForFilter });
 	}
 }
 
-module.exports = { MongoRepository }
+module.exports = { MongoRepository };
