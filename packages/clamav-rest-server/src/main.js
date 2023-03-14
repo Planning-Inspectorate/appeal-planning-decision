e1521e6b7b891f8d0b04e366ef47f3c730db2b2a
Mark Taylor <mark.taylor@kinandcarta.com>
2023-03-14 16:06:50 +0000

    feat(updated the clamav rest service to include application insights): added applicaiton isights
    Added Applicaiton insights to the clamAV rest service and inistalised it
    
    AS-5744

--- packages/clamav-rest-server/src/main.js
+++ packages/clamav-rest-server/src/main.js
@@ -3,10 +3,16 @@
  *
  * This is the main starting point for the application.
  */
+const appInsights = require('applicationinsights');
 const logger = require('./lib/logger');
 const server = require('./server');
 
 async function main() {
+	try {
+		appInsights.setup().start();
+	} catch (err) {
+		logger.warn({ err }, 'Application insights failed to start: ');
+	}
 	server();
 }
 
