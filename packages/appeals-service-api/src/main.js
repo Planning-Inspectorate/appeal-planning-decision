4210bd21c474ff13abd74a55c5a368205633eac0
Mark Taylor <mark.taylor@kinandcarta.com>
2023-03-14 15:51:24 +0000

    feat(runtime configuration of telemetry): adds and initialises application insights lib at startup
    Imports the library needed to run Azure Application Insights to gather telemerty data on the service
    and intilises the service with the required settings, with the APPLICATION_INSIGHTS_* environment
    variables used to externalise these settings
    
    AS-5744

--- packages/appeals-service-api/src/main.js
+++ packages/appeals-service-api/src/main.js
@@ -3,11 +3,18 @@
  *
  * This is the main starting point for the application.
  */
+const appInsights = require('applicationinsights');
 const logger = require('./lib/logger');
 const server = require('./server');
 const mongodb = require('./db/db');
 
 async function main() {
+	try {
+		appInsights.setup().start();
+	} catch (err) {
+		logger.warn({ err }, 'Application insights failed to start: ');
+	}
+
 	await mongodb.connect(() => {
 		server();
 	});
