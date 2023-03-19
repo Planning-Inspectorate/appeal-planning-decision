7f976afe430b04740c0c7667c6296260722ea77b
Will Adams <willadams@will-adams.lan>
2023-03-19 15:30:09 +0000

    page-objects for first half of submitting an appeal

--- packages/platform-feature-tests/cypress/e2e/submit-appeal/submit-appeal.feature
+++ packages/platform-feature-tests/cypress/e2e/submit-appeal/submit-appeal.feature
@@ -2,5 +2,7 @@ Feature: Before you start
 
     Scenario: Appelant views pages and clicks contiune
       Given appellant is on the Before You Start first page
-      When appellant clicks on the continue button
+      Then appellant clicks on the continue button
       Then appellant is navigated to the Local Planning Department page
+      Then appellant enters local planning authority
+      Then appellant clicks on the continue button
---
+++ packages/platform-feature-tests/cypress/page-objects/appeals-sections/type-of-planning.js
@@ -0,0 +1,17 @@
+import { BasePage } from '../../page-objects/base-page'
+
+export class TypeOfPlanning extends BasePage {
+
+    elements = {
+        fullPlanningRadioBtn : () => cy.get('[data-cy="answer-householder-planning"]'),
+        houseHolderPlanningRadioBtn : () => cy.get('[data-cy="answer-full-appeal"]')
+    }
+
+    selectFullPlanning() {
+        this.elements.fullPlanningRadioBtn().click()
+    }
+
+    selectHouseHolderPlanning() {
+        this.elements.houseHolderPlanningRadioBtn().click()
+    }
+}
