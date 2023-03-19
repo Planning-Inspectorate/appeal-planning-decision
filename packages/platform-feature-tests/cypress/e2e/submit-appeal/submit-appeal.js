4755d4ce8f815ece81985ec989b254184a570378
Will Adams <willadams@will-adams.lan>
2023-03-19 15:30:09 +0000

    page-objects for first half of submitting an appeal

--- packages/platform-feature-tests/cypress/e2e/submit-appeal/submit-appeal.js
+++ packages/platform-feature-tests/cypress/e2e/submit-appeal/submit-appeal.js
@@ -1,21 +1,28 @@
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
-import { basePage } from '../../page-objects/base-page';
+import { BasePage } from '../../page-objects/base-page';
+import { BeforeYouStart } from '../../page-objects/appeals-sections/before-you-start';
 
-const BasePage = new basePage;
+const basePage = new BasePage;
+const beforeYouStart = new BeforeYouStart;
 
-
-Given('appellant is on the Before You Start first page', function() {
-  cy.visit(`${Cypress.config('appeals_beta_base_url')}/before-you-start`); // navigates to the 
-  BasePage.verifyPageHeading('Before you start'); // 
+Given('appellant is on the Before You Start first page', () => {
+  cy.visit(`${Cypress.config('appeals_beta_base_url')}/before-you-start`); // navigates to the appeals url
+  basePage.verifyPageHeading('Before you start'); // verifies that you are on the right page
   });
 
-  When('appellant clicks on the continue button', function() {
-    BasePage.clickContiuneBtn(); // clicks on the contiune button  
+  Then('appellant clicks on the continue button', () => {
+    basePage.clickContinueBtn(); // clicks on the contiune button  
   });
 
-  Then('appellant is navigated to the Local Planning Department page', () => {
-    cy.url().should('include', 'local-planning-department');
+  Then('appellant is navigated to the Local Planning Department page', () =>  {
+    cy.url().should('include', 'local-planning-department'); // makes sure we are on the correct local planning page
   });
-  
 
-  
+  Then('appellant enters local planning authority', () =>  {
+    beforeYouStart.enterLPA('System Test Borough Council');
+    beforeYouStart.selectLPA();
+  });
+
+  Then('appellant clicks on the continue button', () => {
+    basePage.clickContinueBtn(); // clicks on the contiune button  
+  });
--- packages/platform-feature-tests/cypress/page-objects/base-page.js
+++ packages/platform-feature-tests/cypress/page-objects/base-page.js
@@ -1,14 +1,22 @@
-export class basePage {
+export class BasePage {
 
     elements = {
         acceptCookiesBtn: () => cy.get('[data-cy="cookie-banner-accept-analytics-cookies"]'),
         declineCookiesBtn: () => cy.get('[data-cy="cookie-banner-reject-analytics-cookies"]'), 
         pageHeading: () => cy.get('.govuk-heading-l'),
-        continueBtn: () => cy.get('[data-cy="start-button"]')
+        continueBtn: () => cy.get('[data-cy="start-button"]'),
+        radioBtn: () => cy.get('.govuk-radios__item'),
+        checkBox: () => cy.get('.govuk-checkboxes__item'),
+        backBtn: () => cy.get('[data-cy="back"]'),
+        saveAndContiuneBtn: () => cy.get('[data-cy="button-save-and-continue"]')
     }
 
-    clickContiuneBtn(){
+    clickContinueBtn(){
         this.elements.continueBtn().click();
+    }
+
+    clickSaveAndContiuneBtn(){
+        this.elements.saveAndContiuneBtn().click();
     }
 
     acceptCookies(){
@@ -21,6 +29,14 @@ export class basePage {
 
     verifyPageHeading(pageHeading){
         this.elements.pageHeading().contains(pageHeading);
+    }
+
+    selectCheckBox(checkBoxValue){
+        this.elements.checkBox().check(checkBoxValue)
+    }
+
+    selectRadioBtn(radioBtnValue){
+        this.elements.radioBtn.check(radioBtnValue)
     }
 }
 
