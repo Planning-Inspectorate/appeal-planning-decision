acdafa92456b560a52e2f7ad20778297e1042611
Chris Howarth <chris.howarth@local.amazerealise.com>
2023-03-23 12:17:18 +0000

    fix(AS-5815): restructure of feature flags, moving the configuration to each package

--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -3,13 +3,11 @@ const healthcheck = require('./health');
 const prometheus = require('./prometheus');
 const utils = require('./utils');
 const documentTypes = require('./document-types');
-const { isFeatureActive } = require('./featureFlag');
 
 module.exports = {
 	functional,
 	healthcheck,
 	prometheus,
 	utils,
-	documentTypes,
-	isFeatureActive
+	documentTypes
 };
--- packages/document-service-api/test/developer/developer.test.js
+++ packages/document-service-api/test/developer/developer.test.js
@@ -6,7 +6,7 @@ const path = require('path');
 const each = require('jest-each').default;
 const { isBlobInStorage } = require('./testcontainer-helpers/azurite-container-helper');
 
-jest.mock('../../../common/src/featureFlag', () => ({
+jest.mock('../../src/configuration/featureFlag', () => ({
 	isFeatureActive: () => true
 }));
 
