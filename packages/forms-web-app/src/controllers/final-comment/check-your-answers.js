2dcdab309dae5445581811b76d825a9dd65612cf
Chris Howarth <chris.howarth@local.amazerealise.com>
2023-03-28 09:47:01 +0100

    feat(as-5787): console log cleanup

--- packages/forms-web-app/src/controllers/final-comment/check-your-answers.js
+++ packages/forms-web-app/src/controllers/final-comment/check-your-answers.js
@@ -1,20 +1,6 @@
 const { VIEW } = require('../../lib/views');
 
 exports.getCheckYourAnswers = async (req, res) => {
-	console.log('-----------------------------------------------');
-	console.log('-----------------------------------------------');
-	console.log('-----------------------------------------------');
-	console.log('-----------------------------------------------');
-	console.log('-----------------------------------------------');
-
-	console.log(req.session?.finalComment);
-
-	console.log('-----------------------------------------------');
-	console.log('-----------------------------------------------');
-	console.log('-----------------------------------------------');
-	console.log('-----------------------------------------------');
-	console.log('-----------------------------------------------');
-
 	res.render(VIEW.FINAL_COMMENT.CHECK_YOUR_ANSWERS, {
 		hasComments: req.session?.finalComment?.hasComments,
 		finalComments: req.session?.finalComment
