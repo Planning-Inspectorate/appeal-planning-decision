6d8898a402376ea994917517e62e77c44cc402c0
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-04-13 10:13:41 +0100

    fix(cookie-banner): remove cookie banner from html when cookie policy has been set

---
+++ packages/forms-web-app/__tests__/unit/middleware/set-locals-display-cookie-banner-value.test.js
@@ -0,0 +1,58 @@
+const {
+	setLocalslDisplayCookieBannerValue
+} = require('../../../src/middleware/set-locals-display-cookie-banner-value');
+
+describe('middleware/set-locals-display-cookie-banner-value', () => {
+	describe('#setLocalslDisplayCookieBannerValue', () => {
+		describe('When assigning a value to the display cookie banner locals variable', () => {
+			describe('and the cookie policy is defined', () => {
+				const mockReq = {
+					cookies: {
+						cookie_policy: 'mock cookie policy'
+					}
+				};
+
+				const mockRes = {
+					locals: {}
+				};
+
+				const mockNext = jest.fn();
+
+				beforeEach(() => {
+					setLocalslDisplayCookieBannerValue(mockReq, mockRes, mockNext);
+				});
+
+				it('should add the displayCookieBanner key with a value of false to the mockRes locals key', () => {
+					expect(mockRes.locals).toEqual({ displayCookieBanner: false });
+				});
+
+				it('should', () => {
+					expect(mockNext).toBeCalled();
+				});
+			});
+			describe('and the cookie policy is not defined', () => {
+				const mockReq = {
+					cookies: {}
+				};
+
+				const mockRes = {
+					locals: {}
+				};
+
+				const mockNext = jest.fn();
+
+				beforeEach(() => {
+					setLocalslDisplayCookieBannerValue(mockReq, mockRes, mockNext);
+				});
+
+				it('should add the displayCookieBanner key with a value of false to the mockRes locals key', () => {
+					expect(mockRes.locals).toEqual({ displayCookieBanner: true });
+				});
+
+				it('should', () => {
+					expect(mockNext).toBeCalled();
+				});
+			});
+		});
+	});
+});
