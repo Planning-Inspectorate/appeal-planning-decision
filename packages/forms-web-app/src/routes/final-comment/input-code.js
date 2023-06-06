ec6d6bf7207e779d6010fcea955fdee26910b660
Ed Packard <edward.packard@kinandcarta.com>
2023-06-06 16:22:18 +0100

    feat(as-5892): add local test route for final comment

--- packages/forms-web-app/src/routes/final-comment/input-code.js
+++ packages/forms-web-app/src/routes/final-comment/input-code.js
@@ -2,10 +2,15 @@ const express = require('express');
 const inputCodeController = require('../../controllers/final-comment/input-code');
 const { validationErrorHandler } = require('../../validators/validation-error-handler');
 const { rules: emailCodeValidationRules } = require('../../validators/final-comment/email-code');
+const checkFinalCommentTestEnabled = require('../../middleware/final-comment/check-final-comment-test-enabled');
 
 const router = express.Router();
 
-router.get('/input-code/:caseReference', inputCodeController.getInputCode);
+router.get(
+	'/input-code/:caseReference',
+	checkFinalCommentTestEnabled,
+	inputCodeController.getInputCode
+);
 router.get('/input-code/resend-code/:caseReference', inputCodeController.getInputCodeResendCode);
 
 router.post(
