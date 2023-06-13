19aeab28a461a66d5e9de139d9546e134d01f95f
charlie Benger-stevenson <suityou01@yahoo.co.uk>
2023-06-13 14:57:46 +0100

    test(/src/lib/token.js): reattempt token generation if len < 5
    If the length of the token is less than 5 characters then regenerate the token

--- packages/appeals-service-api/src/lib/token.js
+++ packages/appeals-service-api/src/lib/token.js
@@ -2,12 +2,23 @@ const crypto = require('crypto');
 const TOKEN_LENGTH = 5;
 const BUFFER_LENGTH = 20;
 const createToken = () => {
-	return crypto
+	let token = crypto
 		.randomBytes(BUFFER_LENGTH)
 		.toString('base64')
 		.replace(/[^\w_]/g, '')
 		.replace(/[AEIOUaeiou]/g, '')
 		.slice(0, TOKEN_LENGTH);
+
+	if (token.length === 5) return token;
+
+	token = crypto
+		.randomBytes(BUFFER_LENGTH)
+		.toString('base64')
+		.replace(/[^\w_]/g, '')
+		.replace(/[AEIOUaeiou]/g, '')
+		.slice(0, TOKEN_LENGTH);
+
+	return token;
 };
 
 module.exports = {
