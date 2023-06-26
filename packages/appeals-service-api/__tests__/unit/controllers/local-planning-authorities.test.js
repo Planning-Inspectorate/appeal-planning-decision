d1eaaf29d473d91eb009ff6f327562997aa32bff
Chris Hawley <christopher.hawley@kinandcarta.com>
2023-06-26 12:29:45 +0100

    feat(appeals service api tests): split out intregration and unit tests
    Split out integration and unit tests on the appeals service api so integration tests can be run only
    on commits to main
    
    AS-5961

---
+++ packages/appeals-service-api/__tests__/unit/controllers/local-planning-authorities.test.js
@@ -0,0 +1,88 @@
+const { list } = require('../../../src/controllers/local-planning-authorities');
+jest.mock('../../../src/services/lpa.service', () => {
+	return jest.fn().mockImplementation(() => ({
+		getLpaById: jest
+			.fn()
+			.mockImplementationOnce(() => ({
+				email: 'AppealPlanningDecisionTest@planninginspectorate.gov.uk',
+				name: 'System Test Borough Council',
+				getName: () => {
+					return 'System Test Borough Council';
+				}
+			}))
+			.mockImplementationOnce(() => {
+				throw new Error('Internal Server Error');
+			})
+			.mockImplementationOnce(() => ({
+				email: 'AppealPlanningDecisionTest@planninginspectorate.gov.uk',
+				name: 'System Test Borough Council',
+				getName: () => {
+					return 'System Test Borough Council';
+				}
+			}))
+			.mockImplementationOnce(() => {
+				throw new Error('Internal Server Error');
+			}),
+		getLpaList: jest
+			.fn()
+			.mockReturnValueOnce(['some array'])
+			.mockReturnValueOnce(['some-filtered-array'])
+	}));
+});
+const LpaService = require('../../../src/services/lpa.service');
+const lpaService = new LpaService();
+let req;
+let res;
+describe('LPAs controller test', () => {
+	beforeEach(() => {
+		req = {
+			log: {
+				info: jest.fn(),
+				debug: jest.fn(),
+				trace: jest.fn()
+			},
+			params: {},
+			query: {}
+		};
+		res = {
+			status: jest.fn(),
+			send: jest.fn()
+		};
+
+		res.status.mockReturnValue(res);
+	});
+
+	describe('#list', () => {
+		it('should return all LPAs sorted if no filter applied', async () => {
+			const data = ['some array'];
+			await list(req, res);
+
+			expect(res.send).toBeCalledWith({
+				data,
+				page: 1,
+				limit: data.length,
+				totalPages: 1,
+				totalResult: data.length
+			});
+		});
+
+		it('should return filtered LPAs if filter applied', async () => {
+			const filter = 'some-filter';
+			req.query = {
+				name: filter
+			};
+			const data = ['some-filtered-array'];
+			lpaService;
+
+			await list(req, res);
+
+			expect(res.send).toBeCalledWith({
+				data,
+				page: 1,
+				limit: data.length,
+				totalPages: 1,
+				totalResult: data.length
+			});
+		});
+	});
+});
---
+++ packages/appeals-service-api/__tests__/unit/controllers/save-and-return.test.js
@@ -0,0 +1,60 @@
+const { mockReq, mockRes } = require('../mocks');
+const { saveAndReturnCreate, saveAndReturnGet } = require('../../../src/controllers/save');
+const { updateAppeal } = require('../../../src/services/appeal.service');
+const {
+	createSavedAppealDocument,
+	getSavedAppealDocument
+} = require('../../../src/services/save-and-return.service');
+
+jest.mock('../../../src/services/save-and-return.service');
+jest.mock('../../../src/services/appeal.service');
+jest.mock('../../../../common/src/lib/notify/notify-builder', () => ({}));
+
+describe('Save And Return API', () => {
+	let req;
+	let res;
+
+	beforeEach(() => {
+		req = mockReq();
+		res = mockRes();
+	});
+
+	afterEach(() => {
+		jest.resetAllMocks();
+	});
+
+	describe('POST - create and send save and return', () => {
+		it('should respond with - OK 200', async () => {
+			const appealStub = {
+				id: '1233123123'
+			};
+			req.body = appealStub;
+
+			updateAppeal.mockReturnValue(appealStub);
+			await (() => expect(createSavedAppealDocument).toBeCalledTimes(1));
+
+			await saveAndReturnCreate(req, res);
+			expect(createSavedAppealDocument).toHaveBeenCalledWith(req.body.id);
+			expect(res.status).toHaveBeenCalledWith(201);
+		});
+
+		it('should respond with - ERROR 400 when appealId is null', async () => {
+			req.body = { appealId: null };
+
+			await expect(async () => saveAndReturnCreate(req, res)).rejects.toThrowError('');
+			expect(res.status).toHaveBeenCalledWith(400);
+			expect(createSavedAppealDocument).toBeCalledTimes(0);
+		});
+	});
+
+	describe('GET - get saved appeal', () => {
+		it('should retrieve saved appeal by appealId', async () => {
+			req.params = { id: '12345' };
+			getSavedAppealDocument.mockReturnValue({ appealId: '12345' });
+			await saveAndReturnGet(req, res);
+			expect(getSavedAppealDocument).toHaveBeenCalledWith('12345');
+			expect(res.status).toHaveBeenCalledWith(200);
+			expect(res.send).toHaveBeenCalledWith({ appealId: '12345' });
+		});
+	});
+});
