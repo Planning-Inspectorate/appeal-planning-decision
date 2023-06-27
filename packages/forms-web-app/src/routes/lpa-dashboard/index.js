8a84b3509713b1744af9e2a884d90d1a6a698ae2
Chris Howarth <chris.howarth@local.amazerealise.com>
2023-06-27 18:08:58 +0100

    feat(as-5896): added expired code route, routing to align with 5822

--- packages/forms-web-app/src/routes/lpa-dashboard/index.js
+++ packages/forms-web-app/src/routes/lpa-dashboard/index.js
@@ -5,4 +5,5 @@ router.use(require('./enter-code'));
 router.use(require('./lpa-dashboard'));
 router.use(require('./request-new-code'));
 router.use(require('./need-new-code'));
+router.use(require('./code-expired'));
 module.exports = router;
