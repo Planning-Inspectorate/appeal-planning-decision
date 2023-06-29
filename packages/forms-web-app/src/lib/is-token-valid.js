b986ca64ef30a1dfd4a8a6220a2de9c2d621e1e3
charlie Benger-stevenson <suityou01@yahoo.co.uk>
2023-06-29 16:33:07 +0100

    fix(regression issue regarding token checks): isTokenValid was failing unit tests
    Session object no longer passed to function causing regression issues

--- packages/forms-web-app/src/lib/is-token-valid.js
+++ packages/forms-web-app/src/lib/is-token-valid.js
@@ -13,10 +13,10 @@ const isTestLPACheckTokenAndSession = (token, session) => {
 	return utils.isTestLPA(session?.appeal?.lpaCode) && token === testConfirmEmailToken;
 };
 
-const getToken = async (id, token) => {
+const getToken = async (id, token, session) => {
 	let tokenDocument;
 	try {
-		tokenDocument = await checkToken(id, token);
+		tokenDocument = await checkToken(id, token, session?.enterCode?.action);
 		return tokenDocument;
 	} catch (err) {
 		console.log(err);
@@ -25,7 +25,7 @@ const getToken = async (id, token) => {
 		if (err.message === 'Too Many Requests') {
 			return {
 				valid: false,
-				action: '',
+				action: session?.enterCode?.action,
 				tooManyAttempts: true
 			};
 		} else {
@@ -34,7 +34,64 @@ const getToken = async (id, token) => {
 	}
 };
 
-const isTokenValid = async (id, token) => {
+/*
+const isTokenValid = async (id, token, session) => {
+	let result = {
+		valid: false,
+		action: ''
+	};
+
+	if (!id || typeof id !== 'string' || !token || typeof token !== 'string') {
+		return result;
+	}
+
+	// is test LPA + Test environment + test token
+	// then allow through as a confirm email token
+	if (
+		config.server.allowTestingOverrides &&
+		utils.isTestLPA(session?.appeal?.lpaCode) &&
+		token === testConfirmEmailToken
+	) {
+		result.valid = true;
+		result.action = enterCodeConfig.actions.confirmEmail;
+		return result;
+	}
+
+	let tokenDocument;
+	try {
+		tokenDocument = await checkToken(id, token, session?.enterCode?.action);
+	} catch (err) {
+		// todo: can we improve this to not rely on string matching,
+		// handler swallows response and only gives back status message
+		if (err.message === 'Too Many Requests') {
+			result.tooManyAttempts = true;
+			result.action = session?.enterCode?.action;
+			return result;
+		} else {
+			throw err;
+		}
+	}
+
+	if (tokenDocument === null || typeof tokenDocument !== 'object') {
+		return result;
+	}
+
+	const tokenCreatedTime = Object.keys(tokenDocument).includes('createdAt')
+		? new Date(tokenDocument.createdAt)
+		: undefined;
+
+	result.expired = isTokenExpired(30, tokenCreatedTime);
+
+	result.valid =
+		tokenDocument?.id === id && typeof tokenCreatedTime !== 'undefined' && !result.expired;
+
+	result.action = tokenDocument.action;
+
+	return result;
+};
+*/
+
+const isTokenValid = async (id, token, session) => {
 	let result = {
 		valid: false,
 		action: ''
@@ -42,7 +99,7 @@ const isTokenValid = async (id, token) => {
 
 	if (!id || typeof id !== 'string' || !token || typeof token !== 'string') return result;
 
-	let tokenDocument = await getToken(id, token);
+	let tokenDocument = await getToken(id, token, session);
 
 	if (tokenDocument && 'tooManyAttempts' in tokenDocument && tokenDocument.tooManyAttempts) {
 		return tokenDocument;
