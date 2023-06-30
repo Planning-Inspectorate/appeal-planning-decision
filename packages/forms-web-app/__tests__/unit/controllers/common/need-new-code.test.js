bf191dd44776769f56c83fe26ecb1f89dce231f8
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-06-30 16:24:29 +0100

    feat(as-5914): lPA need-new-code sends new token to user

--- packages/forms-web-app/__tests__/unit/controllers/common/need-new-code.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/common/need-new-code.test.js
@@ -2,8 +2,11 @@ const {
 	getNeedNewCode,
 	postNeedNewCode
 } = require('../../../../src/controllers/common/need-new-code');
+const { enterCodeConfig } = require('@pins/common');
 
 const { mockRes, mockReq } = require('../../mocks');
+const { sendToken, getUserById } = require('../../../../src/lib/appeals-api-wrapper');
+jest.mock('../../../../src/lib/appeals-api-wrapper');
 
 describe('controllers/common/enter-code', () => {
 	let req;
@@ -17,7 +20,7 @@ describe('controllers/common/enter-code', () => {
 	});
 
 	describe('getNeedNewCode', () => {
-		it('should redirect to correct page', () => {
+		it('should redirect to correct page', async () => {
 			const {
 				VIEW: {
 					LPA_DASHBOARD: { NEED_NEW_CODE }
@@ -26,14 +29,39 @@ describe('controllers/common/enter-code', () => {
 			const views = { NEED_NEW_CODE };
 
 			const returnedFunction = getNeedNewCode(views);
-			returnedFunction(req, res);
+			await returnedFunction(req, res);
 
 			expect(res.render).toBeCalledWith(`${NEED_NEW_CODE}`);
 		});
 	});
 
 	describe('postRequestNewCode', () => {
-		it('should redirect to correct page', () => {
+		const badUserResponseValues = [undefined, null, {}, { prop: 'test' }];
+
+		for (const badVal of badUserResponseValues) {
+			it(`should redirect to enter code page if user response is not valid`, async () => {
+				const {
+					VIEW: {
+						LPA_DASHBOARD: { NEED_NEW_CODE, ENTER_CODE }
+					}
+				} = require('../../../../src/lib/views');
+				const views = { NEED_NEW_CODE, ENTER_CODE };
+				const tokenId = '1552441a-1e56-4e83-8d85-de7b246d2594';
+
+				getUserById.mockResolvedValue(badVal);
+
+				const returnedFunction = postNeedNewCode(views);
+				req.params = {
+					id: tokenId
+				};
+				await returnedFunction(req, res);
+
+				expect(getUserById).toBeCalledWith(tokenId);
+				expect(res.redirect).toBeCalledWith(`/${ENTER_CODE}/${tokenId}`);
+			});
+		}
+
+		it(`should redirect to enter code page if user response returns an error`, async () => {
 			const {
 				VIEW: {
 					LPA_DASHBOARD: { NEED_NEW_CODE, ENTER_CODE }
@@ -42,12 +70,79 @@ describe('controllers/common/enter-code', () => {
 			const views = { NEED_NEW_CODE, ENTER_CODE };
 			const tokenId = '1552441a-1e56-4e83-8d85-de7b246d2594';
 
+			getUserById.mockImplementation(() => {
+				throw new Error('Failed');
+			});
+
 			const returnedFunction = postNeedNewCode(views);
 			req.params = {
 				id: tokenId
 			};
-			returnedFunction(req, res);
+			await returnedFunction(req, res);
 
+			expect(getUserById).toBeCalledWith(tokenId);
+			expect(res.redirect).toBeCalledWith(`/${ENTER_CODE}/${tokenId}`);
+		});
+
+		it(`should redirect to enter code page if sendToken returns an error`, async () => {
+			const {
+				VIEW: {
+					LPA_DASHBOARD: { NEED_NEW_CODE, ENTER_CODE }
+				}
+			} = require('../../../../src/lib/views');
+			const views = { NEED_NEW_CODE, ENTER_CODE };
+			const tokenId = '1552441a-1e56-4e83-8d85-de7b246d2594';
+			const fakeUserResponse = {
+				email: 'test@example.com'
+			};
+
+			getUserById.mockResolvedValue(fakeUserResponse);
+
+			sendToken.mockImplementation(() => {
+				throw new Error('Failed');
+			});
+
+			const returnedFunction = postNeedNewCode(views);
+			req.params = {
+				id: tokenId
+			};
+			await returnedFunction(req, res);
+
+			expect(getUserById).toBeCalledWith(tokenId);
+			expect(sendToken).toBeCalledWith(
+				tokenId,
+				enterCodeConfig.actions.lpaDashboard,
+				fakeUserResponse.email
+			);
+			expect(res.redirect).toBeCalledWith(`/${ENTER_CODE}/${tokenId}`);
+		});
+
+		it(`should redirect to enter code page if sendToken works`, async () => {
+			const {
+				VIEW: {
+					LPA_DASHBOARD: { NEED_NEW_CODE, ENTER_CODE }
+				}
+			} = require('../../../../src/lib/views');
+			const views = { NEED_NEW_CODE, ENTER_CODE };
+			const tokenId = '1552441a-1e56-4e83-8d85-de7b246d2594';
+			const fakeUserResponse = {
+				email: 'test@example.com'
+			};
+
+			getUserById.mockResolvedValue(fakeUserResponse);
+
+			const returnedFunction = postNeedNewCode(views);
+			req.params = {
+				id: tokenId
+			};
+			await returnedFunction(req, res);
+
+			expect(getUserById).toBeCalledWith(tokenId);
+			expect(sendToken).toBeCalledWith(
+				tokenId,
+				enterCodeConfig.actions.lpaDashboard,
+				fakeUserResponse.email
+			);
 			expect(res.redirect).toBeCalledWith(`/${ENTER_CODE}/${tokenId}`);
 		});
 	});
--- packages/forms-web-app/src/controllers/common/need-new-code.js
+++ packages/forms-web-app/src/controllers/common/need-new-code.js
@@ -1,11 +1,37 @@
+const logger = require('../../lib/logger');
+const { sendToken, getUserById } = require('../../lib/appeals-api-wrapper');
+const { enterCodeConfig } = require('@pins/common');
+
 const getNeedNewCode = (views) => {
 	return async (req, res) => {
 		res.render(views.NEED_NEW_CODE);
 	};
 };
 
+/**
+ * Sends a new token to the lpa user referenced by the id in the url params
+ * @param {ExpressRequest} req
+ * @returns {Promise}
+ */
+async function resendTokenToLpaUser(req) {
+	const user = await getUserById(req.params.id);
+
+	if (user?.email) {
+		await sendToken(req.params.id, enterCodeConfig.actions.lpaDashboard, user.email);
+		req.session.enterCode = req.session.enterCode || {};
+		req.session.enterCode.newCode = true;
+	}
+}
+
 const postNeedNewCode = (views) => {
 	return async (req, res) => {
+		try {
+			await resendTokenToLpaUser(req);
+		} catch (err) {
+			logger.error(err);
+		}
+
+		// even if we error, return to enter code page so as to not give anyway any user detail
 		const url = `/${views.ENTER_CODE}/${req.params.id}`;
 		res.redirect(url);
 	};
