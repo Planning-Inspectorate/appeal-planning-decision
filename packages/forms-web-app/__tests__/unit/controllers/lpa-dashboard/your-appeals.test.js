c7f33cf0ee9ed13bde8b6fb7be23baaf52e2ddc1
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-07-06 15:22:17 +0100

    feat(aapd-5): add lpa name to manage appeals page

--- packages/forms-web-app/__tests__/unit/controllers/lpa-dashboard/your-appeals.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/lpa-dashboard/your-appeals.test.js
@@ -1,11 +1,26 @@
 const { getYourAppeals } = require('../../../../src/controllers/lpa-dashboard/your-appeals');
+const { getLPA } = require('../../../../src/lib/appeals-api-wrapper');
+const { getLPAUserFromSession } = require('../../../../src/services/lpa-user.service');
+
 const { VIEW } = require('../../../../src/lib/views');
 const { mockReq, mockRes } = require('../../mocks');
+
+jest.mock('../../../../src/lib/appeals-api-wrapper');
+jest.mock('../../../../src/services/lpa-user.service');
 
 const req = {
 	...mockReq(null)
 };
 const res = mockRes();
+
+const mockUser = {
+	lpaCode: 'test',
+	email: 'test@example.com'
+};
+
+const mockLpa = {
+	name: 'test-lpa'
+};
 
 describe('controllers/lpa-dashboard/your-appeals', () => {
 	beforeEach(() => {
@@ -14,8 +29,15 @@ describe('controllers/lpa-dashboard/your-appeals', () => {
 
 	describe('getYourAppeals', () => {
 		it('should render the view with a link to add-remove', async () => {
-			getYourAppeals(req, res);
+			getLPAUserFromSession.mockReturnValue(mockUser);
+			getLPA.mockResolvedValue(mockLpa);
+
+			await getYourAppeals(req, res);
+
+			expect(getLPAUserFromSession).toHaveBeenCalledWith(req);
+			expect(getLPA).toHaveBeenCalledWith(mockUser.lpaCode);
 			expect(res.render).toHaveBeenCalledWith(VIEW.LPA_DASHBOARD.DASHBOARD, {
+				lpaName: mockLpa.name,
 				addOrRemoveLink: `/${VIEW.LPA_DASHBOARD.ADD_REMOVE_USERS}`
 			});
 		});
