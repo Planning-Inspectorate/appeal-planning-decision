7e09453c82a08127726c72a4ad3481b65eee8f7c
charlie Benger-stevenson <suityou01@yahoo.co.uk>
2023-07-10 14:38:48 +0100

    feat(require-user middleware): change middleware to redirect
    redirect to your-email-address endpoint instead of 401 unauth

--- packages/forms-web-app/src/middleware/lpa-dashboard/require-user.js
+++ packages/forms-web-app/src/middleware/lpa-dashboard/require-user.js
@@ -3,7 +3,7 @@ const logger = require('../../lib/logger');
 
 const {
 	VIEW: {
-		ERROR_PAGES: { UNAUTHORIZED }
+		LPA_DASHBOARD: { YOUR_EMAIL_ADDRESS }
 	}
 } = require('../../lib/views');
 
@@ -12,10 +12,10 @@ const requireUser = (req, res, next) => {
 		session: { lpaUser }
 	} = req;
 
-	if (!lpaUser || lpaUser.status === STATUS_CONSTANTS.REMOVED) {
-		res.status(401);
-		logger.info('User not logged in: 401');
-		return res.render(UNAUTHORIZED);
+	if (!lpaUser || lpaUser.enabled === STATUS_CONSTANTS.REMOVED) {
+		logger.info('User not logged in');
+		return res.redirect(`/${YOUR_EMAIL_ADDRESS}`);
+
 	}
 
 	return next();
