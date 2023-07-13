8c6e4b325e85242156ed2f062ee977ca1ea8587a
Ed Packard <edward.packard@kinandcarta.com>
2023-07-13 16:16:18 +0100

    feat(aapd-12): user added success banner

--- packages/forms-web-app/__tests__/unit/controllers/lpa-dashboard/add-remove-users.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/lpa-dashboard/add-remove-users.test.js
@@ -20,5 +20,22 @@ describe('controllers/lpa-dashboard/add-remove-users', () => {
 				addUserLink: `/${VIEW.LPA_DASHBOARD.EMAIL_ADDRESS}`
 			});
 		});
+
+		it('should render view with correct successMessage if req.session.addUserEmailAddress exists', async () => {
+			req.session.addUserEmailAddress = 'test@example.com';
+			const successMessage = [
+				`test@example.com has been added to the account`,
+				'They will receive an email with a link to the service'
+			];
+
+			getAddRemoveUsers(req, res);
+
+			expect(res.render).toHaveBeenCalledWith(VIEW.LPA_DASHBOARD.ADD_REMOVE_USERS, {
+				dashboardUrl: `/${VIEW.LPA_DASHBOARD.DASHBOARD}`,
+				addUserLink: `/${VIEW.LPA_DASHBOARD.EMAIL_ADDRESS}`,
+				successMessage: successMessage
+			});
+			expect(req.session.addUserEmailAddress).toBe(undefined);
+		});
 	});
 });
--- packages/forms-web-app/src/controllers/lpa-dashboard/add-remove-users.js
+++ packages/forms-web-app/src/controllers/lpa-dashboard/add-remove-users.js
@@ -5,9 +5,20 @@ const {
 } = require('../../lib/views');
 
 const getAddRemoveUsers = async (req, res) => {
+	let successMessage;
+
+	if (req.session.addUserEmailAddress) {
+		successMessage = [
+			`${req.session.addUserEmailAddress} has been added to the account`,
+			'They will receive an email with a link to the service'
+		];
+		delete req.session.addUserEmailAddress;
+	}
+
 	return res.render(ADD_REMOVE_USERS, {
 		dashboardUrl: `/${DASHBOARD}`,
-		addUserLink: `/${EMAIL_ADDRESS}`
+		addUserLink: `/${EMAIL_ADDRESS}`,
+		successMessage: successMessage
 	});
 };
 
