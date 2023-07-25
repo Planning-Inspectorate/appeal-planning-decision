4e0fcb6358739db1155499da1bd1432a12fc4db3
charlie Benger-stevenson <suityou01@yahoo.co.uk>
2023-07-25 13:44:18 +0100

    feat(front end): /appeal-details: appeal headlines

--- packages/forms-web-app/__tests__/unit/controllers/lpa-dashboard/appeal-details.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/lpa-dashboard/appeal-details.test.js
@@ -1,30 +1,17 @@
 const { getAppealDetails } = require('../../../../src/controllers/lpa-dashboard/appeal-details');
-const { getLPAUserFromSession } = require('../../../../src/services/lpa-user.service');
-const { getExistingAppealByLPACodeAndId } = require('../../../../src/lib/appeals-api-wrapper');
+const { getAppealByLPACodeAndId } = require('../../../../src/lib/appeals-api-wrapper');
 const { VIEW } = require('../../../../src/lib/views');
 const { mockReq, mockRes } = require('../../mocks');
 const {
 	constants: { APPEAL_ID }
 } = require('@pins/business-rules');
 
-jest.mock('../../../../src/services/lpa-user.service');
-jest.mock('../../../../src/lib/appeals-api-wrapper');
-
 const req = {
 	...mockReq(null)
 };
 const res = mockRes();
 
-const mockUser = {
-	lpaCode: 'test',
-	email: 'test@example.com',
-	lpaName: 'test-lpa'
-};
-
-const mockAppeal = {
-	appealValidDate: new Date('2023-07-24T12:21:11.208Z'),
-	type: APPEAL_ID.HOUSEHOLDER
-};
+jest.mock('../../../../src/lib/appeals-api-wrapper');
 
 describe('controllers/lpa-dashboard/your-appeals', () => {
 	beforeEach(() => {
@@ -33,17 +20,26 @@ describe('controllers/lpa-dashboard/your-appeals', () => {
 
 	describe('getAppealDetails', () => {
 		it('should render the view with a link to add-remove', async () => {
-			getLPAUserFromSession.mockReturnValue(mockUser);
-			getExistingAppealByLPACodeAndId.mockResolvedValue(mockAppeal);
-
+			const appeal = {
+				type: APPEAL_ID.HOUSEHOLDER,
+				caseReference: '1345264',
+				appealType: 'Householder appeal',
+				siteAddressLine1: '2 Aubrey House',
+				siteAddressLine2: 'Aubrey Road',
+				siteAddressTown: '',
+				siteAddressCounty: '',
+				siteAddressPostcode: 'BS3 3EX',
+				appellant: 'Rachel Silver',
+				LPAApplicationReference: '23/04125/FUL'
+			};
+			getAppealByLPACodeAndId.mockResolvedValue(appeal);
 			await getAppealDetails(req, res);
 			const expectedQuesionnaireDueDate = 'Monday, 31 July 2023';
 
-			expect(getLPAUserFromSession).toHaveBeenCalledWith(req);
 			expect(res.render).toHaveBeenCalledWith(VIEW.LPA_DASHBOARD.APPEAL_DETAILS, {
-				lpaName: mockUser.lpaName,
 				dashboardLink: `/${VIEW.LPA_DASHBOARD.DASHBOARD}`,
-				questionnaireDueDate: expectedQuesionnaireDueDate
+				expectedQuesionnaireDueDate,
+				appeal
 			});
 		});
 	});
