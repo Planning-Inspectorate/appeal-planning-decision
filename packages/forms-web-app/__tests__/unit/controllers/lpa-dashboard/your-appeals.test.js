c7ce7b74e607853af3835bf1352a3e01512f3d87
Ed Packard <edward.packard@kinandcarta.com>
2023-07-27 22:27:11 +0100

    feat(aapd-42): add questionnaire hyperlink in rows on to do column

--- packages/forms-web-app/__tests__/unit/controllers/lpa-dashboard/your-appeals.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/lpa-dashboard/your-appeals.test.js
@@ -19,12 +19,14 @@ const mockUser = {
 	lpaName: 'test-lpa'
 };
 
-const mockAppealData = [{
-	_id: '89aa8504-773c-42be-bb68-029716ad9756',
-	LPAApplicationReference: '12/3456789/PLA',
-	caseReference: 'APP/Q9999/W/22/3221288',
-	questionnaireDueDate: '2023-07-07T13:53:31.6003126+00:00'
-}];
+const mockAppealData = [
+	{
+		_id: '89aa8504-773c-42be-bb68-029716ad9756',
+		LPAApplicationReference: '12/3456789/PLA',
+		caseReference: 'APP/Q9999/W/22/3221288',
+		questionnaireDueDate: '2023-07-07T13:53:31.6003126+00:00'
+	}
+];
 
 describe('controllers/lpa-dashboard/your-appeals', () => {
 	beforeEach(() => {
@@ -41,8 +43,9 @@ describe('controllers/lpa-dashboard/your-appeals', () => {
 			expect(res.render).toHaveBeenCalledWith(VIEW.LPA_DASHBOARD.DASHBOARD, {
 				lpaName: mockUser.lpaName,
 				addOrRemoveLink: `/${VIEW.LPA_DASHBOARD.ADD_REMOVE_USERS}`,
+				appealsCaseData: mockAppealData,
 				appealDetailsLink: `/${VIEW.LPA_DASHBOARD.APPEAL_DETAILS}`,
-				appealsCaseData: mockAppealData
+				appealQuestionnaireLink: `/${VIEW.LPA_QUESTIONNAIRE.QUESTIONNAIRE}`
 			});
 		});
 
