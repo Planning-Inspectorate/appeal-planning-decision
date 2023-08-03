22f63f2bb04c4dd1a0639dd940eaea5633fea0d8
Chris Howarth <chris.howarth@local.amazerealise.com>
2023-08-03 11:05:12 +0100

    feat(AAPD-51): addition of appeal submission table

--- packages/forms-web-app/__tests__/unit/controllers/lpa-dashboard/appeal-details.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/lpa-dashboard/appeal-details.test.js
@@ -32,7 +32,12 @@ describe('controllers/lpa-dashboard/your-appeals', () => {
 				siteAddressPostcode: 'BS3 3EX',
 				appellant: 'Rachel Silver',
 				LPAApplicationReference: '23/04125/FUL',
-				questionnaireDueDate: '2023-07-07T13:53:31.600Z'
+				questionnaireDueDate: '2023-07-07T13:53:31.600Z',
+				agent: 'Jane Fischold',
+				ownershipCertificate: 'Fully owned',
+				isSiteVisible: 'Yes',
+				doesSiteHaveHealthAndSafetyIssues: 'Yes',
+				healthAndSafetyIssuesDetails: 'The site needs to be seen from a height'
 			};
 			const applicationForm = {
 				filename: 'a.pdf',
