c62722d6e6157172484e7d7a7b462a807e6adf30
Chris Hawley <christopher.hawley@kinandcarta.com>
2023-08-03 18:28:21 +0100

    chore(na): custom commit prompts
    Support for custo commit prompt messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,12 @@ module.exports = {
 	ignores: [
 		(message) => message.startsWith('Auto-release'),
 		(message) => message.includes('[ci skip]')
-	]
+	],
+	prompt: {
+		questions: {
+			scope: {
+				description: 'Please add the Jira ticket number if available or a short title if not'
+			}
+		}
+	}
 };
