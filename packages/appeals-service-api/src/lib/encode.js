6c3a08be0501a0ee7896bba3e209b68e0ddf9bde
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-08-04 12:34:03 +0100

    fix(aapd-24): only use encodeURIComponent for link to appeals detail page

--- packages/appeals-service-api/src/lib/encode.js
+++ packages/appeals-service-api/src/lib/encode.js
@@ -1,28 +1,12 @@
 /**
- * Replaces /'s and encodes URI component unsafe chars for the string passed in
- * handles non string values as per encodeURIComponent
+ * encodes URI component unsafe chars for the string passed in
  * @param {string} ref
  * @returns {string} The url-friendly string
  */
 function encodeUrlSlug(ref) {
-	if (typeof ref === 'string') {
-		ref = ref.replace(/\//g, '_');
-	}
-
 	return encodeURIComponent(ref);
 }
 
-function decodeUrlSlug(ref) {
-	ref = decodeURIComponent(ref);
-
-	if (typeof ref === 'string') {
-		ref = ref.replace(/_/g, '/');
-	}
-
-	return ref;
-}
-
 module.exports = {
-	encodeUrlSlug,
-	decodeUrlSlug
+	encodeUrlSlug
 };
