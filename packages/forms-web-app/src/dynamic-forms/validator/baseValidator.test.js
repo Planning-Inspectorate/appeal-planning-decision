f37c376964a59ffe96b0dcb54d301eae38c7cac7
charlie Benger-stevenson <suityou01@yahoo.co.uk>
2023-08-16 14:29:14 +0100

    test(AAPD-148): unit tests

---
+++ packages/forms-web-app/src/dynamic-forms/validator/baseValidator.test.js
@@ -0,0 +1,14 @@
+const BaseValidator = require('./baseValidator');
+
+describe('src/dynamic-forms/validator/baseValidator.js', () => {
+	it('should not be possible to instantiate the base class', () => {
+		expect(() => new BaseValidator()).toThrow("Abstract classes can't be instantiated.");
+	});
+	it('should be possible to inherit from the base class', () => {
+		class MyValidator extends BaseValidator {}
+
+		const myValidator = new MyValidator();
+		expect(myValidator.constructor).toEqual(MyValidator);
+		expect(myValidator instanceof BaseValidator).toEqual(true);
+	});
+});
