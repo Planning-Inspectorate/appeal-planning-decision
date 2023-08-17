11bfd9e4d27f725c197a11a8dbb64c8e44a3d615
charlie Benger-stevenson <suityou01@yahoo.co.uk>
2023-08-17 14:32:44 +0100

    refactor(AAPD-113): renamed BooleanValidator to RequiredValidator

--- packages/forms-web-app/src/dynamic-forms/validator/requiredValidator.js
+++ packages/forms-web-app/src/dynamic-forms/validator/requiredValidator.js
@@ -1,10 +1,10 @@
 const { body } = require('express-validator');
 const BaseValidator = require('./baseValidator.js');
 
-class BooleaValidator extends BaseValidator {
+class RequiredValidator extends BaseValidator {
 	validate(questionObj, errorMessage) {
 		return body(questionObj.fieldName).notEmpty().withMessage(errorMessage);
 	}
 }
 
-module.exports = BooleaValidator;
+module.exports = RequiredValidator;
