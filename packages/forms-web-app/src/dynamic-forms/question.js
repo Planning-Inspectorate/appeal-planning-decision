c9b8d99b765d25e9a2ab815c73a0fdc70a3743a0
charlie Benger-stevenson <suityou01@yahoo.co.uk>
2023-08-18 12:03:26 +0100

    feat(AAPD-182): base question class

--- packages/forms-web-app/src/dynamic-forms/question.js
+++ packages/forms-web-app/src/dynamic-forms/question.js
@@ -1,3 +1,10 @@
+/***********************************************************
+ * This file holds the class definitions for the different *
+ * components (question types) including base types        *
+ ***********************************************************/
+
+const nunjucks = require('nunjucks');
+
 class Question {
 	title;
 	question;
@@ -17,6 +24,36 @@ class Question {
 		this.fieldName = fieldName;
 		//todo: taskList default to true, or pass in as param if question shouldn't be displayed in task (summary) list
 		//or possibly add taskList condition to the Section class as part of withCondition method(or similar) if possible?
+	}
+
+	prepQuestionForRendering(answers) {
+		var answer = answers[this.fieldName];
+		var processedQuestion = { ...this };
+		processedQuestion.value = answer;
+		if (this.options !== undefined && this.options.length > 0) {
+			processedQuestion.options = [];
+			for (var i = 0; i < this.options.length; i++) {
+				var option = { ...this.options[i] };
+				if (option.value !== undefined) {
+					option.checked = (',' + answer + ',').includes(',' + option.value + ',');
+				}
+				//also need to handle dependent fields & set their answers
+				if (option.conditional !== undefined) {
+					var conditionalField = { ...option.conditional };
+					conditionalField.fieldName =
+						processedQuestion.fieldName + '_' + conditionalField.fieldName;
+					conditionalField.value = answers[conditionalField.fieldName] || '';
+					option.conditional = {
+						html: nunjucks.render(
+							`../views/questions/conditional/${conditionalField.type}.njk`,
+							conditionalField
+						)
+					};
+				}
+				processedQuestion.options.push(option);
+			}
+		}
+		return processedQuestion;
 	}
 }
 
