23f4e3c62d2422f812aa7cba33caa4f7473eb4de
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-08-21 13:43:40 +0100

    feat(db-indexes): add indexes for appealCaseData and documentMetadata tables

--- packages/appeals-service-api/src/db/setup.js
+++ packages/appeals-service-api/src/db/setup.js
@@ -17,8 +17,15 @@ const mongodb = require('./db');
 // 	try {
 // 		const saveAndReturnCollection = mongodb.get().collection('appeal');
 
-// 		await saveAndReturnCollection.createIndex({ 'appeal.horizonId': 1 }, { unique: true });
 // 		await saveAndReturnCollection.createIndex({ 'appeal.id': 1 }, { unique: true }); // todo: replace calls with _id and this index won't be needed?
+
+// 		await saveAndReturnCollection.createIndex({ 'appeal.horizonId': 1 }, { unique: true });
+// 		await saveAndReturnCollection.createIndex({ 'appeal.email': 1 }, { unique: false });
+// 		await saveAndReturnCollection.createIndex({ 'appeal.planningApplicationNumber': 1 }, { unique: false });
+// 		await saveAndReturnCollection.createIndex({ 'appeal.planningApplicationNumber': 1 }, { unique: false });
+// 		await saveAndReturnCollection.createIndex({ 'appeal.typeOfPlanningApplication': 1 }, { unique: false });
+// 		await saveAndReturnCollection.createIndex({ 'appeal.state': 1 }, { unique: false });
+// 		await saveAndReturnCollection.createIndex({ 'appeal.appealSiteSection.siteAddress.postcode': 1 }, { unique: false });
 // 	} catch (err) {
 // 		logger.error(err, `Error: error setting up appeal indexes in mongo`);
 // 		throw err;
@@ -50,10 +57,46 @@ async function setupUserIndexes() {
 	}
 }
 
+async function setupAppealCaseDataIndexes() {
+	try {
+		const caseDataCollection = mongodb.get().collection('appealsCaseData');
+
+		await caseDataCollection.createIndex({ caseReference: 1 }, { unique: true });
+		await caseDataCollection.createIndex({ LPAApplicationReference: 1 }, { unique: false });
+		await caseDataCollection.createIndex({ LPACode: 1 }, { unique: false });
+		await caseDataCollection.createIndex({ appealType: 1 }, { unique: false });
+		await caseDataCollection.createIndex({ validity: 1 }, { unique: false });
+		await caseDataCollection.createIndex({ questionnaireDueDate: 1 }, { unique: false });
+		await caseDataCollection.createIndex({ questionnaireReceived: 1 }, { unique: false });
+	} catch (err) {
+		logger.error(err, `Error: error setting up appealsCaseData indexes in mongo`);
+		throw err;
+	}
+}
+
+async function setupDocumentMetadataIndexes() {
+	try {
+		const documentCollection = mongodb.get().collection('documentMetadata');
+
+		await documentCollection.createIndex({ caseRef: 1 }, { unique: false });
+		await documentCollection.createIndex({ version: 1 }, { unique: false });
+		await documentCollection.createIndex({ documentId: 1 }, { unique: false });
+		await documentCollection.createIndex({ documentType: 1 }, { unique: false });
+		await documentCollection.createIndex({ virusCheckStatus: 1 }, { unique: false });
+		await documentCollection.createIndex({ redactedStatus: 1 }, { unique: false });
+		await documentCollection.createIndex({ publishedStatus: 1 }, { unique: false });
+	} catch (err) {
+		logger.error(err, `Error: error setting up documentMetadata indexes in mongo`);
+		throw err;
+	}
+}
+
 async function setupIndexes() {
 	try {
 		await setupLpaIndexes();
 		await setupUserIndexes();
+		await setupAppealCaseDataIndexes();
+		await setupDocumentMetadataIndexes();
 	} catch (err) {
 		logger.error(err, `Error: error setting up indexes in mongo`);
 	}
