d78e0761b8948b6203fda03890ac88aa81b48315
charlie Benger-stevenson <suityou01@yahoo.co.uk>
2023-08-24 16:25:23 +0100

    feat(AAPD-248): azure function and basic http client

--- packages/integration-functions/appeals-case-data/appeals-case-data-fo-integration/index.js
+++ packages/integration-functions/appeals-case-data/appeals-case-data-fo-integration/index.js
@@ -1,5 +1,19 @@
+const httpClient = require('../../common/src/http/httpClient');
+/*
+Service Bus Namespace:
+    Deployment name: appeals-case-data-ukw-dev
+    Subscription: pins-odt-apps-dev-sub
+    Resource group: pins-rg-appeals-service-dev-uks-001
+    Correlation ID: db204df6-27b9-44de-8d8e-132b78b7898d
+    Connection String: Endpoint=sb://appeals-case-data-ukw-dev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ZHLEbaUP/jKVV1Gz5RRnmftEBdHR42Rco+ASbBJhU8w=
+Queue:
+    Queue name: appeals-case-data-queue
+*/
 
-
-module.exports = async function(context, appealsCaseMessage) {
-    context.log('JavaScript ServiceBus topic trigger function processed message', appealsCaseMessage);
+module.exports = async function (context, appealsCaseMessage) {
+	context.log('JavaScript ServiceBus topic trigger function processed message', appealsCaseMessage);
+	const { body } = appealsCaseMessage;
+	const BO_APPEALS_API = `http://${process.env.BO_APPEALS_API}/`;
+	context.log(BO_APPEALS_API);
+	await httpClient.jsonRequest(context, body, httpClient.METHOD_POST, BO_APPEALS_API);
 };
