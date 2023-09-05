2ea898ba37feafb716c97590326dc90910f966a7
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-09-05 17:32:15 +0100

    fix(remove-files): uses original filename

--- packages/forms-web-app/__tests__/unit/lib/multi-file-upload.test.js
+++ packages/forms-web-app/__tests__/unit/lib/multi-file-upload.test.js
@@ -22,9 +22,9 @@ describe('lib/multi-file-upload-helpers/getValidFiles', () => {
 
 describe('lib/multi-file-upload-helpers/removeFiles', () => {
 	it('returns array of files with specified files removed ', () => {
-		const file1 = { name: 'file1.jpeg' };
-		const file2 = { name: 'file2.jpeg' };
-		const file3 = { name: 'file3.jpeg' };
+		const file1 = { name: 'file1.jpeg', originalFileName: 'file1.jpeg' };
+		const file2 = { name: 'file2.jpeg', originalFileName: 'file2.jpeg' };
+		const file3 = { name: 'file3.jpeg', originalFileName: 'file3.jpeg' };
 
 		const files = [file1, file2, file3];
 		const removedFiles = [{ name: file1.name }, { name: file3.name }];
