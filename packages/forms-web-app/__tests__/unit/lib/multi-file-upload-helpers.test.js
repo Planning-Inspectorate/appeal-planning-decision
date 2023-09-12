21db853e656edf8112abf80e0b40be26a12c893d
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-09-12 19:08:45 +0100

    feat(aapd-255): remove files from blob storage if baselocation provided

--- packages/forms-web-app/__tests__/unit/lib/multi-file-upload-helpers.test.js
+++ packages/forms-web-app/__tests__/unit/lib/multi-file-upload-helpers.test.js
@@ -1,5 +1,12 @@
 const { getValidFiles, removeFiles } = require('../../../src/lib/multi-file-upload-helpers');
+const { removeDocument } = require('../../../src/lib/documents-api-wrapper');
+const logger = require('../../../src/lib/logger');
+
+jest.mock('../../../src/lib/documents-api-wrapper');
+jest.mock('../../../src/lib/logger');
+
 let testFiles;
+const testBaseLocation = 'base-path';
 
 describe('lib/multi-file-upload-helpers/getValidFiles', () => {
 	it('returns array of files with invalid files removed', () => {
@@ -23,18 +30,27 @@ describe('lib/multi-file-upload-helpers/getValidFiles', () => {
 
 describe('lib/multi-file-upload-helpers/removeFiles', () => {
 	beforeEach(() => {
+		jest.clearAllMocks();
 		testFiles = [
-			{ name: 'file1.txt', originalFileName: 'file1.txt' },
-			{ name: 'file2.txt', originalFileName: 'file2.txt' },
-			{ name: 'file3.txt', originalFileName: 'file3.txt' }
+			{ id: '1', name: 'fileName1.txt', originalFileName: 'file1.txt' },
+			{ id: '2', name: 'fileName2.txt', originalFileName: 'file2.txt' },
+			{ id: '3', name: 'fileName3.txt', originalFileName: 'file3.txt' }
 		];
 	});
 
 	it('should remove files from the input array', async () => {
-		const removedFiles = [{ name: 'file1.txt' }, { name: 'file3.txt' }];
+		const removedFiles = [
+			{ name: testFiles[0].originalFileName },
+			{ name: testFiles[2].originalFileName }
+		];
 
-		const result = await removeFiles(testFiles, removedFiles);
+		removeDocument.mockResolvedValue();
 
+		const result = await removeFiles(testFiles, removedFiles, testBaseLocation);
+
+		expect(removeDocument).toHaveBeenCalledTimes(2);
+		expect(removeDocument).toHaveBeenCalledWith(testBaseLocation, testFiles[0].id);
+		expect(removeDocument).toHaveBeenCalledWith(testBaseLocation, testFiles[2].id);
 		expect(result).toEqual([testFiles[1]]);
 	});
 
@@ -43,14 +59,66 @@ describe('lib/multi-file-upload-helpers/removeFiles', () => {
 
 		const result = await removeFiles(testFiles, removedFiles);
 
+		expect(removeDocument).not.toHaveBeenCalled();
 		expect(result).toEqual(testFiles);
 	});
 
 	it('should return an empty array if all files are removed', async () => {
-		const removedFiles = [{ name: 'file1.txt' }, { name: 'file2.txt' }, { name: 'file3.txt' }];
+		const removedFiles = [
+			{ name: testFiles[0].originalFileName },
+			{ name: testFiles[1].originalFileName },
+			{ name: testFiles[2].originalFileName }
+		];
+
+		removeDocument.mockResolvedValue();
+
+		const result = await removeFiles(testFiles, removedFiles, testBaseLocation);
+
+		expect(removeDocument).toHaveBeenCalledTimes(3);
+		expect(removeDocument).toHaveBeenCalledWith(testBaseLocation, testFiles[0].id);
+		expect(removeDocument).toHaveBeenCalledWith(testBaseLocation, testFiles[1].id);
+		expect(removeDocument).toHaveBeenCalledWith(testBaseLocation, testFiles[2].id);
+		expect(result).toEqual([]);
+	});
+
+	it('should not remove from blob storage if baseLocation is not passed through', async () => {
+		const removedFiles = [
+			{ name: testFiles[0].originalFileName },
+			{ name: testFiles[1].originalFileName },
+			{ name: testFiles[2].originalFileName }
+		];
 
 		const result = await removeFiles(testFiles, removedFiles);
 
+		expect(removeDocument).not.toHaveBeenCalled();
 		expect(result).toEqual([]);
+	});
+
+	it('should indicate if a file failed to be removed but continue to process other files, array will be reorderd', async () => {
+		const removedFiles = [
+			{ name: testFiles[0].originalFileName },
+			{ name: testFiles[2].originalFileName }
+		];
+
+		removeDocument.mockRejectedValueOnce(new Error('Removal failed'));
+		removeDocument.mockResolvedValueOnce();
+
+		const result = await removeFiles(testFiles, removedFiles, testBaseLocation);
+
+		expect(logger.error).toHaveBeenCalledTimes(1);
+		expect(removeDocument).toHaveBeenCalledTimes(2);
+		expect(removeDocument).toHaveBeenCalledWith(testBaseLocation, testFiles[0].id);
+		expect(removeDocument).toHaveBeenCalledWith(testBaseLocation, testFiles[2].id);
+		expect(result).toEqual([
+			testFiles[1],
+			{
+				...testFiles[0],
+				failedToRemove: true
+			}
+		]);
+	});
+
+	it('should error with bad inputs', async () => {
+		await expect(removeFiles()).rejects.toThrow();
 	});
 });
