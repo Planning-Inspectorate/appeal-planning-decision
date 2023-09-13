010afc1e8890d3b51a679c5822a6df60d8821f6f
Chris Hawley <christopher.hawley@kinandcarta.com>
2023-09-13 16:29:21 +0100

    feat(AAPD-45): refactor to use basic got http client
    The buffer length feature on the http client options isn't working on azure so just use basic npm
    got client for now

--- packages/integration-functions/appeals-case-data-fo-integration/index.js
+++ packages/integration-functions/appeals-case-data-fo-integration/index.js
@@ -1,15 +1,12 @@
-const httpClient = require('../common/src/http/httpClient');
+const got = require('../node_modules/got');
 
 module.exports = async function (context, appealsCaseMessage) {
 	context.log('JavaScript ServiceBus topic trigger function processed message', appealsCaseMessage);
 	const { body } = appealsCaseMessage;
-	const FO_APPEALS_API = `http://${process.env.FO_APPEALS_API}/`;
-	context.log(FO_APPEALS_API);
-	var response = await httpClient.jsonRequest(
-		context,
-		body,
-		httpClient.METHOD_POST,
-		FO_APPEALS_API
-	);
-	context.log.info(response);
+	const FO_APPEALS_API = `https://${process.env.FO_APPEALS_API}/`;
+	await got
+		.post(FO_APPEALS_API, {
+			json: body
+		})
+		.json();
 };
