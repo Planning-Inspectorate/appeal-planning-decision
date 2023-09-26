ff49a3f0fd97e6a9161a8bbaeefa92ff5b93d52d
Ed Packard <edward.packard@kinandcarta.com>
2023-09-26 11:11:41 +0100

    feat(aapd-324): add AddressAddMoreQuestion class

---
+++ packages/forms-web-app/src/dynamic-forms/dynamic-components/address-add-more/question.js
@@ -0,0 +1,40 @@
+const AddMoreQuestion = require('../add-more/question');
+const Address = require('@pins/common/src/lib/address');
+
+const uuid = require('uuid');
+
+class AddressAddMoreQuestion extends AddMoreQuestion {
+	/**
+	 * @param {Object} params
+	 * @param {string} params.title
+	 * @param {string} params.question
+	 * @param {string} params.fieldName
+	 * @param {Array.<BaseValidator>} [params.validators]
+	 */
+	constructor({ title, question, fieldName, viewFolder, validators }) {
+		super({
+			title: title,
+			viewFolder: viewFolder,
+			fieldName: fieldName,
+			question: question,
+			validators: validators
+		});
+	}
+
+	/**
+	 * adds a uuid and an address object for save data using req body fields
+	 * @param {ExpressRequest} req
+	 * @returns
+	 */
+	async getDataToSave(req) {
+		const address = new Address({
+			addressLine1: req.body[this.fieldName + '_addressLine1'],
+			addressLine2: req.body[this.fieldName + '_addressLine2'],
+			townCity: req.body[this.fieldName + '_townCity'],
+			postcode: req.body[this.fieldName + '_postcode']
+		});
+		return { addMoreId: uuid.v4(), value: address };
+	}
+}
+
+module.exports = AddressAddMoreQuestion;
