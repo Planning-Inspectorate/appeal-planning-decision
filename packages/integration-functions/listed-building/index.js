986c1a804da02223308e1c1d45a2e5d8d1442091
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-10-02 15:19:15 +0100

    feat(aapd-369): add listed building function

---
+++ packages/integration-functions/listed-building/index.js
@@ -0,0 +1,25 @@
+const got = require('got');
+
+/**
+ * @param {import('@azure/functions').Context} context
+ * @param {*} msg
+ */
+module.exports = async function (context, msg) {
+	context.log('Handle listed building message', msg);
+
+	if (Array.isArray(msg)) {
+		await processListedBuilding(msg);
+		return;
+	}
+
+	await processListedBuilding([msg]);
+};
+
+async function processListedBuilding(listedBuildingMessages) {
+	const APPEALS_CASE_DATA_URL = `https://${process.env.FO_APPEALS_API}/listed-buildings`;
+	await got
+		.post(APPEALS_CASE_DATA_URL, {
+			json: listedBuildingMessages
+		})
+		.json();
+}
