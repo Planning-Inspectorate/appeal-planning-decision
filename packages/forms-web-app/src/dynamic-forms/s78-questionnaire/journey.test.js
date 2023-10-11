c28ec7d3d2dcf477238fd9563e903c6a48bb271e
Ed Packard <edward.packard@kinandcarta.com>
2023-10-11 12:15:47 +0100

    feat(aapd-453): add s78 journey class

---
+++ packages/forms-web-app/src/dynamic-forms/s78-questionnaire/journey.test.js
@@ -0,0 +1,46 @@
+const { S78Journey, baseS78Url } = require('./journey');
+
+const mockResponse = {
+	referenceId: '123',
+	answers: []
+};
+
+describe('S78 Journey class', () => {
+	describe('constructor', () => {
+		it('should error if no response', () => {
+			expect(() => {
+				new S78Journey();
+			}).toThrow("Cannot read properties of undefined (reading 'referenceId')");
+		});
+
+		it('should set baseUrl', () => {
+			const journey = new S78Journey(mockResponse);
+			expect(journey.baseUrl).toEqual(expect.stringContaining(baseS78Url));
+			expect(journey.baseUrl).toEqual(expect.stringContaining(mockResponse.referenceId));
+		});
+
+		it('should set template', () => {
+			const journey = new S78Journey(mockResponse);
+			expect(journey.journeyTemplate).toBe('../has-questionnaire/template.njk');
+		});
+
+		it('should set listingPageViewPath', () => {
+			const journey = new S78Journey(mockResponse);
+			expect(journey.listingPageViewPath).toBe('dynamic-components/task-list/questionnaire');
+		});
+
+		it('should set journeyTitle', () => {
+			const journey = new S78Journey(mockResponse);
+			expect(journey.journeyTitle).toBe('Manage your appeals');
+		});
+
+		it('should define sections and questions', () => {
+			const journey = new S78Journey(mockResponse);
+			expect(Array.isArray(journey.sections)).toBe(true);
+			expect(journey.sections.length > 0).toBe(true);
+			//todo: when questions are added uncomment the following expectations
+			// expect(Array.isArray(journey.sections[0].questions)).toBe(true);
+			// expect(journey.sections[0].questions.length > 0).toBe(true);
+		});
+	});
+});
