387a563284dffaf96ac5a09ae3c87cdd52cb003f
Chris Hawley <christopher.hawley@kinandcarta.com>
2023-11-08 14:21:43 +0000

    fix(AAPD-582): fix topic name for appellant submissions

--- packages/appeals-service-api/src/data-producers/appeal-producer.js
+++ packages/appeals-service-api/src/data-producers/appeal-producer.js
@@ -2,7 +2,7 @@ const eventClient = require('../infrastructure/event-client');
 const { EventType } = require('../event-client/event-type');
 
 const broadcast = async (data) => {
-	return await eventClient.sendEvents('appeal', data, EventType.Create);
+	return await eventClient.sendEvents('appeal-fo-appellant-submission', data, EventType.Create);
 };
 
 module.exports = { broadcast };
