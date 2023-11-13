d397231680a9a976ee6ed5e3e037ca409e1077b4
Chris Howarth <chris.howarth@local.amazerealise.com>
2023-11-13 10:16:58 +0000

    feat(aapd-658): updating document downloads to have additional security

--- packages/forms-web-app/src/controllers/document.js
+++ packages/forms-web-app/src/controllers/document.js
@@ -5,19 +5,37 @@ const getDocument = async (req, res) => {
 	const { appealOrQuestionnaireId, documentId } = req.params;
 
 	try {
-		const { headers, body } = await fetchDocument(appealOrQuestionnaireId, documentId);
+		if (req.session.lpaUser) {
+			const { headers, body } = await fetchDocument(appealOrQuestionnaireId, documentId);
+			const sessionLpaCode = req.session.lpaUser.lpaCode;
+			const associatedLpaCode = headers.get('x-original-file-name').match(/[A-z][0-9]{4}/gm)[0];
 
-		res.set({
-			'content-length': headers.get('content-length'),
-			'content-disposition': `attachment;filename="${headers.get('x-original-file-name')}"`,
-			'content-type': headers.get('content-type')
-		});
+			if (sessionLpaCode != associatedLpaCode) {
+				logger.error('Failed to get document');
+				return res.sendStatus(401);
+			}
 
-		return body.pipe(res);
+			return await returnResult(headers, body, res);
+		} else {
+			const sessionAppealId = req.session.appeal.id;
+			const { headers, body } = await fetchDocument(sessionAppealId, documentId);
+
+			return await returnResult(headers, body, res);
+		}
 	} catch (err) {
 		logger.error({ err }, 'Failed to get document');
 		return res.sendStatus(500);
 	}
+};
+
+const returnResult = async (headers, body, res) => {
+	res.set({
+		'content-length': headers.get('content-length'),
+		'content-disposition': `attachment;filename="${headers.get('x-original-file-name')}"`,
+		'content-type': headers.get('content-type')
+	});
+
+	return body.pipe(res);
 };
 
 module.exports = {
