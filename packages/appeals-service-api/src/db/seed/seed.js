5475875d9c24d2e3f64843bf8bd8038c0422b852
Marc-Steeven Eyeni-Kantsey <marc-steeveneyeni-kantsey@MacBook-Air-de-Salome.local>
2023-11-15 15:10:38 +0000

    feat(aapd-617): prisma set up added

---
+++ packages/appeals-service-api/src/db/seed/seed.js
@@ -0,0 +1,32 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+	const alice = await prisma.user.upsert({
+		where: { email: 'alice@prisma.io' },
+		update: {},
+		create: {
+			email: 'alice@prisma.io',
+			name: 'Alice'
+		}
+	});
+
+	const bob = await prisma.user.upsert({
+		where: { email: 'bob@prisma.io' },
+		update: {},
+		create: {
+			email: 'bob@prisma.io',
+			name: 'Bob'
+		}
+	});
+	console.log({ alice, bob });
+}
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
