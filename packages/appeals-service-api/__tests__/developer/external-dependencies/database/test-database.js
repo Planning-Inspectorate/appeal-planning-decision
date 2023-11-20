86fa9784e955b1aa8df0ee45113d519edd018e2b
Marc-Steeven Eyeni-Kantsey <marc-steeveneyeni-kantsey@MacBook-Air-de-Salome.local>
2023-11-20 11:50:42 +0000

    feat(aapd-618): sql repo to query database from api

--- packages/appeals-service-api/__tests__/developer/external-dependencies/database/test-database.js
+++ packages/appeals-service-api/__tests__/developer/external-dependencies/database/test-database.js
@@ -1,23 +1,23 @@
 const { GenericContainer, Wait } = require('testcontainers/');
 
-let startedContainer;
+let mongoDbContainer;
 
 const create = async () => {
-	startedContainer = await new GenericContainer('mongo')
+	mongoDbContainer = await new GenericContainer('mongo')
 		.withName('appeals-api-it-mongodb')
 		.withExposedPorts(27017)
 		.withWaitStrategy(Wait.forLogMessage('Waiting for connections'))
 		.start();
 
 	let dbName = 'appeals-api-integration-test';
-	process.env.INTEGRATION_TEST_DB_URL = `mongodb://localhost:${startedContainer.getMappedPort(
+	process.env.INTEGRATION_TEST_DB_URL = `mongodb://localhost:${mongoDbContainer.getMappedPort(
 		27017
 	)}/${dbName}`;
 };
 
 const teardown = async () => {
-	if (startedContainer) {
-		await startedContainer.stop();
+	if (mongoDbContainer) {
+		await mongoDbContainer.stop();
 	}
 };
 
--- packages/appeals-service-api/__tests__/setup-jest.js
+++ packages/appeals-service-api/__tests__/setup-jest.js
@@ -1,1 +1,3 @@
 process.env.APP_LPA_QUESTIONNAIRE_BASE_URL = 'http://fake-lpa-questionnaire-base-url';
+process.env.SQL_CONNECTION_STRING_ADMIN =
+	'sqlserver://localhost:1433;database=pins_front_office_development;user=sa;password=DockerDatabaseP@22word!;trustServerCertificate=true';
