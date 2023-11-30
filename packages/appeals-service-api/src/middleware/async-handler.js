b4ee14bcf02b1a159688311b765a772d7f624f5c
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-11-30 17:12:36 +0000

    feat(aapd-854): add async handler

---
+++ packages/appeals-service-api/src/middleware/async-handler.js
@@ -0,0 +1,22 @@
+/**
+ * Wrap route handlers in the following function to handle async errors
+ * express doesn't natively handle async handlers, so next isn't called, and the request hangs
+ * @template T
+ * @param {import('express').RequestHandler<T>| function(): Promise<void>} requestHandler
+ * @returns {import('express').RequestHandler<T>}
+ */
+const asyncHandler = (requestHandler) => {
+	return (request, response, next) => {
+		try {
+			const p = requestHandler(request, response, next);
+			if (p instanceof Promise) {
+				p.catch(next);
+			}
+		} catch (e) {
+			// in case a sync function is passed in
+			next(e);
+		}
+	};
+};
+
+module.exports = asyncHandler;
--- packages/appeals-service-api/src/routes/token-v2.js
+++ packages/appeals-service-api/src/routes/token-v2.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const { tokenPutV2, tokenPostV2 } = require('../controllers/token');
+const asyncHandler = require('../middleware/async-handler');
 
 const router = express.Router();
 
-router.put('/', tokenPutV2);
-router.post('/', tokenPostV2);
+router.put('/', asyncHandler(tokenPutV2));
+router.post('/', asyncHandler(tokenPostV2));
 
 module.exports = router;
