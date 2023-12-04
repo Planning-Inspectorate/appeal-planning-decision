460381332239a518e0b649c1d2def33aec7b5e6f
Mark Potter <si3rr4wow@gmail.com>
2023-12-04 10:58:27 +0000

    fix(AAPD-825): correct urls

--- packages/web-comment/src/routes/enter-appeal-reference/index.js
+++ packages/web-comment/src/routes/enter-appeal-reference/index.js
@@ -3,21 +3,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.render(`appeal-search-number/index`);
+	res.render(`enter-appeal-reference/index`);
 });
 
 router.post('/', (req, res) => {
 	const { 'appeal-reference': appealReference } = req.body;
 
 	if (!appealReference) {
-		return res.render(`appeal-search-number/index`, {
+		return res.render(`enter-appeal-reference/index`, {
 			inlineErrorMessage: { text: 'Enter the appeal reference' },
 			value: appealReference
 		});
 	}
 
 	if (!/^[0-9]{7}$/.exec(appealReference)) {
-		return res.render(`appeal-search-number/index`, {
+		return res.render(`enter-appeal-reference/index`, {
 			inlineErrorMessage: { text: 'Enter the appeal reference using numbers 0 to 9' },
 			value: appealReference
 		});
