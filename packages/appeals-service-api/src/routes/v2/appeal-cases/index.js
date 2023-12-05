9496cc9d12a4556d07b264bd78038f6efd8e1af6
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-12-05 20:10:14 +0000

    fix(aapd-954): wrap async handlers to catch errors

--- packages/appeals-service-api/src/routes/v2/appeal-cases/index.js
+++ packages/appeals-service-api/src/routes/v2/appeal-cases/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { list, getByCaseReference, getCount } = require('./controller');
+const asyncHandler = require('#middleware/async-handler');
 const router = express.Router();
 
-router.get('/', list);
-router.get('/count', getCount);
-router.get('/:caseReference', getByCaseReference);
+router.get('/', asyncHandler(list));
+router.get('/count', asyncHandler(getCount));
+router.get('/:caseReference', asyncHandler(getByCaseReference));
 
 module.exports = { router };
