44d6cbe326d5aef06f49ea32077ce5fa39fa24dd
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-12-05 21:39:39 +0000

    fix(api): ensure integration tests return a relevant status code

--- packages/appeals-service-api/__tests__/developer/globalSetup.js
+++ packages/appeals-service-api/__tests__/developer/globalSetup.js
@@ -1,5 +1,21 @@
+const { exec } = require('child_process');
+
+function run(cmd) {
+	return new Promise((resolve, reject) => {
+		exec(cmd, (err, stdout) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(stdout);
+			}
+		});
+	});
+}
+
 module.exports = async () => {
 	const { create } = require('./external-dependencies/database/test-database');
 	process.env.LOGGER_LEVEL = 'fatal';
 	await create();
+
+	await run(`npx prisma migrate deploy`);
 };
