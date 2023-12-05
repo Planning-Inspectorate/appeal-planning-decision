8123e88d8889723c4c9bb20eade37ccdcb08774b
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-12-05 21:39:39 +0000

    fix(api): ensure integration tests return a relevant status code

--- packages/appeals-service-api/__tests__/developer/external-dependencies/database/test-database.js
+++ packages/appeals-service-api/__tests__/developer/external-dependencies/database/test-database.js
@@ -1,24 +1,54 @@
 const { GenericContainer, Wait } = require('testcontainers/');
 
-let mongoDbContainer;
+/**
+ * @type {import('testcontainers').StartedTestContainer[]}
+ */
+const containers = [];
 
 const create = async () => {
-	mongoDbContainer = await new GenericContainer('mongo')
+	await Promise.all([startMongo(), startSql()]);
+};
+
+async function startMongo() {
+	const container = await new GenericContainer('mongo')
 		.withName('appeals-api-it-mongodb')
 		.withExposedPorts(27017)
 		.withWaitStrategy(Wait.forLogMessage('Waiting for connections'))
 		.start();
+	containers.push(container);
 
-	let dbName = 'appeals-api-integration-test';
-	process.env.INTEGRATION_TEST_DB_URL = `mongodb://localhost:${mongoDbContainer.getMappedPort(
+	const dbName = 'appeals-api-integration-test';
+	process.env.INTEGRATION_TEST_DB_URL = `mongodb://localhost:${container.getMappedPort(
 		27017
 	)}/${dbName}`;
-};
+}
+
+async function startSql() {
+	const container = await new GenericContainer('mcr.microsoft.com/azure-sql-edge:latest')
+		.withName('appeals-mssql-integration-tests')
+		.withExposedPorts(1433)
+		.withAddedCapabilities('SYS_PTRACE')
+		.withUser('root')
+		.withEnvironment({ ACCEPT_EULA: '1', MSSQL_SA_PASSWORD: 'DockerDatabaseP@22word!' })
+		.withWaitStrategy(Wait.forListeningPorts())
+		.start();
+
+	containers.push(container);
+
+	const sqlDbName = 'pins_front_office_integration_test';
+	const connectionString = [
+		`sqlserver://localhost:${container.getMappedPort(1433)}`,
+		`database=${sqlDbName}`,
+		`user=sa`,
+		`password=DockerDatabaseP@22word!`,
+		`trustServerCertificate=true`
+	].join(';');
+	process.env.SQL_CONNECTION_STRING_ADMIN = connectionString;
+	process.env.SQL_CONNECTION_STRING = connectionString;
+}
 
 const teardown = async () => {
-	if (mongoDbContainer) {
-		await mongoDbContainer.stop();
-	}
+	await Promise.all(containers.map((c) => c.stop()));
 };
 
 module.exports = {
