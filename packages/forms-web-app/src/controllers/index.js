c1712f04cb288c56f3d198df5d94d4bd0bf431d4
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-12-07 18:00:58 +0000

    docs(aapd-861): update jsdoc and use flag from const

--- packages/forms-web-app/src/controllers/index.js
+++ packages/forms-web-app/src/controllers/index.js
@@ -1,8 +1,9 @@
 const config = require('../config');
 const { isFeatureActive } = require('../featureFlag');
+const { FLAG } = require('@pins/common/src/feature-flags');
 
 exports.getIndex = async (_, res) => {
-	const isEnrolUsersActive = await isFeatureActive('enrol-users');
+	const isEnrolUsersActive = await isFeatureActive(FLAG.ENROL_USERS);
 	res.redirect(
 		isEnrolUsersActive ? config.appeals.startingPointEnrolUsersActive : config.appeals.startingPoint
 	);
