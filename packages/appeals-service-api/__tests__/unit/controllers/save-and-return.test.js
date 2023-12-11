19aafe4b1aa85f86b040cdb1fc427755b64975c4
Mark Potter <si3rr4wow@gmail.com>
2023-12-11 16:08:33 +0000

    feat(AAPD-855): more mocking

--- packages/appeals-service-api/__tests__/unit/controllers/save-and-return.test.js
+++ packages/appeals-service-api/__tests__/unit/controllers/save-and-return.test.js
@@ -9,6 +9,11 @@ const {
 jest.mock('../../../src/services/save-and-return.service');
 jest.mock('../../../src/services/appeal.service');
 jest.mock('../../../../common/src/lib/notify/notify-builder', () => ({}));
+jest.mock('../../../src/repositories/sql/appeals-repository', () => ({
+	AppealsRepository: class {
+		createAppeal = Promise.resolve;
+	}
+}));
 
 describe('Save And Return API', () => {
 	let req;
