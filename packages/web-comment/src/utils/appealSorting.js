ab8f729e14d869fabbe5f7eae957f6ecaa292672
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-12-18 15:17:27 +0000

    fix(aapd-837): fix date sort if field is missing

--- packages/web-comment/src/utils/appealSorting.js
+++ packages/web-comment/src/utils/appealSorting.js
@@ -1,29 +1,75 @@
+/**
+ * @typedef {import('appeals-service-api').Api.AppealCaseWithAppellant} AppealCaseWithAppellant
+ */
+
+/**
+ * @param {AppealCaseWithAppellant[]} appeals
+ * @returns {AppealCaseWithAppellant[]}
+ */
 const getOpenAppeals = (appeals) => {
 	const currentDate = new Date();
-	return appeals.filter((appeal) => new Date(appeal.interestedPartyRepsDueDate) > currentDate);
+	// only include appeals with interestedPartyRepsDueDate set.
+	return appeals.filter(
+		(appeal) =>
+			appeal.interestedPartyRepsDueDate && new Date(appeal.interestedPartyRepsDueDate) > currentDate
+	);
 };
 
+/**
+ * @param {AppealCaseWithAppellant[]} appeals
+ * @returns {AppealCaseWithAppellant[]}
+ */
 const getClosedAppeals = (appeals) => {
 	const currentDate = new Date();
-	return appeals.filter((appeal) => new Date(appeal.interestedPartyRepsDueDate) < currentDate);
+	// include appeals with interestedPartyRepsDueDate not set.
+	return appeals.filter(
+		(appeal) =>
+			!appeal.interestedPartyRepsDueDate ||
+			new Date(appeal.interestedPartyRepsDueDate) < currentDate
+	);
 };
 
-const sortByInterestedPartyRepsDueDate = (a, b) => {
-	return new Date(a.interestedPartyRepsDueDate) - new Date(b.interestedPartyRepsDueDate);
-};
+/**
+ * @typedef {function(AppealCaseWithAppellant, AppealCaseWithAppellant): number} AppealSorter
+ */
 
+/** @type {AppealSorter} */
+const sortByInterestedPartyRepsDueDate = sortByDateFieldDesc('interestedPartyRepsDueDate');
+
+/** @type {AppealSorter} */
 const sortByCaseReference = (a, b) => {
 	return a.caseReference.localeCompare(b.caseReference, undefined, { numeric: true });
 };
 
-const sortByCaseDecisionDate = (a, b) => {
-	return new Date(b.caseDecisionDate) - new Date(a.caseDecisionDate);
-};
+/** @type {AppealSorter} */
+const sortByCaseDecisionDate = sortByDateFieldDesc('caseDecisionDate');
+
+/**
+ * @param {string} field
+ * @returns {function(any, any): number}
+ */
+function sortByDateFieldDesc(field) {
+	return (a, b) => {
+		const aSet = typeof a[field] === 'string';
+		const bSet = typeof b[field] === 'string';
+		if (aSet && !bSet) {
+			return -1;
+		}
+		if (!aSet && bSet) {
+			return 1;
+		}
+		if (!aSet && !bSet) {
+			return 0;
+		}
+		return new Date(b[field]).getTime() - new Date(a[field]).getTime();
+	};
+}
 
 module.exports = {
 	getOpenAppeals,
 	getClosedAppeals,
 	sortByInterestedPartyRepsDueDate,
 	sortByCaseReference,
-	sortByCaseDecisionDate
+	sortByCaseDecisionDate,
+	sortByDateFieldDesc
 };
