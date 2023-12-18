974369292be8f90381bbd7f8a83072de2287c588
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-12-18 15:17:27 +0000

    fix(aapd-837): fix date sort if field is missing

---
+++ packages/web-comment/src/utils/appealSorting.test.js
@@ -0,0 +1,61 @@
+const { sortByDateFieldDesc } = require('./appealSorting');
+
+describe('appealSorting', () => {
+	describe('sortByDateFieldDesc', () => {
+		/**
+		 * @param {string} field
+		 * @param {number} day
+		 * @returns {Object<string, string>}
+		 */
+		function objWithDateField(field, day) {
+			return {
+				[field]: `2023-12-${day}T10:00Z`
+			};
+		}
+		const tests = [
+			{
+				name: 'empty',
+				field: 'empty',
+				list: [],
+				want: []
+			},
+			{
+				name: 'all present',
+				field: 'caseDecisionDate',
+				list: [
+					objWithDateField('caseDecisionDate', 15),
+					objWithDateField('caseDecisionDate', 20),
+					objWithDateField('caseDecisionDate', 18),
+					objWithDateField('caseDecisionDate', 12)
+				],
+				want: [
+					objWithDateField('caseDecisionDate', 20),
+					objWithDateField('caseDecisionDate', 18),
+					objWithDateField('caseDecisionDate', 15),
+					objWithDateField('caseDecisionDate', 12)
+				]
+			},
+			{
+				name: 'some missing',
+				field: 'caseDecisionDate',
+				list: [
+					objWithDateField('caseDecisionDate', 15),
+					{ case: 1 },
+					objWithDateField('caseDecisionDate', 18),
+					objWithDateField('caseDecisionDate', 12)
+				],
+				want: [
+					objWithDateField('caseDecisionDate', 18),
+					objWithDateField('caseDecisionDate', 15),
+					objWithDateField('caseDecisionDate', 12),
+					{ case: 1 }
+				]
+			}
+		];
+
+		test.each(tests)('$name', ({ field, list, want }) => {
+			const got = list.sort(sortByDateFieldDesc(field));
+			expect(got).toEqual(want);
+		});
+	});
+});
