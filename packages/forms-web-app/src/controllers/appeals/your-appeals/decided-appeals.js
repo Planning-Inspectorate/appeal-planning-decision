40c5a8178ad3fe645ab5b7efe11b7d309d3742e4
Marc-Steeven Eyeni-Kantsey <marc-steeveneyeni-kantsey@MacBook-Air-de-Salome.local>
2023-12-19 12:16:27 +0000

    feat(aapd-987): get email from appeal in session for testing purposes

--- packages/forms-web-app/src/controllers/appeals/your-appeals/decided-appeals.js
+++ packages/forms-web-app/src/controllers/appeals/your-appeals/decided-appeals.js
@@ -3,7 +3,7 @@ const { mapToAppellantDashboardDisplayData } = require('../../../lib/dashboard-f
 const { VIEW } = require('../../../lib/views');
 
 exports.get = async (req, res) => {
-	const { email } = req.session;
+	const { email } = req.session.appeal;
 	const user = await getUserByEmail(email, true);
 	let appeals = await getUserAppealsById(user.id);
 	appeals = appeals.map(mapToAppellantDashboardDisplayData).filter((item) => item.decisionOutcome);
