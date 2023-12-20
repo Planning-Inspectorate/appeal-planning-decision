66ec820bce16f37e4e98e68be67471a5a15b30ab
Marc-Steeven Eyeni-Kantsey <marc-steeveneyeni-kantsey@MacBook-Air-de-Salome.local>
2023-12-20 09:23:50 +0000

    feat(aapd-987): make your appeals and decided appeals page display with no user found

--- packages/forms-web-app/src/controllers/appeals/your-appeals.js
+++ packages/forms-web-app/src/controllers/appeals/your-appeals.js
@@ -1,11 +1,21 @@
 const { getUserAppealsById, getUserByEmail } = require('../../lib/appeals-api-wrapper');
 const { mapToAppellantDashboardDisplayData } = require('../../lib/dashboard-functions');
 const { VIEW } = require('../../lib/views');
+const logger = require('../../lib/logger');
 
 exports.get = async (req, res) => {
-	const { email } = req.session.appeal;
-	const user = await getUserByEmail(email, true);
-	let appeals = await getUserAppealsById(user.id);
-	appeals = appeals.map(mapToAppellantDashboardDisplayData);
-	res.render(VIEW.APPEALS.YOUR_APPEALS, { toDoAppeals: appeals, waitingForReviewAppeals: appeals });
+	const email = req.session.appeal?.email;
+	let viewContext = {};
+	try {
+		const user = await getUserByEmail(email, true);
+		let appeals = await getUserAppealsById(user.id);
+		if (appeals?.length > 0) {
+			appeals = appeals.map(mapToAppellantDashboardDisplayData);
+			viewContext = { toDoAppeals: appeals, waitingForReviewAppeals: appeals };
+		}
+	} catch (error) {
+		logger.error(`Failed to get user appeals: ${error}`);
+	} finally {
+		res.render(VIEW.APPEALS.YOUR_APPEALS, viewContext);
+	}
 };
