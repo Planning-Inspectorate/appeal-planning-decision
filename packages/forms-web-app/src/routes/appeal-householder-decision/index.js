6a44ec0e10a500580ef7ab0a22eb2586f619a75b
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-01-12 12:16:02 +0000

    feat(aapd-863): add missing route for HAS

--- packages/forms-web-app/src/routes/appeal-householder-decision/index.js
+++ packages/forms-web-app/src/routes/appeal-householder-decision/index.js
@@ -4,10 +4,15 @@ const router = express.Router();
 const checkDecisionDateDeadline = require('#middleware/check-decision-date-deadline');
 const checkAppealExists = require('#middleware/check-appeal-exists');
 const checkLoggedIn = require('#middleware/check-logged-in');
+const fetchExistingAppealMiddleware = require('#middleware/fetch-existing-appeal');
+const {
+	getAppealAlreadySubmitted
+} = require('../../controllers/full-appeal/submit-appeal/appeal-already-submitted');
 
 router.use(require('./cannot-appeal'));
 router.use(require('./login'));
 
+router.get('/appeal-already-submitted', [fetchExistingAppealMiddleware], getAppealAlreadySubmitted);
 router.use(checkAppealExists, checkDecisionDateDeadline, require('./planning-application-number'));
 
 // post login
