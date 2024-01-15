c0e7d6cd833447f8e5ff93a97835f96cb7e6b3dc
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-01-15 12:44:35 +0000

    feat(aapd-490): add open api request validator library

---
+++ packages/appeals-service-api/src/validators/validate-open-api.js
@@ -0,0 +1,38 @@
+const OpenApiValidator = require('express-openapi-validator');
+const { generateOpenApiSpec } = require('../spec/gen-api-spec');
+const ApiError = require('#errors/apiError');
+const { BadRequest } = require('express-openapi-validator/dist/openapi.validator');
+
+/**
+ * Create middleware to validate requests against the OpenApi spec
+ *
+ * @returns {import('express-openapi-validator/dist/framework/types').OpenApiRequestHandler[]}
+ */
+function openApiValidatorMiddleware() {
+	const spec = generateOpenApiSpec();
+
+	return OpenApiValidator.middleware({
+		apiSpec: spec,
+		validateApiSpec: true,
+		validateRequests: true,
+		ignoreUndocumented: true
+	});
+}
+
+/**
+ * Error handler for OpenAPI Validation errors - maps to an ApiError
+ *
+ * @type {import('express').ErrorRequestHandler}
+ */
+function openApiValidationErrorHandler(err, req, res, next) {
+	// handle validation errors
+	if (err instanceof BadRequest) {
+		throw ApiError.badRequest({ errors: err.errors.map((e) => e.message) });
+	}
+	next(err); // pass up the chain
+}
+
+module.exports = {
+	openApiValidatorMiddleware,
+	openApiValidationErrorHandler
+};
