a9aa23efc5c3fd001ae3cf893957a9ac879c3ecf
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-01-17 15:45:29 +0000

    refactor(aapd-863): simplify token check logic

--- packages/forms-web-app/__tests__/unit/lib/is-token-valid.test.js
+++ packages/forms-web-app/__tests__/unit/lib/is-token-valid.test.js
@@ -1,4 +1,4 @@
-const { isTokenValid, testConfirmEmailToken } = require('../../../src/lib/is-token-valid');
+const { isTokenValid } = require('../../../src/lib/is-token-valid');
 const { checkToken } = require('../../../src/lib/appeals-api-wrapper');
 const { isTokenExpired } = require('../../../src/lib/is-token-expired');
 const { utils } = require('@pins/common');
@@ -9,6 +9,9 @@ jest.mock('../../../src/lib/is-token-expired');
 jest.mock('../../../src/config');
 jest.mock('@pins/common');
 
+const CODE = '63654';
+const ID = 'e2813fb0-e269-4fe2-890e-6405dbd4a5ea';
+
 describe('lib/is-token-valid', () => {
 	beforeEach(() => {
 		jest.resetAllMocks();
@@ -18,7 +21,7 @@ describe('lib/is-token-valid', () => {
 		it('should return false without calling checkToken or isTokenExpired if id parameter is missing', async () => {
 			checkToken.mockReturnValue(undefined);
 
-			const result = await isTokenValid(undefined, '63654');
+			const result = await isTokenValid(CODE);
 
 			expect(checkToken).not.toBeCalled();
 			expect(isTokenExpired).not.toBeCalled();
@@ -27,7 +30,7 @@ describe('lib/is-token-valid', () => {
 		it('should return false without calling checkToken or isTokenExpired if token parameter is missing', async () => {
 			checkToken.mockReturnValue(undefined);
 
-			const result = await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', undefined);
+			const result = await isTokenValid(undefined, ID);
 
 			expect(checkToken).not.toBeCalled();
 			expect(isTokenExpired).not.toBeCalled();
@@ -36,7 +39,7 @@ describe('lib/is-token-valid', () => {
 		it('should return false without calling checkToken or isTokenExpired if id parameter is empty', async () => {
 			checkToken.mockReturnValue(undefined);
 
-			const result = await isTokenValid('', '63654');
+			const result = await isTokenValid(CODE, '');
 
 			expect(checkToken).not.toBeCalled();
 			expect(isTokenExpired).not.toBeCalled();
@@ -45,7 +48,7 @@ describe('lib/is-token-valid', () => {
 		it('should return false without calling checkToken or isTokenExpired if token parameter is empty', async () => {
 			checkToken.mockReturnValue(undefined);
 
-			const result = await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '');
+			const result = await isTokenValid('', ID);
 
 			expect(checkToken).not.toBeCalled();
 			expect(isTokenExpired).not.toBeCalled();
@@ -54,7 +57,7 @@ describe('lib/is-token-valid', () => {
 		it('should return false without calling checkToken or isTokenExpired if id parameter is of an unexpected type', async () => {
 			checkToken.mockReturnValue(undefined);
 
-			const result = await isTokenValid({}, '63654');
+			const result = await isTokenValid(CODE, {});
 
 			expect(checkToken).not.toBeCalled();
 			expect(isTokenExpired).not.toBeCalled();
@@ -63,41 +66,48 @@ describe('lib/is-token-valid', () => {
 		it('should return false without calling checkToken or isTokenExpired if token parameter is of an unexpected type', async () => {
 			checkToken.mockReturnValue(undefined);
 
-			const result = await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', {});
+			const result = await isTokenValid({}, ID);
 
 			expect(checkToken).not.toBeCalled();
 			expect(isTokenExpired).not.toBeCalled();
 			expect(result.valid).toBe(false);
 		});
-		test.each([
-			[[false, false, 'a']],
-			[[true, false, 'a']],
-			[[false, true, 'a']],
-			[[true, true, 'a']],
-			[[false, false, testConfirmEmailToken]],
-			[[true, false, testConfirmEmailToken]],
-			[[false, true, testConfirmEmailToken]]
-			// [[true, true, utils.testLPACode]],
-		])('should not skip if any testing params arent set', async (input) => {
-			checkToken.mockReturnValue({
-				id: 'abc',
-				createdAt: '2023-03-17T15:47:55.654Z'
-			});
-			isTokenExpired.mockReturnValue(true);
-			config.server = {
-				allowTestingOverrides: input[0]
-			};
-			utils.isTestLPA.mockReturnValue(input[1]);
-			const result = await isTokenValid('abc', input[2], {
-				appeal: {
-					lpaCode: 'something'
-				}
-			});
 
-			expect(result.valid).toBe(false);
-			expect(checkToken).toHaveBeenCalledTimes(1);
-			expect(isTokenExpired).toHaveBeenCalledTimes(1);
+		const getAllowTestScenario = (allowTestingOverrides, isTestLpa, token) => ({
+			allowTestingOverrides,
+			isTestLpa,
+			token
 		});
+
+		test.each([
+			[getAllowTestScenario(false, false, 'a')],
+			[getAllowTestScenario(true, false, 'a')],
+			[getAllowTestScenario(false, true, 'a')],
+			[getAllowTestScenario(true, true, 'a')],
+			[getAllowTestScenario(false, false, '12345')],
+			[getAllowTestScenario(true, false, '12345')],
+			[getAllowTestScenario(false, true, '12345')]
+		])(
+			'should not skip if any testing params arent set',
+			async ({ allowTestingOverrides, isTestLpa, token }) => {
+				checkToken.mockReturnValue({
+					id: 'abc',
+					createdAt: '2023-03-17T15:47:55.654Z'
+				});
+				isTokenExpired.mockReturnValue(true);
+
+				config.server = {
+					allowTestingOverrides: allowTestingOverrides
+				};
+				utils.isTestLPA.mockReturnValue(isTestLpa);
+
+				const result = await isTokenValid(token, 'id', 'email', 'action', 'lpa-code');
+
+				expect(result.valid).toBe(false);
+				expect(checkToken).toHaveBeenCalledTimes(1);
+				expect(isTokenExpired).toHaveBeenCalledTimes(1);
+			}
+		);
 
 		it('should return false if too many attempts', async () => {
 			const tooManyAttempts = new Error('Too Many Requests');
@@ -106,9 +116,7 @@ describe('lib/is-token-valid', () => {
 			});
 			isTokenExpired.mockReturnValue(false);
 
-			const result = await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654', '', {
-				enterCode: { action: 'saveAndReturn' }
-			});
+			const result = await isTokenValid(CODE, ID, '', 'saveAndReturn');
 			expect(result.valid).toBe(false);
 			expect(result.tooManyAttempts).toBe(true);
 			expect(result.action).toBe('saveAndReturn');
@@ -116,16 +124,16 @@ describe('lib/is-token-valid', () => {
 		it('should call checkToken if both parameters are valid', async () => {
 			checkToken.mockReturnValue(undefined);
 
-			await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654', '');
+			await isTokenValid(ID, CODE, '');
 
-			expect(checkToken).toBeCalledWith('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654', '');
+			expect(checkToken).toBeCalledWith(ID, CODE, '');
 		});
 		it('should return false without calling isTokenExpired if returned token document is undefined', async () => {
 			checkToken.mockReturnValue(undefined);
 
-			const result = await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654', '');
+			const result = await isTokenValid(CODE, ID, '');
 
-			expect(checkToken).toBeCalledWith('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654', '');
+			expect(checkToken).toBeCalledWith(CODE, ID, '');
 			expect(isTokenExpired).not.toBeCalled();
 			expect(result.valid).toBe(false);
 		});
@@ -134,16 +142,16 @@ describe('lib/is-token-valid', () => {
 				createdAt: '2023-03-17T15:47:55.654Z'
 			});
 			isTokenExpired.mockReturnValue(true);
-			const result = await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654');
+			const result = await isTokenValid(CODE, ID);
 			expect(isTokenExpired).toBeCalled();
 			expect(result.valid).toBe(false);
 		});
 		it('should return false if returned token document is missing createdAt prop', async () => {
 			checkToken.mockReturnValue({
-				id: 'e2813fb0-e269-4fe2-890e-6405dbd4a5ea'
+				id: ID
 			});
 			isTokenExpired.mockReturnValue(true);
-			const result = await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654');
+			const result = await isTokenValid(CODE, ID);
 			expect(isTokenExpired).toBeCalled();
 			expect(result.valid).toBe(false);
 		});
@@ -153,37 +161,37 @@ describe('lib/is-token-valid', () => {
 				createdAt: '2023-03-17T15:47:55.654Z'
 			});
 			isTokenExpired.mockReturnValue(true);
-			const result = await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654');
+			const result = await isTokenValid(CODE, ID);
 			expect(isTokenExpired).toBeCalled();
 			expect(result.valid).toBe(false);
 		});
 		it('should call isTokenExpired if checkToken returns a token document containing a correct id and createdAt prop', async () => {
 			checkToken.mockReturnValue({
-				id: 'e2813fb0-e269-4fe2-890e-6405dbd4a5ea',
+				id: ID,
 				createdAt: '2023-03-17T15:47:55.654Z'
 			});
 
-			await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654');
+			await isTokenValid(CODE, ID);
 			expect(isTokenExpired).toBeCalledWith(30, new Date('2023-03-17T15:47:55.654Z'));
 		});
 		it('should return false if returned token document id matches id parameter but isTokenExpired returns true', async () => {
 			checkToken.mockReturnValue({
-				id: 'e2813fb0-e269-4fe2-890e-6405dbd4a5ea',
+				id: ID,
 				createdAt: '2023-03-17T15:47:55.654Z'
 			});
 			isTokenExpired.mockReturnValue(true);
 
-			const result = await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654');
+			const result = await isTokenValid(CODE, ID);
 			expect(result.valid).toBe(false);
 		});
 		it('should return true if returned token document id matches id parameter and isTokenExpired returns false', async () => {
 			checkToken.mockReturnValue({
-				id: 'e2813fb0-e269-4fe2-890e-6405dbd4a5ea',
+				id: ID,
 				createdAt: '2023-03-17T15:47:55.654Z'
 			});
 			isTokenExpired.mockReturnValue(false);
 
-			const result = await isTokenValid('e2813fb0-e269-4fe2-890e-6405dbd4a5ea', '63654');
+			const result = await isTokenValid(CODE, ID);
 			expect(result.valid).toBe(true);
 		});
 	});
