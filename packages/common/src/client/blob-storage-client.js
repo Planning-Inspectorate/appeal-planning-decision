641c81175b84610874fa120b2874a484788b79cc
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-01-23 14:43:26 +0000

    feat(aapd-672): add blob storage client

---
+++ packages/common/src/client/blob-storage-client.js
@@ -0,0 +1,181 @@
+const { DefaultAzureCredential } = require('@azure/identity');
+const {
+	BlobServiceClient,
+	BlobSASPermissions,
+	SASProtocol,
+	generateBlobSASQueryParameters
+} = require('@azure/storage-blob');
+const logger = require('../lib/logger');
+const BlobStorageError = require('./blob-storage-error');
+
+/**
+ * todo: seed local files
+ * todo: get user delegation working locally
+ */
+class BlobStorageClient {
+	/**
+	 * @param {string} host - host for blob account
+	 * @param {string} [connectionString] - if possible leave this undefined, fallback option for if user delegation doesn't work
+	 */
+	constructor(host, connectionString) {
+		if (!host) {
+			throw new Error('host is required');
+		}
+
+		this.host = host;
+
+		if (!connectionString) {
+			// use azure rbac + managed identity to access (preferred route)
+			this.blobServiceClient = new BlobServiceClient(this.host, new DefaultAzureCredential());
+			this.isSharedKeyCredential = false;
+			logger.info('BlobStorageClient using user delegation');
+		} else {
+			// fallback route
+			this.blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+			this.isSharedKeyCredential = true;
+			logger.info('BlobStorageClient using connection string');
+		}
+	}
+
+	/**
+	 * @param {string} containerName
+	 * @param {string} blobName
+	 * @returns {import('@azure/storage-blob').BlockBlobClient}
+	 */
+	#getBlockBlobClient(containerName, blobName) {
+		const containerClient = this.blobServiceClient.getContainerClient(containerName);
+		return containerClient.getBlockBlobClient(blobName);
+	}
+
+	/**
+	 * Generates a url with short lived access to the given blob
+	 * @param {string} containerName - Blob storage container name
+	 * @param {string} blobName - Blob storage container name
+	 * @param {Date} [expiresOn] The end time for the user delegation SAS. Must be within 7 days of the current time, defaults to 10 mins from now
+	 * @returns {Promise<string>} - empty string if the
+	 */
+	async getBlobSASUrl(containerName, blobName, expiresOn) {
+		const TEN_MINUTES = 10 * 60 * 1000;
+		const NOW = new Date();
+		const startsOn = new Date(NOW.valueOf() - TEN_MINUTES);
+
+		if (!expiresOn) {
+			expiresOn = new Date(NOW.valueOf() + TEN_MINUTES);
+		}
+
+		logger.info(`creating sas: ${this.host}/${containerName}/${blobName} expires: ${expiresOn}`);
+
+		const blob = this.#getBlockBlobClient(containerName, blobName);
+
+		if (!(await blob.exists())) {
+			logger.error('blob does not exist');
+			throw new BlobStorageError('blob does not exist', 404);
+		}
+
+		const blobPermissions = 'r'; // read permission
+
+		if (this.isSharedKeyCredential) {
+			logger.info(`creating shared-key sas url`);
+
+			const url = blob.generateSasUrl({
+				protocol: SASProtocol.Https,
+				startsOn: startsOn,
+				expiresOn: expiresOn,
+				permissions: BlobSASPermissions.parse(blobPermissions)
+			});
+
+			return url;
+		}
+
+		logger.info(`creating user delegation sas url`);
+
+		// https://learn.microsoft.com/en-us/rest/api/storageservices/create-user-delegation-sas
+		const userDelegationKey = await this.blobServiceClient.getUserDelegationKey(
+			startsOn,
+			expiresOn
+		);
+
+		const sasOptions = {
+			blobName,
+			containerName,
+			permissions: BlobSASPermissions.parse(blobPermissions),
+			protocol: SASProtocol.Https,
+			startsOn: startsOn,
+			expiresOn: expiresOn
+		};
+
+		// https://learn.microsoft.com/en-us/azure/storage/common/storage-sas-overview?toc=%2Fazure%2Fstorage%2Fblobs%2Ftoc.json#sas-token
+		const sasToken = generateBlobSASQueryParameters(
+			sasOptions,
+			userDelegationKey,
+			this.host
+		).toString();
+
+		return `${this.host}/${containerName}/${blobName}?${sasToken}`;
+	}
+
+	/**
+	 * downloads blob and returns as a stream
+	 * @param {string} containerName - Blob storage container name
+	 * @param {string} blobName - Blob storage container name
+	 * @returns {Promise<import('@azure/storage-blob').BlobGetPropertiesResponse>}
+	 */
+	async getProperties(containerName, blobName) {
+		const blob = this.#getBlockBlobClient(containerName, blobName);
+
+		if (!(await blob.exists())) {
+			logger.error('blob does not exist');
+			throw new BlobStorageError('blob does not exist', 404);
+		}
+
+		logger.info(`get metadata for: ${this.host}/${containerName}/${blobName}`);
+
+		return blob.getProperties();
+	}
+
+	/**
+	 * downloads blob and returns as a stream
+	 * @param {string} containerName - Blob storage container name
+	 * @param {string} blobName - Blob storage container name
+	 * @returns {Promise<import('@azure/storage-blob').BlobDownloadResponseParsed>}
+	 */
+	async downloadBlob(containerName, blobName) {
+		const blob = this.#getBlockBlobClient(containerName, blobName);
+
+		if (!(await blob.exists())) {
+			logger.error('blob does not exist');
+			throw new BlobStorageError('blob does not exist', 404);
+		}
+
+		logger.info(`downloading: ${this.host}/${containerName}/${blobName}`);
+
+		return blob.download();
+	}
+
+	/**
+	 * uploads file to container, requires create permissions for the given container
+	 * @param {string} containerName - Blob storage container name
+	 * @param {string} blobName - Blob name, may include path
+	 * @param {Object<string,string>} metadata - PINs Data Model metadata, requires contentType
+	 * @param {import('node:stream').Readable} fileStream - file
+	 * @returns {Promise<import('@azure/storage-blob').BlobUploadCommonResponse>}
+	 */
+	async uploadBlob(containerName, blobName, metadata, fileStream) {
+		const containerClient = this.blobServiceClient.getContainerClient(containerName);
+
+		await containerClient.createIfNotExists();
+
+		const blob = containerClient.getBlockBlobClient(blobName);
+
+		logger.info(`uploading file: ${this.host}/${containerName}/${blobName}`);
+
+		return await blob.uploadStream(fileStream, undefined, undefined, {
+			blobHTTPHeaders: {
+				blobContentType: metadata.contentType
+			},
+			metadata: metadata
+		});
+	}
+}
+
+module.exports = BlobStorageClient;
