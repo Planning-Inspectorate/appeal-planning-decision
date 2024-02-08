53583020216a7be728f31bd74e65ae4dfee6554e
Tom Pickering <topickering@gmail.com>
2024-02-08 12:22:52 +0000

    feat(951): add headline data

--- packages/forms-web-app/src/controllers/selected-appeal/selected-appeal.js
+++ packages/forms-web-app/src/controllers/selected-appeal/selected-appeal.js
@@ -1,23 +1,78 @@
 const { VIEW } = require('../../lib/views');
 const { apiClient } = require('../../lib/appeals-api-client');
+const { formatAddress } = require('@pins/common/src/lib/format-address');
+
+/**
+ * @typedef {import('appeals-service-api').Api.AppealCaseWithAppellant} AppealCaseWithAppellant
+ */
 
 exports.get = async (req, res) => {
 	const appealNumber = req.params.appealNumber;
 
 	const caseData = await apiClient.getAppealCaseDataByCaseReference(appealNumber);
 
-	// type
-	// procedure
-	// site
-	// applicant
-	// application number
+	console.log(caseData);
+
+	const headlineData = formatHeadlineData(caseData[0]);
 
 	const viewContext = {
 		appeal: {
 			appealNumber: appealNumber,
-			appealType: caseData[0].appealTypeName
+			headlineData
 		}
 	};
 
 	res.render(VIEW.SELECTED_APPEAL.APPEAL, viewContext);
+};
+
+/**
+ * @param {AppealCaseWithAppellant} caseData
+ */
+const formatHeadlineData = (caseData) => {
+	const { appealTypeName, procedure, LPAApplicationReference } = caseData;
+
+	const address = formatAddress(caseData);
+
+	return [
+		{
+			key: {
+				text: 'Appeal type'
+			},
+			value: {
+				text: appealTypeName
+			}
+		},
+		{
+			key: {
+				text: 'Appeal procedure'
+			},
+			value: {
+				text: procedure
+			}
+		},
+		{
+			key: {
+				text: 'Appeal site'
+			},
+			value: {
+				text: address
+			}
+		},
+		{
+			key: {
+				text: 'Applicant'
+			},
+			value: {
+				text: 'placeholding'
+			}
+		},
+		{
+			key: {
+				text: 'Application number'
+			},
+			value: {
+				text: LPAApplicationReference
+			}
+		}
+	];
 };
--- packages/forms-web-app/src/routes/appeals/your-appeals/index.js
+++ packages/forms-web-app/src/routes/appeals/your-appeals/index.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const decidedAppealsRouter = require('./decided-appeals');
 const yourAppealsController = require('../../../controllers/appeals/your-appeals');
 const yourAppealsContinueController = require('../../../controllers/appeals/continue');
-const selectedAppealRouter = require('./selected-appeal');
 
 router.get('/', yourAppealsController.get);
 router.get('/continue/:appealId', yourAppealsContinueController.get);
