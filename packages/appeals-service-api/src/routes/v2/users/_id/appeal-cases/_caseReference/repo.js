3b7cb02d13d473eef4ce5c003c2c84a7325cae11
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-02-14 15:32:12 +0000

    fix(aapd-951): accept appellant|agent roles

--- packages/appeals-service-api/src/routes/v2/users/_id/appeal-cases/_caseReference/repo.js
+++ packages/appeals-service-api/src/routes/v2/users/_id/appeal-cases/_caseReference/repo.js
@@ -1,4 +1,5 @@
 const { createPrismaClient } = require('#db-client');
+const { APPEAL_USER_ROLES } = require('@pins/common/src/constants');
 const { PrismaClientKnownRequestError } = require('@prisma/client/runtime/library');
 
 /**
@@ -36,6 +37,22 @@ module.exports = class Repo {
 	 */
 	async get({ caseReference, role, userId }) {
 		try {
+			/**
+			 * @type {import('@prisma/client').Prisma.AppealToUserWhereInput}
+			 */
+			const where = {};
+			if (role === APPEAL_USER_ROLES.APPELLANT || role === APPEAL_USER_ROLES.AGENT) {
+				// if appellant or agent, search for both as we don't know the role before we fetch the appeal
+				// and access is equivalent
+				where.OR = [
+					{ userId, role: APPEAL_USER_ROLES.APPELLANT },
+					{ userId, role: APPEAL_USER_ROLES.AGENT }
+				];
+			} else {
+				where.userId = userId;
+				where.role = role;
+			}
+
 			return await this.dbClient.appealCase.findUnique({
 				where: {
 					caseReference
@@ -44,10 +61,7 @@ module.exports = class Repo {
 					Appeal: {
 						include: {
 							Users: {
-								where: {
-									userId,
-									role
-								}
+								where
 							}
 						}
 					}
