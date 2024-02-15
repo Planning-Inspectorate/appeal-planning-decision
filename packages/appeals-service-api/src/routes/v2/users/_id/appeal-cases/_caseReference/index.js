0bb09844c88aeada8cc11d641daa153df5971790
Dean Hasley <dhasley1990@gmail.com>
2024-02-15 08:42:38 +0000

    fix(aapd-951): update api call to use id

--- packages/appeals-service-api/src/routes/v2/users/_id/appeal-cases/_caseReference/index.js
+++ packages/appeals-service-api/src/routes/v2/users/_id/appeal-cases/_caseReference/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { get } = require('./controller');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const asyncHandler = require('@pins/common/src/middleware/async-handler');
 
 router.get('/', asyncHandler(get));
