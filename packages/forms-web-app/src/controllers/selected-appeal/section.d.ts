8cfdc52186da5472f7e046370b2f79605e2a0bb1
Lucy Potter <si3rr4wow@gmail.com>
2024-02-16 14:09:05 +0000

    feat(aadp-945): fix type, make best guesses at fields

--- packages/forms-web-app/src/controllers/selected-appeal/section.d.ts
+++ packages/forms-web-app/src/controllers/selected-appeal/section.d.ts
@@ -1,10 +1,10 @@
-import { AppealCaseWithAppellant } from '@pins/common';
+import type { Api } from 'appeals-service-api';
 
 export type Section = Array<{
 	heading: string;
 	links: Array<{
 		url: string;
 		text: string;
-		condition: (appealCase: AppealCaseWithAppellant) => boolean;
+		condition: (appealCase: Api.AppealCaseWithAppellant) => boolean;
 	}>;
 }>;
