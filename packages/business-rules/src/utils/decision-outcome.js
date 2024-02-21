3c11171ec01f26e64fb3a94f440aa2c8c355b504
Lucy Potter <si3rr4wow@gmail.com>
2024-02-21 10:47:05 +0000

    feat(aapd-950): move tag formatting to business rules

--- packages/business-rules/src/utils/decision-outcome.js
+++ packages/business-rules/src/utils/decision-outcome.js
@@ -16,6 +16,41 @@ function mapDecisionColour(decision) {
 	return (decision && decisionColourMap.get(decision)) || 'grey';
 }
 
+/**
+ * Map an appeal decision outcome to it's corresponding tag text
+ *
+ * @param {string|undefined} decision
+ * @returns {string | null}
+ */
+const mapDecisionLabel = (decision) => {
+	if (!decision || decision === 'invalid') return null;
+
+	const labels = {
+		[DECISION_OUTCOME.ALLOWED]: 'Allowed',
+		[DECISION_OUTCOME.SPLIT_DECISION]: 'Allowed in part',
+		[DECISION_OUTCOME.DISMISSED]: 'Dismissed'
+	};
+
+	return labels[decision];
+};
+
+/**
+ * Map an appeal decision outcome to it's corresponding tag text
+ *
+ * @param {string|undefined} decision
+ * @returns {{ color: string, label: string | null } | null}
+ */
+const mapDecisionTag = (decision) => {
+	if (!decision || decision === 'invalid') return null;
+
+	return {
+		color: mapDecisionColour(decision),
+		label: mapDecisionLabel(decision)
+	};
+};
+
 module.exports = {
-	mapDecisionColour
+	mapDecisionColour,
+	mapDecisionLabel,
+	mapDecisionTag
 };
