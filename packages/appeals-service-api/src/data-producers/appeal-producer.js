4ce7fab6383aa5d9e9803be782d5ac914e6fd1d0
Lucy Potter <si3rr4wow@gmail.com>
2024-02-22 17:04:27 +0000

    feat(aapd-1033): big rearrangening

--- packages/appeals-service-api/src/data-producers/appeal-producer.js
+++ packages/appeals-service-api/src/data-producers/appeal-producer.js
@@ -1,6 +1,10 @@
 const eventClient = require('../infrastructure/event-client');
 const { EventType } = require('../event-client/event-type');
 
+/**
+ * @param {Array<*>} data
+ * @returns {Promise<Array<*>>}
+ */
 const broadcast = async (data) => {
 	return await eventClient.sendEvents('appeal-fo-appellant-submission', data, EventType.Create);
 };
