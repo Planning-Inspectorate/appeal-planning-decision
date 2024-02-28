05ace600246af0913a3c44aa3cc8f1ee8a26e878
Tom Pickering <topickering@gmail.com>
2024-02-28 18:36:45 +0000

    feat(1061): r6 appeal details headlines

--- packages/forms-web-app/src/routes/rule-6-appeals/index.js
+++ packages/forms-web-app/src/routes/rule-6-appeals/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const selectedAppealController = require('../../controllers/selected-appeal/selected-appeal');
+const appealDetailsController = require('../../controllers/selected-appeal/appeal-details/appeal-details');
 
 router.get('/:appealNumber', selectedAppealController.get);
+router.get('/:appealNumber/appeal-details', appealDetailsController.get);
 
 module.exports = router;
