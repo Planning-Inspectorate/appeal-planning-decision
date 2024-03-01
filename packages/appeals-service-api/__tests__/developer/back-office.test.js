559770a86d00eee397a1d2be54ad657afd1e90ea
Lucy Potter <si3rr4wow@gmail.com>
2024-03-01 09:44:12 +0000

    feat(aapd-1033): really, is that enough?

--- packages/appeals-service-api/__tests__/developer/back-office.test.js
+++ packages/appeals-service-api/__tests__/developer/back-office.test.js
@@ -40,6 +40,7 @@ let testHorizonLpaCodeWales = 'H1234';
 jest.setTimeout(240000); // The Horizon integration tests need a bit of time to complete! This seemed like a good number (4 mins)
 jest.mock('../../src/db/db'); // TODO: We shouldn't need to do this, but we didn't have time to look at making this better. It should be possible to use the DB connection directly (not mock it)
 jest.mock('../../src/configuration/featureFlag');
+jest.mock('../../src/services/back-office-v2');
 
 const dbName = 'integration-test-back-office-db';
 
