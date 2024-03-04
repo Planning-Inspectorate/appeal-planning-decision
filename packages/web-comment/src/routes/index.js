21f75cf63599f6dd8ed5e4c243ffc8749a63751d
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-03-04 11:44:42 +0000

    feat(aapd-1196): add home page redirect for web comment

--- packages/web-comment/src/routes/index.js
+++ packages/web-comment/src/routes/index.js
@@ -1,5 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+/** @type {import('express').RequestHandler} */
+router.get('/', (_, res) => {
+	res.redirect('/enter-appeal-reference');
+});
+
 const { getRoutes } = require('@pins/common');
 
 const routes = getRoutes(__dirname);
+routes['/'] = router;
 
 module.exports = { routes };
