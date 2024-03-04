683dc7898cb57a5fa9efb0f82715a2a5b3ea5b62
Lucy Potter <si3rr4wow@gmail.com>
2024-03-04 11:36:24 +0000

    feat(aapd-1033): split out module reduction

--- packages/appeals-service-api/src/services/back-office-v2/formatters/index.js
+++ packages/appeals-service-api/src/services/back-office-v2/formatters/index.js
@@ -1,7 +1,29 @@
 const { APPEAL_ID } = require('@pins/business-rules/src/constants');
 /**
- * @typedef {import('src/spec/api-types').AppealSubmission} AppealSubmission
+ * @typedef {import('src/spec/api-types').AppealSubmission['appeal']} AppealSubmission
+ * @typedef {AppealSubmission['appealType']} AppealType
  * @typedef {import('../../../routes/v2/appeal-cases/_caseReference/lpa-questionnaire-submission/questionnaire-submission').LPAQuestionnaireSubmission} LPAQuestionnaireSubmission
+ */
+
+/**
+ * @template TArg0
+ * @template TArg1
+ * @template TReturn
+ * @typedef {(arg0: TArg0, arg1?: TArg1) => TReturn} Formatter
+ */
+
+/**
+ * @template TArg0
+ * @template TArg1
+ * @template TReturn
+ * @typedef {Record<AppealType, Formatter<TArg0, TArg1, TReturn>>} Formatters
+ */
+
+/**
+ * @template TArg0
+ * @template TArg1
+ * @template TReturn
+ * @typedef {Record<string, Formatters<TArg0, TArg1, TReturn>>} Module
  */
 
 const appealTypeToDirName = {
@@ -9,25 +31,38 @@ const appealTypeToDirName = {
 };
 
 /**
+ * @template TArg0
+ * @template TArg1
+ * @template TReturn
+ * @param {string} submittable
+ * @returns {(formatters: Formatters<TArg0, TArg1, TReturn>, entries: [AppealType, string]) => Formatters<TArg0, TArg1, TReturn>}
+ */
+const reduceFormatters =
+	(submittable) =>
+	(formatters, [appealType, dirName]) => ({
+		...formatters,
+		[appealType]: require(`./${dirName}/${submittable}`).formatter
+	});
+
+/**
+ * @template TArg0
+ * @template TArg1
+ * @template TReturn
+ * @type {(mod: Module<TArg0, TArg1, TReturn>, submittable: string) => Module<TArg0, TArg1, TReturn>}
+ */
+const reduceModules = (mod, submittable) => ({
+	...mod,
+	[submittable]: {
+		...Object.entries(appealTypeToDirName).reduce(reduceFormatters(submittable), {})
+	}
+});
+
+/**
  * this does "work" (the type is visible in autocomplete etc)
  * we should probably just tighten up what we expect to be in the db
  * @type {{
- *   appeal: Record<AppealSubmission['appeal']['appealType'], (appeal: AppealSubmission['appeal']) => *>,
- *   questionnaire: Record<AppealSubmission['appeal']['appealType'], (caseReference: string, questionnaireResponse: LPAQuestionnaireSubmission) => *>
+ *   appeal: Formatters<AppealSubmission, undefined, *>
+ *   questionnaire: Formatters<string, LPAQuestionnaireSubmission, *>
  * }}
  */
-module.exports = ['appeal', 'questionnaire'].reduce(
-	(mod, submittable) => ({
-		...mod,
-		[submittable]: {
-			...Object.entries(appealTypeToDirName).reduce(
-				(formatters, [appealType, dirName]) => ({
-					...formatters,
-					[appealType]: require(`./${dirName}/${submittable}`).formatter
-				}),
-				{}
-			)
-		}
-	}),
-	{}
-);
+module.exports = ['appeal', 'questionnaire'].reduce(reduceModules, {});
