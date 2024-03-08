5fb6fac11e7b03e94c618b87dac0e4765282b2a0
Dean Hasley <dhasley1990@gmail.com>
2024-03-08 14:19:41 +0000

    feat(aapd-1081): add questionnaire details controller, initial page set up

---
+++ packages/forms-web-app/src/controllers/selected-appeal/questionnaire-details/index.js
@@ -0,0 +1,55 @@
+const { LPA_USER_ROLE } = require('@pins/common/src/constants');
+const { formatHeadlineData } = require('@pins/common');
+
+const { VIEW } = require('../../../lib/views');
+const { apiClient } = require('../../../lib/appeals-api-client');
+const { determineUser } = require('../../../lib/determine-user');
+const {
+	formatQuestionnaireHeading,
+	formatTitleSuffix,
+	determineServicePage
+} = require('../../../lib/selected-appeal-page-setup');
+
+/**
+ * @type {import('express').Handler}
+ */
+exports.get = async (req, res) => {
+	const appealNumber = req.params.appealNumber;
+	const userRouteUrl = req.originalUrl;
+
+	// determine user based on route to selected appeal
+	//i.e '/appeals/' = appellant | agent
+	const userType = determineUser(userRouteUrl);
+
+	if (userType === null) {
+		throw new Error('Unknown role');
+	}
+
+	const userEmail = userType === LPA_USER_ROLE ? req.session.lpaUser?.email : req.session.email;
+
+	if (!userEmail) {
+		throw new Error('no session email');
+	}
+
+	const user = await apiClient.getUserByEmailV2(userEmail);
+
+	const caseData = await apiClient.getUsersAppealCase({
+		caseReference: appealNumber,
+		role: userType,
+		userId: user.id
+	});
+
+	const headlineData = formatHeadlineData(caseData, userType);
+
+	const viewContext = {
+		servicePage: determineServicePage(userType),
+		titleSuffix: formatTitleSuffix(userType),
+		mainHeading: formatQuestionnaireHeading(userType),
+		appeal: {
+			appealNumber,
+			headlineData
+		}
+	};
+
+	res.render(VIEW.SELECTED_APPEAL.APPEAL_QUESTIONNAIRE, viewContext);
+};
