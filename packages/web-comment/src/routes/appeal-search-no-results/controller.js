c295544f97f10a990b9af5fbe491f4938109e131
Dean Hasley <dhasley1990@gmail.com>
2024-03-13 12:36:06 +0000

    fix(aapd-1344): refactor web comment no results page to distinguish between type of search

--- packages/web-comment/src/routes/appeal-search-no-results/controller.js
+++ packages/web-comment/src/routes/appeal-search-no-results/controller.js
@@ -1,7 +1,26 @@
+const {
+	isPostcodeOrReferenceNumber,
+	formatTitlePrefix,
+	formatParagraphWording,
+	formatLink
+} = require('./format-no-results-page');
+
 /** @type {import('express').RequestHandler} */
 const appealSearchNoResults = async (req, res) => {
+	const searchQuery = req.query.search;
+	const typeOfSearch = isPostcodeOrReferenceNumber(searchQuery);
+
+	const pageTitle = formatTitlePrefix(typeOfSearch);
+	const pageHeading = pageTitle;
+	const paragraph = formatParagraphWording(typeOfSearch);
+	const linkToRelatedSearchPage = formatLink(typeOfSearch);
+
 	res.render(`appeal-search-no-results/index`, {
-		appealReference: req.query.search
+		pageTitle,
+		pageHeading,
+		paragraph,
+		linkToRelatedSearchPage,
+		searchQuery
 	});
 };
 
