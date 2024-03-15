91acab18448ce4f6b8b0e07ae4f8f29ccd306589
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-03-15 09:23:32 +0000

    fix(aapd-1347): allow variation of template used by shared appeal + detail pages

--- packages/forms-web-app/src/controllers/selected-appeal/appeal-details/index.js
+++ packages/forms-web-app/src/controllers/selected-appeal/appeal-details/index.js
@@ -8,57 +8,62 @@ const { getLPAUserFromSession } = require('../../../services/lpa-user.service');
 const { detailsRows } = require('./appeal-details-rows');
 const { documentsRows } = require('./appeal-documents-rows');
 
-
 /**
- * @type {import('express').Handler}
+ * Shared controller for /appeals/:caseRef/appeal-details, manage-appeals/:caseRef/appeal-details rule-6-appeals/:caseRef/appeal-details
+ * @param {string} layoutTemplate - njk template to extend
+ * @returns {import('express').RequestHandler}
  */
-exports.get = async (req, res) => {
-	const appealNumber = req.params.appealNumber;
-	const userRouteUrl = req.originalUrl;
+exports.get = (layoutTemplate = 'layouts/no-banner-link/main.njk') => {
+	return async (req, res) => {
+		const appealNumber = req.params.appealNumber;
+		const userRouteUrl = req.originalUrl;
 
-	// determine user based on route to selected appeal
-	//i.e '/appeals/' = appellant | agent
-	const userType = determineUser(userRouteUrl);
+		// determine user based on route to selected appeal
+		// i.e '/appeals/' = appellant | agent
+		// todo: use oauth token
+		const userType = determineUser(userRouteUrl);
 
-	if (userType === null) {
-		throw new Error('Unknown role');
-	}
-
-	const userEmail =
-		userType === LPA_USER_ROLE ? getLPAUserFromSession(req).email : req.session.email;
-
-	if (!userEmail) {
-		throw new Error('no session email');
-	}
-
-	const user = await apiClient.getUserByEmailV2(userEmail);
-
-	const caseData = await apiClient.getUsersAppealCase({
-		caseReference: appealNumber,
-		role: userType,
-		userId: user.id
-	});
-
-	const headlineData = formatHeadlineData(caseData, userType);
-
-	const appealDetailsRows = detailsRows(caseData);
-	const appealDetails = formatRows(appealDetailsRows, caseData);
-
-	const appealDocumentsRows = documentsRows(caseData);
-	const appealDocuments = formatRows(appealDocumentsRows, caseData);
-
-	const viewContext = {
-		titleSuffix: formatTitleSuffix(userType),
-		appealDetailsSuffix: formatDetailsSuffix(userType),
-		appeal: {
-			appealNumber,
-			headlineData,
-			appealDetails,
-			appealDocuments
+		if (userType === null) {
+			throw new Error('Unknown role');
 		}
-	};
 
-	res.render(VIEW.SELECTED_APPEAL.APPEAL_DETAILS, viewContext);
+		const userEmail =
+			userType === LPA_USER_ROLE ? getLPAUserFromSession(req).email : req.session.email;
+
+		if (!userEmail) {
+			throw new Error('no session email');
+		}
+
+		const user = await apiClient.getUserByEmailV2(userEmail);
+
+		const caseData = await apiClient.getUsersAppealCase({
+			caseReference: appealNumber,
+			role: userType,
+			userId: user.id
+		});
+
+		const headlineData = formatHeadlineData(caseData, userType);
+
+		const appealDetailsRows = detailsRows(caseData);
+		const appealDetails = formatRows(appealDetailsRows, caseData);
+
+		const appealDocumentsRows = documentsRows(caseData);
+		const appealDocuments = formatRows(appealDocumentsRows, caseData);
+
+		const viewContext = {
+			layoutTemplate,
+			titleSuffix: formatTitleSuffix(userType),
+			appealDetailsSuffix: formatDetailsSuffix(userType),
+			appeal: {
+				appealNumber,
+				headlineData,
+				appealDetails,
+				appealDocuments
+			}
+		};
+
+		res.render(VIEW.SELECTED_APPEAL.APPEAL_DETAILS, viewContext);
+	};
 };
 
 /**
--- packages/forms-web-app/src/routes/lpa-dashboard/selected-appeal.js
+++ packages/forms-web-app/src/routes/lpa-dashboard/selected-appeal.js
@@ -4,7 +4,10 @@ const router = express.Router({ mergeParams: true });
 const selectedAppealController = require('../../controllers/selected-appeal');
 const appealDetailsController = require('../../controllers/selected-appeal/appeal-details');
 
-router.get('/:appealNumber', selectedAppealController.get);
-router.get('/:appealNumber/appeal-details', appealDetailsController.get);
+router.get('/:appealNumber', selectedAppealController.get('layouts/lpa-dashboard/main.njk'));
+router.get(
+	'/:appealNumber/appeal-details',
+	appealDetailsController.get('layouts/lpa-dashboard/main.njk')
+);
 
 module.exports = router;
