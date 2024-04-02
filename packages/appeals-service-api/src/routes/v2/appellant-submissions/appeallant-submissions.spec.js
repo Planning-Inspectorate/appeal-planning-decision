6fbe04762be75bd0031b59619b9b766648d1e617
Lucy Potter <si3rr4wow@gmail.com>
2024-04-02 15:07:16 +0100

    feat(aapd-1502): hopefully fix tests in pipeline

--- packages/appeals-service-api/src/routes/v2/appellant-submissions/appeallant-submissions.spec.js
+++ packages/appeals-service-api/src/routes/v2/appellant-submissions/appeallant-submissions.spec.js
@@ -2,6 +2,7 @@ const supertest = require('supertest');
 const app = require('../../../app');
 const { createPrismaClient } = require('../../../db/db-client');
 const { seedStaticData } = require('@pins/database/src/seed/data-static');
+const { seedDev } = require('@pins/database/src/seed/data-dev');
 
 const { isFeatureActive } = require('../../../configuration/featureFlag');
 
@@ -18,7 +19,7 @@ jest.mock('express-oauth2-jwt-bearer', () => ({
 		return (req, _res, next) => {
 			req.auth = {
 				payload: {
-					sub: '29670d0f-c4b4-4047-8ee0-d62b93e91a11'
+					sub: '29670d0f-c4b4-4047-8ee0-d62b93e91a13'
 				}
 			};
 			next();
@@ -28,7 +29,7 @@ jest.mock('express-oauth2-jwt-bearer', () => ({
 
 jest.mock('express-oauth2-jwt-bearer');
 
-jest.setTimeout(10000);
+jest.setTimeout(30000);
 
 beforeAll(async () => {
 	///////////////////////////////
@@ -42,6 +43,7 @@ beforeAll(async () => {
 	appealsApi = supertest(app);
 
 	await seedStaticData(sqlClient);
+	await seedDev(sqlClient);
 });
 
 beforeEach(async () => {
@@ -53,36 +55,42 @@ beforeEach(async () => {
 
 afterEach(async () => {
 	jest.clearAllMocks();
+	await _clearSqlData();
 });
 
 afterAll(async () => {
 	// clear sql db
-	await _clearSqlData();
 	await sqlClient.$disconnect();
 });
-
-let createdAppellantSubmissionId = '';
 
 describe('/appellant-submissions', () => {
 	it('put', async () => {
 		const response = await appealsApi.put('/api/v2/appellant-submissions').send({
 			LPACode: 'Q9999',
 			appealTypeCode: 'HAS',
-			appealId: 'd679eff9-c073-4349-8237-aefc1b5ae783'
+			appealId: '756d6bfb-dde8-4532-a041-86c226a23a03'
 		});
 
 		expect(response.body).toMatchObject({
 			id: expect.stringMatching(/[a-f0-9-]{36}/),
 			LPACode: 'Q9999',
 			appealTypeCode: 'HAS',
-			appealId: 'd679eff9-c073-4349-8237-aefc1b5ae783'
+			appealId: '756d6bfb-dde8-4532-a041-86c226a23a03'
 		});
-
-		createdAppellantSubmissionId = response.body.id;
 	});
 
 	describe('/appellant-submissions/:id', () => {
 		it('get', async () => {
+			let createdAppellantSubmissionId = '';
+
+			const putResponse = await appealsApi.put('/api/v2/appellant-submissions').send({
+				LPACode: 'Q9999',
+				appealTypeCode: 'HAS',
+				appealId: '756d6bfb-dde8-4532-a041-86c226a23a03'
+			});
+
+			createdAppellantSubmissionId = putResponse.body.id;
+
 			expect(createdAppellantSubmissionId).toMatch(/[a-f0-9-]{36}/);
 
 			const response = await appealsApi.get(
@@ -93,25 +101,37 @@ describe('/appellant-submissions', () => {
 				id: createdAppellantSubmissionId,
 				LPACode: 'Q9999',
 				appealTypeCode: 'HAS',
-				appealId: 'd679eff9-c073-4349-8237-aefc1b5ae783'
+				appealId: '756d6bfb-dde8-4532-a041-86c226a23a03'
 			});
 		});
 
 		it('patch', async () => {
+			let createdAppellantSubmissionId = '';
+
+			const putResponse = await appealsApi.put('/api/v2/appellant-submissions').send({
+				LPACode: 'Q9999',
+				appealTypeCode: 'HAS',
+				appealId: '756d6bfb-dde8-4532-a041-86c226a23a03'
+			});
+
+			createdAppellantSubmissionId = putResponse.body.id;
+
 			expect(createdAppellantSubmissionId).toMatch(/[a-f0-9-]{36}/);
 
 			const response = await appealsApi
 				.patch(`/api/v2/appellant-submissions/${createdAppellantSubmissionId}`)
 				.send({
 					id: createdAppellantSubmissionId,
-					appealTypeCode: 'S78'
+					LPACode: 'Q9999',
+					appealTypeCode: 'S78',
+					appealId: '756d6bfb-dde8-4532-a041-86c226a23a03'
 				});
 
 			expect(response.body).toEqual({
 				id: createdAppellantSubmissionId,
 				LPACode: 'Q9999',
 				appealTypeCode: 'S78',
-				appealId: 'd679eff9-c073-4349-8237-aefc1b5ae783'
+				appealId: '756d6bfb-dde8-4532-a041-86c226a23a03'
 			});
 		});
 	});
@@ -121,6 +141,5 @@ describe('/appellant-submissions', () => {
  * @returns {Promise.<void>}
  */
 const _clearSqlData = async () => {
-	const { count } = await sqlClient.appellantSubmission.deleteMany();
-	console.log(`Purged ${count} appellant submissions`);
+	await sqlClient.appellantSubmission.deleteMany();
 };
