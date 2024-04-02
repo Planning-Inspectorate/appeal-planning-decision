2f97c148c8c043e0555b06114661af8ec67632b7
Lucy Potter <si3rr4wow@gmail.com>
2024-04-02 13:40:48 +0100

    feat(aapd-1502): add tests

--- packages/appeals-service-api/src/routes/v2/appellant-submissions/_id/index.js
+++ packages/appeals-service-api/src/routes/v2/appellant-submissions/_id/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const { auth } = require('express-oauth2-jwt-bearer');
 const { get, patch } = require('./controller');
-const router = express.Router();
+const { AUTH } = require('@pins/common/src/constants');
+const config = require('../../../../configuration/config');
 const asyncHandler = require('@pins/common/src/middleware/async-handler');
+const validateToken = require('@pins/common/src/middleware/validate-token');
 const { openApiValidatorMiddleware } = require('../../../../validators/validate-open-api');
+
+const router = express.Router({ mergeParams: true });
+
+router.use(
+	auth({
+		issuerBaseURL: `${config.auth.authServerUrl}${AUTH.OIDC_ENDPOINT}`,
+		audience: AUTH.RESOURCE
+	})
+);
+
+router.use(
+	validateToken({
+		headerName: 'authentication',
+		reqPropertyName: 'id_token',
+		jwksUri: `${config.auth.authServerUrl}${AUTH.JWKS_ENDPOINT}`,
+		enforceToken: false
+	})
+);
 
 router.get('/', openApiValidatorMiddleware(), asyncHandler(get));
 router.patch('/', openApiValidatorMiddleware(), asyncHandler(patch));
--- packages/appeals-service-api/src/routes/v2/appellant-submissions/index.js
+++ packages/appeals-service-api/src/routes/v2/appellant-submissions/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const { auth } = require('express-oauth2-jwt-bearer');
+const { AUTH } = require('@pins/common/src/constants');
+const config = require('../../../configuration/config');
 const { put } = require('./controller');
-const router = express.Router();
 const asyncHandler = require('@pins/common/src/middleware/async-handler');
+const validateToken = require('@pins/common/src/middleware/validate-token');
 const { openApiValidatorMiddleware } = require('../../../validators/validate-open-api');
+
+const router = express.Router();
+
+router.use(
+	auth({
+		issuerBaseURL: `${config.auth.authServerUrl}${AUTH.OIDC_ENDPOINT}`,
+		audience: AUTH.RESOURCE
+	})
+);
+
+router.use(
+	validateToken({
+		headerName: 'authentication',
+		reqPropertyName: 'id_token',
+		jwksUri: `${config.auth.authServerUrl}${AUTH.JWKS_ENDPOINT}`,
+		enforceToken: false
+	})
+);
 
 router.put('/', openApiValidatorMiddleware(), asyncHandler(put));
 
