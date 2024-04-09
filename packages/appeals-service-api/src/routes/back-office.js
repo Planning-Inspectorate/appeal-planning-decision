1fe5e1ac89127fba8a827b42befd31b4d71db138
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-04-09 10:56:12 +0100

    fix(aapd-1535): comment out v1 appeal submission to new back office

--- packages/appeals-service-api/src/routes/back-office.js
+++ packages/appeals-service-api/src/routes/back-office.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const ApiError = require('../errors/apiError');
 
 const BackOfficeService = require('../services/back-office.service');
-const BackOfficeV2Service = require('../services/back-office-v2');
-
 const backOfficeService = new BackOfficeService();
-const backOfficeV2Service = new BackOfficeV2Service();
+
+// const BackOfficeV2Service = require('../services/back-office-v2');
+// const backOfficeV2Service = new BackOfficeV2Service();
 
 router.post('/appeals/:id', async (req, res) => {
 	try {
-		await backOfficeV2Service.submitAppeal(req.params.id);
+		// leaving commented out for now, not expecting to need to submit v1 appeals to new back office: AAPD-582 + AAPD-1535
+		// await backOfficeV2Service.submitAppeal(req.params.id);
 		await backOfficeService.saveAppealForSubmission(req.params.id);
 		return res.status(202).send({});
 	} catch (error) {
