275e5056633bf0d92fbf261785d37a67b12f7040
Tom Pickering <topickering@gmail.com>
2024-04-19 14:15:03 +0100

    fix(1466): remove console logs

--- packages/forms-web-app/src/dynamic-forms/dynamic-components/text-entry/question.js
+++ packages/forms-web-app/src/dynamic-forms/dynamic-components/text-entry/question.js
@@ -38,16 +38,11 @@ class TextEntryQuestion extends Question {
 	}
 
 	prepQuestionForRendering(section, journey, customViewData, payload) {
-		console.log('arrg');
-		console.log(journey);
 		let viewModel = super.prepQuestionForRendering(section, journey, customViewData);
 		viewModel.question.label = this.label;
 		viewModel.question.value = payload
 			? payload[viewModel.question.fieldName]
 			: viewModel.question.value;
-		console.log('BOO');
-		console.log(payload);
-		console.log(viewModel.question);
 		return viewModel;
 	}
 }
