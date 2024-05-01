23eaf10ca9713dbc409808d0336cf3cba2124722
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-05-01 18:25:58 +0100

    chore: add auth, api-spec, tests to lpa-submision addresses

---
+++ packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/lpa-questionnaire-submission/lpa-can-modify-case.js
@@ -0,0 +1,25 @@
+const ApiError = require('#errors/apiError');
+
+const { LPAQuestionnaireSubmissionRepository } = require('./repo');
+
+const submissionRepo = new LPAQuestionnaireSubmissionRepository();
+
+/**
+ * Middleware to check the user owns the appellant submission record
+ * @type {import('express').Handler}
+ */
+module.exports = async (req, res, next) => {
+	try {
+		await submissionRepo.lpaCanModifyCase({
+			caseReference: req.params.caseReference,
+			userLpa: req.id_token.lpaCode
+		});
+		next();
+	} catch (error) {
+		if (error instanceof ApiError) {
+			res.status(error.code || 500).send(error.message.errors);
+		} else {
+			res.status(500).send('An unexpected error occurred');
+		}
+	}
+};
