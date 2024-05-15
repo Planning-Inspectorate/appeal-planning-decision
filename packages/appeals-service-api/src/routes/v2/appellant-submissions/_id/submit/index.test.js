1d73cdc2da847244b5a81abcea136197627a39fb
Tom Pickering <topickering@gmail.com>
2024-05-15 11:57:55 +0100

    feat(1518): tidy up test

--- packages/appeals-service-api/src/routes/v2/appellant-submissions/_id/submit/index.test.js
+++ packages/appeals-service-api/src/routes/v2/appellant-submissions/_id/submit/index.test.js
@@ -4,7 +4,7 @@
 const supertest = require('supertest');
 const http = require('http');
 const app = require('../../../../../app');
-// const { sendEvents } = require('../../../../../../src/infrastructure/event-client');
+const { sendEvents } = require('../../../../../../src/infrastructure/event-client');
 
 const server = http.createServer(app);
 const appealsApi = supertest(server);
@@ -27,21 +27,12 @@ jest.mock('../service', () => ({
 					applicationDecision: 'denied',
 					onApplicationDate: '2024-01-01',
 					isAppellant: true,
-					// appellantFirstName:   String?
-					// appellantLastName:    String?
-					// appellantCompanyName: String?
 					contactFirstName: 'Testy',
 					contactLastName: 'McTest',
-					// contactCompanyName:   String?
+					appellantEmailAddress: 'test@test.com',
 					ownsAllLand: true,
-					// ownsSomeLand:                 Boolean?
-					// knowsAllOwners:               String?
-					// knowsOtherOwners:             String?
-					// informedOwners:               Boolean?
-					// advertisedAppeal:             Boolean?
 					appellantGreenBelt: false,
 					updateDevelopmentDescription: false,
-					// identifiedOwners:             Boolean?
 					costApplication: false,
 					appellantSiteSafety: 'yes',
 					appellantSiteSafety_appellantSiteSafetyDetails: "It's dangerous",
@@ -65,83 +56,87 @@ jest.mock('../service', () => ({
 							id: 'img_001',
 							fileName: 'img.jpg',
 							originalFileName: 'oimg.jpg',
-							questionnaireId: '001',
+							appellantSubmissionId: '001',
 							name: 'img.jpg',
 							location: '/img.jpg',
-							type: 'jpg'
+							type: 'jpg',
+							storageId: '001'
 						}
 					],
 					siteAddress: true,
 					SubmissionAddress: [
 						{
 							id: 'add_001',
-							questionnaireId: '001',
+							appellantSubmissionId: '001',
 							addressLine1: 'Somewhere',
 							addressLine2: 'Somewhere St',
 							townCity: 'Somewhereville',
-							postcode: 'SOM3 W3R'
+							postcode: 'SOM3 W3R',
+							fieldName: 'siteAddress'
 						}
 					],
 					SubmissionListedBuilding: []
 				};
 			case '002':
 				return {
-					addChangedListedBuilding: true,
-					areaOutstandingBeauty: true,
-					changesListedBuilding: true,
-					changedListedBuildingNumber: '010101',
-					columnTwoThreshold: true,
-					environmentalStatement: true,
-					consultationResponses: true,
-					statutoryConsultees_consultedBodiesDetails: 'consultation details',
-					designatedSites: 'yes',
-					developmentDescription: '',
-					emergingPlan: true,
-					environmentalImpactSchedule: '',
-					gypsyTraveller: true,
-					infrastructureLevy: false,
-					infrastructureLevyAdopted: false,
-					infrastructureLevyAdoptedDate: null,
-					infrastructureLevyExpectedDate: null,
-					lpaPreferHearingDetails: 'Hearing details',
-					lpaPreferInquiryDetails: 'Inquiry details',
-					lpaProcedurePreference_lpaPreferInquiryDuration: 'Very long',
-					lpaProcedurePreference: 'Hearing',
-					designatedSites_otherDesignations: 'other designations',
-					protectedSpecies: false,
-					publicRightOfWay: true,
-					affectsScheduledMonument: true,
-					screeningOpinion: false,
-					sensitiveArea: 'yes',
-					sensitiveArea_sensitiveAreaDetails: 'Sensitive area details',
-					statutoryConsultees: 'Mrs Consultee',
-					supplementaryPlanningDocs: true,
-					treePreservationOrder: false,
-					AppealCase: {
-						LPACode: 'LPA_001',
-						appealTypeCode: 'S78'
-					},
+					LPACode: 'LPA_002',
+					appealTypeCode: 'HAS',
+					applicationDecisionDate: '2024-01-01',
+					applicationDecision: 'denied',
+					onApplicationDate: '2024-01-01',
+					isAppellant: false,
+					appellantFirstName: 'Test App',
+					appellantLastName: 'Testington',
+					appellantEmailAddress: 'test@test.com',
+					contactFirstName: 'Testy',
+					contactLastName: 'McTest',
+					contactCompanyName: 'Test Agents',
+					ownsAllLand: true,
+					appellantGreenBelt: false,
+					updateDevelopmentDescription: false,
+					costApplication: false,
+					appellantSiteSafety: 'yes',
+					appellantSiteSafety_appellantSiteSafetyDetails: "It's dangerous",
+					appellantSiteAccess: 'yes',
+					appellantSiteAccess_appellantSiteAccessDetails: 'Come and see',
+					applicationReference: '234',
+					developmentDescriptionOriginal: 'A test description',
+					appellantLinkedCaseReference: 'no',
+					appellantPhoneNumber: '12345657',
+					siteAreaSquareMetres: 25.0,
+					appellantLinkedCaseAdd: false,
+					appellantLinkedCase: false,
+					SubmissionLinkedCase: [],
+					uploadOriginalApplicationForm: true,
+					uploadApplicationDecisionLetter: false,
+					uploadAppellantStatement: false,
+					uploadCostApplication: false,
+					uploadChangeOfDescriptionEvidence: false,
+					SubmissionDocumentUpload: [
+						{
+							id: 'img_002',
+							fileName: 'img.jpg',
+							originalFileName: 'oimg.jpg',
+							appellantSubmissionId: '002',
+							name: 'img.jpg',
+							location: '/img.jpg',
+							type: 'jpg',
+							storageId: '001'
+						}
+					],
+					siteAddress: true,
 					SubmissionAddress: [
 						{
-							id: 'add_001',
-							questionnaireId: '001',
+							id: 'add_002',
+							appellantSubmissionId: '002',
 							addressLine1: 'Somewhere',
 							addressLine2: 'Somewhere St',
 							townCity: 'Somewhereville',
-							postcode: 'SOM3 W3R'
+							postcode: 'SOM3 W3R',
+							fieldName: 'siteAddress'
 						}
 					],
-					SubmissionDocumentUpload: [
-						{
-							id: 'img_001',
-							fileName: 'img.jpg',
-							originalFileName: 'oimg.jpg',
-							questionnaireId: '001',
-							name: 'img.jpg',
-							location: '/img.jpg',
-							type: 'jpg'
-						}
-					]
+					SubmissionListedBuilding: []
 				};
 			default:
 				return null;
@@ -149,90 +144,98 @@ jest.mock('../service', () => ({
 	}
 }));
 
-jest.mock('../../../../../../../src/configuration/featureFlag', () => ({
+jest.mock('../../../../../../src/configuration/featureFlag', () => ({
 	isFeatureActive() {
 		return true;
 	}
 }));
 
-jest.mock('../../../../../../../src/infrastructure/event-client', () => ({
+jest.mock('../../../../../../src/infrastructure/event-client', () => ({
 	sendEvents: jest.fn()
 }));
 
-jest.mock('../../../../../../../src/services/object-store');
+jest.mock('../../../../../../src/services/object-store');
 
-// const formattedHAS1 = {
-// 	LPACode: 'LPA_001',
-// 	caseReference: '001',
-// 	documents: [
-// 		{
-// 			dateCreated: '2024-03-01T13:48:35.847Z',
-// 			documentType: undefined,
-// 			documentURI: 'https://example.com',
-// 			filename: 'img.jpg',
-// 			lastModified: '2024-03-01T14:48:35.847Z',
-// 			mime: 'image/jpeg',
-// 			origin: 'citizen',
-// 			originalFilename: 'oimg.jpg',
-// 			size: 10293,
-// 			sourceSystem: 'appeals',
-// 			stage: 'lpa_questionnaire'
-// 		}
-// 	],
-// 	questionnaire: {
-// 		addAffectedListedBuilding: undefined,
-// 		addNearbyAppeal: undefined,
-// 		addNeighbouringSiteAccess: undefined,
-// 		affectedListedBuildingNumber: NaN,
-// 		affectsListedBuilding: true,
-// 		conservationArea: true,
-// 		correctAppealType: true,
-// 		greenBelt: true,
-// 		lpaSiteAccess: true,
-// 		lpaSiteAccessDetails: undefined,
-// 		lpaSiteSafety: true,
-// 		lpaSiteSafetyDetails: "oh it's bad",
-// 		lpaStatement: null,
-// 		lpaStatementDocuments: null,
-// 		nearbyAppealReference: '002',
-// 		nearbyAppeals: undefined,
-// 		'neighbouring-address': [
-// 			{
-// 				county: null,
-// 				line1: 'Somewhere',
-// 				line2: 'Somewhere St',
-// 				postcode: 'SOM3 W3R',
-// 				town: 'Somewhereville'
-// 			}
-// 		],
-// 		neighbouringSiteAccess: true,
-// 		neighbouringSiteAccessDetails: undefined,
-// 		newConditionDetails: 'I have new conditions',
-// 		newConditions: true,
-// 		notificationMethod: [
-// 			'A public notice at the site',
-// 			'Letters to neighbours',
-// 			'Advert in the local press'
-// 		],
-// 		otherPartyRepresentations: null
-// 	}
-// };
+const formattedHAS1 = {
+	appeal: {
+		LPACode: 'LPA_001',
+		LPAName: 'testLPA',
+		appealType: 'Householder (HAS) Appeal',
+		decision: 'denied',
+		originalCaseDecisionDate: '2024-01-01',
+		costAppliedForIndicator: false,
+		LPAApplicationReference: '123',
+		appellant: {
+			firstName: 'Testy',
+			lastName: 'McTest',
+			emailAddress: 'test@test.com'
+		},
+		agent: undefined,
+		siteAddressLine1: 'Somewhere',
+		siteAddressLine2: 'Somewhere St',
+		siteAddressTown: 'Somewhereville',
+		siteAddressCounty: undefined,
+		siteAddressPostcode: 'SOM3 W3R',
+		isSiteFullyOwned: true,
+		hasToldOwners: undefined,
+		isSiteVisible: 'yes',
+		inspectorAccessDetails: 'Come and see',
+		doesSiteHaveHealthAndSafetyIssues: 'yes',
+		healthAndSafetyIssuesDetails: "It's dangerous"
+	},
+	documents: []
+};
+
+const formattedHAS2 = {
+	appeal: {
+		LPACode: 'LPA_002',
+		LPAName: 'testLPA',
+		appealType: 'Householder (HAS) Appeal',
+		decision: 'denied',
+		originalCaseDecisionDate: '2024-01-01',
+		costAppliedForIndicator: false,
+		LPAApplicationReference: '234',
+		appellant: {
+			firstName: 'Test App',
+			lastName: 'Testington',
+			emailAddress: 'test@test.com'
+		},
+		agent: {
+			firstName: 'Testy',
+			lastName: 'McTest',
+			emailAddress: 'test@test.com'
+		},
+		siteAddressLine1: 'Somewhere',
+		siteAddressLine2: 'Somewhere St',
+		siteAddressTown: 'Somewhereville',
+		siteAddressCounty: undefined,
+		siteAddressPostcode: 'SOM3 W3R',
+		isSiteFullyOwned: true,
+		hasToldOwners: undefined,
+		isSiteVisible: 'yes',
+		inspectorAccessDetails: 'Come and see',
+		doesSiteHaveHealthAndSafetyIssues: 'yes',
+		healthAndSafetyIssuesDetails: "It's dangerous"
+	},
+	documents: []
+};
 
 describe('/api/v2/appeal-cases/:caseReference/submit', () => {
-	// it.skip.each([
-	// 	['HAS', '001', formattedHAS1],
-	// 	['HAS', '002', formattedHAS2]
-	// ])('Formats %s appeal submission then sends it to back office', async (_, id, expectation) => {
-	// 	await appealsApi
-	// 		.post(`/api/v2/appellant-submissions/${id}/submit`)
-	// 		.expect(200);
+	it.skip.each([
+		['HAS', '001', formattedHAS1],
+		['HAS', '002', formattedHAS2]
+	])('Formats %s appeal submission then sends it to back office', async (_, id, expectation) => {
+		await appealsApi
+			.post(`/api/v2/appellant-submissions/${id}/submit`)
+			.set('Authorization', 'test123')
+			.expect(200);
 
-	// 	expect(sendEvents).toHaveBeenCalledWith(
-	// 		'appeal-fo-lpa-response-submission',
-	// 		expectation,
-	// 		'Create'
-	// 	);
-	// });
+		expect(sendEvents).toHaveBeenCalledWith(
+			'appeal-fo-lpa-response-submission',
+			expectation,
+			'Create'
+		);
+	});
 
 	it.skip('404s if the appeal submission can not be found', () => {
 		appealsApi
