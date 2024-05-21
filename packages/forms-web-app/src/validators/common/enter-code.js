c8e4176b7877d8c73b31df2ed257b2f426e9f4bb
lauren-PINS <lauren.page@planninginspectorate.gov.uk>
2024-05-21 14:09:41 +0100

    feat(aapd-1615): updates to error messages to meet security report requirements

--- packages/forms-web-app/src/validators/common/enter-code.js
+++ packages/forms-web-app/src/validators/common/enter-code.js
@@ -5,18 +5,18 @@ const isVowelOrZeroRegEx = /[AEIOUaeiou0]/g;
 const ruleEnterCode = () =>
 	body('email-code')
 		.notEmpty()
-		.withMessage('Enter the code')
+		.withMessage('Enter the correct code')
 		.bail()
 		.isLength({ min: 5 })
-		.withMessage('Enter the code')
+		.withMessage('Enter the correct code')
 		.bail()
 		.isLength({ max: 5 })
-		.withMessage('Enter the code')
+		.withMessage('Enter the correct code')
 		.bail()
 		.custom((value) => {
 			return !(value.match(isAlphaNumericRegEx) || value.match(isVowelOrZeroRegEx));
 		})
-		.withMessage('Enter the code');
+		.withMessage('Enter the correct code');
 
 const rules = () => {
 	return [ruleEnterCode()];
