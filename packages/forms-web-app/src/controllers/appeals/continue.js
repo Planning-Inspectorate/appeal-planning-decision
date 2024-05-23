8f0c240d73425bc41cf3224d222b41f2881ca9be
Dean Hasley <dhasley1990@gmail.com>
2024-05-23 16:50:39 +0100

    fix(aapd-1542): fix redirect for v2 submissions continue button on dashboard

--- packages/forms-web-app/src/controllers/appeals/continue.js
+++ packages/forms-web-app/src/controllers/appeals/continue.js
@@ -1,10 +1,14 @@
 const { apiClient } = require('#lib/appeals-api-client');
 const { APPEAL_ID } = require('@pins/business-rules/src/constants');
-const { isAppealSubmission } = require('@pins/common/src/lib/format-address');
+const { isAppealSubmission, isV2Submission } = require('@pins/common/src/lib/format-address');
 
 const appealSubmissionContinueUrls = {
 	[APPEAL_ID.HOUSEHOLDER]: '/appeal-householder-decision/task-list',
 	[APPEAL_ID.PLANNING_SECTION_78]: '/full-appeal/submit-appeal/task-list'
+};
+
+const v2SubmissionDynamicContinueUrls = {
+	v2Has: (submissionId) => `/appeals/householder/appeal-form/your-appeal?id=${submissionId}`
 };
 
 /**
@@ -20,27 +24,46 @@ exports.get = async (req, res) => {
 		throw new Error(`Continue your appeal cannot be invoked without specifying an appeal id`);
 
 	const user = await apiClient.getUserByEmailV2(email);
-	const appealSubmission = await apiClient.getUserAppealById({
-		userId: user.id,
-		appealSubmissionId: appealId
-	});
+
+	const userAppeals = await apiClient.getUserAppealsById(user.id);
+
+	const appealSubmission = userAppeals.find(
+		(appeal) => appeal.id === appealId || appeal._id === appealId
+	);
+	console.log('INSIDE', appealSubmission);
 
 	if (!appealSubmission) throw new Error(`Appeal ${appealId} does not belong to user ${user.id}`);
 
-	if (!isAppealSubmission(appealSubmission))
-		throw new Error('Continue your appeal can only be invoked with appeal submissions');
+	if (isAppealSubmission(appealSubmission)) {
+		// v1 submission
+		req.session.appeal = appealSubmission.appeal;
 
-	req.session.appeal = appealSubmission.appeal;
+		const appealType = appealSubmission.appeal?.appealType;
 
-	if (!appealSubmission.appeal?.appealType)
-		throw new Error(`Appeal ${appealId} does not have an appeal type`);
+		if (!appealType) throw new Error(`Appeal ${appealId} does not have an appeal type`);
 
-	const redirectUrl = appealSubmissionContinueUrls[appealSubmission.appeal.appealType];
+		const redirectUrl = appealSubmissionContinueUrls[appealType];
 
-	if (!redirectUrl)
-		throw new Error(
-			`No redirection is available for appeal type ${appealSubmission.appeal.appealType}`
+		if (!redirectUrl) throw new Error(`No redirection is available for appeal type ${appealType}`);
+
+		return res.redirect(redirectUrl);
+	} else if (isV2Submission(appealSubmission)) {
+		// v2 submission
+		req.session.appeal = appealSubmission;
+
+		const appealTypeCode = appealSubmission?.AppellantSubmission?.appealTypeCode;
+
+		if (!appealTypeCode) throw new Error(`Appeal ${appealId} does not have an appeal type code`);
+
+		const redirectUrl = v2SubmissionDynamicContinueUrls.v2Has(
+			appealSubmission?.AppellantSubmission?.id
 		);
 
-	res.redirect(redirectUrl);
+		if (!redirectUrl)
+			throw new Error(`No redirection is available for appeal type ${appealTypeCode}`);
+
+		return res.redirect(redirectUrl);
+	} else {
+		throw new Error('Continue your appeal can only be invooked with appeal submissions');
+	}
 };
