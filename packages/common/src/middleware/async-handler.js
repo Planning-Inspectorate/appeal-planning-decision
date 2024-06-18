a18ee4fedc7633fc0c5404332c9ef3b28b27fa96
Lucy Potter <si3rr4wow@gmail.com>
2024-06-18 10:29:24 +0100

    feat(ASI-58): remove log

--- packages/common/src/middleware/async-handler.js
+++ packages/common/src/middleware/async-handler.js
@@ -10,10 +10,7 @@ const asyncHandler = (requestHandler) => {
 		try {
 			const p = requestHandler(request, response, next);
 			if (p instanceof Promise) {
-				p.catch((e) => {
-					console.log('🚀 ~ return ~ e:', e);
-					next(e);
-				});
+				p.catch(next);
 			}
 		} catch (e) {
 			// in case a sync function is passed in
