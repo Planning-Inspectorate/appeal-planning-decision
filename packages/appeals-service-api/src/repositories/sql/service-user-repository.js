03f1b6511c36066241da5fecdf848843bea80a14
Lucy Potter <si3rr4wow@gmail.com>
2024-06-18 15:33:38 +0100

    feat(ASI-58): add relations

--- packages/appeals-service-api/src/repositories/sql/service-user-repository.js
+++ packages/appeals-service-api/src/repositories/sql/service-user-repository.js
@@ -4,6 +4,18 @@ const { createPrismaClient } = require('#db-client');
  * @typedef {import("@prisma/client").ServiceUser} ServiceUser
  */
 
+/** @type {(modelRole: string) => 'appellant' | 'agent' | null} */
+const mapDataModelRoleToInternalRole = (modelRole) => {
+	switch (modelRole) {
+		case 'Appellant':
+			return 'appellant';
+		case 'Agent':
+			return 'agent';
+		default:
+			return null;
+	}
+};
+
 class ServiceUserRepository {
 	dbClient;
 
@@ -33,6 +45,60 @@ class ServiceUserRepository {
 	 */
 	async put(data) {
 		return this.dbClient.$transaction(async (tx) => {
+			/** @type {import('@prisma/client').AppealUser | null} */
+			let appealUser = null;
+			if (data.emailAddress) {
+				appealUser = await tx.appealUser.findFirst({
+					where: {
+						email: data.emailAddress
+					}
+				});
+
+				if (!appealUser) {
+					appealUser = await tx.appealUser.create({
+						data: {
+							email: data.emailAddress,
+							serviceUserId: data.id
+						}
+					});
+				} else {
+					appealUser = await tx.appealUser.update({
+						where: {
+							email: data.emailAddress
+						},
+						data: {
+							serviceUserId: data.id
+						}
+					});
+				}
+			}
+
+			const role = mapDataModelRoleToInternalRole(data.serviceUserType);
+			if (role && appealUser) {
+				const appealCase = await tx.appealCase.findFirst({
+					where: {
+						caseReference: data.caseReference
+					},
+					select: {
+						Appeal: {
+							select: {
+								id: true
+							}
+						}
+					}
+				});
+
+				if (appealCase) {
+					await tx.appealToUser.create({
+						data: {
+							appealId: appealCase.Appeal.id,
+							userId: appealUser.id,
+							role
+						}
+					});
+				}
+			}
+
 			const { internalId } =
 				(await tx.serviceUser.findFirst({
 					where: {
--- packages/appeals-service-api/src/routes/v2/service-users/index.spec.js
+++ packages/appeals-service-api/src/routes/v2/service-users/index.spec.js
@@ -58,27 +58,150 @@ describe('service users v2', () => {
 			expect(response.status).toBe(500);
 		});
 
-		it('should create user', async () => {
+		it('should create a service user', async () => {
 			const response = await appealsApi.put('/api/v2/service-users').send({
 				id: 'usr_001',
 				serviceUserType: 'Appellant',
-				caseReference: '000001'
+				caseReference: 'ref_001'
 			});
 
 			expect(response.status).toEqual(200);
+
+			const serviceUser = await sqlClient.serviceUser.findFirst({
+				where: {
+					id: 'usr_001'
+				}
+			});
+
+			expect(serviceUser.serviceUserType).toBe('Appellant');
+		});
+
+		it('should create an appeal user if non exist with a matching email address', async () => {
+			const response = await appealsApi.put('/api/v2/service-users').send({
+				id: 'usr_002',
+				serviceUserType: 'Appellant',
+				caseReference: 'ref_002',
+				emailAddress: 'newhuman@example.com'
+			});
+
+			expect(response.status).toEqual(200);
+
+			const appealUser = await sqlClient.appealUser.findFirst({
+				where: {
+					email: 'newhuman@example.com'
+				}
+			});
+
+			expect(appealUser.serviceUserId).toBe('usr_002');
+		});
+
+		it('should update an appeal user if one exists with a matching email address', async () => {
+			await sqlClient.appealUser.create({
+				data: {
+					email: 'existinghuman@example.com'
+				}
+			});
+
+			const response = await appealsApi.put('/api/v2/service-users').send({
+				id: 'usr_003',
+				serviceUserType: 'Appellant',
+				caseReference: 'ref_003',
+				emailAddress: 'existinghuman@example.com'
+			});
+
+			expect(response.status).toEqual(200);
+
+			const appealUser = await sqlClient.appealUser.findFirst({
+				where: {
+					email: 'existinghuman@example.com'
+				}
+			});
+
+			expect(appealUser.serviceUserId).toBe('usr_003');
+		});
+
+		it('should add an appealToUser relation if a matching appeal exists', async () => {
+			await sqlClient.appealCase.create({
+				data: {
+					caseReference: 'ref_004',
+					LPACode: 'lpa_001',
+					LPAName: 'test',
+					appealTypeCode: '1001',
+					appealTypeName: 'HAS',
+					decision: 'refused',
+					originalCaseDecisionDate: new Date().toISOString(),
+					costsAppliedForIndicator: false,
+					LPAApplicationReference: '010101',
+					siteAddressLine1: 'address',
+					siteAddressPostcode: 'POST CODE',
+					Appeal: {
+						create: {}
+					}
+				}
+			});
+
+			const appealCase = await sqlClient.appealCase.findFirst({
+				where: {
+					caseReference: 'ref_004'
+				},
+				select: {
+					Appeal: {
+						select: {
+							id: true
+						}
+					}
+				}
+			});
+
+			if (!appealCase?.Appeal?.id)
+				throw new Error('something went wrong when creating test appealCase');
+
+			const response = await appealsApi.put('/api/v2/service-users').send({
+				id: 'usr_004',
+				serviceUserType: 'Appellant',
+				emailAddress: 'newnewhuman@example.com',
+				caseReference: 'ref_004'
+			});
+
+			expect(response.status).toEqual(200);
+
+			const appealToUser = await sqlClient.appealToUser.findFirst({
+				where: { appealId: appealCase.Appeal.id }
+			});
+
+			expect(appealToUser).not.toBe(null);
+			expect(appealToUser.role).toBe('appellant');
 		});
 	});
 
 	describe('update user', () => {
-		it('should update service-users', async () => {
+		it('should update service users', async () => {
+			await sqlClient.serviceUser.create({
+				data: {
+					id: 'usr_005',
+					serviceUserType: 'Appellant',
+					caseReference: '000001',
+					emailAddress: 'name@example.com'
+				}
+			});
+
 			const response = await appealsApi.put('/api/v2/service-users').send({
-				id: 'usr_001',
+				id: 'usr_005',
 				serviceUserType: 'Appellant',
 				caseReference: '000001',
-				emailAddress: 'name@example.com'
+				emailAddress: 'name@example.com',
+				postcode: 'POST CODE'
 			});
 
 			expect(response.status).toEqual(200);
+
+			const serviceUser = await sqlClient.serviceUser.findFirst({
+				where: {
+					id: 'usr_005'
+				}
+			});
+
+			expect(serviceUser?.postcode).toBe('POST CODE');
 		});
 	});
 });
