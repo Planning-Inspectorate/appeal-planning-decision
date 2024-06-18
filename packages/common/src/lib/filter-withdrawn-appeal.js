a8809c024697a63f92f59ed30cd75cb72691c913
Dean Hasley <dhasley1990@gmail.com>
2024-06-18 10:29:20 +0100

    refactor(aapd-1676): remove duplicated filter withdrawn appeal from common

--- packages/common/src/lib/filter-withdrawn-appeal.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {import('appeals-service-api').Api.AppealCaseWithAppellant} appeal
- * @returns {boolean}
- */
-const isNotWithdrawn = (appeal) => {
-	return !(appeal.appealWithdrawnDate && appeal.appealStatus === 'withdrawn');
-};
-
-module.exports = { isNotWithdrawn };
