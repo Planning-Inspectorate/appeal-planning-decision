6d0e67ccbc4b3e1ea4ff7cbff2dba758b7984461
Lucy Potter <si3rr4wow@gmail.com>
2024-06-21 11:24:38 +0100

    feat(ASI-67): service user consumer

--- packages/integration-functions/src/functions/appeal-service-user.js
+++ packages/integration-functions/src/functions/appeal-service-user.js
@@ -4,14 +4,14 @@
  */
 
 const { app } = require('@azure/functions');
+const createApiClient = require('../common/api-client');
 
 /**
  * @type {import('@azure/functions').ServiceBusTopicHandler}
  */
-const handler = async (message, context) => {
-	context.log('Handle service user message', message);
-
-	throw new Error('not implemented');
+const handler = async (serviceUser) => {
+	const client = await createApiClient();
+	return await client.putServiceUser(serviceUser); // API will validate the message and throw if there is an error
 };
 
 app.serviceBusTopic('serviceUser', {
