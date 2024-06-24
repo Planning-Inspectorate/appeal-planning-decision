f25ab3793b6ef173ab5f2a632b14689461b4d35a
Lucy Potter <si3rr4wow@gmail.com>
2024-06-24 14:44:51 +0100

    feat(ASI-65): tests

--- packages/integration-functions/__tests__/functions/appeal-document.test.js
+++ packages/integration-functions/__tests__/functions/appeal-document.test.js
@@ -1,69 +1,65 @@
 const { InvocationContext } = require('@azure/functions');
 const handler = require('../../src/functions/appeal-document');
-const got = require('got');
-jest.mock('got');
+const createApiClient = require('../../src/common/api-client');
+const config = require('../../src/common/config');
 
+jest.mock('../../src/common/api-client');
+
+/** @type {import('pins-data-model/src/schemas').AppealDocument} */
 const testData = {
-	body: {
-		documentId: 'a443f10a-e6c2-416a-8eb7-dd82ad8db2a0',
-		caseRef: '3221288',
-		documentReference: '<APP/Q9999/W/22/3221288><1>',
-		version: '1',
-		examinationRefNo: 'XXX-123',
-		filename: 'a.pdf',
-		originalFilename: 'a.pdf',
-		size: 1,
-		mime: 'application/pdf',
-		documentURI: 'https://example.org/a.pdf',
-		path: '/a.pdf',
-		virusCheckStatus: 'scanned',
-		fileMD5: 'b57987f7594c89366f7183ee9b7ae6b2',
-		dateCreated: '2023-06-21T09:47:24.563Z',
-		lastModified: '2023-06-21T10:47:24.563Z',
-		caseType: 'has',
-		documentStatus: 'submitted',
-		redactedStatus: 'redacted',
-		publishedStatus: 'published',
-		datePublished: '2023-06-21T11:47:24.563Z',
-		documentType: 'Planning application form',
-		securityClassification: 'public',
-		sourceSystem: 'back_office',
-		origin: 'pins',
-		owner: 'someone',
-		author: 'someone',
-		representative: 'some agency',
-		description: 'this is a description',
-		stage: 'decision',
-		filter1: 'Deadline 2',
-		filter2: 'Scoping Option Report'
-	}
+	documentId: 'a443f10a-e6c2-416a-8eb7-dd82ad8db2a0',
+	caseRef: '3221288',
+	documentReference: '<APP/Q9999/W/22/3221288><1>',
+	version: 1,
+	examinationRefNo: 'XXX-123',
+	filename: 'a.pdf',
+	originalFilename: 'a.pdf',
+	size: 1,
+	mime: 'application/pdf',
+	documentURI: 'https://example.org/a.pdf',
+	path: '/a.pdf',
+	virusCheckStatus: 'scanned',
+	fileMD5: 'b57987f7594c89366f7183ee9b7ae6b2',
+	dateCreated: '2023-06-21T09:47:24.563Z',
+	lastModified: '2023-06-21T10:47:24.563Z',
+	caseType: 'C',
+	documentStatus: 'submitted',
+	redactedStatus: 'redacted',
+	publishedStatus: 'published',
+	datePublished: '2023-06-21T11:47:24.563Z',
+	documentType: 'appellantCaseCorrespondence',
+	securityClassification: 'public',
+	sourceSystem: 'back-office-appeals',
+	origin: 'pins',
+	owner: 'someone',
+	author: 'someone',
+	representative: 'some agency',
+	description: 'this is a description',
+	stage: 'decision',
+	filter1: 'Deadline 2',
+	filter2: 'Scoping Option Report',
+	caseId: 123,
+	caseReference: '0000001',
+	publishedDocumentURI: 'https://example.com/picture',
+	dateReceived: new Date('2024-01-01').toISOString(),
+	caseStage: 'appellant-case',
+	horizonFolderId: 'horz_001'
 };
 
 describe('appeal-document', () => {
 	const ctx = new InvocationContext({ functionName: 'appeal-document' });
 	ctx.log = jest.fn();
-
-	const original_FO_APPEALS_API = process.env.FO_APPEALS_API;
-
-	beforeAll(() => {
-		process.env.FO_APPEALS_API = 'test';
-	});
-
-	afterAll(() => {
-		if (original_FO_APPEALS_API === undefined) {
-			process.env.FO_APPEALS_API = undefined;
-		} else {
-			process.env.FO_APPEALS_API = original_FO_APPEALS_API;
-		}
-	});
+	const mockClient = {
+		putAppealDocument: jest.fn()
+	};
 
 	beforeEach(async () => {
 		jest.clearAllMocks();
-		got.put.mockImplementation(() => {
-			return {
-				json: jest.fn()
-			};
-		});
+		createApiClient.mockResolvedValue(mockClient);
+		config.API = {
+			...config.API,
+			HOSTNAME: 'test'
+		};
 	});
 
 	it('should send valid message to api', async () => {
@@ -71,55 +67,39 @@ describe('appeal-document', () => {
 
 		expect(ctx.log).toHaveBeenCalledWith('Handle document metadata message', testData);
 		expect(ctx.log).toHaveBeenCalledWith('Sending document metadata message to API');
-		expect(got.put).toHaveBeenCalledWith(
-			`https://test/document-meta-data/${testData.body.documentId}`,
-			{ json: testData.body }
-		);
-		expect(ctx.log).toHaveBeenCalledWith(`Finished handling: ${testData.body.documentId}`);
+		expect(mockClient.putAppealDocument).toHaveBeenCalledWith(testData);
+		expect(ctx.log).toHaveBeenCalledWith(`Finished handling: ${testData.documentId}`);
 		expect(result).toEqual({});
 	});
 
 	it('should error if call to api fails', async () => {
-		got.put.mockImplementation(() => {
-			throw new Error('test error');
+		createApiClient.mockResolvedValue({
+			putAppealDocument: async () => {
+				throw new Error('badness 10000');
+			}
 		});
 
-		await expect(async () => handler(testData, ctx)).rejects.toThrowError('test error');
+		await expect(async () => handler(testData, ctx)).rejects.toThrowError('badness 10000');
 	});
 
 	it('should error if message schema is invalid', async () => {
 		await expect(async () => handler({ body: {} }, ctx)).rejects.toThrowError(
 			'Invalid message schema'
 		);
-		expect(got.put).not.toHaveBeenCalled();
+		expect(mockClient.putAppealDocument).not.toHaveBeenCalled();
 	});
 
-	const invalidPublishedStatuses = [
-		'not_checked',
-		'checked',
-		'ready_to_publish',
-		'do_not_publish',
-		'publishing',
-		'publishing',
-		'unknown'
-	];
+	it('should ignore unpublished documents', async () => {
+		const invalidMessage = {
+			...testData,
+			datePublished: null
+		};
 
-	it.each(invalidPublishedStatuses)(
-		'should ignore message if published status is not valid: [%s]',
-		async (status) => {
-			const invalidMessage = {
-				body: {
-					...testData.body,
-					publishedStatus: status
-				}
-			};
-
-			await handler(invalidMessage, ctx);
-
-			expect(ctx.log).toHaveBeenCalledWith('Invalid message status, skipping');
-			expect(got.put).not.toHaveBeenCalled();
-		}
-	);
+		await expect(async () => handler(invalidMessage, ctx)).rejects.toThrowError(
+			'Invalid message schema'
+		);
+		expect(mockClient.putAppealDocument).not.toHaveBeenCalled();
+	});
 
 	const invalidVirusStatuses = ['not_scanned', 'affected', 'unknown'];
 
@@ -127,16 +107,14 @@ describe('appeal-document', () => {
 		'should ignore message if virus status is not valid: [%s]',
 		async (status) => {
 			const invalidMessage = {
-				body: {
-					...testData.body,
-					virusCheckStatus: status
-				}
+				...testData,
+				virusCheckStatus: status
 			};
 
 			await handler(invalidMessage, ctx);
 
 			expect(ctx.log).toHaveBeenCalledWith('Invalid message status, skipping');
-			expect(got.put).not.toHaveBeenCalled();
+			expect(mockClient.putAppealDocument).not.toHaveBeenCalled();
 		}
 	);
 
@@ -146,16 +124,14 @@ describe('appeal-document', () => {
 		'should ignore message if redacted status is not valid: [%s]',
 		async (status) => {
 			const invalidMessage = {
-				body: {
-					...testData.body,
-					virusCheckStatus: status
-				}
+				...testData,
+				virusCheckStatus: status
 			};
 
 			await handler(invalidMessage, ctx);
 
 			expect(ctx.log).toHaveBeenCalledWith('Invalid message status, skipping');
-			expect(got.put).not.toHaveBeenCalled();
+			expect(mockClient.putAppealDocument).not.toHaveBeenCalled();
 		}
 	);
 });
