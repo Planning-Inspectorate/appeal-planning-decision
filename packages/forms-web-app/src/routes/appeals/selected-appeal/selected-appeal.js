08e87aaccf23e0c63c049cb0a4735cf5f30cb777
Dean Hasley <dhasley1990@gmail.com>
2024-07-03 12:25:58 +0100

    feat(aapd-1066): add routes to lpa and appellant final comments for appellant view

--- packages/forms-web-app/src/routes/appeals/selected-appeal/selected-appeal.js
+++ packages/forms-web-app/src/routes/appeals/selected-appeal/selected-appeal.js
@@ -3,11 +3,14 @@ const express = require('express');
 const selectedAppealController = require('../../../controllers/selected-appeal');
 const appealDetailsController = require('../../../controllers/selected-appeal/appeal-details');
 const questionnaireDetailsController = require('../../../controllers/selected-appeal/questionnaire-details');
+const finalCommentsController = require('../../../controllers/selected-appeal/final-comments-details');
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/:appealNumber', selectedAppealController.get());
 router.get('/:appealNumber/appeal-details', appealDetailsController.get());
 router.get('/:appealNumber/questionnaire', questionnaireDetailsController.get());
+router.get('/:appealNumber/final-comments', finalCommentsController.get());
+router.get('/:appealNumber/lpa-final-comments', finalCommentsController.get());
 
 module.exports = router;
