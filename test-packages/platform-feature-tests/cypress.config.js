a8cec71e97b201d2d5ad4dcdfa18f10023683d65
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2024-07-04 06:53:25 +0100

    version1 qatest

--- test-packages/platform-feature-tests/cypress.config.js
+++ test-packages/platform-feature-tests/cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
 		},
 		appeals_beta_base_url: 'https://appeals-service-test.planninginspectorate.gov.uk', //TODO: make this an env var?
 		horizon_base_url: 'http://10.0.7.4:8000/horizon',
-		supportFile: false,
+		supportFile: 'cypress/support/e2e.js',
 		testIsolation: false
 	}
 });
--- test-packages/platform-feature-tests/cypress/page-objects/appeal-information-upload/task-list.js
+++ test-packages/platform-feature-tests/cypress/page-objects/appeal-information-upload/task-list.js
@@ -1,5 +1,6 @@
 export class TaskList {
 	elements = {
+		//applicationNameSection: () => cy.get(),
 		contactDetailsLink: () => cy.get('[data-cy="contactDetailsSection"]'), // Provide your contact details
 		appealSiteSectionLink: () => cy.get('[data-cy="appealSiteSection"]'), // Tell us about the appeal site
 		appealDecisionSectionLink: () => cy.get('[data-cy="appealDecisionSection"]'), // Tell us how you would prefer us to decide your appeal
