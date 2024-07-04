58747ada82274c37e0f99abc8f457f9ec1fa9218
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2024-07-04 07:28:19 +0100

    hoseholder refuesed enabled qatest

--- test-packages/platform-feature-tests/cypress/e2e/submit-householder-appeal.cy.js
+++ test-packages/platform-feature-tests/cypress/e2e/submit-householder-appeal.cy.js
@@ -4,84 +4,8 @@ const { submitAppealFlow } = require('../support/flows/appeal');
 
 describe('Appeal uploads', () => {
 	[
-		// {
-		// 	statusOfOriginalApplication: 'refused',
-		// 	typeOfDecisionRequested: 'hearing',
-		// 	statusOfPlanningObligation: 'in draft',
-		// 	typeOfPlanningApplication: 'answer-householder-planning',
-		// 	finalComments: {
-		// 		check: false, // TODO: set these to true after feature flag introduced
-		// 		uploadAdditionalDocuments: false
-		// 	},
-		// 	expectedFilesAndFoldersInHorizon: [
-		// 		{
-		// 			expectedFolderHierarchy: ['0 Inspector File', "01 Appelant's Initial Documents"],
-		// 			expectedFileName: 'planning-application-form.pdf'
-		// 		},
-		// 		{
-		// 			expectedFolderHierarchy: [
-		// 				'0 Inspector File',
-		// 				"01 Appelant's Initial Documents",
-		// 				'05 Plans',
-		// 				'01 Application Plans'
-		// 			],
-		// 			expectedFileName: 'plans-drawings-and-supporting-documents.pdf'
-		// 		},
-		// 		{
-		// 			expectedFolderHierarchy: ['0 Inspector File', "01 Appelant's Initial Documents"],
-		// 			expectedFileName: 'design-and-access-statement.pdf'
-		// 		},
-		// 		{
-		// 			expectedFolderHierarchy: ['0 Inspector File', "01 Appelant's Initial Documents"],
-		// 			expectedFileName: 'draft-statement-of-common-ground.pdf'
-		// 		},
-		// 		{
-		// 			expectedFolderHierarchy: ['0 Inspector File', "01 Appelant's Initial Documents"],
-		// 			expectedFileName: 'letter-confirming-planning-application.pdf'
-		// 		},
-		// 		{
-		// 			expectedFolderHierarchy: [
-		// 				'0 Inspector File',
-		// 				"01 Appelant's Initial Documents",
-		// 				'02 Statement and appendicies'
-		// 			],
-		// 			expectedFileName: 'ownership-certificate-and-agricultural-land-declaration.pdf'
-		// 		},
-		// 		{
-		// 			expectedFolderHierarchy: ['0 Inspector File', "01 Appelant's Initial Documents"],
-		// 			expectedFileName: 'letter-confirming-planning-obligation.pdf'
-		// 		},
-		// 		{
-		// 			expectedFolderHierarchy: [
-		// 				'0 Inspector File',
-		// 				"01 Appelant's Initial Documents",
-		// 				'05 Plans',
-		// 				'02 Plans submitted after LPA decision'
-		// 			],
-		// 			expectedFileName: 'plans-drawings.jpeg'
-		// 		},
-		// 		{
-		// 			expectedFolderHierarchy: ['0 Inspector File', '07 Planning Obligation'],
-		// 			expectedFileName: 'draft-planning-obligation.pdf'
-		// 		},
-		// 		{
-		// 			expectedFolderHierarchy: ['1 Main Party', 'Appellant/Agent/Applicant'],
-		// 			expectedFileName: 'other-supporting-documents.pdf'
-		// 		},
-		// 		{
-		// 			expectedFolderHierarchy: [
-		// 				'0 Inspector File',
-		// 				'05 Final comments',
-		// 				'Appellant final comments'
-		// 			],
-		// 			expectedFileName: 'final-comments.pdf'
-		// 		}
-				
-		// 	]
-		// },
-
 		{
-			statusOfOriginalApplication: 'granted',
+			statusOfOriginalApplication: 'refused',
 			typeOfDecisionRequested: 'hearing',
 			statusOfPlanningObligation: 'in draft',
 			typeOfPlanningApplication: 'answer-householder-planning',
@@ -155,6 +79,82 @@ describe('Appeal uploads', () => {
 				
 			]
 		}
+
+		// {
+		// 	statusOfOriginalApplication: 'granted',
+		// 	typeOfDecisionRequested: 'hearing',
+		// 	statusOfPlanningObligation: 'in draft',
+		// 	typeOfPlanningApplication: 'answer-householder-planning',
+		// 	finalComments: {
+		// 		check: false, // TODO: set these to true after feature flag introduced
+		// 		uploadAdditionalDocuments: false
+		// 	},
+		// 	expectedFilesAndFoldersInHorizon: [
+		// 		{
+		// 			expectedFolderHierarchy: ['0 Inspector File', "01 Appelant's Initial Documents"],
+		// 			expectedFileName: 'planning-application-form.pdf'
+		// 		},
+		// 		{
+		// 			expectedFolderHierarchy: [
+		// 				'0 Inspector File',
+		// 				"01 Appelant's Initial Documents",
+		// 				'05 Plans',
+		// 				'01 Application Plans'
+		// 			],
+		// 			expectedFileName: 'plans-drawings-and-supporting-documents.pdf'
+		// 		},
+		// 		{
+		// 			expectedFolderHierarchy: ['0 Inspector File', "01 Appelant's Initial Documents"],
+		// 			expectedFileName: 'design-and-access-statement.pdf'
+		// 		},
+		// 		{
+		// 			expectedFolderHierarchy: ['0 Inspector File', "01 Appelant's Initial Documents"],
+		// 			expectedFileName: 'draft-statement-of-common-ground.pdf'
+		// 		},
+		// 		{
+		// 			expectedFolderHierarchy: ['0 Inspector File', "01 Appelant's Initial Documents"],
+		// 			expectedFileName: 'letter-confirming-planning-application.pdf'
+		// 		},
+		// 		{
+		// 			expectedFolderHierarchy: [
+		// 				'0 Inspector File',
+		// 				"01 Appelant's Initial Documents",
+		// 				'02 Statement and appendicies'
+		// 			],
+		// 			expectedFileName: 'ownership-certificate-and-agricultural-land-declaration.pdf'
+		// 		},
+		// 		{
+		// 			expectedFolderHierarchy: ['0 Inspector File', "01 Appelant's Initial Documents"],
+		// 			expectedFileName: 'letter-confirming-planning-obligation.pdf'
+		// 		},
+		// 		{
+		// 			expectedFolderHierarchy: [
+		// 				'0 Inspector File',
+		// 				"01 Appelant's Initial Documents",
+		// 				'05 Plans',
+		// 				'02 Plans submitted after LPA decision'
+		// 			],
+		// 			expectedFileName: 'plans-drawings.jpeg'
+		// 		},
+		// 		{
+		// 			expectedFolderHierarchy: ['0 Inspector File', '07 Planning Obligation'],
+		// 			expectedFileName: 'draft-planning-obligation.pdf'
+		// 		},
+		// 		{
+		// 			expectedFolderHierarchy: ['1 Main Party', 'Appellant/Agent/Applicant'],
+		// 			expectedFileName: 'other-supporting-documents.pdf'
+		// 		},
+		// 		{
+		// 			expectedFolderHierarchy: [
+		// 				'0 Inspector File',
+		// 				'05 Final comments',
+		// 				'Appellant final comments'
+		// 			],
+		// 			expectedFileName: 'final-comments.pdf'
+		// 		}
+				
+		// 	]
+		// }
 		// ,
 		// {
 		// 	statusOfOriginalApplication: 'refused',
