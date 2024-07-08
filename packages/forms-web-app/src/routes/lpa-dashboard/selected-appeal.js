edc3cea13b8cec40b01c3b0f1fdac16a54757b59
Dean Hasley <dhasley1990@gmail.com>
2024-07-08 10:33:31 +0100

    feat(aapd-1090): add route to ip comments for lpa view

--- packages/forms-web-app/src/routes/lpa-dashboard/selected-appeal.js
+++ packages/forms-web-app/src/routes/lpa-dashboard/selected-appeal.js
@@ -5,6 +5,7 @@ const selectedAppealController = require('../../controllers/selected-appeal');
 const appealDetailsController = require('../../controllers/selected-appeal/appeal-details');
 const questionnaireDetailsController = require('../../controllers/selected-appeal/questionnaire-details');
 const finalCommentsController = require('../../controllers/selected-appeal/final-comments-details');
+const interestedPartyCommentsController = require('../../controllers/selected-appeal/ip-comment-details');
 
 router.get('/:appealNumber', selectedAppealController.get('layouts/lpa-dashboard/main.njk'));
 router.get(
@@ -22,6 +23,10 @@ router.get(
 router.get(
 	'/:appealNumber/appellant-final-comments',
 	finalCommentsController.get('layouts/lpa-dashboard/main.njk')
+);
+router.get(
+	'/:appealNumber/interested-party-comments',
+	interestedPartyCommentsController.get('layouts/lpa-dashboard/main.njk')
 );
 
 module.exports = router;
