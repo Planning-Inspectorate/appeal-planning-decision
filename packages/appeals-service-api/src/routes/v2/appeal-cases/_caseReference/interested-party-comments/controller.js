392c5f8f0c66292f3bad6fe170af58b5859fd18a
Dean Hasley <dhasley1990@gmail.com>
2024-07-10 12:06:05 +0100

    test(aapd-1096): add tests for api endpoint

--- packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/interested-party-comments/controller.js
+++ packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/interested-party-comments/controller.js
@@ -1,5 +1,6 @@
 const { getInterestedPartyComments, createInterestedPartyComment } = require('./service');
 const ApiError = require('#errors/apiError');
+const logger = require('#lib/logger');
 
 /**
  * @type {import('express').Handler}
@@ -8,13 +9,13 @@ async function getCommentsForCase(req, res) {
 	const { caseReference } = req.params;
 
 	if (!caseReference) {
-		throw ApiError.withMessage(400, 'case reference is required');
+		throw ApiError.withMessage(400, 'Case reference is required');
 	}
 
 	const comments = await getInterestedPartyComments(caseReference);
 
-	if (!comments) {
-		throw ApiError.withMessage(404, 'no comments found for this case reference');
+	if (comments?.length === 0) {
+		throw ApiError.withMessage(404, 'No comments found for this case reference');
 	}
 
 	res.status(200).send(comments);
@@ -24,15 +25,22 @@ async function getCommentsForCase(req, res) {
  * @type {import('express').Handler}
  */
 async function createComment(req, res) {
-	const commentData = req.body;
+	try {
+		const commentData = req.body;
+		const { caseReference } = req.params;
 
-	if (!commentData.caseReference || !commentData.comment) {
-		throw ApiError.withMessage(400, 'case reference and comments are required');
+		if (!commentData.caseReference || !commentData.comment) {
+			logger.error('Validation error', { caseReference, commentData });
+			throw ApiError.withMessage(400, 'Case reference and comments are required');
+		}
+
+		const createdComment = await createInterestedPartyComment({ ...commentData, caseReference });
+
+		res.status(200).send(createdComment);
+	} catch (err) {
+		logger.error('Error creating comment', { err });
+		throw err;
 	}
-
-	const createdComment = await createInterestedPartyComment(commentData);
-
-	res.status(200).send(createdComment);
 }
 
 module.exports = { getCommentsForCase, createComment };
