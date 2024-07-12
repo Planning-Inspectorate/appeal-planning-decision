18e92ee8cc194716217908b9eb5274d5341c2697
Tom Pickering <topickering@gmail.com>
2024-07-12 16:57:00 +0100

    fix(489): amend confirm user route for download

--- packages/appeals-service-api/src/routes/v2/appellant-submissions/_id/controller.js
+++ packages/appeals-service-api/src/routes/v2/appellant-submissions/_id/controller.js
@@ -44,3 +44,22 @@ exports.patch = async (req, res) => {
 
 	res.send(submission);
 };
+
+/**
+ * @type {import('express').Handler}
+ */
+exports.confirm = async (req, res) => {
+	const appellantSubmissionId = req.params.id;
+
+	if (!appellantSubmissionId) {
+		throw ApiError.badRequest({ errors: ['Submission id is required'] });
+	}
+
+	const userId = req.auth.payload.sub;
+
+	if (!userId) {
+		throw ApiError.invalidToken();
+	}
+
+	res.status(200).send(true);
+};
