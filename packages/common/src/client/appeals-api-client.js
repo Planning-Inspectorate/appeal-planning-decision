0f7715e6e883b4c894f510a71cafba4806163be1
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-07-12 13:19:07 +0100

    fix(asi-56): send HAS appeal to api

--- packages/common/src/client/appeals-api-client.js
+++ packages/common/src/client/appeals-api-client.js
@@ -195,7 +195,7 @@ class AppealsApiClient {
 	 */
 	async putAppealCase(data) {
 		const endpoint = `${v2}/appeal-cases/${data.caseReference}`;
-		const response = await this.#makePutRequest(endpoint);
+		const response = await this.#makePutRequest(endpoint, data);
 		return response.json();
 	}
 
@@ -571,15 +571,6 @@ class AppealsApiClient {
 	async createAppellantSubmission(data) {
 		const endpoint = `${v2}/appellant-submissions`;
 		return (await this.#makePutRequest(endpoint, data)).json();
-	}
-
-	/**
-	 * @param {Omit<AppellantSubmission, 'id'>} data
-	 * @returns {Promise<AppellantSubmission>}
-	 */
-	async createDebugAppellantSubmission(data) {
-		const endpoint = `${v2}/appellant-submissions`;
-		return (await this.#makePostRequest(endpoint, data)).json();
 	}
 
 	/**
