ff2fd507b1bb0cabdd04ff2dadfbb12317c5075b
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2024-07-17 07:34:51 +0100

    added has test cases

--- test-packages/platform-feature-tests/cypress/e2e/submit-full-appeal.cy.js
+++ test-packages/platform-feature-tests/cypress/e2e/submit-full-appeal.cy.js
@@ -19,8 +19,15 @@ describe('Appeal uploads', () => {
 			// Full planning
 			//typeOfPlanningApplication?.forEach((planning) => {
 				it(`sends a Full planning application successfully to Horizon where the original application status is "${statusOfOriginalApplication}", 
-				the decision type requested is "${typeOfDecisionRequested}", the planning obligation status is "${statusOfPlanningObligation}", and 
-				application form has area unit ${applicationForm?.areaUnits}  and know other land owners ${applicationForm?.knowsOtherOwners ?? applicationForm?.knowsAllOwners} final comments config is: ${finalComments}`, () => {
+				the decision type requested is "${typeOfDecisionRequested}", the planning obligation status is "${statusOfPlanningObligation}", application type is 
+				${typeOfPlanningApplication} and appellant is ${applicationForm?.isAppellant} application form has area unit ${applicationForm?.areaUnits}  
+				and know other land owners ${applicationForm?.knowsOtherOwners ?? applicationForm?.knowsAllOwners} appellant in green belt is ${applicationForm?.appellantInGreenBelt}
+				with all own land ${applicationForm?.isOwnsAllLand} and with own some land ${applicationForm?.isOwnsSomeLand} and knows all owners ${applicationForm?.knowsAllOwners}
+				and knows other owners ${applicationForm?.knowsOtherOwners} and Agricultural Holding ${applicationForm?.isAgriculturalHolding} with 
+				tenant agriculture holding ${applicationForm?.isTenantAgricultureHolding} with any other tenant ${applicationForm?.anyOtherTenants} and 
+				inspector need access ${applicationForm?.isInspectorNeedAccess} with  appellant site safety ${applicationForm?.isAppellantSiteSafety} and 
+				Update Development Description to ${applicationForm?.iaUpdateDevelopmentDescription} with appellant procedure preference ${applicationForm?.appellantProcedurePreference}
+				and any other appeals ${applicationForm?.anyOtherAppeals} with Appellant Linked Case Add ${applicationForm?.isAppellantLinkedCaseAdd}`, () => {
 				submitAppealFlow({
 					statusOfOriginalApplication,
 					typeOfDecisionRequested,
--- test-packages/platform-feature-tests/cypress/support/flows/sections/initialiseHouseHolderPlanning.js
+++ test-packages/platform-feature-tests/cypress/support/flows/sections/initialiseHouseHolderPlanning.js
@@ -7,7 +7,11 @@ const greenBeltPage = require("../pages/prepare-appeal/greenBeltPage");
 const ownAllLandPage = require("../pages/prepare-appeal/ownAllLandPage");
 const ownSomeLandPage = require("../pages/prepare-appeal/ownSomeLandPage");
 const ownsLandInvolvedPage = require("../pages/prepare-appeal/ownsLandInvolvedPage");
-
+const inspectorNeedAccessPage = require("../pages/prepare-appeal/inspectorNeedaccessPage");
+const healthSafetyIssuesPage = require("../pages/prepare-appeal/healthSafetyIssuesPage");
+const otherAppealsPage = require("../pages/prepare-appeal/otherAppealsPage");
+const uploadApplicationFormPage = require("../pages/upload-documents/uploadApplicationFormPage");
+const applyAppealCostsPage = require("../pages/upload-documents/applyAppealCostsPage");
 module.exports = (statusOfOriginalApplication,planning, grantedOrRefusedId,context) => {
 	
 	cy.get(grantedOrRefusedId).click();
@@ -28,14 +32,12 @@ module.exports = (statusOfOriginalApplication,planning, grantedOrRefusedId,conte
     cy.url().should('include','/appeal-householder-decision/planning-application-number');
 
 	const applicationNumber = `TEST-${Date.now()}`;
-	cy.get('#application-number').type(applicationNumber);
+	cy.get('[data-cy="application-number"]').type(applicationNumber);
 	cy.advanceToNextPage();
 
     cy.url().should('include','/appeal-householder-decision/email-address');
-
-	//cy.get('#email-address').type('appealplanningdecisiontest@planninginspectorate.gov.uk');
-	//cy.intercept('POST','/full-appeal/submit-appeal/list-of-documents').as('postRequest');
-	cy.get('#email-address').type('appellant2@planninginspectorate.gov.uk');
+	
+	cy.get('[data-cy="email-address"]').type('appellant2@planninginspectorate.gov.uk');
 	cy.advanceToNextPage();
 
 	
@@ -48,8 +50,7 @@ module.exports = (statusOfOriginalApplication,planning, grantedOrRefusedId,conte
 	cy.wait(2000);
 	cy.location('search').then((search)=>{
 		const params = new URLSearchParams(search);
-		const dynamicId = params.get('id');
-		//cy.log('Test id in task page',dynamicId);
+		const dynamicId = params.get('id');		
 		applicationFormPage('householder','other',dynamicId);
 		//Contact details
 		applicationNamePage(context?.applicationForm?.isAppellant);
@@ -72,17 +73,17 @@ module.exports = (statusOfOriginalApplication,planning, grantedOrRefusedId,conte
 			ownSomeLandPage(context?.applicationForm?.isOwnsSomeLand,context);
 
 		}
-
 		//Will an inspector need to access your land or property?
-		cy.get('#appellantSiteAccess').click();
-		cy.get('#appellantSiteAccess_appellantSiteAccessDetails').type('appellantSiteAccess_appellantSiteAccessDetails1234567890!"£$%^&*(9)');
-		cy.advanceToNextPage();
+		inspectorNeedAccessPage(context?.applicationForm?.isInspectorNeedAccess);			
+	
 		//Health and safety issues
-		cy.get('#appellantSiteSafety').click();
-		cy.get('#appellantSiteSafety_appellantSiteSafetyDetails').type('appellantSiteSafety_appellantSiteSafetyDetails1234567890!"£$%^&*(10)');
-		cy.advanceToNextPage();
+		healthSafetyIssuesPage(context);
+		
 		//What is the application reference number?
-		cy.get('#applicationReference').type('12345x6');
+		cy.get('#applicationReference').invoke('val').then((inputValue)=>{
+			expect(inputValue).to.equal(applicationNumber);
+		});
+	
 		cy.advanceToNextPage();
 		//What date did you submit your application?
 		cy.get('#onApplicationDate_day').type(currentDate.getDate() - 1);
@@ -93,84 +94,25 @@ module.exports = (statusOfOriginalApplication,planning, grantedOrRefusedId,conte
 		cy.get('#developmentDescriptionOriginal').type ('developmentDescriptionOriginal-hint123456789!£$%&*j');
 		cy.advanceToNextPage();
 		//Did the local planning authority change the description of development?
-		cy.get('#updateDevelopmentDescription').click();
-		cy.advanceToNextPage();
-
-		cy.get('[data-cy="answer-yes"]').click();
-		cy.advanceToNextPage();
-
-		//How would you prefer us to decide your appeal?
-		// cy.get('#appellantProcedurePreference-2').click();
-		// cy.advanceToNextPage();
-		// //Why would you prefer a hearing?
-		// cy.get('#appellantPreferHearingDetails').type('To Argue in the court12345!£%^&*');
-		// cy.advanceToNextPage();
-		//Are there other appeals linked to your development?(Ans:YES)
-		// cy.get('#appellantLinkedCase').click();
-		// cy.advanceToNextPage();
-		//Enter the appeal reference number
-		cy.get('#appellantLinkedCase').type('9876T40');
-		cy.advanceToNextPage();
-		//You’ve added a linked appeal(Ans:No)
-		cy.get('[data-cy="answer-no"]').click();
-		cy.advanceToNextPage();
-
+		if(context?.applicationForm?.iaUpdateDevelopmentDescription){
+			cy.get('[data-cy="answer-yes"]').click();
+			cy.advanceToNextPage();
+		} else{
+			cy.get('[data-cy="answer-no"]').click();
+			cy.advanceToNextPage();
+		}
+		
+		otherAppealsPage(context?.applicationForm?.anyOtherAppeals,context);
+		
 		cy.uploadDocuments('householder','upload-application-form',dynamicId);
-		//uploadApplicationFormPage(context,dynamicId);
-		//Upload your application form
-		cy.uploadFileFromFixtureDirectory('letter-confirming-planning-application.pdf');
-		cy.advanceToNextPage();
-		//Upload evidence of your agreement to change the description of development
-		cy.uploadFileFromFixtureDirectory('additional-final-comments-1.pdf');
-		cy.advanceToNextPage();
-		//Upload the decision letter from the local planning authority
-		cy.uploadFileFromFixtureDirectory('additional-final-comments-2.pdf');
-		cy.advanceToNextPage();
-		// //Do you plan to submit a planning obligation to support your appeal?
-		// cy.get('#planningObligation').click();
-		// cy.advanceToNextPage();
-		// //What is the status of your planning obligation?
-		// cy.get('#statusPlanningObligation-2').click();
-		// cy.advanceToNextPage();
-		// //Did you submit a separate ownership certificate and agricultural land declaration with your application?
-		// cy.get('#ownershipCertificate').click();
-		// cy.advanceToNextPage();
-		// //Upload your separate ownership certificate and agricultural land declaration
-		// cy.uploadFileFromFixtureDirectory('draft-planning-obligation.pdf');
-		// cy.advanceToNextPage();
+		uploadApplicationFormPage(context,dynamicId);
+		
 		//Upload your appeal statement
 		cy.uploadFileFromFixtureDirectory('appeal-statement-valid.pdf');
 		cy.advanceToNextPage();
-		// //Upload your draft statement of common ground
-		// cy.uploadFileFromFixtureDirectory('draft-statement-of-common-ground.pdf');
-		// cy.advanceToNextPage();
 		//Do you need to apply for an award of appeal costs?
-		cy.get('#costApplication').click();
-		cy.advanceToNextPage();
-		//Upload your application for an award of appeal costs
-		cy.uploadFileFromFixtureDirectory('other-supporting-docs.pdf');
-		cy.advanceToNextPage();
-		// //Did you submit a design and access statement with your application?
-		// cy.get('#designAccessStatement').click();
-		// cy.advanceToNextPage();
-		// //Upload your design and access statement
-		// cy.uploadFileFromFixtureDirectory('design-and-access-statement.pdf');
-		// cy.advanceToNextPage();
-		// //Upload your plans, drawings and supporting documents you submitted with your application
-		// cy.uploadFileFromFixtureDirectory('plans-drawings-and-supporting-documents.pdf');
-		// cy.advanceToNextPage();
-		// //Do you have any new plans or drawings that support your appeal?
-		// cy.get('#newPlansDrawings').click();
-		// cy.advanceToNextPage();
-		// //Upload your new plans or drawings
-		// cy.uploadFileFromFixtureDirectory('plans-drawings.jpeg');
-		// cy.advanceToNextPage();
-		//Do you have any other new documents that support your appeal?#
-		// cy.get('#otherNewDocuments').click();
-		// cy.advanceToNextPage();
-		// //Upload your other new supporting documents
-		// cy.uploadFileFromFixtureDirectory('other-supporting-docs.pdf');
-		// cy.advanceToNextPage();
+		applyAppealCostsPage(context);
+				
 		//submit
 		cy.get(`a[href*="/appeals/householder/submit/declaration?id=${dynamicId}"]`).click();
 		cy.wait(2000);
