c9dd9306671d5d263bd756f0baf40b22c1a2eea5
Dean Hasley <dhasley1990@gmail.com>
2024-07-19 13:07:29 +0100

    refactor(A2-480): use href to improve accessibility

--- packages/forms-web-app/src/lib/client-side/comment-actions.js
+++ packages/forms-web-app/src/lib/client-side/comment-actions.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-	document.body.classList.add('js-enabled');
 	document.body.addEventListener('click', (event) => {
 		if (event?.target.classList.contains('toggle-button')) {
 			const target = event.target;
 
 			const index = target?.id.split('-').pop();
 			const commentTextElement = document.getElementById(`comment-text-${index}`);
+			event.preventDefault();
 
 			if (commentTextElement) {
 				const isExpanded = target?.textContent === 'Close';
