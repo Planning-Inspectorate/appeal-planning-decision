f3b5bb11b31097d52130ff7354c3bff6ffb45d88
Tom Pickering <topickering@gmail.com>
2024-07-22 17:51:20 +0100

    feat(a2-176): refactor validators, add comments

--- packages/forms-web-app/src/routes/file-based-router/comment-planning-appeal/add-comments/index.js
+++ packages/forms-web-app/src/routes/file-based-router/comment-planning-appeal/add-comments/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { addCommentsGet, addCommentsPost } = require('./controller');
+const {
+	rules: yourNameValidationRules
+} = require('../../../../validators/interested-parties/email-address');
+const { validationErrorHandler } = require('../../../../validators/validation-error-handler');
 const asyncHandler = require('@pins/common/src/middleware/async-handler');
 
 const router = express.Router();
 
 router.get('/', asyncHandler(addCommentsGet));
-router.post('/', asyncHandler(addCommentsPost));
+router.post('/', yourNameValidationRules(), validationErrorHandler, asyncHandler(addCommentsPost));
 
 module.exports = { router };
--- packages/forms-web-app/src/routes/file-based-router/comment-planning-appeal/your-name/index.js
+++ packages/forms-web-app/src/routes/file-based-router/comment-planning-appeal/your-name/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { yourNameGet, yourNamePost } = require('./controller');
+const {
+	rules: yourNameValidationRules
+} = require('../../../../validators/interested-parties/your-name');
+const { validationErrorHandler } = require('../../../../validators/validation-error-handler');
 const asyncHandler = require('@pins/common/src/middleware/async-handler');
 
 const router = express.Router();
 
 router.get('/', asyncHandler(yourNameGet));
-router.post('/', asyncHandler(yourNamePost));
+router.post('/', yourNameValidationRules(), validationErrorHandler, asyncHandler(yourNamePost));
 
 module.exports = { router };
