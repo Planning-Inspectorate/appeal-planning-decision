b5a0d26266829efb2c41cbe25596dd9d746e71ae
Tom Pickering <topickering@gmail.com>
2024-07-23 16:52:48 +0100

    feat(a2-177): check answer post

---
+++ packages/forms-web-app/src/routes/file-based-router/comment-planning-appeal/check-answers/controller.js
@@ -0,0 +1,86 @@
+/** @type {import('express').RequestHandler} */
+const checkAnswersGet = (req, res) => {
+	if (!req.session.interestedParty?.appealNumber) {
+		return res.redirect(`enter-appeal-reference`);
+	}
+
+	if (!req.session.interestedParty?.firstName || !req.session.interestedParty?.lastName) {
+		return res.redirect(`your-name`);
+	}
+
+	if (!req.session.interestedParty?.comments) {
+		return res.redirect(`add-comments`);
+	}
+
+	const interestedParty = req.session.interestedParty;
+
+	const ipSummaryList = formatIpSummaryList(interestedParty);
+
+	res.render(`comment-planning-appeal/check-answers/index`, { interestedParty, ipSummaryList });
+};
+
+/** @type {import('express').RequestHandler} */
+const checkAnswersPost = async (req, res) => {
+	// const interestedParty = req.session.interestedParty;
+
+	// const result = await req.appealsApiClient.submitInterestedPartyComment(interestedParty);
+
+	// console.log(result)
+
+	return res.redirect(`add-comments`);
+};
+
+const formatIpSummaryList = (interestedParty) => {
+	return [
+		{
+			key: {
+				text: 'Your name'
+			},
+			value: {
+				text: `${interestedParty.firstName} ${interestedParty.lastName}`
+			},
+			actions: {
+				items: [
+					{
+						href: 'your-name',
+						text: 'Change'
+					}
+				]
+			}
+		},
+		{
+			key: {
+				text: 'Your email'
+			},
+			value: {
+				text: interestedParty.emailAddress || 'No email address supplied'
+			},
+			actions: {
+				items: [
+					{
+						href: 'email-address',
+						text: 'Change'
+					}
+				]
+			}
+		},
+		{
+			key: {
+				text: 'Your comments'
+			},
+			value: {
+				text: interestedParty.comments
+			},
+			actions: {
+				items: [
+					{
+						href: 'add-comments',
+						text: 'Change'
+					}
+				]
+			}
+		}
+	];
+};
+
+module.exports = { checkAnswersGet, checkAnswersPost };
