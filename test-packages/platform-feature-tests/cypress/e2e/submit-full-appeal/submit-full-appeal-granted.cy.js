bd91de26efd7a3b0fd8237d4eae218f665197b82
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2024-07-30 19:51:40 +0100

    segregated based on application status

---
+++ test-packages/platform-feature-tests/cypress/e2e/submit-full-appeal/submit-full-appeal-granted.cy.js
@@ -0,0 +1,44 @@
+import { fullAppealGrantedTestCases } from "../../helpers/fullAppeal/fullAppealGrantedData";
+const { submitAppealFlow } = require('../../support/flows/appeal');
+
+describe('Submit Full Appeal Granted Test cases', () => {
+
+    fullAppealGrantedTestCases.forEach((context) => {
+        const {
+            statusOfOriginalApplication,
+            typeOfDecisionRequested,
+            statusOfPlanningObligation,
+            typeOfPlanningApplication,
+            applicationForm
+        } = context;
+        it(`
+			- Should check the status of the original application,
+			- verify the status of planning obligation as "${statusOfPlanningObligation}",
+			- validate the type of planning application as "${typeOfPlanningApplication}",
+			- ensure the application form contains the correct details:
+			* is Appellant: "${applicationForm?.isAppellant}"
+			* Area Units: "${applicationForm?.areaUnits}"
+			* appellant in Green Belt: "${applicationForm?.appellantInGreenBelt}"
+			* Is Owns All Land: "${applicationForm?.isOwnsAllLand}"
+			* Is Owns Some Land: "${applicationForm?.isOwnsSomeLand}"
+			* Knows Other Owners: "${applicationForm?.knowsOtherOwners}"
+			* Is Agricultural Holding: "${applicationForm?.isAgriculturalHolding}"
+			* ${applicationForm?.anyOtherTenants !== undefined ? ` Any Other Tenants:  "${applicationForm?.anyOtherTenants}" ` : "No Other tenants needed"}
+			* Is Inspector Access Needed: "${applicationForm?.isInspectorNeedAccess}"
+			* Is Appellant Site Safety: "${applicationForm?.isAppellantSiteSafety}"
+			* Is Development Description updated: "${applicationForm?.iaUpdateDevelopmentDescription}"
+			* Appellant Procedure Preference: "${applicationForm?.appellantProcedurePreference}"
+			* Any Other appeals: "${applicationForm?.anyOtherAppeals}"
+			* Is Appellant Linked Case Add: "${applicationForm?.isAppellantLinkedCaseAdd}"
+			* 		
+		 `, () => {
+            submitAppealFlow({
+                statusOfOriginalApplication,
+                typeOfDecisionRequested,
+                statusOfPlanningObligation,
+                planning: typeOfPlanningApplication,
+                context
+            });
+        });
+    });
+});
