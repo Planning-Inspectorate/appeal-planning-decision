9201e01d22462d54d58a86afdd3996cf62976a2e
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-08-13 18:10:30 +0100

    feat: add sanitized postcode field for easier searches in future

--- packages/appeals-service-api/src/routes/v2/appeal-cases/repo.js
+++ packages/appeals-service-api/src/routes/v2/appeal-cases/repo.js
@@ -115,6 +115,7 @@ const mapHASDataModelToAppealCase = (
 		lpaQuestionnaireValidationDetails,
 		siteAccessDetails,
 		siteSafetyDetails,
+		siteAddressPostcode,
 		...commonFields
 	}
 ) => ({
@@ -139,6 +140,8 @@ const mapHASDataModelToAppealCase = (
 			}
 		}
 	},
+	siteAddressPostcode: siteAddressPostcode,
+	siteAddressPostcodeSanitized: siteAddressPostcode.replace(' ', '').toUpperCase(),
 	LPACode: lpaCode,
 	caseSpecialisms: caseSpecialisms ? JSON.stringify(caseSpecialisms) : null,
 	caseValidationInvalidDetails: caseValidationInvalidDetails
@@ -312,6 +315,9 @@ class AppealCaseRepository {
 						townCity: address.neighbouringSiteAddressTown,
 						county: address.neighbouringSiteAddressCounty,
 						postcode: address.neighbouringSiteAddressPostcode,
+						postcodeSanitized: address.neighbouringSiteAddressPostcode
+							.replace(' ', '')
+							.toUpperCase(),
 						siteAccessDetails: address.neighbouringSiteAccessDetails,
 						siteSafetyDetails: address.neighbouringSiteSafetyDetails
 					}))
