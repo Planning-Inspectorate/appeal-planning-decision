531b132617a1104f05bf28ae341aa2dcb10391e0
Dean Hasley <dhasley1990@gmail.com>
2024-08-23 12:17:35 +0100

    refactor(A2-236): account for dependent question in redirect middleware and add tests

--- packages/forms-web-app/src/dynamic-forms/middleware/redirect-to-unanswered-question.js
+++ packages/forms-web-app/src/dynamic-forms/middleware/redirect-to-unanswered-question.js
@@ -4,21 +4,19 @@ module.exports = () => (req, res, next) => {
 	const journeyResponse = res.locals.journeyResponse;
 	const journey = getJourney(journeyResponse);
 
-	const questionFieldNames = new Set(
-		journey.sections.flatMap((section) => section.questions.map((question) => question.fieldName))
-	);
+	for (const section of journey.sections) {
+		for (const question of section.questions) {
+			const answer = journey.response?.answers[question.fieldName];
+			if (question.fieldName === 'uploadLpaStatementDocuments') {
+				const additionalDocumentsAnswer = journey.response.answers['additionalDocuments'];
+				if (additionalDocumentsAnswer !== 'yes') {
+					continue;
+				}
+			}
 
-	for (const fieldName of questionFieldNames) {
-		const answer = journey.response.answers[fieldName];
-
-		if (answer === undefined || answer === null || answer === '') {
-			const section = journey.sections.find((s) =>
-				s.questions.some((q) => q.fieldName === fieldName)
-			);
-			const question = section?.questions.find((q) => q.fieldName === fieldName);
-
-			const questionUrl = journey.getCurrentQuestionUrl(section?.segment, question?.fieldName);
-			return res.redirect(questionUrl);
+			if (!answer || answer === '' || answer === undefined) {
+				return res.redirect(journey.getCurrentQuestionUrl(section.segment, question.fieldName));
+			}
 		}
 	}
 	next();
