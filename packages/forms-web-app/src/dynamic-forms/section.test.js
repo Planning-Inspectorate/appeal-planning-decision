ce094d7714d11f7237075841a327d86f0d922906
Lucy Potter <si3rr4wow@gmail.com>
2024-08-29 09:05:32 +0100

    refactor(): update section tests

--- packages/forms-web-app/src/dynamic-forms/section.test.js
+++ packages/forms-web-app/src/dynamic-forms/section.test.js
@@ -3,7 +3,8 @@ const RequiredFileUploadValidator = require('./validator/required-file-upload-va
 const RequiredValidator = require('./validator/required-validator');
 
 const mockQuestion = {
-	fieldName: 'visitFrequently'
+	fieldName: 'visitFrequently',
+	shouldDisplay: () => true
 };
 
 describe('./src/dynamic-forms/section.js', () => {
@@ -31,6 +32,175 @@ describe('./src/dynamic-forms/section.js', () => {
 			expect(section.questions.length).toEqual(1);
 			expect(section.questions[0]).toEqual(mockQuestion);
 		});
+	});
+
+	describe('getStatus', () => {
+		it('should return NOT_STARTED when no answers are given', () => {
+			const mockJourneyResponse = {
+				answers: {}
+			};
+			const section = new Section();
+			section.addQuestion(mockQuestion);
+			const result = section.getStatus(mockJourneyResponse);
+			expect(result).toBe(SECTION_STATUS.NOT_STARTED);
+			const isComplete = section.isComplete(mockJourneyResponse);
+			expect(isComplete).toBe(false);
+		});
+
+		it('should return IN_PROGRESS when at least one answer is given', () => {
+			const mockJourneyResponse = {
+				answers: {
+					visitFrequently: 'Answer 1'
+				}
+			};
+
+			const section = new Section();
+			section.addQuestion(mockQuestion);
+			const result = section.getStatus(mockJourneyResponse);
+			expect(result).toBe(SECTION_STATUS.IN_PROGRESS);
+			const isComplete = section.isComplete(mockJourneyResponse);
+			expect(isComplete).toBe(false);
+		});
+
+		it('should return COMPLETE when all required answers are given', () => {
+			const mockJourneyResponse = {
+				answers: {
+					visitFrequently: 'Answer 1',
+					anotherFieldName: 'Answer 2'
+				}
+			};
+
+			const requiredQuestion = {
+				fieldName: 'visitFrequently',
+				validators: [new RequiredValidator()],
+				shouldDisplay: () => true
+			};
+
+			const anotherRequiredQuestion = {
+				fieldName: 'anotherFieldName',
+				validators: [new RequiredFileUploadValidator()],
+				shouldDisplay: () => true
+			};
+
+			const notARequiredQuestion = {
+				fieldName: 'someQuestion',
+				validators: [],
+				shouldDisplay: () => true
+			};
+
+			const section = new Section();
+			section.addQuestion(requiredQuestion);
+			section.addQuestion(anotherRequiredQuestion);
+			section.addQuestion(notARequiredQuestion);
+
+			const result = section.getStatus(mockJourneyResponse);
+			expect(result).toBe(SECTION_STATUS.COMPLETE);
+			const isComplete = section.isComplete(mockJourneyResponse);
+			expect(isComplete).toBe(true);
+		});
+	});
+	it('should return COMPLETE when a file upload question has files associated with it', () => {
+		const mockJourneyResponse = {
+			answers: {
+				visitFrequently: 'Answer 1',
+				anotherFieldName: 'yes',
+				SubmissionDocumentUpload: [
+					{
+						type: 'testDocType'
+					}
+				]
+			}
+		};
+
+		const requiredQuestion = {
+			fieldName: 'visitFrequently',
+			validators: [new RequiredValidator()],
+			shouldDisplay: () => true
+		};
+
+		const anotherRequiredQuestion = {
+			fieldName: 'anotherFieldName',
+			validators: [new RequiredFileUploadValidator()],
+			documentType: {
+				name: 'testDocType'
+			},
+			shouldDisplay: () => true
+		};
+
+		const notARequiredQuestion = {
+			fieldName: 'someQuestion',
+			validators: [],
+			shouldDisplay: () => true
+		};
+
+		const section = new Section();
+		section.addQuestion(requiredQuestion);
+		section.addQuestion(anotherRequiredQuestion);
+		section.addQuestion(notARequiredQuestion);
+
+		const result = section.getStatus(mockJourneyResponse);
+		expect(result).toBe(SECTION_STATUS.COMPLETE);
+		const isComplete = section.isComplete(mockJourneyResponse);
+		expect(isComplete).toBe(true);
+	});
+	it('should not return COMPLETE when a file upload question has no files associated with it', () => {
+		const mockJourneyResponse = {
+			answers: {
+				visitFrequently: 'Answer 1',
+				anotherFieldName: 'yes',
+				SubmissionDocumentUpload: [
+					{
+						type: 'notTestDocType'
+					}
+				]
+			}
+		};
+
+		const requiredQuestion = {
+			fieldName: 'visitFrequently',
+			validators: [new RequiredValidator()],
+			shouldDisplay: () => true
+		};
+
+		const anotherRequiredQuestion = {
+			fieldName: 'anotherFieldName',
+			validators: [new RequiredFileUploadValidator()],
+			documentType: {
+				name: 'testDocType'
+			},
+			shouldDisplay: () => true
+		};
+
+		const notARequiredQuestion = {
+			fieldName: 'someQuestion',
+			validators: [],
+			shouldDisplay: () => true
+		};
+
+		const section = new Section();
+		section.addQuestion(requiredQuestion);
+		section.addQuestion(anotherRequiredQuestion);
+		section.addQuestion(notARequiredQuestion);
+
+		const result = section.getStatus(mockJourneyResponse);
+		expect(result).not.toBe(SECTION_STATUS.COMPLETE);
+		const isComplete = section.isComplete(mockJourneyResponse);
+		expect(isComplete).toBe(false);
+	});
+	it('should do a noop', () => {
+		const section = new Section();
+		const question = {
+			title: 'ice breaker',
+			question: 'Do you come here often?',
+			description: 'Chit chat',
+			type: 'Boolean',
+			fieldName: 'visitFrequently',
+			shouldDisplay: () => true
+		};
+		section.addQuestion(question);
+		section.withCondition(() => true);
+		expect(section.questions.length).toEqual(1);
+		expect(section.questions[0]).toEqual(question);
 	});
 
 	describe('withCondition', () => {
@@ -78,164 +248,5 @@ describe('./src/dynamic-forms/section.js', () => {
 			expect(section.questions[4].shouldDisplay()).toEqual(true);
 			expect(section.questions[5].shouldDisplay()).toEqual(false);
 		});
-	});
-
-	describe('getStatus', () => {
-		it('should return NOT_STARTED when no answers are given', () => {
-			const mockJourneyResponse = {
-				answers: {}
-			};
-			const section = new Section();
-			section.addQuestion(mockQuestion);
-			const result = section.getStatus(mockJourneyResponse);
-			expect(result).toBe(SECTION_STATUS.NOT_STARTED);
-			const isComplete = section.isComplete(mockJourneyResponse);
-			expect(isComplete).toBe(false);
-		});
-
-		it('should return IN_PROGRESS when at least one answer is given', () => {
-			const mockJourneyResponse = {
-				answers: {
-					visitFrequently: 'Answer 1'
-				}
-			};
-
-			const section = new Section();
-			section.addQuestion(mockQuestion);
-			const result = section.getStatus(mockJourneyResponse);
-			expect(result).toBe(SECTION_STATUS.IN_PROGRESS);
-			const isComplete = section.isComplete(mockJourneyResponse);
-			expect(isComplete).toBe(false);
-		});
-
-		it('should return COMPLETE when all required answers are given', () => {
-			const mockJourneyResponse = {
-				answers: {
-					visitFrequently: 'Answer 1',
-					anotherFieldName: 'Answer 2'
-				}
-			};
-
-			const requiredQuestion = {
-				fieldName: 'visitFrequently',
-				validators: [new RequiredValidator()]
-			};
-
-			const anotherRequiredQuestion = {
-				fieldName: 'anotherFieldName',
-				validators: [new RequiredFileUploadValidator()]
-			};
-
-			const notARequiredQuestion = {
-				fieldName: 'someQuestion',
-				validators: []
-			};
-
-			const section = new Section();
-			section.addQuestion(requiredQuestion);
-			section.addQuestion(anotherRequiredQuestion);
-			section.addQuestion(notARequiredQuestion);
-
-			const result = section.getStatus(mockJourneyResponse);
-			expect(result).toBe(SECTION_STATUS.COMPLETE);
-			const isComplete = section.isComplete(mockJourneyResponse);
-			expect(isComplete).toBe(true);
-		});
-	});
-	it('should return COMPLETE when a file upload question has files associated with it', () => {
-		const mockJourneyResponse = {
-			answers: {
-				visitFrequently: 'Answer 1',
-				anotherFieldName: 'yes',
-				SubmissionDocumentUpload: [
-					{
-						type: 'testDocType'
-					}
-				]
-			}
-		};
-
-		const requiredQuestion = {
-			fieldName: 'visitFrequently',
-			validators: [new RequiredValidator()]
-		};
-
-		const anotherRequiredQuestion = {
-			fieldName: 'anotherFieldName',
-			validators: [new RequiredFileUploadValidator()],
-			documentType: {
-				name: 'testDocType'
-			}
-		};
-
-		const notARequiredQuestion = {
-			fieldName: 'someQuestion',
-			validators: []
-		};
-
-		const section = new Section();
-		section.addQuestion(requiredQuestion);
-		section.addQuestion(anotherRequiredQuestion);
-		section.addQuestion(notARequiredQuestion);
-
-		const result = section.getStatus(mockJourneyResponse);
-		expect(result).toBe(SECTION_STATUS.COMPLETE);
-		const isComplete = section.isComplete(mockJourneyResponse);
-		expect(isComplete).toBe(true);
-	});
-	it('should not return COMPLETE when a file upload question has no files associated with it', () => {
-		const mockJourneyResponse = {
-			answers: {
-				visitFrequently: 'Answer 1',
-				anotherFieldName: 'yes',
-				SubmissionDocumentUpload: [
-					{
-						type: 'notTestDocType'
-					}
-				]
-			}
-		};
-
-		const requiredQuestion = {
-			fieldName: 'visitFrequently',
-			validators: [new RequiredValidator()]
-		};
-
-		const anotherRequiredQuestion = {
-			fieldName: 'anotherFieldName',
-			validators: [new RequiredFileUploadValidator()],
-			documentType: {
-				name: 'testDocType'
-			}
-		};
-
-		const notARequiredQuestion = {
-			fieldName: 'someQuestion',
-			validators: []
-		};
-
-		const section = new Section();
-		section.addQuestion(requiredQuestion);
-		section.addQuestion(anotherRequiredQuestion);
-		section.addQuestion(notARequiredQuestion);
-
-		const result = section.getStatus(mockJourneyResponse);
-		expect(result).not.toBe(SECTION_STATUS.COMPLETE);
-		const isComplete = section.isComplete(mockJourneyResponse);
-		expect(isComplete).toBe(false);
-	});
-	it('should do a noop', () => {
-		const section = new Section();
-		const question = {
-			title: 'ice breaker',
-			question: 'Do you come here often?',
-			description: 'Chit chat',
-			type: 'Boolean',
-			fieldName: 'visitFrequently'
-		};
-		section.addQuestion(question);
-		section.withCondition(() => true);
-		expect(section.questions.length).toEqual(1);
-		expect(section.questions[0]).toEqual(question);
 	});
 });
