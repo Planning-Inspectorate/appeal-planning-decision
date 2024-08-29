0d86551ce8f0eb2ffc122063f4e49f66f18ede47
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2024-08-29 07:36:49 +0100

    comparator opertor ===  added

--- test-packages/platform-feature-tests/cypress/support/flows/appeal.js
+++ test-packages/platform-feature-tests/cypress/support/flows/appeal.js
@@ -4,19 +4,19 @@ function submitAppealFlow(appealOptions) {
 	const { statusOfOriginalApplication, typeOfDecisionRequested, statusOfPlanningObligation, planning, context, prepareAppealData } =
 		appealOptions;
 
-	if (['granted', 'refused', 'no decision'].includes(statusOfOriginalApplication) == false) {
+	if (['granted', 'refused', 'no decision'].includes(statusOfOriginalApplication) === false) {
 		throw new Error(
 			`The status of original application "${statusOfOriginalApplication}" is not supported!`
 		);
 	}
 
-	if (['written', 'hearing', 'inquiry'].includes(typeOfDecisionRequested) == false) {
+	if (['written', 'hearing', 'inquiry'].includes(typeOfDecisionRequested) === false) {
 		throw new Error(
 			`The stype of decision requested "${typeOfDecisionRequested}" is not supported!`
 		);
 	}
 
-	if (['not started', 'finalised', 'in draft'].includes(statusOfPlanningObligation) == false) {
+	if (['not started', 'finalised', 'in draft'].includes(statusOfPlanningObligation) === false) {
 		throw new Error(
 			`The status of planning obligation "${statusOfPlanningObligation}" is not supported!`
 		);
