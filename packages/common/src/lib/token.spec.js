9baea890ea69f53113f746312eb02d2d4cf06fcd
Tom Pickering <topickering@gmail.com>
2024-08-29 11:21:40 +0100

    feat(a2-451): amend code format

--- packages/common/src/lib/token.spec.js
+++ packages/common/src/lib/token.spec.js
@@ -4,10 +4,11 @@ describe('lib/token', () => {
 	describe('createToken', () => {
 		it('should return a token as a string, 5 characters in length, containing only digits, and containing no zeros', async () => {
 			const tokenValidationRegex = new RegExp(/^[\w_]{5}$/g);
-			const vowelOmissionValidationRegex = new RegExp(/^[AEIOUaeiou0]{5}$/g);
+			const vowelOmissionValidationRegex = new RegExp(/^[AEIOUaeiou0-9]{5}$/g);
 
 			for (let i = 0; i < 1000; i++) {
 				const token = createToken();
+				console.log(token);
 
 				expect(typeof token).toBe('string');
 				expect(token.length).toBe(5);
