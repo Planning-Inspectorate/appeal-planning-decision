ca2808e71812887ded36d01b5f4f332732bb934d
Tom Pickering <topickering@gmail.com>
2024-09-10 16:32:08 +0100

    feat(a2-249): final comment document upload

--- packages/common/src/document-types.js
+++ packages/common/src/document-types.js
@@ -609,6 +609,17 @@ const documentTypes = {
 		publiclyAccessible: false,
 		horizonDocumentType: '', // Does not exist in horizon
 		horizonDocumentGroupType: '' // Does not exist in horizon
+	},
+	uploadAppellantFinalCommentDocuments: {
+		name: 'uploadAppellantFinalCommentDocuments',
+		dataModelName: '', // To be added once data model confirmed
+		multiple: true,
+		displayName: '',
+		involvement: '',
+		owner: appellantOwner,
+		publiclyAccessible: false,
+		horizonDocumentType: '', // Does not exist in horizon
+		horizonDocumentGroupType: '' // Does not exist in horizon
 	}
 };
 
