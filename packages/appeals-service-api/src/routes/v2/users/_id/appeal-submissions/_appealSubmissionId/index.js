7005c37c8de95b5e8fc69ec24590ab8f95f9f209
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-09-18 15:16:38 +0100

    chore: remove unused routes

--- packages/appeals-service-api/src/routes/v2/users/_id/appeal-submissions/_appealSubmissionId/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getUserAppealSubmission } = require('./controller');
-const asyncHandler = require('@pins/common/src/middleware/async-handler');
-const router = express.Router({ mergeParams: true });
-
-router.get('/', asyncHandler(getUserAppealSubmission));
-
-module.exports = { router };
