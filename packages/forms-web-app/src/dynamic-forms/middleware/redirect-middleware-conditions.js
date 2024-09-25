70743c44be91cc70045f04915e20ccfbd06c88f9
Tom Pickering <topickering@gmail.com>
2024-09-25 17:20:08 +0100

    fix(a2-273): add navigation conditions for final comment journeys

--- packages/forms-web-app/src/dynamic-forms/middleware/redirect-middleware-conditions.js
+++ packages/forms-web-app/src/dynamic-forms/middleware/redirect-middleware-conditions.js
@@ -11,4 +11,42 @@ const skipIfNoAdditionalDocuments = (question, journeyResponse) => {
 	return false;
 };
 
-module.exports = { skipIfNoAdditionalDocuments };
+/**
+ * @param {import('../section').Question} question
+ * @param {import('../journey-response').JourneyResponse} journeyResponse
+ * @returns {boolean}
+ */
+const appellantFinalCommentSkipConditions = (question, journeyResponse) => {
+	if (
+		question.fieldName === 'appellantFinalCommentDetails' ||
+		question.fieldName === 'appellantFinalCommentDocuments'
+	) {
+		return journeyResponse.answers['appellantFinalComment'] !== 'yes';
+	} else if (question.fieldName === 'uploadAppellantFinalCommentDocuments') {
+		return journeyResponse.answers['appellantFinalCommentDocuments'] !== 'yes';
+	}
+	return false;
+};
+
+/**
+ * @param {import('../section').Question} question
+ * @param {import('../journey-response').JourneyResponse} journeyResponse
+ * @returns {boolean}
+ */
+const lpaFinalCommentSkipConditions = (question, journeyResponse) => {
+	if (
+		question.fieldName === 'lpaFinalCommentDetails' ||
+		question.fieldName === 'lpaFinalCommentDocuments'
+	) {
+		return journeyResponse.answers['lpaFinalComment'] !== 'yes';
+	} else if (question.fieldName === 'uploadLPAFinalCommentDocuments') {
+		return journeyResponse.answers['lpaFinalCommentDocuments'] !== 'yes';
+	}
+	return false;
+};
+
+module.exports = {
+	skipIfNoAdditionalDocuments,
+	appellantFinalCommentSkipConditions,
+	lpaFinalCommentSkipConditions
+};
