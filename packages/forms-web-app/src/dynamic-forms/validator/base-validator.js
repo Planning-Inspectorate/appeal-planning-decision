25e0e2beca061888ee84be7ffb605c8fe16c3bc5
Lucy Potter <si3rr4wow@gmail.com>
2024-09-30 15:13:37 +0100

    refactor(a2-1099): checkbox

--- packages/forms-web-app/src/dynamic-forms/validator/base-validator.js
+++ packages/forms-web-app/src/dynamic-forms/validator/base-validator.js
@@ -6,12 +6,24 @@ class BaseValidator {
 	/**
 	 * @type {string} error message to display to user
 	 */
+	// @ts-ignore
 	errorMessage;
 
 	constructor() {
 		if (this.constructor == BaseValidator) {
 			throw new Error("Abstract classes can't be instantiated.");
 		}
+	}
+
+	/**
+	 * @param {import('../question')} _question
+	 * @param {import('../journey-response').JourneyResponse} _journeyResponse
+	 * @returns {import('express-validator').ValidationChain | import('express-validator').ValidationChain[]}
+	 */
+	validate(_question, _journeyResponse) {
+		throw new Error(
+			'BaseValidator validation called directly. BaseValidator is an abstract class. This method should be overridden'
+		);
 	}
 }
 
