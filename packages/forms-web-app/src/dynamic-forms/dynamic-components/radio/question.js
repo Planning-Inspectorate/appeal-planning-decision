044c525f678ec6467f1344a667804317df00e146
Lucy Potter <si3rr4wow@gmail.com>
2024-09-30 16:16:22 +0100

    refactor(a2-1099): boolean and radio

--- packages/forms-web-app/src/dynamic-forms/dynamic-components/radio/question.js
+++ packages/forms-web-app/src/dynamic-forms/dynamic-components/radio/question.js
@@ -1,4 +1,13 @@
-const OptionsQuestion = require('../../options-question');
+const {
+	OptionsQuestion,
+	optionIsDivider,
+	conditionalIsJustHTML
+} = require('../../options-question');
+
+/**
+ * @typedef {import('../../options-question').OptionsViewModel} OptionsViewModel
+ * @typedef {OptionsViewModel & { question: OptionsViewModel['question'] & { label?: string, legend?: string } }} RadioViewModel
+ */
 
 class RadioQuestion extends OptionsQuestion {
 	/**
@@ -14,36 +23,44 @@ class RadioQuestion extends OptionsQuestion {
 	 * @param {string} [params.label]
 	 * @param {string} [params.html]
 	 * @param {string} [params.legend] - optional legend, used instead of h1
-	 * @param {Array.<import('../../options-question').Option>} params.options
+	 * @param {Array.<import('../../options-question').Option>} [params.options]
 	 * @param {Array.<import('../../question').BaseValidator>} [params.validators]
+	 *
+	 * @param {Record<string, Function>} [methodOverrides]
 	 */
-	constructor({
-		title,
-		question,
-		fieldName,
-		viewFolder,
-		url,
-		hint,
-		pageTitle,
-		description,
-		label,
-		html,
-		legend,
-		options,
-		validators
-	}) {
-		super({
+	constructor(
+		{
 			title,
 			question,
-			viewFolder: !viewFolder ? 'radio' : viewFolder,
 			fieldName,
+			viewFolder,
 			url,
 			hint,
 			pageTitle,
 			description,
+			label,
+			html,
 			options,
-			validators
-		});
+			validators,
+			legend
+		},
+		methodOverrides
+	) {
+		super(
+			{
+				title,
+				question,
+				viewFolder: !viewFolder ? 'radio' : viewFolder,
+				fieldName,
+				url,
+				hint,
+				pageTitle,
+				description,
+				options,
+				validators
+			},
+			methodOverrides
+		);
 
 		this.html = html;
 		this.label = label;
@@ -52,8 +69,14 @@ class RadioQuestion extends OptionsQuestion {
 
 	/**
 	 * adds label property to view model
+	 * @param {import('../../section').Section} section - the current section
+	 * @param {import('../../journey').Journey} journey - the journey we are in
+	 * @param {Record<string, unknown>} [customViewData] additional data to send to view
+	 * @param {Record<string, unknown>} [payload]
+	 * @returns {RadioViewModel}
 	 */
 	prepQuestionForRendering(section, journey, customViewData, payload) {
+		/** @type {RadioViewModel} */
 		let viewModel = super.prepQuestionForRendering(section, journey, customViewData, payload);
 		viewModel.question.label = this.label;
 		viewModel.question.legend = this.legend;
@@ -62,21 +85,37 @@ class RadioQuestion extends OptionsQuestion {
 
 	/**
 	 * returns the formatted answers values to be used to build task list elements
-	 * @param {Object} answer
-	 * @param {Journey} journey
+	 * @param {string | import('../../options-question').OptionWithoutDivider} answer
+	 * @param {import('../../journey').Journey} journey
 	 * @param {String} sectionSegment
-	 * @returns {Array.<Object>}
+	 * @returns {Array<{
+	 *   key: string;
+	 *   value: string | Object;
+	 *   action: {
+	 *     href: string;
+	 *     text: string;
+	 *     visuallyHiddenText: string;
+	 *   };
+	 * }>}
 	 */
 	formatAnswerForSummary(sectionSegment, journey, answer) {
-		if (answer?.conditional) {
-			const selectedOption = this.options.find((option) => option.value === answer.value);
-			const conditionalAnswerText = selectedOption.conditional?.label
-				? `${selectedOption.conditional.label} ${answer.conditional}`
-				: answer.conditional;
-			const formattedAnswer = [selectedOption.text, conditionalAnswerText].join('\n');
-			return super.formatAnswerForSummary(sectionSegment, journey, formattedAnswer, false);
-		}
-		return super.formatAnswerForSummary(sectionSegment, journey, answer);
+		if (typeof answer === 'string')
+			return super.formatAnswerForSummary(sectionSegment, journey, answer);
+
+		/** @type {import('../../options-question').OptionWithoutDivider | undefined} */
+		// @ts-ignore
+		const selectedOption = this.options.find(
+			(option) => !optionIsDivider(option) && option.value === answer.value
+		);
+
+		const conditionalAnswerText = (() => {
+			if (!selectedOption?.conditional || conditionalIsJustHTML(selectedOption.conditional))
+				return answer.conditional;
+			return `${selectedOption.conditional.label} ${answer.conditional}`;
+		})();
+
+		const formattedAnswer = [selectedOption?.text, conditionalAnswerText].join('\n');
+		return super.formatAnswerForSummary(sectionSegment, journey, formattedAnswer, false);
 	}
 }
 
