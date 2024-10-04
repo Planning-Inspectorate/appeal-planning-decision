f27736018eb777f697373b04434f1ef1cbec7a25
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-10-04 13:57:26 +0100

    chore: remove v1 final comments

--- packages/forms-web-app/src/routes/final-comment/final-comment-submitted.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const finalCommentSubmittedController = require('../../controllers/final-comment/final-comment-submitted');
-
-const router = express.Router();
-
-router.get('/final-comment-submitted', finalCommentSubmittedController.getFinalCommentSubmitted);
-
-module.exports = router;
