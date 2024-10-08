70c2ecae5fa59bddf57f6eff4bc7bfb4d35f295b
Tom Pickering <topickering@gmail.com>
2024-10-08 12:28:04 +0100

    feat(a2-1191): add rule 6 login routes

---
+++ packages/forms-web-app/src/routes/rule-6-appeals/request-new-code.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const {
+	getRequestNewCode,
+	postRequestNewCode
+} = require('../../controllers/common/request-new-code');
+
+const {
+	VIEW: {
+		APPELLANT_SUBMISSION: { REQUEST_NEW_CODE, NEED_NEW_CODE, ENTER_CODE }
+	}
+} = require('#lib/views');
+
+const router = express.Router();
+
+router.get('/request-new-code', getRequestNewCode(REQUEST_NEW_CODE));
+router.post('/request-new-code', postRequestNewCode(ENTER_CODE));
+router.get('/need-new-code', getRequestNewCode(NEED_NEW_CODE));
+router.post('/need-new-code', postRequestNewCode(ENTER_CODE));
+
+module.exports = router;
