c718e5e902abef1e29499e86287a3b0289ea5b8e
Tom Pickering <topickering@gmail.com>
2024-10-10 09:58:09 +0100

    feat(a2-1191): rule 6 enter code

--- packages/forms-web-app/src/routes/rule-6/enter-code.js
+++ packages/forms-web-app/src/routes/rule-6/enter-code.js
@@ -1,32 +1,32 @@
-// const express = require('express');
-// const { rules: ruleEnterCode } = require('../../validators/common/enter-code');
-// const { validationErrorHandler } = require('../../validators/validation-error-handler');
-// const { getEnterCodeLPA, postEnterCodeLPA } = require('../../controllers/rule-6/enter-code');
-// const {
-// 	VIEW: {
-// 		RULE_6_APPEALS: {
-// 			ENTER_CODE,
-// 			CODE_EXPIRED,
-// 			NEED_NEW_CODE,
-// 			REQUEST_NEW_CODE,
-// 			DASHBOARD,
-// 			EMAIL_ADDRESS
-// 		}
-// 	}
-// } = require('../../lib/views');
+const express = require('express');
+const { rules: ruleEnterCode } = require('../../validators/common/enter-code');
+const { validationErrorHandler } = require('../../validators/validation-error-handler');
+const { getEnterCodeR6, postEnterCodeR6 } = require('../../controllers/rule-6/enter-code');
+const {
+	VIEW: {
+		RULE_6_APPEALS: {
+			ENTER_CODE,
+			CODE_EXPIRED,
+			NEED_NEW_CODE,
+			REQUEST_NEW_CODE,
+			DASHBOARD,
+			EMAIL_ADDRESS
+		}
+	}
+} = require('../../lib/views');
 
-// const router = express.Router();
+const router = express.Router();
 
-// const views = {
-// 	ENTER_CODE,
-// 	EMAIL_ADDRESS,
-// 	CODE_EXPIRED,
-// 	NEED_NEW_CODE,
-// 	REQUEST_NEW_CODE,
-// 	DASHBOARD
-// };
+const views = {
+	ENTER_CODE,
+	EMAIL_ADDRESS,
+	CODE_EXPIRED,
+	NEED_NEW_CODE,
+	REQUEST_NEW_CODE,
+	DASHBOARD
+};
 
-// router.get('/enter-code/:id', validationErrorHandler, getEnterCodeR6(views));
-// router.post('/enter-code/:id', ruleEnterCode(), validationErrorHandler, postEnterCodeR6(views));
+router.get('/enter-code/:id', validationErrorHandler, getEnterCodeR6(views));
+router.post('/enter-code/:id', ruleEnterCode(), validationErrorHandler, postEnterCodeR6(views));
 
-// module.exports = router;
+module.exports = router;
