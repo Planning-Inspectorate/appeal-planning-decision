b477a5db31f0efe43f3d66712cda904b8645c27f
Dean Hasley <dhasley1990@gmail.com>
2024-10-15 17:14:36 +0100

    feat(A2-469): add final comment seed data for lpa and appellant

--- packages/database/src/seed/data-dev.js
+++ packages/database/src/seed/data-dev.js
@@ -167,6 +167,13 @@ const appealStatementIds = {
 	appealStatementTwo: 'd24447a2-ad41-42b7-be86-7a222ae57448'
 };
 
+const appealFinalCommentIds = {
+	appealFinalCommentOne: '16258e95-cd25-47ec-8953-8674c74dbc79',
+	appealFinalCommentTwo: 'e2860281-647f-46ee-8099-d53d32b43daf',
+	appealFinalCommentThree: 'c04acd1d-be07-4150-ba49-ecda79343ca5',
+	appealFinalCommentFour: 'c9304307-a57d-4411-9334-1ce831179a53'
+};
+
 const rule6Documents = {
 	proofEvidenceSubmitted: false,
 	proofEvidenceReceived: false,
@@ -401,6 +408,8 @@ const appealCases = [
 		lpaQuestionnaireCreatedDate: pickRandom(datesNMonthsAgo(1)),
 		lpaQuestionnairePublishedDate: pickRandom(datesNMonthsAgo(1)),
 		lpaQuestionnaireSubmittedDate: new Date(),
+		lpaFinalCommentsPublished: true,
+		appellantFinalCommentsSubmitted: true,
 		caseValidDate: new Date(),
 		lpaStatementPublished: true,
 		rule6StatementPublished: true,
@@ -730,6 +739,14 @@ const serviceUsers = [
 		id: '123451',
 		serviceUserType: 'Appellant',
 		caseReference: caseReferences.caseReferenceOne,
+		firstName: 'Appellant',
+		lastName: 'One'
+	},
+	{
+		internalId: '19d01551-e0cb-414f-95d9-fd71422c9a89',
+		id: '123451',
+		serviceUserType: 'Appellant',
+		caseReference: '1000014',
 		firstName: 'Appellant',
 		lastName: 'One'
 	},
@@ -1151,6 +1168,104 @@ const statementDocuments = [
 ];
 
 /**
+ * @type {import('@prisma/client').Prisma.AppealFinalCommentCreateInput[]}
+ */
+const appealFinalComments = [
+	{
+		id: appealFinalCommentIds.appealFinalCommentOne,
+		lpaCode: 'Q9999',
+		submittedDate: pickRandom(datesNMonthsAgo(0.5)),
+		comments:
+			'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.',
+		wantsFinalComment: true,
+		AppealCase: {
+			connect: {
+				caseReference: '1000014'
+			}
+		}
+	},
+	{
+		id: appealFinalCommentIds.appealFinalCommentTwo,
+		lpaCode: 'Q9999',
+		submittedDate: pickRandom(datesNMonthsAgo(0.5)),
+		comments:
+			'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.',
+		wantsFinalComment: true,
+		AppealCase: {
+			connect: {
+				caseReference: caseReferences.caseReferenceOne
+			}
+		}
+	},
+	{
+		id: appealFinalCommentIds.appealFinalCommentThree,
+		submittedDate: pickRandom(datesNMonthsAgo(0.5)),
+		comments:
+			'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.',
+		wantsFinalComment: true,
+		AppealCase: {
+			connect: {
+				caseReference: '1000014'
+			}
+		},
+		ServiceUser: {
+			connect: {
+				internalId: '19d01551-e0cb-414f-95d9-fd71422c9a89'
+			}
+		}
+	},
+	{
+		id: appealFinalCommentIds.appealFinalCommentFour,
+		submittedDate: pickRandom(datesNMonthsAgo(0.5)),
+		comments:
+			'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.',
+		wantsFinalComment: true,
+		AppealCase: {
+			connect: {
+				caseReference: caseReferences.caseReferenceOne
+			}
+		},
+		ServiceUser: {
+			connect: {
+				internalId: '19d01551-e0cb-414f-95d9-fd71422c9a89'
+			}
+		}
+	}
+];
+
+/**
+ * @type {import('@prisma/client').Prisma.FinalCommentDocumentCreateInput[]}
+ */
+const finalCommentDocuments = [
+	{
+		id: '319612c2-9cad-48b3-bfde-faeffba61555',
+		AppealFinalComment: {
+			connect: {
+				id: appealFinalCommentIds.appealFinalCommentOne
+			}
+		},
+		Document: {
+			connect: {
+				id: '35cb4ad1-9ba3-43fb-b102-e845804ba2f7'
+			}
+		}
+	},
+	{
+		id: 'bdea7f30-d25f-49b2-8b0b-f5b9d38c16a7',
+		AppealFinalComment: {
+			connect: {
+				id: appealFinalCommentIds.appealFinalCommentThree
+			}
+		},
+		Document: {
+			connect: {
+				id: 'a1b60dc2-2253-48eb-aaea-4ec665f15fbd'
+			}
+		}
+	}
+];
+
+/**
  * @type {import('@prisma/client').Prisma.AppellantSubmissionCreateInput[]}
  */
 const appellantSubmissions = [
@@ -1398,6 +1513,22 @@ async function seedDev(dbClient) {
 			create: statementDocument,
 			update: statementDocument,
 			where: { id: statementDocument.id }
+		});
+	}
+
+	for (const appealFinalComment of appealFinalComments) {
+		await dbClient.appealFinalComment.upsert({
+			create: appealFinalComment,
+			update: appealFinalComment,
+			where: { id: appealFinalComment.id }
+		});
+	}
+
+	for (const finalCommentDocument of finalCommentDocuments) {
+		await dbClient.finalCommentDocument.upsert({
+			create: finalCommentDocument,
+			update: finalCommentDocument,
+			where: { id: finalCommentDocument.id }
 		});
 	}
 
