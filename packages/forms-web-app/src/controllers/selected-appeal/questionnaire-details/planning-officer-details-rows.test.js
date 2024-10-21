b67a3aeb8827421fac5033b560d4ba3a72d75328
Ed Packard <edward.packard@kinandcarta.com>
2024-10-21 16:12:26 +0100

    fix: correct constant for appeal document type

--- packages/forms-web-app/src/controllers/selected-appeal/questionnaire-details/planning-officer-details-rows.test.js
+++ packages/forms-web-app/src/controllers/selected-appeal/questionnaire-details/planning-officer-details-rows.test.js
@@ -7,22 +7,33 @@ describe('planningOfficerReportRows', () => {
 		expect(rows.length).toEqual(12);
 	});
 
-	it('should show a document', () => {
+	it('should show documents', () => {
 		const rows = planningOfficerReportRows({
 			Documents: [
 				{
 					id: 1,
 					documentType: APPEAL_DOCUMENT_TYPE.PLANNING_OFFICER_REPORT,
 					filename: 'test.txt'
+				},
+				{
+					id: 2,
+					documentType: APPEAL_DOCUMENT_TYPE.DEVELOPMENT_PLAN_POLICIES,
+					filename: 'test2.txt'
 				}
 			]
 		});
 
 		expect(rows[0].condition()).toEqual(true);
 		expect(rows[0].isEscaped).toEqual(true);
-		expect(rows[0].keyText).toEqual('Uploaded planning officer’s report');
+		expect(rows[0].keyText).toEqual("Uploaded planning officer's report");
 		expect(rows[0].valueText).toEqual(
 			'<a href="/published-document/1" class="govuk-link">test.txt</a>'
+		);
+		expect(rows[1].condition()).toEqual(true);
+		expect(rows[1].isEscaped).toEqual(true);
+		expect(rows[1].keyText).toEqual('Uploaded policies from statutory development plan');
+		expect(rows[1].valueText).toEqual(
+			'<a href="/published-document/2" class="govuk-link">test2.txt</a>'
 		);
 	});
 });
