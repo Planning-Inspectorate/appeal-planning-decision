2620b17d7adce98a758a2f2f99adfe862a60599d
Tom Pickering <topickering@gmail.com>
2024-10-22 14:27:29 +0100

    fix: routes for code expired

--- packages/forms-web-app/src/routes/rule-6/code-expired.js
+++ packages/forms-web-app/src/routes/rule-6/code-expired.js
@@ -13,7 +13,9 @@ const {
 
 const router = express.Router();
 
+router.get('/code-expired', getRequestNewCode(CODE_EXPIRED));
 router.get('/code-expired/:id', getRequestNewCode(CODE_EXPIRED));
+router.post('/code-expired', postRequestNewCode(ENTER_CODE));
 router.post('/code-expired/:id', postRequestNewCode(ENTER_CODE));
 
 module.exports = router;
