21ee0926bc13ad58ccda78083cc38d0121e368b9
Tom Pickering <topickering@gmail.com>
2024-10-22 16:44:58 +0100

    fix(a2-1236): remove unneeded routes

--- packages/forms-web-app/src/routes/rule-6/code-expired.js
+++ packages/forms-web-app/src/routes/rule-6/code-expired.js
@@ -14,8 +14,6 @@ const {
 const router = express.Router();
 
 router.get('/code-expired', getRequestNewCode(CODE_EXPIRED));
-router.get('/code-expired/:id', getRequestNewCode(CODE_EXPIRED));
 router.post('/code-expired', postRequestNewCode(ENTER_CODE));
-router.post('/code-expired/:id', postRequestNewCode(ENTER_CODE));
 
 module.exports = router;
--- packages/forms-web-app/src/routes/rule-6/need-new-code.js
+++ packages/forms-web-app/src/routes/rule-6/need-new-code.js
@@ -12,8 +12,6 @@ const {
 const needViews = { NEED_NEW_CODE, ENTER_CODE };
 
 router.get('/need-new-code', getNeedNewCode(needViews));
-router.get('/need-new-code/:id', getNeedNewCode(needViews));
 router.post('/need-new-code', postNeedNewCode(needViews));
-router.post('/need-new-code/:id', postNeedNewCode(needViews));
 
 module.exports = router;
