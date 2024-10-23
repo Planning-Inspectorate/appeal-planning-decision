efaa3ff4d7f02a62119dbe47a3f87dc998621adb
Ed Packard <edward.packard@kinandcarta.com>
2024-10-23 11:26:56 +0100

    feat(A2-745): update questionnaire constraintsRows logic

--- packages/forms-web-app/src/controllers/selected-appeal/questionnaire-details/constraints-details-rows.js
+++ packages/forms-web-app/src/controllers/selected-appeal/questionnaire-details/constraints-details-rows.js
@@ -4,7 +4,6 @@ const {
 	formatDocumentDetails,
 	documentExists
 } = require('@pins/common');
-const { LPA_USER_ROLE } = require('@pins/common/src/constants');
 const { APPEAL_DOCUMENT_TYPE } = require('pins-data-model');
 
 /**
@@ -14,15 +13,16 @@ const { APPEAL_DOCUMENT_TYPE } = require('pins-data-model');
 
 /**
  * @param {import('appeals-service-api').Api.AppealCaseDetailed} caseData
- * @param {AppealToUserRoles|LpaUserRole|null} user
+ * @param {AppealToUserRoles|LpaUserRole|null} _user
  * @returns {import("@pins/common/src/view-model-maps/rows/def").Rows}
  */
 
-exports.constraintsRows = (caseData, user) => {
+exports.constraintsRows = (caseData, _user) => {
 	const documents = caseData.Documents || [];
 
 	const affectedListedBuildings = caseData.AffectedListedBuildings;
 	const showAffectedListed = !!(affectedListedBuildings && affectedListedBuildings.length);
+	const affectedListedBuildingText = showAffectedListed ? 'Yes' : 'No';
 
 	const rows = [
 		// todo: s78 needs a type on relation
@@ -37,72 +37,82 @@ exports.constraintsRows = (caseData, user) => {
 		// 	condition: () => (caseData.changedListedBuildingNumber ? true : undefined)
 		// },
 		{
+			keyText: 'Is this the correct type of appeal',
+			valueText: formatYesOrNo(caseData, 'isCorrectAppealType'),
+			condition: () => conditionYesOrNo(caseData.isCorrectAppealType)
+		},
+		{
 			keyText: 'Affects a listed building',
-			valueText: 'Yes',
-			condition: () => showAffectedListed
+			valueText: affectedListedBuildingText,
+			condition: () => conditionYesOrNo(caseData.AffectedListedBuildings)
 		},
 		{
 			// todo: bring in listed building details after move of listed building data to sql
 			keyText: 'Listed building details',
-			valueText: affectedListedBuildings?.map((x) => x.listedBuildingReference).join('\n'),
+			valueText: affectedListedBuildings?.map((x) => x.listedBuildingReference).join('\n') || '',
 			condition: () => showAffectedListed
 		},
 		{
 			keyText: 'Affects a scheduled monument',
 			valueText: formatYesOrNo(caseData, 'scheduledMonument'),
-			condition: () => caseData.scheduledMonument
+			condition: () =>
+				caseData.scheduledMonument !== undefined && caseData.scheduledMonument !== null
 		},
 		{
 			keyText: 'Conservation area',
 			valueText: formatYesOrNo(caseData, 'conservationArea'),
-			condition: () => caseData.conservationArea
+			condition: () => conditionYesOrNo(caseData.conservationArea)
 		},
 		{
 			keyText: 'Uploaded conservation area map and guidance',
 			valueText: formatDocumentDetails(documents, APPEAL_DOCUMENT_TYPE.CONSERVATION_MAP),
-			condition: () => documentExists(documents, APPEAL_DOCUMENT_TYPE.CONSERVATION_MAP),
+			condition: () =>
+				!!caseData.conservationArea &&
+				documentExists(documents, APPEAL_DOCUMENT_TYPE.CONSERVATION_MAP),
 			isEscaped: true
 		},
 		{
 			keyText: 'Protected species',
 			valueText: formatYesOrNo(caseData, 'protectedSpecies'),
-			condition: () => caseData.protectedSpecies
+			condition: () => conditionYesOrNo(caseData.protectedSpecies)
 		},
 		{
 			keyText: 'Green belt',
 			valueText: formatYesOrNo(caseData, 'isGreenBelt'),
-			condition: () => caseData.isGreenBelt
+			condition: () => conditionYesOrNo(caseData.isGreenBelt)
 		},
 		{
 			keyText: 'Area of outstanding natural beauty',
 			valueText: formatYesOrNo(caseData, 'areaOutstandingBeauty'),
-			condition: () => caseData.areaOutstandingBeauty
+			condition: () => conditionYesOrNo(caseData.areaOutstandingBeauty)
 		},
 		{
 			keyText: 'Designated sites',
 			valueText: formatDesignations(caseData),
-			condition: () => caseData.designatedSites
+			condition: () => !!formatDesignations(caseData)
 		},
 		{
 			keyText: 'Tree Preservation Order',
 			valueText: formatYesOrNo(caseData, 'treePreservationOrder'),
-			condition: () => caseData.treePreservationOrder
+			condition: () => conditionYesOrNo(caseData.treePreservationOrder)
 		},
 		{
 			keyText: 'Uploaded Tree Preservation Order extent',
 			valueText: formatDocumentDetails(documents, APPEAL_DOCUMENT_TYPE.TREE_PRESERVATION_PLAN),
-			condition: () => documentExists(documents, APPEAL_DOCUMENT_TYPE.TREE_PRESERVATION_PLAN),
+			condition: () =>
+				!!caseData.treePreservationOrder &&
+				documentExists(documents, APPEAL_DOCUMENT_TYPE.TREE_PRESERVATION_PLAN),
 			isEscaped: true
 		},
 		{
 			keyText: 'Gypsy or Traveller',
 			valueText: formatYesOrNo(caseData, 'gypsyTraveller'),
-			condition: () => caseData.gypsyTraveller
+			condition: () => conditionYesOrNo(caseData.gypsyTraveller)
 		},
 		{
 			keyText: 'Public right of way',
 			valueText: formatYesOrNo(caseData, 'publicRightOfWay'),
-			condition: () => caseData.publicRightOfWay
+			condition: () => conditionYesOrNo(caseData.publicRightOfWay)
 		},
 		{
 			keyText: 'Uploaded definitive map and statement extract',
@@ -112,13 +122,13 @@ exports.constraintsRows = (caseData, user) => {
 		}
 	];
 
-	if (user === LPA_USER_ROLE) {
-		rows.unshift({
-			keyText: 'Is this the correct type of appeal',
-			valueText: formatYesOrNo(caseData, 'isCorrectAppealType'),
-			condition: () => caseData.isCorrectAppealType
-		});
-	}
-
 	return rows;
+};
+
+/**
+ * @param {any} input
+ * @returns {boolean}
+ */
+const conditionYesOrNo = (input) => {
+	return !(input === undefined || input === null);
 };
