76b3c4a62d7760e6d9fcd1cbebe1cd6e51552dd6
Dean Hasley <dhasley1990@gmail.com>
2024-10-23 15:30:31 +0100

    refactor(A2-1021): add appellant ownership check for short journeys

--- packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/appellant-proof-evidence-submission/index.js
+++ packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/appellant-proof-evidence-submission/index.js
@@ -8,6 +8,7 @@ const { AUTH } = require('@pins/common/src/constants');
 const config = require('../../../../../configuration/config');
 const { auth } = require('express-oauth2-jwt-bearer');
 const { validateToken } = require('@pins/common/src/middleware/validate-token');
+const appellantCanModifyCase = require('../appellant-can-modify-case');
 const asyncHandler = require('@pins/common/src/middleware/async-handler');
 const router = express.Router({ mergeParams: true });
 
@@ -29,7 +30,7 @@ router.use(
 
 // Appellant Proof Of Evidence routes
 router.get('/', asyncHandler(getAppellantProofOfEvidenceSubmission));
-router.post('/', asyncHandler(createAppellantProofOfEvidenceSubmission));
-router.patch('/', asyncHandler(patchAppellantProofOfEvidenceSubmission));
+router.post('/', appellantCanModifyCase, asyncHandler(createAppellantProofOfEvidenceSubmission));
+router.patch('/', appellantCanModifyCase, asyncHandler(patchAppellantProofOfEvidenceSubmission));
 
 module.exports = { router };
