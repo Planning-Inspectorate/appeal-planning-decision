1e25afdf99823e193bbcd03144f69d74942e5f30
Tom Pickering <topickering@gmail.com>
2024-10-25 10:00:18 +0100

    feat(a2-221): seed data for rule 6 dashboard

--- packages/database/src/seed/lpa-appeal-case-data-dev.js
+++ packages/database/src/seed/lpa-appeal-case-data-dev.js
@@ -208,6 +208,7 @@ const lpaAppealCaseData = [
 		statementDueDate: pickRandom(datesNMonthsAhead(1)),
 		finalCommentsDueDate: pickRandom(datesNMonthsAhead(1)),
 		interestedPartyRepsDueDate: pickRandom(datesNMonthsAhead(1)),
+		rule6StatementDueDate: pickRandom(datesNMonthsAhead(1)),
 		developmentDescription: 'test description',
 		isGreenBelt: true,
 		ownsSomeLand: true,
@@ -277,6 +278,7 @@ const lpaAppealCaseData = [
 		statementDueDate: pickRandom(datesNMonthsAgo(1)),
 		LPAStatementSubmitted: pickRandom(datesNMonthsAgo(1)),
 		appellantStatementSubmitted: pickRandom(datesNMonthsAgo(1)),
+		CaseType: { connect: { processCode: 'HAS' } },
 		CaseStatus: {
 			connect: { key: APPEAL_CASE_STATUS.ISSUE_DETERMINATION }
 		},
@@ -620,6 +622,7 @@ const lpaAppealCaseData = [
 		statementDueDate: pickRandom(datesNMonthsAgo(1)),
 		LPAStatementSubmitted: pickRandom(datesNMonthsAgo(1)),
 		proofsOfEvidenceDueDate: pickRandom(datesNMonthsAhead(1)),
+		rule6ProofEvidenceDueDate: pickRandom(datesNMonthsAhead(1)),
 		interestedPartyRepsDueDate: pickRandom(datesNMonthsAgo(1)),
 		ProcedureType: { connect: { key: APPEAL_CASE_PROCEDURE.INQUIRY } },
 		CaseType: { connect: { processCode: 'S78' } },
