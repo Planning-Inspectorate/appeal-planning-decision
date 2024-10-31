98d82cf3655152176a41ae90227ed968a1f53847
Tom Pickering <topickering@gmail.com>
2024-10-31 16:16:10 +0000

    feat(a2-866): add selected appeal routes for rule 6

---
+++ packages/forms-web-app/src/routes/rule-6/selected-appeal.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+
+const selectedAppealController = require('../../controllers/selected-appeal');
+const appealDetailsController = require('../../controllers/selected-appeal/appeal-details');
+const questionnaireDetailsController = require('../../controllers/selected-appeal/questionnaire-details');
+// const finalCommentsController = require('../../controllers/selected-appeal/final-comments-details');
+// const interestedPartyCommentsController = require('../../controllers/selected-appeal/ip-comment-details');
+// const statementDetailsController = require('../../controllers/selected-appeal/statements');
+const planningObligationDetailsController = require('../../controllers/selected-appeal/planning-obligation-details');
+
+router.get('/:appealNumber', selectedAppealController.get());
+router.get('/:appealNumber/appeal-details', appealDetailsController.get());
+router.get('/:appealNumber/questionnaire', questionnaireDetailsController.get());
+// router.get('/:appealNumber/final-comments', finalCommentsController.get());
+// router.get('/:appealNumber/appellant-final-comments', finalCommentsController.get());
+// router.get('/:appealNumber/interested-party-comments', interestedPartyCommentsController.get());
+// router.get('/:appealNumber/statement', statementDetailsController.get());
+router.get(
+	'/:appealNumber/appellant-planning-obligation',
+	planningObligationDetailsController.get()
+);
+
+module.exports = router;
