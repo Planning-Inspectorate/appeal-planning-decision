e378c1eb94c757bfac6f1ab1c31c7c7cf2fad6c1
Dean Hasley <dhasley1990@gmail.com>
2024-10-31 15:51:17 +0000

    feat(A2-1071): add initial api routes for lpa poe submission

---
+++ packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/lpa-proof-evidence-submission/document-upload/controller.js
@@ -0,0 +1,52 @@
+const { createSubmissionDocument, deleteSubmissionDocument } = require('./service');
+const logger = require('#lib/logger');
+const ApiError = require('#errors/apiError');
+
+/**
+ * @type {import('express').RequestHandler}
+ */
+async function createSubmissionDocumentUpload(req, res) {
+	try {
+		const caseReference = req.params.caseReference;
+		const content = await createSubmissionDocument(caseReference, req.body);
+		if (!content) {
+			throw ApiError.unableToCreateDocumentUpload();
+		}
+		res.status(200).send(content);
+	} catch (error) {
+		if (error instanceof ApiError) {
+			logger.error(`Failed to create document upload: ${error.code} // ${error.message.errors}`);
+			res.status(error.code || 500).send(error.message.errors);
+		} else {
+			logger.error(error);
+			res.status(500).send('An unexpected error occurred');
+		}
+	}
+}
+
+/**
+ * @type {import('express').RequestHandler}
+ */
+async function deleteSubmissionDocumentUpload(req, res) {
+	try {
+		const { caseReference, documentId } = req.params;
+		const content = await deleteSubmissionDocument(caseReference, documentId);
+		if (!content) {
+			throw ApiError.unableToDeleteDocumentUpload();
+		}
+		res.status(200).send(content);
+	} catch (error) {
+		if (error instanceof ApiError) {
+			logger.error(`Failed to delete document upload: ${error.code} // ${error.message.errors}`);
+			res.status(error.code || 500).send(error.message.errors);
+		} else {
+			logger.error(error);
+			res.status(500).send('An unexpected error occurred');
+		}
+	}
+}
+
+module.exports = {
+	createSubmissionDocumentUpload,
+	deleteSubmissionDocumentUpload
+};
