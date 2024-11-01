5d80acde6be06d70579d70e701b0b730b9a36404
Tom Pickering <topickering@gmail.com>
2024-11-01 17:00:22 +0000

    feat(a2-1080): add setup for rule 6 party proofs submissions

--- packages/common/src/dynamic-forms/journey-types.js
+++ packages/common/src/dynamic-forms/journey-types.js
@@ -14,5 +14,6 @@ exports.JOURNEY_TYPES = {
 	S78_APPELLANT_FINAL_COMMENTS: 's78-appellant-final-comments',
 	S78_LPA_FINAL_COMMENTS: 's78-lpa-final-comments',
 	S78_APPELLANT_PROOF_EVIDENCE: 's78-appellant-proof-evidence',
-	S78_LPA_PROOF_EVIDENCE: 's78-lpa-proof-evidence'
+	S78_LPA_PROOF_EVIDENCE: 's78-lpa-proof-evidence',
+	S78_RULE_6_PROOF_EVIDENCE: 's78-rule-6-proof-evidence'
 };
