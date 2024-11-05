7fd6672b8427793bde8412d0f06fc8e2451a8a92
Tom Pickering <topickering@gmail.com>
2024-11-05 10:57:01 +0000

    feat(a2-1080): add rule 6 proofs setup

--- packages/database/src/migrations/20241105105201_add_rule6_proofs_table/migration.sql
+++ packages/database/src/migrations/20241105105201_add_rule6_proofs_table/migration.sql
@@ -9,6 +9,7 @@ ALTER TABLE [dbo].[SubmissionDocumentUpload] ADD [rule6ProofOfEvidenceSubmission
 CREATE TABLE [dbo].[Rule6ProofOfEvidenceSubmission] (
     [id] UNIQUEIDENTIFIER NOT NULL CONSTRAINT [Rule6ProofOfEvidenceSubmission_id_df] DEFAULT newid(),
     [caseReference] NVARCHAR(1000) NOT NULL,
+    [userId] UNIQUEIDENTIFIER NOT NULL,
     [createdAt] DATETIME2 NOT NULL CONSTRAINT [Rule6ProofOfEvidenceSubmission_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
     [updatedAt] DATETIME2,
     [submitted] BIT NOT NULL CONSTRAINT [Rule6ProofOfEvidenceSubmission_submitted_df] DEFAULT 0,
@@ -21,6 +22,9 @@ CREATE TABLE [dbo].[Rule6ProofOfEvidenceSubmission] (
 
 -- AddForeignKey
 ALTER TABLE [dbo].[Rule6ProofOfEvidenceSubmission] ADD CONSTRAINT [Rule6ProofOfEvidenceSubmission_caseReference_fkey] FOREIGN KEY ([caseReference]) REFERENCES [dbo].[AppealCase]([caseReference]) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE [dbo].[Rule6ProofOfEvidenceSubmission] ADD CONSTRAINT [Rule6ProofOfEvidenceSubmission_userId_fkey] FOREIGN KEY ([userId]) REFERENCES [dbo].[AppealUser]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE [dbo].[SubmissionDocumentUpload] ADD CONSTRAINT [SubmissionDocumentUpload_rule6ProofOfEvidenceSubmissionId_fkey] FOREIGN KEY ([rule6ProofOfEvidenceSubmissionId]) REFERENCES [dbo].[Rule6ProofOfEvidenceSubmission]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
