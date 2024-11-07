d0db573f1a6e81ca34f510df4cc5ecd654608aed
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-11-07 18:28:21 +0000

    fix(a2-1575): handle rule 6 user and public user access for document download check

--- packages/document-service-api/src/routes/v2/back-office/_id/controller.js
+++ packages/document-service-api/src/routes/v2/back-office/_id/controller.js
@@ -3,7 +3,7 @@ const logger = require('#lib/logger');
 const config = require('#config/config');
 const { isFeatureActive } = require('#config/featureFlag');
 const { FLAG } = require('@pins/common/src/feature-flags');
-const { LPA_USER_ROLE } = require('@pins/common/src/constants');
+const { LPA_USER_ROLE, APPEAL_USER_ROLES } = require('@pins/common/src/constants');
 const BlobStorageError = require('@pins/common/src/client/blob-storage-error');
 const { canAccessBODocument, CLIENT_CREDS_ROLE } = require('./access-rules');
 const { DocumentsRepository } = require('../../../../db/repos/repository');
@@ -11,35 +11,64 @@ const repo = new DocumentsRepository();
 
 /**
  * @param {import("@prisma/client").Document & { AppealCase: { LPACode:string, appealId: string} }} documentWithAppeal
- * @param {object} access_token
- * @param {object} id_token
  * @param {import('express-oauth2-jwt-bearer').JWTPayload} access_token
+ * @param {object} id_token
  */
 async function checkDocAccess(documentWithAppeal, access_token, id_token) {
-	let role = null;
+	const role = await getRole(documentWithAppeal, access_token, id_token);
 
-	if (access_token.sub === access_token.aud) {
-		role = CLIENT_CREDS_ROLE;
-	} else if (id_token?.lpaCode === documentWithAppeal.AppealCase.LPACode) {
-		role = LPA_USER_ROLE;
-	} else {
-		const appealUser = await repo.getAppealUser({
-			appealId: documentWithAppeal.AppealCase.appealId,
-			userId: access_token.sub
-		});
-		role = appealUser?.role;
-	}
+	logger.debug(
+		{ documentWithAppeal, access_token, id_token },
+		`attempt to access document with role: ${role}`
+	);
 
 	const canAccess = canAccessBODocument({
 		docMetaData: documentWithAppeal,
-		role
+		role: role
 	});
 
 	if (!canAccess) {
-		logger.error({ role, documentWithAppeal }, 'failed BO doc auth check');
+		logger.error({ role: role, documentWithAppeal }, 'failed BO doc auth check');
 	}
 
 	return canAccess;
+}
+
+/**
+ * @param {import("@prisma/client").Document & { AppealCase: { LPACode:string, appealId: string} }} documentWithAppeal
+ * @param {import('express-oauth2-jwt-bearer').JWTPayload} access_token
+ * @param {object} id_token
+ */
+async function getRole(documentWithAppeal, access_token, id_token) {
+	// no token then not a valid request
+	if (!access_token) return null;
+
+	// no id token then request without a login
+	if (!id_token) return CLIENT_CREDS_ROLE;
+
+	// if token has lpa code then an lpa user
+	if (id_token.lpaCode === documentWithAppeal.AppealCase.LPACode) return LPA_USER_ROLE;
+
+	// if we have an lpa then it's the wrong one
+	if (id_token.lpaCode) return null;
+
+	// Fetch user roles associated with the appeal
+	const appealUserRoles = await repo.getAppealUserRoles({
+		appealId: documentWithAppeal.AppealCase.appealId,
+		userId: access_token.sub
+	});
+
+	// if no roles, user cannot access this document
+	if (!appealUserRoles.length) return null;
+
+	// owner role has more permission so takes priority
+	const ownerRoles = appealUserRoles?.filter(
+		(x) => x.role === APPEAL_USER_ROLES.APPELLANT || x.role === APPEAL_USER_ROLES.AGENT
+	);
+	if (ownerRoles.length) return ownerRoles[0].role;
+
+	// any other appeal user role
+	return appealUserRoles[0].role;
 }
 
 /**
