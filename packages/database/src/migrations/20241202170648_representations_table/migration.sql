1fda16068cbe5a9b0bd46dc87dcb1f57911e0d06
Tom Pickering <topickering@gmail.com>
2024-12-03 11:15:45 +0000

    feat(a2-1749): generate representation table

---
+++ packages/database/src/migrations/20241202170648_representations_table/migration.sql
@@ -0,0 +1,37 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateTable
+CREATE TABLE [dbo].[Representation] (
+    [id] UNIQUEIDENTIFIER NOT NULL CONSTRAINT [Representation_id_df] DEFAULT newid(),
+    [representationId] NVARCHAR(1000) NOT NULL,
+    [caseId] INT,
+    [caseReference] NVARCHAR(1000) NOT NULL,
+    [status] NVARCHAR(1000),
+    [originalRepresentation] NVARCHAR(1000),
+    [redacted] BIT,
+    [redactedRepresentation] NVARCHAR(1000),
+    [redactedBy] NVARCHAR(1000),
+    [invalidDetails] NVARCHAR(max),
+    [source] NVARCHAR(1000),
+    [serviceUserId] NVARCHAR(1000),
+    [representationType] NVARCHAR(1000),
+    [dateReceived] DATETIME2,
+    [representationDocuments] NVARCHAR(max),
+    CONSTRAINT [Representation_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [Representation_caseReference_key] UNIQUE NONCLUSTERED ([caseReference])
+);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
