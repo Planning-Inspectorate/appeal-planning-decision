c2790f15fb2135e3cd50d35252bbe6c6fb99f375
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2024-12-09 11:29:34 +0000

    completed s78 questionnaire

--- test-packages/platform-feature-tests/cypress/e2e/lpa-manage-appeals/houseHolderQuestionnaire.cy.js
+++ test-packages/platform-feature-tests/cypress/e2e/lpa-manage-appeals/houseHolderQuestionnaire.cy.js
@@ -1,5 +1,5 @@
 import { houseHolderQuestionnaireTestCases } from "../../helpers/lpaManageAppeals/houseHolderQuestionnaireData";
-const { lpaQuestionnaire } = require('../../support/flows/sections/lpaManageAppeals/lpaQuestionnaire');
+const { lpaQuestionnaire } = require('../../support/flows/sections/lpaManageAppeals/houseHolderQuestionnaire');
 const { YourAppealsSelector } = require("../../page-objects/lpa-manage-appeals/your-appeals-selector");
 
 describe('House Holder Questionnaire Test Cases', () => {
@@ -10,8 +10,8 @@ describe('House Holder Questionnaire Test Cases', () => {
                         lpaQuestionnaireData = data;
                 })
                 cy.visit(`${Cypress.config('appeals_beta_base_url')}/manage-appeals/your-email-address`);
-                const emailurl = `${Cypress.config('appeals_beta_base_url')}/manage-appeals/your-email-address`;
-                const appealurl = `${Cypress.config('appeals_beta_base_url')}/manage-appeals/your-appeals`;
+               // const emailurl = `${Cypress.config('appeals_beta_base_url')}/manage-appeals/your-email-address`;
+               // const appealurl = `${Cypress.config('appeals_beta_base_url')}/manage-appeals/your-appeals`;
                 cy.url().then((url) => {
                         if (url.includes('/manage-appeals/your-email-address')) {
                                 cy.getByData(yourAppealsSelector?._selectors?.emailAddress).clear().type(lpaQuestionnaireData?.emailAddress);
@@ -29,11 +29,21 @@ describe('House Holder Questionnaire Test Cases', () => {
                 it(`
             Should validate House holder questionnaire screen, Appeal Type: HAS 
             - User selects correct type of appeal ${context.constraintsAndDesignations?.isCorrectTypeOfAppeal}
-            - User selects affect listed buildings ${context.constraintsAndDesignations?.affectListedBuildings}
-            - User selects affected listed buidings ${context.constraintsAndDesignations?.affectedListedBuildings}
-            - User selects conservation area ${context.constraintsAndDesignations?.conservationArea}
-            - User selects green belt area ${context.constraintsAndDesignations?.isGreenBelt}          	
-		 `, () => {
+            - User selects affect listed buildings ${context.constraintsAndDesignations?.isAffectListedBuildings}
+            - User selects affected listed buidings ${context.constraintsAndDesignations?.isAffectedListedBuildings}
+            - User selects conservation area ${context.constraintsAndDesignations?.isConservationArea}
+            - User selects green belt area ${context.constraintsAndDesignations?.isGreenBelt}              
+            - User selects other party representations ${context.consultResponseAndRepresent?.isOtherPartyRepresentations}
+
+            - User selects emerging plan ${context.poReportAndSupportDocs?.isEmergingPlan}
+            - User selects supplementary planning doc ${context.poReportAndSupportDocs?.isSupplementaryPlanningDocs}
+            - User selects lpa site access ${context.siteAccess?.isLpaSiteAccess}
+            - User selects neighbour site access${context.siteAccess?.isNeighbourSiteAccess}
+            - User selects lpa site safety risks${context.siteAccess?.isLpaSiteSafetyRisks}
+            - User selects ongoing appeals${context.appealProcess?.isOngoingAppeals}
+            - User selects nearby appeals${context.appealProcess?.isNearbyAppeals}
+            - User selects new conditions${context.appealProcess?.isNewConditions}          
+             `, () => {
                         lpaQuestionnaire(context, lpaQuestionnaireData);
                 });
         });
