01c2ccbe5ac7e8b4280c58ee10ba56cb93ad6245
Tom Pickering <topickering@gmail.com>
2024-12-18 15:18:19 +0000

    feat(a2-200): add test for representations controller

--- packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/representations/index.js
+++ packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/representations/index.js
@@ -3,6 +3,7 @@ const { getAppealCaseWithRepresentations } = require('./controller');
 const { AUTH } = require('@pins/common/src/constants');
 const config = require('../../../../../configuration/config');
 const { auth } = require('express-oauth2-jwt-bearer');
+const { validateToken } = require('@pins/common/src/middleware/validate-token');
 
 const asyncHandler = require('@pins/common/src/middleware/async-handler');
 const router = express.Router({ mergeParams: true });
@@ -11,6 +12,15 @@ router.use(
 	auth({
 		issuerBaseURL: `${config.auth.authServerUrl}${AUTH.OIDC_ENDPOINT}`,
 		audience: AUTH.RESOURCE
+	})
+);
+
+router.use(
+	validateToken({
+		headerName: 'authentication',
+		reqPropertyName: 'id_token',
+		jwksUri: `${config.auth.authServerUrl}${AUTH.JWKS_ENDPOINT}`,
+		enforceToken: false
 	})
 );
 
