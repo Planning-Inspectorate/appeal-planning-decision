63f143f474152c714c9428325d21a4b59c9f8308
Tom Pickering <topickering@gmail.com>
2024-12-24 14:53:48 +0000

    feat(a2-197): rule 6 self views

--- packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/representations/controller.js
+++ packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/representations/controller.js
@@ -1,6 +1,7 @@
 const {
 	getAppealCaseWithAllRepresentations,
-	getAppealCaseWithRepresentationsByType
+	getAppealCaseWithRepresentationsByType,
+	filterRule6Representations
 } = require('./service');
 const logger = require('#lib/logger');
 const ApiError = require('#errors/apiError');
@@ -9,8 +10,10 @@ const ApiError = require('#errors/apiError');
  * @type {import('express').Handler}
  */
 async function getAppealCaseWithRepresentations(req, res) {
+	const userId = req.auth.payload.sub;
+
 	const { caseReference } = req.params;
-	const { type } = req.query;
+	const { type, rule6Parties } = req.query;
 
 	if (!caseReference) {
 		throw ApiError.withMessage(400, 'case reference required');
@@ -25,11 +28,17 @@ async function getAppealCaseWithRepresentations(req, res) {
 		} else {
 			caseWithRepresentations = await getAppealCaseWithAllRepresentations(caseReference);
 		}
-		res.status(200).send(caseWithRepresentations);
 	} catch (error) {
 		logger.error(`Failed to get case with representations for ${caseReference}: ${error}`);
 		throw error;
 	}
+
+	// @ts-ignore
+	const result = rule6Parties
+		? filterRule6Representations(caseWithRepresentations, userId, rule6Parties)
+		: caseWithRepresentations;
+
+	res.status(200).send(result);
 }
 
 module.exports = { getAppealCaseWithRepresentations };
