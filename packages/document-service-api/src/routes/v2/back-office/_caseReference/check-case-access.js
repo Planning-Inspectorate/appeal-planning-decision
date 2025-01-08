74ac7c3df035f8ae56165bfe62ef59f867ec007b
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-01-08 16:17:32 +0000

    fix(a2-1927): add auth check via middleware for bulk downloads

---
+++ packages/document-service-api/src/routes/v2/back-office/_caseReference/check-case-access.js
@@ -0,0 +1,49 @@
+const { DocumentsRepository } = require('../../../../db/repos/repository');
+const repo = new DocumentsRepository();
+
+/**
+ * @type {import('express').Handler}
+ */
+const checkCaseAccess = async (req, res, next) => {
+	const { caseReference } = req.params ?? {};
+	const id_token = req.id_token;
+	const access_token = req.auth?.payload;
+
+	if (!caseReference) {
+		res.sendStatus(400);
+		return;
+	}
+
+	const appealCase = await repo.getAppealCase({ caseReference });
+
+	if (!appealCase) {
+		res.sendStatus(404);
+		return;
+	}
+
+	// if token has lpa code then an lpa user
+	if (id_token.lpaCode === appealCase.LPACode) return next();
+
+	// if we have an lpa then it's the wrong one
+	if (id_token.lpaCode) {
+		res.sendStatus(401);
+		return;
+	}
+
+	// Fetch user roles associated with the appeal
+	const appealUserRoles = await repo.getAppealUserRoles({
+		appealId: appealCase.appealId,
+		userId: access_token.sub
+	});
+
+	// if no roles, user cannot access this document
+	if (!appealUserRoles.length) {
+		res.sendStatus(401);
+		return;
+	}
+
+	// otherwise has a role on appeal
+	return next();
+};
+
+module.exports = checkCaseAccess;
