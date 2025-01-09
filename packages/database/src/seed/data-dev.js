c6854ab94f10a63f95ecffbffb9f19db7187d0bc
Dean Hasley <dhasley1990@gmail.com>
2025-01-09 13:59:47 +0000

    refactor(A2-1784): remove duplicate data

--- packages/database/src/seed/data-dev.js
+++ packages/database/src/seed/data-dev.js
@@ -1485,23 +1485,6 @@ const appealFinalComments = [
 				internalId: '19d01551-e0cb-414f-95d9-fd71422c9a89'
 			}
 		}
-	},
-	{
-		id: appealFinalCommentIds.appealFinalCommentFour,
-		submittedDate: pickRandom(datesNMonthsAgo(0.5)),
-		comments:
-			'This is the appellant final comment. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. ',
-		wantsFinalComment: true,
-		AppealCase: {
-			connect: {
-				caseReference: caseReferences.caseReferenceOne
-			}
-		},
-		ServiceUser: {
-			connect: {
-				internalId: '19d01551-e0cb-414f-95d9-fd71422c9a89'
-			}
-		}
 	}
 ];
 
