734c3ea923b0d15c98ab7d929f17bcea521567df
Dean Hasley <dhasley1990@gmail.com>
2025-01-16 16:37:49 +0000

    feat(A2-2042): add new s20 journey

---
+++ packages/forms-web-app/src/dynamic-forms/s20-appeal-form/journey.test.js
@@ -0,0 +1,36 @@
+const { Journey } = require('../journey');
+const { baseS20SubmissionUrl, ...params } = require('./journey');
+
+const mockResponse = {
+	journeyId: 'S20',
+	LPACode: 'Q9999',
+	referenceId: '123',
+	answers: {}
+};
+
+describe('S20 Appeal Form Journey', () => {
+	it('should error if no response', () => {
+		expect(() => {
+			new Journey(params);
+		}).toThrow("Cannot read properties of undefined (reading 'referenceId')");
+	});
+	it('should set baseUrl', () => {
+		const journey = new Journey({ ...params, response: mockResponse });
+		expect(journey.baseUrl).toEqual(expect.stringContaining(baseS20SubmissionUrl));
+		expect(journey.baseUrl).toEqual(expect.stringContaining(mockResponse.referenceId));
+	});
+
+	it('should set taskListUrl', () => {
+		const journey = new Journey({ ...params, response: mockResponse });
+		expect(journey.taskListUrl).toEqual('/appeals/listed-building/appeal-form/your-appeal?id=123');
+	});
+
+	it('should set template', () => {
+		const journey = new Journey({ ...params, response: mockResponse });
+		expect(journey.journeyTemplate).toBe('submission-form-template.njk');
+	});
+	it('should set journeyTitle', () => {
+		const journey = new Journey({ ...params, response: mockResponse });
+		expect(journey.journeyTitle).toBe('Appeal a planning decision');
+	});
+});
