98457e7000ad91e5bc4d7b7289a03af878dcc420
Ed Packard <edward.packard@kinandcarta.com>
2025-01-16 17:22:00 +0000

    fix(a2-2091): update postcode search and validation

--- packages/common/src/regex.js
+++ packages/common/src/regex.js
@@ -4,8 +4,7 @@
  * Tests for a valid postcode
  */
 module.exports.fullPostcodeRegex =
-	/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/;
-// todo: use simple regex and ignore spaces in sql /^[a-z]{1,2}\d[a-z\d]?\s*\d[a-z]{2}$/i
+	/([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/;
 
 /**
  * Tests for the outward code section of a postcode
--- packages/forms-web-app/src/lib/valid-postcode.js
+++ packages/forms-web-app/src/lib/valid-postcode.js
@@ -1,7 +1,7 @@
+const { fullPostcodeRegex } = require('@pins/common/src/regex');
+
 const validatePostcode = (postcode, errorMessage = 'Enter a full UK postcode') => {
-	const pattern =
-		/([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/;
-	const result = pattern.exec(postcode);
+	const result = fullPostcodeRegex.exec(postcode);
 	if (!result) {
 		throw new Error(errorMessage);
 	}
