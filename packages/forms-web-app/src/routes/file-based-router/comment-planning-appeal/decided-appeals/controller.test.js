b13e70d036037ce0983569a52f28bdc0b4b0848b
Ed Packard <edward.packard@kinandcarta.com>
2025-01-21 12:06:05 +0000

    fix(a2-2136): update decided appeals page backlink

--- packages/forms-web-app/src/routes/file-based-router/comment-planning-appeal/decided-appeals/controller.test.js
+++ packages/forms-web-app/src/routes/file-based-router/comment-planning-appeal/decided-appeals/controller.test.js
@@ -4,6 +4,17 @@ const { APPEAL_CASE_DECISION_OUTCOME } = require('pins-data-model');
 jest.mock('#utils/appeal-sorting');
 jest.mock('pins-data-model');
 
+const appeal = {
+	caseDecisionOutcomeDate: '2024-12-31',
+	caseDecisionOutcome: 'allowed',
+	appealTypeCode: 'HAS',
+	Documents: [{ documentType: 'CASE_DECISION_LETTER', id: 'doc1' }],
+	siteAddressLine1: 'a',
+	siteAddressLine2: 'b',
+	siteAddressTown: 'c',
+	siteAddressPostcode: 'd'
+};
+
 describe('decidedAppeals', () => {
 	let req, res;
 
@@ -12,7 +23,8 @@ describe('decidedAppeals', () => {
 			query: { search: 'AB12 3CD' },
 			appealsApiClient: {
 				getPostcodeSearchResults: jest.fn()
-			}
+			},
+			session: {}
 		};
 		res = {
 			redirect: jest.fn(),
@@ -27,19 +39,8 @@ describe('decidedAppeals', () => {
 	});
 
 	it('should format and sort appeals correctly and render the results', async () => {
-		const appeals = [
-			{
-				caseDecisionOutcomeDate: '2024-12-31',
-				caseDecisionOutcome: 'allowed',
-				appealTypeCode: 'HAS',
-				Documents: [{ documentType: 'CASE_DECISION_LETTER', id: 'doc1' }],
-				siteAddressLine1: 'a',
-				siteAddressLine2: 'b',
-				siteAddressTown: 'c',
-				siteAddressPostcode: 'd'
-			}
-		];
-		req.appealsApiClient.getPostcodeSearchResults.mockResolvedValue(appeals);
+		req.session = { interestedParty: { searchPostcode: 'AB12 3CD' } };
+		req.appealsApiClient.getPostcodeSearchResults.mockResolvedValue([appeal]);
 
 		await decidedAppeals(req, res);
 
@@ -47,7 +48,7 @@ describe('decidedAppeals', () => {
 			postcode: 'AB12 3CD',
 			decidedAppeals: [
 				{
-					...appeals[0],
+					...appeal,
 					formattedAddress: 'a, b, c, d',
 					formattedCaseDecisionDate: '31 Dec 2024',
 					formattedDecisionColour: 'green',
@@ -55,6 +56,28 @@ describe('decidedAppeals', () => {
 					caseDecisionOutcome: APPEAL_CASE_DECISION_OUTCOME.ALLOWED
 				}
 			]
+		});
+	});
+
+	it('should override default backlink if no postcode in session', async () => {
+		req.session = { navigationHistory: ['thispage', 'lastpage'] };
+		req.appealsApiClient.getPostcodeSearchResults.mockResolvedValue([appeal]);
+
+		await decidedAppeals(req, res);
+
+		expect(res.render).toHaveBeenCalledWith('comment-planning-appeal/decided-appeals/index', {
+			postcode: 'AB12 3CD',
+			decidedAppeals: [
+				{
+					...appeal,
+					formattedAddress: 'a, b, c, d',
+					formattedCaseDecisionDate: '31 Dec 2024',
+					formattedDecisionColour: 'green',
+					appealTypeName: 'Householder',
+					caseDecisionOutcome: APPEAL_CASE_DECISION_OUTCOME.ALLOWED
+				}
+			],
+			navigation: [undefined, 'lastpage?search=AB12 3CD']
 		});
 	});
 });
