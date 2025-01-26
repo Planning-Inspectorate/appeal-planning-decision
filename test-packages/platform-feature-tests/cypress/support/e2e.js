16f76bb993480aecc244fe730db22e96ac617bd9
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2025-01-26 09:33:19 +0000

    LPA download and view verification added

--- test-packages/platform-feature-tests/cypress/support/e2e.js
+++ test-packages/platform-feature-tests/cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 import 'cypress-mochawesome-reporter/register';
+import 'cypress-wait-until';
 
 
 // Alternatively you can use CommonJS syntax:
