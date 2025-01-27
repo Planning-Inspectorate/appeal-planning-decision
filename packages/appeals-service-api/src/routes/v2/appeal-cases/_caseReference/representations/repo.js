5865eb62ee76d0d2e5360d95cc2a4ddf11f7ff22
Tom Pickering <topickering@gmail.com>
2025-01-27 17:30:53 +0000

    feat(a2-2163): put representations

--- packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/representations/repo.js
+++ packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/representations/repo.js
@@ -3,7 +3,21 @@ const { PrismaClientKnownRequestError } = require('@prisma/client/runtime/librar
 
 /**
  * @typedef {import('@prisma/client').AppealCase} AppealCase
+ * @typedef {import ('pins-data-model').Schemas.AppealRepresentation} AppealRepresentation
  */
+
+// /**
+//  * @param {AppealRepresentation} dataModel
+//  * @returns {RepresentationCreate}
+//  */
+// const mapRepresentationDataModelToRepresentation = (
+// 	{
+// 		...commonFields
+// 	}
+// ) => ({
+
+// });
+
 class RepresentationsRepository {
 	dbClient;
 
@@ -106,6 +120,96 @@ class RepresentationsRepository {
 			throw e;
 		}
 	}
+
+	// /**
+	//  * Put a representation by representation id (ie BO id)
+	//  * @param {string} representationId
+	//  * @param {AppealRepresentation} data
+	//  * @returns {Promise<Representation>}
+	//  */
+	// async putRepresentationByRepresentationId(representationId, data) {
+	// 	// const mappedData = mapRepresentationDataModelToRepresentation(data);
+
+	// 	const result = await this.dbClient.representation.upsert({
+	// 		create: {
+	// 			// ...mappedData,
+	// 			AppealCase: data.caseReference ? { connect: { caseReference: data.caseReference } } : { create: {} }
+	// 		},
+	// 		update: mappedData,
+	// 		where: {
+	// 			representationId
+	// 		}
+	// 	});
+
+	// 	// case relations
+	// 	// nearby cases are referenced both ways
+	// 	// lead/child are only reference from child(1) to lead(2)
+	// 	await this.dbClient.$transaction(async (tx) => {
+	// 		// delete all relations that use this case
+	// 		await tx.representationDocument.deleteMany({
+	// 			where: {
+	// 				representationId
+	// 			}
+	// 		});
+
+	// 		// add nearby references both ways
+	// 		if (data.documentIds?.length) {
+	// 			const direction1 = data.documentIds.map((documentId
+	// 			) => ({
+	// 				caseReference: caseReference,
+	// 				caseReference2: nearby
+	// 			}));
+	// 			const direction2 = data.nearbyCaseReferences.map((nearby) => ({
+	// 				caseReference: nearby,
+	// 				caseReference2: caseReference
+	// 			}));
+
+	// 			await tx.appealCaseRelationship.createMany({
+	// 				data: direction1.concat(direction2)
+	// 			});
+	// 		}
+
+	// 		// add lead case (only referenced from child(1) -> lead(2))
+	// 		if (data.leadCaseReference) {
+	// 			await tx.appealCaseRelationship.create({
+	// 				data: {
+	// 					caseReference,
+	// 					caseReference2: data.leadCaseReference,
+	// 					type: CASE_RELATION_TYPES.linked
+	// 				}
+	// 			});
+	// 		}
+	// 	});
+
+	// 	// neighbour addresses
+	// 	await this.dbClient.$transaction(async (tx) => {
+	// 		// delete all of the existing neighbouringAddresses
+	// 		await tx.neighbouringAddress.deleteMany({
+	// 			where: {
+	// 				caseReference: caseReference
+	// 			}
+	// 		});
+
+	// 		// add all
+	// 		if (data.neighbouringSiteAddresses?.length) {
+	// 			await tx.neighbouringAddress.createMany({
+	// 				data: data.neighbouringSiteAddresses.map((address) => ({
+	// 					caseReference,
+	// 					addressLine1: address.neighbouringSiteAddressLine1,
+	// 					addressLine2: address.neighbouringSiteAddressLine2,
+	// 					townCity: address.neighbouringSiteAddressTown,
+	// 					county: address.neighbouringSiteAddressCounty,
+	// 					postcode: address.neighbouringSiteAddressPostcode,
+	// 					postcodeSanitized: sanitizePostcode(address.neighbouringSiteAddressPostcode),
+	// 					siteAccessDetails: address.neighbouringSiteAccessDetails,
+	// 					siteSafetyDetails: address.neighbouringSiteSafetyDetails
+	// 				}))
+	// 			});
+	// 		}
+	// 	});
+
+	// 	return result;
+	// }
 }
 
 module.exports = { RepresentationsRepository };
