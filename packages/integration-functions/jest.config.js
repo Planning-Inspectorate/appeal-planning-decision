8685546d5bf593800c3dc6a1e5ea2780323918a7
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-01-29 18:24:19 +0000

    chore: combine integration and unit tests into 1 run, add coverage to all packages

--- packages/integration-functions/jest.config.js
+++ packages/integration-functions/jest.config.js
@@ -2,15 +2,14 @@ const sharedConfig = require('../../jest.config.js');
 
 module.exports = {
 	...sharedConfig,
-	clearMocks: true,
 	collectCoverageFrom: ['./src/**/*.js'],
+	coveragePathIgnorePatterns: ['node_modules'],
 	coverageThreshold: {
 		global: {
-			statements: 38,
-			branches: 42,
-			functions: 29,
-			lines: 38
+			statements: 90,
+			branches: 81,
+			functions: 79,
+			lines: 90
 		}
-	},
-	setupFilesAfterEnv: ['<rootDir>/setupTests.js']
+	}
 };
