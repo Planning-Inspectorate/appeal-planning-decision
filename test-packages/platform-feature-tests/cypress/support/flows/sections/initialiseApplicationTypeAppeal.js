129f1b3af4bf97f210f201e5da04a6f0c2edb43d
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2025-02-03 22:04:46 +0000

    BYS changes fixed for full appeal

--- test-packages/platform-feature-tests/cypress/support/flows/sections/initialiseApplicationTypeAppeal.js
+++ test-packages/platform-feature-tests/cypress/support/flows/sections/initialiseApplicationTypeAppeal.js
@@ -26,19 +26,13 @@ module.exports = (statusOfOriginalApplication, planning, context, prepareAppealD
 	} else {
 		grantedOrRefusedId =  basePage._selectors?.answerGranted;
 	}
+	cy.getByData(basePage._selectors?.answerListedBuilding).click();
+	cy.advanceToNextPage();
 
-	if (planning === prepareAppealSelector?._selectors?.answerFullAppeal) {
-
-		//cy.get(basePage._selectors?.siteSelectionSeven).click();
-		cy.getByData(basePage._selectors?.answerListedBuilding).click();
-		cy.advanceToNextPage();
+	if (planning === prepareAppealSelector?._selectors?.answerFullAppeal) {			
 		initialiseFullPlanning(planning, grantedOrRefusedId, prepareAppealSelector?._selectors?.fullAppealText, context, prepareAppealData);
 	}
-	else if (planning === prepareAppealSelector?._selectors?.answerHouseholderPlanning) {
-
-		cy.getByData(basePage._selectors?.answerListedBuilding).click();
-		cy.advanceToNextPage();
-
+	else if (planning === prepareAppealSelector?._selectors?.answerHouseholderPlanning) {		
 		statusOfOriginalApplication === prepareAppealSelector?._selectors?.statusOfOriginalApplicationRefused ? initialiseHouseHolderPlanning(planning, grantedOrRefusedId, context, prepareAppealData) : initialiseFullPlanning(planning, grantedOrRefusedId, prepareAppealSelector?._selectors?.householderPlanningText, context, prepareAppealData);
 	}
 };
