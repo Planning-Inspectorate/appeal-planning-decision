aca193184ce4d7f89a86e4be501d41d4b3f064d3
Tom Pickering <topickering@gmail.com>
2025-02-18 17:59:01 +0000

    feat(a2-2070): add interested party comments to representations formatter

--- packages/appeals-service-api/src/services/back-office-v2/formatters/s78/representation.js
+++ packages/appeals-service-api/src/services/back-office-v2/formatters/s78/representation.js
@@ -3,8 +3,8 @@ const { APPEAL_REPRESENTATION_TYPE } = require('pins-data-model');
 const { APPEAL_USER_ROLES, LPA_USER_ROLE } = require('@pins/common/src/constants');
 
 /**
- * @typedef {import('@pins/common/src/constants').RepresentationTypes} RepresentationTypes
- * @typedef {'appellantFinalCommentDetails' | 'lpaFinalCommentDetails' | 'lpaStatement' | 'rule6Statement'} AllowedRepresentationField
+ * @typedef {import('pins-data-model/src/enums').APPEAL_REPRESENTATION_TYPE} RepresentationTypes
+ * @typedef {'appellantFinalCommentDetails' | 'lpaFinalCommentDetails' | 'lpaStatement' | 'rule6Statement' | 'comments'} AllowedRepresentationField
  * @typedef {import ('pins-data-model').Schemas.AppealRepresentationSubmission} AppealRepresentationSubmission
  * @typedef {import('../../../../routes/v2/appeal-cases/_caseReference/appellant-final-comment-submission/appellant-final-comment-submission').AppellantFinalCommentSubmission} AppellantFinalCommentSubmission
  * @typedef {import('../../../../routes/v2/appeal-cases/_caseReference/lpa-final-comment-submission/lpa-final-comment-submission').LPAFinalCommentSubmission} LPAFinalCommentSubmission
@@ -13,7 +13,19 @@ const { APPEAL_USER_ROLES, LPA_USER_ROLE } = require('@pins/common/src/constants
  * @typedef {import('../../../../routes/v2/appeal-cases/_caseReference/appellant-proof-evidence-submission/appellant-proof-evidence-submission').AppellantProofOfEvidenceSubmission} AppellantProofOfEvidenceSubmission
  * @typedef {import('../../../../routes/v2/appeal-cases/_caseReference/lpa-proof-evidence-submission/lpa-proof-evidence-submission').LPAProofOfEvidenceSubmission} LPAProofOfEvidenceSubmission
  * @typedef {import('../../../../routes/v2/appeal-cases/_caseReference/rule-6-proof-evidence-submission/rule-6-proof-evidence-submission').Rule6ProofOfEvidenceSubmission} Rule6ProofOfEvidenceSubmission
- * @typedef {(AppellantFinalCommentSubmission | LPAFinalCommentSubmission | LPAStatementSubmission | Rule6StatementSubmission | AppellantProofOfEvidenceSubmission | LPAProofOfEvidenceSubmission | Rule6ProofOfEvidenceSubmission) & Record<AllowedRepresentationField, string|null>} TypedRepresentationSubmission
+ * @typedef {import('../../../../routes/v2/interested-party-submissions/repo').DetailedInterestedPartySubmission} InterestedPartySubmission
+ * @typedef {(AppellantFinalCommentSubmission | LPAFinalCommentSubmission | LPAStatementSubmission | Rule6StatementSubmission | AppellantProofOfEvidenceSubmission | LPAProofOfEvidenceSubmission | Rule6ProofOfEvidenceSubmission | InterestedPartySubmission) & Record<AllowedRepresentationField, string|null>} TypedRepresentationSubmission
+ */
+
+/**
+ * @typedef {Object} NewUser
+ * @property {string | null} salutation
+ * @property {string | null} firstName
+ * @property {string | null} lastName
+ * @property {string | null} emailAddress
+ * @property {string | null} telephoneNumber
+ * @property {string | null} organisation
+ * @property {'InterestedParty'} serviceUserType
  */
 
 /**
@@ -27,7 +39,27 @@ const REPRESENTATION_FIELDS = {
 	[APPEAL_REPRESENTATION_TYPE.STATEMENT]: {
 		[LPA_USER_ROLE]: 'lpaStatement',
 		[APPEAL_USER_ROLES.RULE_6_PARTY]: 'rule6Statement'
+	},
+	[APPEAL_REPRESENTATION_TYPE.COMMENT]: {
+		[APPEAL_USER_ROLES.INTERESTED_PARTY]: 'comments'
 	}
+};
+
+/**
+ * @param {InterestedPartySubmission} interestedPartySubmission
+ * @returns {NewUser}
+ */
+
+const createInterestedPartyNewUser = (interestedPartySubmission) => {
+	return {
+		salutation: null,
+		firstName: interestedPartySubmission.firstName,
+		lastName: interestedPartySubmission.lastName,
+		emailAddress: interestedPartySubmission.emailAddress,
+		telephoneNumber: null,
+		organisation: null,
+		serviceUserType: 'InterestedParty'
+	};
 };
 
 /**
@@ -43,7 +75,9 @@ exports.formatter = async (caseReference, serviceUserId, repType, representation
 
 	let party;
 	let lpaCode;
-	if (!serviceUserId) {
+	if (repType === APPEAL_REPRESENTATION_TYPE.COMMENT) {
+		party = APPEAL_USER_ROLES.INTERESTED_PARTY;
+	} else if (!serviceUserId) {
 		lpaCode = representationSubmission.AppealCase.LPACode;
 		party = LPA_USER_ROLE;
 	} else if (serviceUserId && repType === APPEAL_REPRESENTATION_TYPE.STATEMENT) {
@@ -60,18 +94,25 @@ exports.formatter = async (caseReference, serviceUserId, repType, representation
 		}
 	}
 
+	const documents =
+		repType === APPEAL_REPRESENTATION_TYPE.COMMENT
+			? []
+			: await getDocuments(representationSubmission);
+
 	/** @type {AppealRepresentationSubmission} */
 	const payload = {
-		caseReference: caseReference,
+		caseReference,
 		representation: representationText,
 		representationSubmittedDate: new Date().toISOString(),
 		representationType: repType,
-		documents: await getDocuments(representationSubmission)
+		documents
 	};
-	if (serviceUserId) {
+	if (repType === APPEAL_REPRESENTATION_TYPE.COMMENT) {
+		payload.newUser = createInterestedPartyNewUser(representationSubmission);
+	} else if (serviceUserId) {
 		payload.serviceUserId = serviceUserId;
 	} else if (lpaCode) {
 		payload.lpaCode = lpaCode;
-	} //todo: add newUser for IP comment
+	}
 	return payload;
 };
