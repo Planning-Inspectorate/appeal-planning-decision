b51fdd74ad40207d8ae8ab66cee346eb0c37611b
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-02-27 18:34:12 +0000

    feat(a2-2382): add application-about question to S78 and S20 forms

--- packages/common/src/dynamic-forms/field-values.js
+++ packages/common/src/dynamic-forms/field-values.js
@@ -10,5 +10,16 @@ exports.fieldValues = {
 		MAJOR: 'major',
 		MINOR: 'minor',
 		OTHER: 'other'
+	},
+	applicationAbout: {
+		HOUSEHOLDER: 'householder',
+		CHANGE_OF_USE: 'change-of-use',
+		MINERAL_WORKINGS: 'mineral-workings',
+		DWELLINGS: 'dwellings',
+		INDUSTRY_STORAGE: 'industry-storage',
+		OFFICES: 'offices',
+		RETAIL_SERVICES: 'retail-services',
+		TRAVELLER_CARAVAN: 'traveller-caravan',
+		OTHER: 'other'
 	}
 };
