75e8d6c4d03de28676c5cf7802497d2e9c8521dc
Peter Uzor <peter.uzor.i7@planninginspectorate.gov.uk>
2025-02-27 15:41:23 +0000

    feat: a2-2371 - added new question for s20 - what grade is the listed building

--- packages/forms-web-app/src/dynamic-forms/s20-lpa-questionairre/journey.js
+++ packages/forms-web-app/src/dynamic-forms/s20-lpa-questionairre/journey.js
@@ -28,6 +28,8 @@ const sections = [
 		.addQuestion(questions.affectedListedBuildings)
 		.withCondition((response) => questionHasAnswer(response, questions.listedBuildingCheck, 'yes'))
 		.addQuestion(questions.demolishAlterExtend)
+		.addQuestion(questions.listedBuildingGrade)
+		.withCondition((response) => questionHasAnswer(response, questions.demolishAlterExtend, 'yes'))
 		.addQuestion(questions.consultHistoricEngland)
 		.addQuestion(questions.scheduledMonument)
 		.addQuestion(questions.conservationArea)
