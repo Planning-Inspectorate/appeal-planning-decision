7546e44b82a991fd675f7e61c6b20e0441a8ac34
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-03-07 10:23:35 +0000

    feat(a2-2544): display development type on dashboards

--- packages/common/src/lib/format-appeal-details.test.js
+++ packages/common/src/lib/format-appeal-details.test.js
@@ -1,4 +1,8 @@
-const { formatHealthAndSafety, formatAccessDetails } = require('./format-appeal-details');
+const {
+	formatHealthAndSafety,
+	formatAccessDetails,
+	formatDevelopmentType
+} = require('./format-appeal-details');
 
 describe('format-appeal-details', () => {
 	describe('formatHealthAndSafety', () => {
@@ -39,6 +43,24 @@ describe('format-appeal-details', () => {
 			const resultAllIndexesEmpty = formatAccessDetails({ siteAccessDetails: ['', ''] });
 			expect(resultIndex0Empty).toEqual('No');
 			expect(resultAllIndexesEmpty).toEqual('No');
+		});
+	});
+
+	describe('formatDevelopmentType', () => {
+		it('returns expected value from development type', () => {
+			expect(formatDevelopmentType('minor-offices')).toEqual(
+				'Minor offices, light industry or research and development'
+			);
+		});
+
+		it('returns empty string if development type is nullish', () => {
+			expect(formatDevelopmentType(null)).toEqual('');
+			expect(formatDevelopmentType('')).toEqual('');
+			expect(formatDevelopmentType(undefined)).toEqual('');
+		});
+
+		it('throws if unknown development type', () => {
+			expect(() => formatDevelopmentType('nope')).toThrow('unhandled developmentType mapping');
 		});
 	});
 });
