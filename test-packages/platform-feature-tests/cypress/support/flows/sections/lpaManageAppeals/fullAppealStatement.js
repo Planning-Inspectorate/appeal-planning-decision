7eda55da595f4dcc91b8fd7354a800c17f7e9f28
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2025-03-19 07:43:51 +0000

    added lpa full appeal statement

---
+++ test-packages/platform-feature-tests/cypress/support/flows/sections/lpaManageAppeals/fullAppealStatement.js
@@ -0,0 +1,39 @@
+/* eslint-disable cypress/unsafe-to-chain-command */
+// @ts-nocheck
+/// <reference types="cypress"/>
+
+import { BasePage } from "../../../../page-objects/base-page";
+import { Statement } from "../../pages/lpa-manage-appeals/lpa-statement/statement";
+
+export const fullAppealStatement = (context, lpaManageAppealsData) => {
+	const basePage = new BasePage();
+	const statement = new Statement();
+	let appealId;
+	let counter = 0;
+	cy.get(basePage?._selectors.trgovukTableRow).each(($row) => {
+		const rowtext = $row.text();
+		if (rowtext.includes(lpaManageAppealsData?.s78AppealType) && rowtext.includes(lpaManageAppealsData?.todoStatement)) {
+			if (counter === 0) {				
+				cy.wrap($row).within(() => {
+					cy.get(basePage?._selectors.trgovukTableCell).contains(lpaManageAppealsData?.s78AppealType).should('be.visible');					
+					cy.get('a').each(($link) => {						
+						if ($link.attr('href')?.includes(lpaManageAppealsData?.appealStatementLink)) {
+							const parts = $link.attr('href')?.split('/');
+							appealId = parts[parts.length - 2];					
+							cy.wrap($link).scrollIntoView().should('be.visible').click({ force: true });
+							return false;
+						}
+					});
+				});
+			}
+			counter++;
+		}
+	}).then(() => {
+		cy.url().should('include', `/manage-appeals/appeal-statement/${appealId}/appeal-statement`);
+		statement.addStatement(context);
+		statement.haveAdditionalDocumentforStatement(context);
+	});	
+	// commented for test during coding
+	// 	cy.getByData(lpaManageAppealsData?.submitAppealStatement).click();
+	// 	cy.get(basePage?._selectors.govukPanelTitle).contains(lpaManageAppealsData?.questionnaireSubmitted);
+};
