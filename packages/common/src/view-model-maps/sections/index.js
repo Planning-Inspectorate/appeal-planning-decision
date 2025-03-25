64c933042b2498e79ca2a944d6eda61bd6ada0db
Tom Pickering <topickering@gmail.com>
2025-03-25 14:57:42 +0000

    feat(a2-2429): add submission date display to lpa appeal details page

--- packages/common/src/view-model-maps/sections/index.js
+++ packages/common/src/view-model-maps/sections/index.js
@@ -2,13 +2,32 @@
 exports.isSection = (maybeSections) => !!maybeSections;
 
 /**
+ * @typedef FormattedSection
+ * @property {string} heading
+ * @property {Array<object>} links
+ */
+
+/**
  * @param {{
  *   caseData: import('appeals-service-api').Api.AppealCaseDetailed,
  *	 sections: import('./def').Sections,
- * }} args * @returns {import('./def').Sections}
+ * }} args * @returns {FormattedSection[]}
  */
-exports.formatSections = ({ sections, caseData }) =>
-	sections.map((section) => ({
-		...section,
-		links: section.links.filter(({ condition }) => condition(caseData))
-	}));
+exports.formatSections = ({ sections, caseData }) => {
+	return sections.map((section) => {
+		const filteredLinks = section.links
+			.filter(({ condition }) => condition(caseData))
+			.map((link) => {
+				const submissionDate = link.submissionDate ? link.submissionDate.text(caseData) : null;
+				return {
+					...link,
+					submissionDate
+				};
+			});
+
+		return {
+			...section,
+			links: filteredLinks
+		};
+	});
+};
