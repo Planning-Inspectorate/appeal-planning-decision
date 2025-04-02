4a5fb5d254e2c9d0b3c2607158ba9ff7cea40f93
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2025-04-02 09:02:58 +0100

    LPA Final comment pages added

--- test-packages/platform-feature-tests/cypress/helpers/lpaManageAppeals/fullAppealFinalCommentData.js
+++ test-packages/platform-feature-tests/cypress/helpers/lpaManageAppeals/fullAppealFinalCommentData.js
@@ -1,17 +1,29 @@
 const documents = {    
-    uploadStatement: 'upload-statement.pdf'
+    uploadSupportDocsFinalComments: 'upload-support-docs-final-comments.pdf',
+    uploadAdditionalDocsSupportFinalComments: 'upload-additional-docs-support-final-comments.pdf'
 };
 
 export const fullAppealFinalCommentTestCases = [
     {  
         submitFinalComments:{
-            selectAnswer: false
+            selectAnswer: false,
+        },
+    },
+    {  
+        submitFinalComments:{
+            selectAnswer: true,
+        },
+        additionalDocuments:{
+            selectAnswer: true,
         },
         documents,
     },
     {  
         submitFinalComments:{
-            selectAnswer: true
+            selectAnswer: true,
+        },
+        additionalDocuments:{
+            selectAnswer: false,
         },
         documents,
     }
