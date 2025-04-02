558742e3a4fca61c92ae1098967c24e6531dacd7
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-04-02 17:24:00 +0100

    fix: experiment to improve jest performance

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
+/** @type {import('jest').Config} */
 module.exports = {
 	coverageDirectory: '<rootDir>/jest-reports/coverage',
 	coverageReporters: ['cobertura', 'json', 'html', 'text', 'text-summary'],
 	moduleFileExtensions: ['js', 'json'],
 	reporters: ['default', ['jest-junit', { outputDirectory: '<rootDir>/jest-reports' }]],
 	testEnvironment: 'node',
-	testMatch: ['**/?(*.)+(spec|test).js']
+	testMatch: ['**/?(*.)+(spec|test).js'],
+	workerThreads: true,
+	workerIdleMemoryLimit: '500mb'
 };
