6eb5e6f7d59ef87cb73e61046be66acb850294db
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2025-04-05 08:49:09 +0100

    indentation changes done

--- test-packages/platform-feature-tests/cypress/support/flows/sections/lpaManageAppeals/fullAppealStatement.js
+++ test-packages/platform-feature-tests/cypress/support/flows/sections/lpaManageAppeals/fullAppealStatement.js
@@ -13,13 +13,13 @@ export const fullAppealStatement = (context, lpaManageAppealsData) => {
 	cy.get(basePage?._selectors.trgovukTableRow).each(($row) => {
 		const rowtext = $row.text();
 		if (rowtext.includes(lpaManageAppealsData?.s78AppealType) && rowtext.includes(lpaManageAppealsData?.todoStatement)) {
-			if (counter === 0) {				
+			if (counter === 0) {
 				cy.wrap($row).within(() => {
-					cy.get(basePage?._selectors.trgovukTableCell).contains(lpaManageAppealsData?.s78AppealType).should('be.visible');					
-					cy.get('a').each(($link) => {						
+					cy.get(basePage?._selectors.trgovukTableCell).contains(lpaManageAppealsData?.s78AppealType).should('be.visible');
+					cy.get('a').each(($link) => {
 						if ($link.attr('href')?.includes(lpaManageAppealsData?.appealStatementLink)) {
 							const parts = $link.attr('href')?.split('/');
-							appealId = parts[parts.length - 2];					
+							appealId = parts[parts.length - 2];
 							cy.wrap($link).scrollIntoView().should('be.visible').click({ force: true });
 							return false;
 						}
@@ -32,7 +32,7 @@ export const fullAppealStatement = (context, lpaManageAppealsData) => {
 		cy.url().should('include', `/manage-appeals/appeal-statement/${appealId}/appeal-statement`);
 		statement.addStatement(context);
 		statement.haveAdditionalDocumentforStatement(context);
-	});	
+	});
 	// commented for test during coding
 	// 	cy.getByData(lpaManageAppealsData?.submitAppealStatement).click();
 	// 	cy.get(basePage?._selectors.govukPanelTitle).contains(lpaManageAppealsData?.questionnaireSubmitted);
