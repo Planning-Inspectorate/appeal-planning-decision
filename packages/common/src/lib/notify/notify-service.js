38fd962eeaf70c4426f56c7589f7e83fe89c2b3f
Dean Hasley <dhasley1990@gmail.com>
2025-04-24 14:04:09 +0100

    refactor(A2-2881): add missing comma after rebase

--- packages/common/src/lib/notify/notify-service.js
+++ packages/common/src/lib/notify/notify-service.js
@@ -21,7 +21,7 @@ class NotifyService {
 			v2LpaStatement: '/representations/v2-lpa-statement.md',
 			v2AppellantFinalComment: '/representations/v2-appellant-final-comments.md',
 			v2LpaFinalComment: '/representations/v2-lpa-final-comments.md',
-			v2ProofOfEvidenceSubmitted: '/representations/v2-proof-of-evidence-submitted.md'
+			v2ProofOfEvidenceSubmitted: '/representations/v2-proof-of-evidence-submitted.md',
 			v2LpaProofsEvidence: '/representations/v2-lpa-proofs-evidence.md'
 		}
 	};
