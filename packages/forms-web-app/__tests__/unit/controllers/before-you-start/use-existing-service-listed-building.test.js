df29f0c8b7b734cf45913617f1eb220ffb17b658
Bait-Toyin Ajani <bait-toyin.ajani.6h@planninginspectorate.gov.uk>
2025-04-30 09:43:15 +0100

    fix(test/e2e): updating unit tests to remove banner override no longer used

--- packages/forms-web-app/__tests__/unit/controllers/before-you-start/use-existing-service-listed-building.test.js
+++ packages/forms-web-app/__tests__/unit/controllers/before-you-start/use-existing-service-listed-building.test.js
@@ -3,7 +3,6 @@ const {
 } = require('../../../../src/controllers/before-you-start/use-existing-service-listed-building');
 const { VIEW } = require('../../../../src/lib/views');
 const { mockReq, mockRes } = require('../../mocks');
-const config = require('../../../../src/config');
 
 const req = mockReq();
 const res = mockRes();
@@ -16,7 +15,6 @@ describe('controllers/before-you-start/use-existing-service-listed-building', ()
 			expect(res.render).toHaveBeenCalledWith(
 				VIEW.BEFORE_YOU_START.USE_EXISTING_SERVICE_LISTED_BUILDING,
 				{
-					bannerHtmlOverride: config.betaBannerText,
 					acpLink: 'https://acp.planninginspectorate.gov.uk/'
 				}
 			);
