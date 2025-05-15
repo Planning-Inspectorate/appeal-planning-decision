a46b63a8f5c739fbc71d5c755dba26bdf26a7fac
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2025-05-15 19:54:27 +0100

    Proof of evidence validations added

--- test-packages/platform-feature-tests/cypress/e2e/rule-6-appeals/r6-full-appeal-proofs-of-evidence-validation.cy.js
+++ test-packages/platform-feature-tests/cypress/e2e/rule-6-appeals/r6-full-appeal-proofs-of-evidence-validation.cy.js
@@ -27,7 +27,7 @@ describe('Rule 6 Proof of Evidence Validations', () => {
         });
         let counter = 0;
         cy.get(basePage?._selectors.trgovukTableRow).each(($row) => {
-            const rowtext = $row.text();           
+            const rowtext = $row.text();
             if (rowtext.includes(lpaManageAppealsData?.s78AppealType) && rowtext.includes(lpaManageAppealsData?.rule6todoProofOfEvidence)) {
                 if (counter === 0) {
                     cy.wrap($row).within(() => {
@@ -38,7 +38,7 @@ describe('Rule 6 Proof of Evidence Validations', () => {
                                 const parts = $link.attr('href')?.split('/');
                                 appealId = parts?.[parts.length - 2];
                                 //appealId = $link.attr('href')?.split('/').pop();
-                                cy.log(appealId);						
+                                cy.log(appealId);
                                 cy.wrap($link).scrollIntoView().should('be.visible').click({ force: true });
                                 return false;
                             }
@@ -56,16 +56,16 @@ describe('Rule 6 Proof of Evidence Validations', () => {
     });
 
     it(`Validate Upload Proof of Evidence page error validation`, () => {
-        
-        cy.get(basePage?._selectors.govukHeadingOne).contains('Upload your proof of evidence and summary');        
+
+        cy.get(basePage?._selectors.govukHeadingOne).contains('Upload your proof of evidence and summary');
         //basePage.verifyPageHeading('Upload your proof of evidence and summary');
         // <a href="#uploadRule6ProofOfEvidenceDocuments">Select your proof of evidence and summary</a>
-         if(cy.get(basePage?._selectors.govukHeadingM).contains('Files added')){   
-            cy.advanceToNextPage();             
-         }
-         else{
+        if (cy.get(basePage?._selectors.govukHeadingM).contains('Files added')) {
+            cy.advanceToNextPage();
+        }
+        else {
             cy.shouldHaveErrorMessage(basePage?._selectors?.govukErrorSummaryBody, 'Select your proof of evidence and summary');
-         }
+        }
     });
 
     it(`Validate user should not be allowed to upload wrong format file`, () => {
@@ -101,13 +101,13 @@ describe('Rule 6 Proof of Evidence Validations', () => {
         cy.get('input[name="rule6Witnesses"]:checked').then(($checked) => {
             if ($checked.length > 0) {
                 cy.log("Radio Button already selected");
-                return;             
+                return;
             }
             else {
                 cy.advanceToNextPage();
                 cy.shouldHaveErrorMessage(basePage?._selectors?.govukErrorSummaryBody, 'Select yes if you need to add any witnesses');
             }
-        })      
+        })
     });
     it(`Validate Upload Witnesses Evidence page error validation`, () => {
         cy.advanceToNextPage();
@@ -116,12 +116,12 @@ describe('Rule 6 Proof of Evidence Validations', () => {
         //basePage.verifyPageHeading('Upload your witnesses and their evidence');
         //cy.advanceToNextPage();
         // <a href="#uploadRule6WitnessesEvidence">Select your proof of evidence and summary</a>
-        if(cy.get(basePage?._selectors.govukHeadingM).contains('Files added')){   
-            cy.advanceToNextPage();             
-         }
-         else{
+        if (cy.get(basePage?._selectors.govukHeadingM).contains('Files added')) {
+            cy.advanceToNextPage();
+        }
+        else {
             cy.shouldHaveErrorMessage(basePage?._selectors?.govukErrorSummaryBody, 'Select your witnesses and their evidence');
-         }        
+        }
     });
 
 
@@ -150,7 +150,7 @@ describe('Rule 6 Proof of Evidence Validations', () => {
         expectedFileNames.forEach((fileName, index) => {
             cy.get('.moj-multi-file-upload__filename')
                 .eq(index)
-                .should('contain.text', fileName);           
+                .should('contain.text', fileName);
         });
         cy.advanceToNextPage();
     });
@@ -162,32 +162,32 @@ describe('Rule 6 Proof of Evidence Validations', () => {
         cy.get(basePage?._selectors.govukHeadingOne).contains('Check your answers and submit your proof of evidence');
         //basePage.verifyPageHeading('Check your answers and submit your proof of evidence');
         const expectedRows = [
-                {
-                        key: 'Your proof of evidence and summary',
-                        hrefContains: `/rule-6/proof-evidence/${appealId}/upload-proof-evidence`                       
-                },
-                {
-                        key: 'Added witnesses',
-                        hrefContains: `/rule-6/proof-evidence/${appealId}/add-witnesses`
-                },
-                {
-                        key: 'Witness proof of evidence and summary',
-                        hrefContains: `/rule-6/proof-evidence/${appealId}/upload-witnesses-evidence`,
-                        optional: true
-                }
+            {
+                key: 'Your proof of evidence and summary',
+                hrefContains: `/rule-6/proof-evidence/${appealId}/upload-proof-evidence`
+            },
+            {
+                key: 'Added witnesses',
+                hrefContains: `/rule-6/proof-evidence/${appealId}/add-witnesses`
+            },
+            {
+                key: 'Witness proof of evidence and summary',
+                hrefContains: `/rule-6/proof-evidence/${appealId}/upload-witnesses-evidence`,
+                optional: true
+            }
         ];
         cy.get(basePage?._selectors.govukSummaryListRow).each(($row, index) => {
-                const expected = expectedRows[index];
-                if (!expected) return;
-                const rowText = $row.text().trim();
-                if (expected.optional && !rowText.includes(expected.key)) {
-                        cy.log('Skipping optional row:${expected.key}');
-                        return;
-                }
-                expect(rowText).to.include(expected.key);
-                cy.wrap($row).find(basePage?._selectors.govukSummaryListActionsagovuklink).should('contain.text', 'Change').and('have.attr', 'href').then((href) => {
-                        expect(href).to.include(expected.hrefContains);
-                });
+            const expected = expectedRows[index];
+            if (!expected) return;
+            const rowText = $row.text().trim();
+            if (expected.optional && !rowText.includes(expected.key)) {
+                cy.log('Skipping optional row:${expected.key}');
+                return;
+            }
+            expect(rowText).to.include(expected.key);
+            cy.wrap($row).find(basePage?._selectors.govukSummaryListActionsagovuklink).should('contain.text', 'Change').and('have.attr', 'href').then((href) => {
+                expect(href).to.include(expected.hrefContains);
+            });
         });
     });
 
