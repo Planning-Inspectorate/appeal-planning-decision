4a76aaefea93f3250f249c0ef32fa57bc02cf976
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2025-05-24 11:33:23 +0100

    support files added

---
+++ test-packages/platform-feature-tests/cypress/support/flows/pages/lpa-manage-appeals/lpa-final-comment/finalComment.js
@@ -0,0 +1,49 @@
+// @ts-nocheck
+/// <reference types="cypress"/>
+import { BasePage } from "../../../../../page-objects/base-page";
+export class FinalComment {
+    _selectors = {
+
+    }
+    addStatement(context) {
+        cy.get('#lpaStatement').clear();
+        cy.get('#lpaStatement').type("Statement test");
+        cy.advanceToNextPage();
+    }
+
+    selectSubmitAnyFinalComment(context) {
+        const basePage = new BasePage();
+
+        if (context?.submitFinalComments?.selectAnswer) {
+            cy.getByData(basePage?._selectors.answerYes).click();
+            cy.advanceToNextPage();
+            // Add your Final Comments
+            cy.get('#lpaFinalCommentDetails').clear();
+            cy.get('#lpaFinalCommentDetails').type("Final comment test");
+            cy.get('#sensitiveInformationCheckbox').check({ force: true });
+            cy.advanceToNextPage();
+            //Upload your witnesses and their evidence
+            if (context?.additionalDocument?.selectAnswer) {
+                cy.getByData(basePage?._selectors?.answerYes).click();
+                cy.advanceToNextPage();
+                cy.contains('button[name="delete"]', 'Remove', { timeout: 0, log: false }).then($btn => {
+                    if ($btn.length > 0) {
+                        cy.wrap($btn).click();
+                    }
+                })
+                cy.uploadFileFromFixtureDirectories(context?.documents?.uploadAdditionalDocsSupportFinalComments);
+                cy.advanceToNextPage();
+            } else {
+                cy.getByData(basePage?._selectors?.answerNo).click();
+                cy.advanceToNextPage();
+            }
+
+        } else {
+            cy.getByData(basePage?._selectors.answerNo).click();
+            cy.advanceToNextPage();
+            //Upload your witnesses and their evidence
+            //    cy.uploadFileFromFixtureDirectories(context?.documents?.uploadSupportDocsFinalComments);
+            //     cy.advanceToNextPage();
+        }
+    };
+}
---
+++ test-packages/platform-feature-tests/cypress/support/flows/pages/lpa-manage-appeals/waitingForReview.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress"/>
+
+//import { BasePage } from "test-packages/platform-feature-tests/cypress/page-objects/base-page";
+import { BasePage } from "../../../../page-objects/base-page";
+
+export const waitingForReview = (appealId) => {
+    const basePage = new BasePage();
+    cy.get('#tab_waiting-for-review').click();
+    cy.get('a.govuk-link').contains(appealId).click();
+    cy.get(`a[href*="/manage-appeals/${appealId}/appeal-details"]`).click();
+    cy.exec('del /q cypress\\downloads\\*');
+    cy.window().then(win => {
+        cy.stub(win, 'open').as('download')
+    });
+    cy.get(`a[href*="/manage-appeals/${appealId}/appeal-details?pdf=true"]`).click();
+    cy.verifyDownload(`Appeal ${appealId}.pdf`, { contains: true });
+    basePage.backBtn();
+    cy.get(`a[href*="/manage-appeals/${appealId}/questionnaire"]`).click();
+    cy.exec('del /q cypress\\downloads\\*');
+    cy.window().then(win => {
+        cy.stub(win, 'open').as('download')
+    });
+    cy.get(`a[href*="/manage-appeals/${appealId}/questionnaire?pdf=true"]`).click();
+    cy.verifyDownload(`Appeal Questionnaire ${appealId}.pdf`, { contains: true });
+    cy.get(`a[href*="/manage-appeals/${appealId}/download/back-office/documents/lpa-questionnaire"]`).click();
+    cy.verifyDownload(`appeal_${appealId}_lpa-questionnaire`, { contains: true });
+}
