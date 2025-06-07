694cefe3fe2980a84a240ecc200fb0cd57e170a6
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2025-06-07 09:00:29 +0100

    Microsoft Auth implemented using puppeteer

--- test-packages/platform-feature-tests/cypress/fixtures/users.js
+++ test-packages/platform-feature-tests/cypress/fixtures/users.js
@@ -1,24 +1,9 @@
 export const users = {
 	appeals: {
-		validationOfficer: {
-			email: Cypress.env('VALIDATION_OFFICER_EMAIL'),
-			id: 'validation-officer',
-			typeName: 'Validation officer'
-		},
-		caseAdmin: {
-			email: Cypress.env('CASE_ADMIN_EMAIL'),
-			id: 'case-admin',
-			typeName: 'Case admin'
-		},
-		inspector: {
-			email: Cypress.env('INSPECTOR_EMAIL'),
-			id: 'inspector',
-			typeName: 'Inspector'
-		},
-		happyPath: {
-			email: Cypress.env('HAPPY_PATH_EMAIL'),
-			id: 'happy-path',
-			typeName: 'Test user'
+		appellant: {
+			email: Cypress.env('APPELLANT_EMAIL'),
+			id: 'appellant',
+			typeName: 'Appellant'
 		}
 	}
 };
