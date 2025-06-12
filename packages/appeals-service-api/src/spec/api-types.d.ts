9b425b74e48bd447243ecf6fb3ca2a1834827b66
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2025-06-12 12:08:16 +0100

    Revert "feat(appeals-service-api): scopingOpinion added to yaml spec and api-types"
    This reverts commit 9cf422926efdcf195ac13000d0936679db22b7ab.

--- packages/appeals-service-api/src/spec/api-types.d.ts
+++ packages/appeals-service-api/src/spec/api-types.d.ts
@@ -353,8 +353,6 @@ export interface AppealCase {
 	columnTwoThreshold?: boolean;
 	/** Have you issued a screening opinion */
 	screeningOpinion?: boolean;
-	/** Have you issued a scoping opinion */
-	eiaScopingOpinion?: boolean;
 	/** Did your screening opinion say the development needed an environmental statement */
 	requiresEnvironmentalStatement?: boolean;
 	/** Did the applicant submit an environmental statement */
