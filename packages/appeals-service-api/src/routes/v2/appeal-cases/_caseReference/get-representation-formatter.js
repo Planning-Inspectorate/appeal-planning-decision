b0ecd1c14e4e6ea08fc00b749cce9f7b4fdc195c
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2025-06-20 10:46:50 +0100

    s20 notify changes

--- packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/get-representation-formatter.js
+++ packages/appeals-service-api/src/routes/v2/appeal-cases/_caseReference/get-representation-formatter.js
@@ -1,11 +1,11 @@
 const { CASE_TYPES } = require('@pins/common/src/database/data-static');
 const {
-	formatter: s78Formatter
-} = require('../../../../services/back-office-v2/formatters/s78/representation');
+	formatter: s78s20Formatter
+} = require('../../../../services/back-office-v2/formatters/s78s20/representation');
 
 /**
- * @typedef {import('../../../../services/back-office-v2/formatters/s78/representation').TypedRepresentationSubmission} TypedRepresentationSubmission
- * @typedef {import('../../../../services/back-office-v2/formatters/s78/representation').RepresentationTypes} RepresentationTypes
+ * @typedef {import('../../../../services/back-office-v2/formatters/s78s20/representation').TypedRepresentationSubmission} TypedRepresentationSubmission
+ * @typedef {import('../../../../services/back-office-v2/formatters/s78s20/representation').RepresentationTypes} RepresentationTypes
  *
  */
 
@@ -16,7 +16,8 @@ const {
 const getFormatter = (appealTypeCode) => {
 	switch (appealTypeCode) {
 		case CASE_TYPES.S78.processCode:
-			return s78Formatter;
+		case CASE_TYPES.S20.processCode:
+			return s78s20Formatter;
 		default:
 			throw new Error('unknown formatter');
 	}
