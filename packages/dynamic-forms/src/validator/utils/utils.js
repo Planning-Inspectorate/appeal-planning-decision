018e2effb2d11abd52f28da8ebdfb66d01e88179
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-07-04 13:13:59 +0100

    fix: remove duplicate code

--- packages/dynamic-forms/src/validator/utils/utils.js
+++ packages/dynamic-forms/src/validator/utils/utils.js
@@ -31,28 +31,4 @@ function getAddMoreIfPresent(req, questionObj) {
 	return questionObj;
 }
 
-/**
- * Converts booleans in the LPAQuestionnaireSubmission db model into yes/no strings
- * @param {Record<any, any>} dbResponse
- */
-
-function mapDBResponseToJourneyResponseFormat(dbResponse) {
-	return Object.entries(dbResponse).reduce(
-		(acc, [key, value]) => ({
-			...acc,
-			[key]: (() => {
-				switch (value) {
-					case true:
-						return 'yes';
-					case false:
-						return 'no';
-					default:
-						return value;
-				}
-			})()
-		}),
-		{}
-	);
-}
-
-module.exports = { getAddMoreIfPresent, mapDBResponseToJourneyResponseFormat };
+module.exports = { getAddMoreIfPresent };
