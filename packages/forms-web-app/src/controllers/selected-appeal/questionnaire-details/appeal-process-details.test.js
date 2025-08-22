2d56e42f81fadf1e8c6daf83dea9971a3e1e6a63
Andy Mair Solirius <Andy.Mair.FT@planninginspectorate.gov.uk>
2025-08-22 09:17:33 +0100

    feat: a2-3746_cas_planning_questionnaire_dashboard WIP

--- packages/forms-web-app/src/controllers/selected-appeal/questionnaire-details/appeal-process-details.test.js
+++ packages/forms-web-app/src/controllers/selected-appeal/questionnaire-details/appeal-process-details.test.js
@@ -1,6 +1,8 @@
 const { APPEAL_CASE_PROCEDURE } = require('@planning-inspectorate/data-model');
 const { appealProcessRows } = require('./appeal-process-details-rows');
 const { fieldNames } = require('@pins/common/src/dynamic-forms/field-names');
+const { CASE_TYPES } = require('@pins/common/src/database/data-static');
+const { caseTypeLPAQFactory } = require('./test-factory');
 
 describe('appealProcessRows', () => {
 	it('should create rows with correct data if relevant case data fields exist & field values populated', () => {
@@ -68,5 +70,37 @@ describe('appealProcessRows', () => {
 		expect(rows[1].valueText).toEqual('No');
 		expect(rows[2].condition()).toEqual(false);
 		expect(rows[3].condition()).toEqual(false);
+	});
+});
+
+const hasLPAQData = caseTypeLPAQFactory(CASE_TYPES.HAS.processCode, 'appealProcess');
+const casPlanningLPAQData = caseTypeLPAQFactory(
+	CASE_TYPES.CAS_PLANNING.processCode,
+	'appealProcess'
+);
+const s78LPAQData = caseTypeLPAQFactory(CASE_TYPES.S78.processCode, 'appealProcess');
+const s20LPAQData = caseTypeLPAQFactory(CASE_TYPES.S20.processCode, 'appealProcess');
+
+const expectedRowsHas = ['Appeals near the site', 'Appeal references', 'Extra conditions'];
+const expectedRowsS78 = [
+	'Appeal procedure',
+	'Appeals near the site',
+	'Appeal references',
+	'Extra conditions'
+];
+
+describe.only.each([
+	['HAS', hasLPAQData, expectedRowsHas],
+	['CAS Planning', casPlanningLPAQData, expectedRowsHas],
+	['S78', s78LPAQData, expectedRowsS78],
+	['S20', s20LPAQData, expectedRowsS78]
+])('expected min rows for %s appeal', (appealType, caseData, expectedRows) => {
+	it(`should create correct rows for appeal type ${appealType}`, () => {
+		const visibleRows = appealProcessRows(caseData)
+			.filter((row) => row.condition(caseData))
+			.map((visibleRow) => visibleRow.keyText);
+
+		console.log(appealType, visibleRows);
+		expect(visibleRows).toEqual(expectedRows);
 	});
 });
