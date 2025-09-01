3d145ba3b4f2793ee13ebfed23921b6b813d938c
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-09-01 08:36:02 +0100

    chore: use the data-model enum values for message event types

--- packages/appeals-service-api/src/event-client/event-type.js
+++ packages/appeals-service-api/src/event-client/event-type.js
@@ -1,12 +1,15 @@
+const { MESSAGE_EVENT_TYPE } = require('@planning-inspectorate/data-model');
+
 /**
  * @enum {string} EventType
+ * this re-exports the data-model enums to avoid refactoring all the code
  */
 const EventType = {
-	Create: 'Create',
-	Update: 'Update',
-	Delete: 'Delete',
-	Publish: 'Publish',
-	Unpublish: 'Unpublish'
+	Create: MESSAGE_EVENT_TYPE.CREATE,
+	Update: MESSAGE_EVENT_TYPE.UPDATE,
+	Delete: MESSAGE_EVENT_TYPE.DELETE,
+	Publish: MESSAGE_EVENT_TYPE.PUBLISH,
+	Unpublish: MESSAGE_EVENT_TYPE.UNPUBLISH
 };
 
 module.exports = { EventType };
