05750e0c96d470734119c4dfd92b17f48911896f
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-09-22 14:49:38 +0100

    fix: appinsights request logging

--- packages/document-service-api/src/main.js
+++ packages/document-service-api/src/main.js
@@ -5,17 +5,25 @@
  * configures
  */
 const appInsights = require('applicationinsights');
-const logger = require('./lib/logger');
-const server = require('./server');
-
 try {
-	appInsights.setup().setAutoDependencyCorrelation(true, true).setSendLiveMetrics(true);
-	appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] =
-		'document-service-api';
-	appInsights.start();
+	if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
+		appInsights
+			.setup()
+			.setAutoDependencyCorrelation(true)
+			.setAutoCollectRequests(true)
+			.setAutoCollectDependencies(true)
+			.setAutoCollectExceptions(true)
+			.setSendLiveMetrics(true);
+		appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] =
+			'document-service-api';
+		appInsights.start();
+	}
 } catch (err) {
 	logger.warn({ err }, 'Application insights failed to start: ');
 }
+
+const logger = require('./lib/logger');
+const server = require('./server');
 
 const main = async () => {
 	server();
