8a7f458e6834835830fa2ff82298fe47c5aadd69
zoebutterworth <zoe.butterworth.9f@planninginspectorate.gov.uk>
2025-09-23 16:05:03 +0100

    feat(web): adding the remaining cas advert lpaq sections (A2-40(85,87,88,96,97))

--- packages/forms-web-app/src/dynamic-forms/adverts-questionnaire/journey.test.js
+++ packages/forms-web-app/src/dynamic-forms/adverts-questionnaire/journey.test.js
@@ -73,8 +73,9 @@ describe.each([
 });
 
 describe('CAS advert LPAQ sections', () => {
+	const journey = new Journey({ ...casAdverts, response: mockResponseCasAdverts });
+
 	it('should have the correct questions in the Constraints, designations and other issues section', () => {
-		const journey = new Journey({ ...casAdverts, response: mockResponseCasAdverts });
 		const CONSTRAINTS_SECTION_INDEX = 0;
 		const APPEAL_TYPE_INDEX = 0;
 		const AFFECT_LISTED_BUILDING_INDEX = 1;
@@ -122,5 +123,155 @@ describe('CAS advert LPAQ sections', () => {
 		expect(
 			journey.sections[CONSTRAINTS_SECTION_INDEX].questions[DESIGNATED_SITES_INDEX].question
 		).toBe('Is the development in, near or likely to affect any designated sites?');
+	});
+
+	it('should have the correct questions in the Notifying relevant parties section', () => {
+		const NOTIFYING_SECTION_INDEX = 1;
+		const NOTIFYING_APPLICATION_INDEX = 0;
+		const HOW_NOTIFIED_INDEX = 1;
+		const SITE_NOTICE_INDEX = 2;
+		const LETTERS_EMAILS_INDEX = 3;
+		const PRESS_ADVERT_INDEX = 4;
+		const NOTIFICATION_LETTER_INDEX = 5;
+
+		expect(
+			journey.sections[NOTIFYING_SECTION_INDEX].questions[NOTIFYING_APPLICATION_INDEX].question
+		).toBe('Who did you notify about this application?');
+		expect(journey.sections[NOTIFYING_SECTION_INDEX].questions[HOW_NOTIFIED_INDEX].question).toBe(
+			'How did you notify relevant parties about the planning application?'
+		);
+		expect(journey.sections[NOTIFYING_SECTION_INDEX].questions[SITE_NOTICE_INDEX].question).toBe(
+			'Upload the site notice'
+		);
+		expect(journey.sections[NOTIFYING_SECTION_INDEX].questions[LETTERS_EMAILS_INDEX].question).toBe(
+			'Upload letters or emails sent to interested parties with their addresses'
+		);
+		expect(journey.sections[NOTIFYING_SECTION_INDEX].questions[PRESS_ADVERT_INDEX].question).toBe(
+			'Upload the press advertisement'
+		);
+		expect(
+			journey.sections[NOTIFYING_SECTION_INDEX].questions[NOTIFICATION_LETTER_INDEX].question
+		).toBe('Upload the appeal notification letter and the list of people that you notified');
+	});
+
+	it('should have the correct questions in the Consultation responses and representations section', () => {
+		const CONSULTATION_SECTION_INDEX = 2;
+		const CONSULTEES_INDEX = 0;
+		const ANY_REPRESENTATIONS_INDEX = 1;
+		const REPRESENTATIONS_UPLOAD_INDEX = 2;
+
+		expect(journey.sections[CONSULTATION_SECTION_INDEX].questions[CONSULTEES_INDEX].question).toBe(
+			'Did you consult all the relevant statutory consultees about the development?'
+		);
+		expect(
+			journey.sections[CONSULTATION_SECTION_INDEX].questions[ANY_REPRESENTATIONS_INDEX].question
+		).toBe('Did you receive representations from members of the public or other parties?');
+		expect(
+			journey.sections[CONSULTATION_SECTION_INDEX].questions[REPRESENTATIONS_UPLOAD_INDEX].question
+		).toBe('Upload the representations');
+	});
+
+	it('should have the correct questions in the Planning officer’s report and supporting documents section', () => {
+		const PLANNING_OFFICER_SECTION_INDEX = 3;
+		const REPORT_INDEX = 0;
+		const HIGHWAY_SAFETY_INDEX = 1;
+		const PHOTOS_AND_PLANS_INDEX = 2;
+		const POLICIES_INDEX = 3;
+		const UPLOAD_POLICIES_INDEX = 4;
+		const EMERGING_PLAN_INDEX = 5;
+		const UPLOAD_EMERGING_PLAN_INDEX = 6;
+		const OTHER_POLICIES_INDEX = 7;
+		const UPLOAD_OTHER_POLICIES_INDEX = 8;
+		const SUPPLIMENTARY_DOCS_INDEX = 9;
+		const UPLOAD_SUPPLIMENTARY_DOCS_INDEX = 10;
+
+		expect(journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[REPORT_INDEX].question).toBe(
+			'Upload the planning officer’s report or what your decision notice would have said'
+		);
+		expect(
+			journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[HIGHWAY_SAFETY_INDEX].question
+		).toBe('Did you refuse the application because of highway or traffic public safety?');
+		expect(
+			journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[PHOTOS_AND_PLANS_INDEX].question
+		).toBe('Did the appellant submit complete and accurate photographs and plans?');
+		expect(
+			journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[POLICIES_INDEX].question
+		).toBe('Do you have any relevant policies from your statutory development plan?');
+		expect(
+			journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[UPLOAD_POLICIES_INDEX].question
+		).toBe('Upload relevant policies from your statutory development plan');
+		expect(
+			journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[EMERGING_PLAN_INDEX].question
+		).toBe('Do you have an emerging plan that is relevant to this appeal?');
+		expect(
+			journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[UPLOAD_EMERGING_PLAN_INDEX]
+				.question
+		).toBe('Upload the emerging plan and supporting information');
+		expect(
+			journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[OTHER_POLICIES_INDEX].question
+		).toBe('Do you have any other relevant policies to upload?');
+		expect(
+			journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[UPLOAD_OTHER_POLICIES_INDEX]
+				.question
+		).toBe('Upload any other relevant policies');
+		expect(
+			journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[SUPPLIMENTARY_DOCS_INDEX].question
+		).toBe(
+			'Did any supplementary planning documents inform the outcome of the planning application?'
+		);
+		expect(
+			journey.sections[PLANNING_OFFICER_SECTION_INDEX].questions[UPLOAD_SUPPLIMENTARY_DOCS_INDEX]
+				.question
+		).toBe('Upload relevant policy extracts and supplementary planning documents');
+	});
+
+	it('should have the correct questions in the Site access section', () => {
+		const SITE_ACCESS_SECTION_INDEX = 4;
+		const INSPECTOR_ACCESS_INDEX = 0;
+		const NEIGHBOUR_ACCESS_INDEX = 1;
+		const NEIGHBOUR_ADDRESS_INDEX = 2;
+		const SAFETY_RISK_INDEX = 3;
+
+		expect(
+			journey.sections[SITE_ACCESS_SECTION_INDEX].questions[INSPECTOR_ACCESS_INDEX].question
+		).toBe('Might the inspector need access to the appellant’s land or property?');
+		expect(
+			journey.sections[SITE_ACCESS_SECTION_INDEX].questions[NEIGHBOUR_ACCESS_INDEX].question
+		).toBe('Might the inspector need to enter a neighbour’s land or property?');
+		expect(
+			journey.sections[SITE_ACCESS_SECTION_INDEX].questions[NEIGHBOUR_ADDRESS_INDEX].question
+		).toBe('Do you want to add another neighbour to be visited?');
+		expect(journey.sections[SITE_ACCESS_SECTION_INDEX].questions[SAFETY_RISK_INDEX].question).toBe(
+			'Add potential safety risks'
+		);
+	});
+
+	it('should have the correct questions in the Appeal process section', () => {
+		const APPEAL_PROCESS_SECTION_INDEX = 5;
+		const PROCEDURE_TYPE_INDEX = 0;
+		const INQUIRY_INDEX = 1;
+		const HEARING_INDEX = 2;
+		const ONGOING_APPEALS_INDEX = 3;
+		const ONGOING_APPEALS_REF_INDEX = 4;
+		const NEW_CONDITIONS_INDEX = 5;
+
+		expect(
+			journey.sections[APPEAL_PROCESS_SECTION_INDEX].questions[PROCEDURE_TYPE_INDEX].question
+		).toBe('Which procedure do you think is most appropriate for this appeal?');
+		expect(journey.sections[APPEAL_PROCESS_SECTION_INDEX].questions[INQUIRY_INDEX].question).toBe(
+			'Why would you prefer an inquiry?'
+		);
+		expect(journey.sections[APPEAL_PROCESS_SECTION_INDEX].questions[HEARING_INDEX].question).toBe(
+			'Why would you prefer a hearing?'
+		);
+		expect(
+			journey.sections[APPEAL_PROCESS_SECTION_INDEX].questions[ONGOING_APPEALS_INDEX].question
+		).toBe('Are there any other ongoing appeals next to, or close to the site?');
+		expect(
+			journey.sections[APPEAL_PROCESS_SECTION_INDEX].questions[ONGOING_APPEALS_REF_INDEX].question
+		).toBe('Add another appeal?');
+		expect(
+			journey.sections[APPEAL_PROCESS_SECTION_INDEX].questions[NEW_CONDITIONS_INDEX].question
+		).toBe('Check if there are any new conditions');
 	});
 });
--- packages/forms-web-app/src/dynamic-forms/docs/adverts-lpaq-journey.md
+++ packages/forms-web-app/src/dynamic-forms/docs/adverts-lpaq-journey.md
@@ -24,11 +24,107 @@ condition: () => questionHasAnswer(response, questions.conservationArea, 'yes');
 - boolean `/area-of-outstanding-natural-beauty/` Is the appeal site in an area of outstanding natural beauty?
 - checkbox `/designated-sites/` Is the development in, near or likely to affect any designated sites?
 
-## Planning officer's report and supporting documents
+## Notifying relevant parties
 
+- multi-file-upload `/upload-who-you-notified/` Who did you notify about this application?
+- checkbox `/notification-type/` How did you notify relevant parties about the planning application?
+- multi-file-upload `/upload-site-notice/` Upload the site notice
+
+```js
+condition: () => questionHasAnswer(response, questions.howYouNotifiedPeople, 'site-notice');
+```
+
+- multi-file-upload `/letters-interested-parties/` Upload letters or emails sent to interested parties with their addresses
+
+```js
+condition: () => questionHasAnswer(response, questions.howYouNotifiedPeople, 'letters-or-emails');
+```
+
+- multi-file-upload `/upload-press-advert/` Upload the press advertisement
+
+```js
+condition: () => questionHasAnswer(response, questions.howYouNotifiedPeople, 'advert');
+```
+
+- multi-file-upload `/appeal-notification-letter/` Upload the appeal notification letter and the list of people that you notified
+
+## Consultation responses and representations
+
+- radio `/statutory-consultees/` Did you consult all the relevant statutory consultees about the development?
+- boolean `/representations/` Did you receive representations from members of the public or other parties?
+- multi-file-upload `/upload-representations/` Upload the representations
+
+```js
+condition: () => questionHasAnswer(response, questions.representationsFromOthers, 'yes');
+```
+
+## Planning officer’s report and supporting documents
+
+- multi-file-upload `/upload-planning-officers-report-decision-notice/` Upload the planning officer’s report or what your decision notice would have said
 - boolean `/public-safety/` Did you refuse the application because of highway or traffic public safety?
 - boolean `/accurate-photographs-plans/` Did the appellant submit complete and accurate photographs and plans?
+- boolean `/other-development-plan-policies/` Do you have any relevant policies from your statutory development plan?
+- multi-file-upload `/upload-development-plan-policies/` Upload relevant policies from your statutory development plan
+
+```js
+condition: () => questionHasAnswer(response, questions.developmentPlanPolicies, 'yes');
+```
+
+- boolean `/emerging-plan/` Do you have an emerging plan that is relevant to this appeal?
+- multi-file-upload `/upload-emerging-plan/` Upload the emerging plan and supporting information
+
+```js
+condition: () => questionHasAnswer(response, questions.emergingPlan, 'yes');
+```
+
+- boolean `/other-relevant-policies/` Do you have any other relevant policies to upload?
+- multi-file-upload `/upload-other-relevant-policies/` Upload any other relevant policies
+
+```js
+condition: () => questionHasAnswer(response, questions.otherRelevantPolicies, 'yes');
+```
+
+- boolean `/supplementary-planning-documents/` Did any supplementary planning documents inform the outcome of the planning application?
+- multi-file-upload `/upload-policies-supplementary-planning-documents/` Upload relevant policy extracts and supplementary planning documents
+
+```js
+condition: () => questionHasAnswer(response, questions.supplementaryPlanning, 'yes');
+```
+
+## Site access
+
+- radio `/inspector-access-appeal-site/` Might the inspector need access to the appellant’s land or property?
+- radio `/inspector-enter-neighbour-site/` Might the inspector need to enter a neighbour’s land or property?
+- list-add-more `/neighbour-address/` Do you want to add another neighbour to be visited?
+
+```js
+condition: () => questionHasAnswer(response, questions.neighbouringSite, 'yes');
+```
+
+- radio `/potential-safety-risks/` Add potential safety risks
 
 ## Appeal process
+
+- radio `/procedure-type/` Which procedure do you think is most appropriate for this appeal?
+- text-entry `/lpaPreferInquiryDetails/` Why would you prefer an inquiry?
+
+```js
+condition: () =>
+	questionHasAnswer(response, questions.procedureType, APPEAL_CASE_PROCEDURE.INQUIRY);
+```
+
+- text-entry `/lpaPreferHearingDetails/` Why would you prefer a hearing?
+
+```js
+condition: () =>
+	questionHasAnswer(response, questions.procedureType, APPEAL_CASE_PROCEDURE.HEARING);
+```
+
+- boolean `/ongoing-appeals/` Are there any other ongoing appeals next to, or close to the site?
+- list-add-more `/appeal-reference-number/` Add another appeal?
+
+```js
+condition: () => questionHasAnswer(response, questions.appealsNearSite, 'yes');
+```
 
 - radio `/new-conditions/` Check if there are any new conditions
