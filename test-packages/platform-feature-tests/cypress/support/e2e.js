44966d472c4bfb212ef8772fc6daabeb988f5daa
Satti <lakshmi.satti.vb@planninginspectorate.gov.uk>
2025-10-12 09:00:07 +0100

    test: exception handling for ip comments

--- test-packages/platform-feature-tests/cypress/support/e2e.js
+++ test-packages/platform-feature-tests/cypress/support/e2e.js
@@ -23,5 +23,16 @@ registerCypressGrep();
 
 require('cy-verify-downloads').addCustomCommand();
 
+// Ignore transient AAD CDN script load errors that should not fail user flows
+Cypress.on('uncaught:exception', (err) => {
+  const msg = err && err.message ? err.message : '';
+  if (/Failed to load external resource.*aadcdn\.msftauth\.net/i.test(msg)) {
+    // return false => prevent Cypress from failing the test
+    return false;
+  }
+  // allow other errors to fail tests
+  return true;
+});
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
