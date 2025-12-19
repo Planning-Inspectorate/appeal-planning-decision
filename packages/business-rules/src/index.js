2fbbccc13dd44517630d90ae3d442171fb140599
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-12-19 17:42:13 +0000

    feat(ad-35): add bops api lookup

--- packages/business-rules/src/index.js
+++ packages/business-rules/src/index.js
@@ -3,11 +3,13 @@ const validation = require('./validation');
 const schemas = require('./schemas');
 const constants = require('./constants');
 const config = require('./config');
+const mappings = require('./mappings');
 
 module.exports = {
 	rules,
 	validation,
 	schemas,
 	constants,
-	config
+	config,
+	mappings
 };
