d1887b5bff7d56784beae94d49dcfff5d74538bf
zoebutterworth <zoe.butterworth.9f@planninginspectorate.gov.uk>
2026-01-08 14:27:59 +0000

    feat(common): create ldc feature flag (A2-5516)

--- packages/common/src/feature-flags.js
+++ packages/common/src/feature-flags.js
@@ -3,6 +3,7 @@ const FLAG = {
 	CAS_ADVERTS_APPEAL_FORM_V2: 'cas-adverts-appeal-form-v2',
 	CAS_PLANNING_APPEAL_FORM_V2: 'cas-planning-appeal-form-v2',
 	ENFORCEMENT_APPEAL_FORM_V2: 'enforcement-appeal-form-v2',
+	LDC_APPEAL_FORM_V2: 'ldc-appeal-form-v2',
 	APPLICATION_API_LOOKUP: 'enable-application-api-lookup'
 };
 
