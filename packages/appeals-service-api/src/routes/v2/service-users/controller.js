a1e1e00fea1c91a5eaa98e28911327fb97baef95
lachlandow1 <lachlan.dow@solirius.com>
2026-01-28 16:17:43 +0000

    Revert "feat(A2-5707): add notify emails for Rule 6 user added/updated"
    This reverts commit 54034d959d92dd2db85a866a6c17599624e66537.

--- packages/appeals-service-api/src/routes/v2/service-users/controller.js
+++ packages/appeals-service-api/src/routes/v2/service-users/controller.js
@@ -1,54 +1,12 @@
 const { APPEAL_USER_ROLES } = require('@pins/common/src/constants');
 const { getUserByEmail, unlinkUserFromAppeal } = require('../users/service');
-const { put, getForEmailCaseAndType, getServiceUserById } = require('./service');
-const { SERVICE_USER_TYPE } = require('@planning-inspectorate/data-model');
-const { getCaseAndAppellant } = require('../appeal-cases/service');
-const {
-	sendRule6PartyAddedEmailToRule6Party,
-	sendRule6PartyAddedEmailToMainParties,
-	sendRule6PartyUpdatedEmailToMainParties
-} = require('#lib/notify');
+const { put } = require('./service');
 
 /**
  * @type {import('express').RequestHandler}
  */
 exports.put = async (req, res) => {
-	const { emailAddress, caseReference, serviceUserType, id } = req.body;
-	let isNewRule6Party = false;
-	if (serviceUserType === SERVICE_USER_TYPE.RULE_6_PARTY) {
-		let existingUser;
-		if (id) {
-			existingUser = await getServiceUserById(id);
-		}
-
-		if (!existingUser) {
-			existingUser = await getForEmailCaseAndType(emailAddress, caseReference, [
-				SERVICE_USER_TYPE.RULE_6_PARTY
-			]);
-		}
-
-		if (existingUser) {
-			req.body.id = existingUser.id;
-		} else {
-			isNewRule6Party = true;
-		}
-	}
-
 	const serviceUser = await put(req.body);
-
-	if (serviceUser && serviceUser.serviceUserType === SERVICE_USER_TYPE.RULE_6_PARTY) {
-		const appealCase = await getCaseAndAppellant({ caseReference: serviceUser.caseReference });
-
-		if (appealCase) {
-			if (isNewRule6Party) {
-				await sendRule6PartyAddedEmailToRule6Party(serviceUser, appealCase);
-				await sendRule6PartyAddedEmailToMainParties(serviceUser, appealCase);
-			} else {
-				await sendRule6PartyUpdatedEmailToMainParties(serviceUser, appealCase);
-			}
-		}
-	}
-
 	res.send(serviceUser);
 };
 
