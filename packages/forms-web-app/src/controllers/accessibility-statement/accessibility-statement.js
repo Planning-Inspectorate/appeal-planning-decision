e6e1590c5ab159c61dfe58b787834a93fe68317e
Kade Neveling <kade.neveling@solirius.com>
2026-03-09 12:43:44 +0000

    fix(web): updated accessibility statement and header (A2-4025)
    fix(web): removed outdated accessibility statement non-complianec pt (A2-4025)

--- packages/forms-web-app/src/controllers/accessibility-statement/accessibility-statement.js
+++ packages/forms-web-app/src/controllers/accessibility-statement/accessibility-statement.js
@@ -1,5 +1,13 @@
 const { VIEW } = require('../../lib/views');
 
-exports.getAccessibilityStatement = async (_, res) => {
-	res.render(VIEW.ACCESSIBILITY_STATEMENT);
+exports.getAccessibilityStatement = async (req, res) => {
+	let layoutTemplate = 'layouts/full-appeal-banner/main.njk';
+
+	if (req.session?.user?.isLpaUser) {
+		layoutTemplate = 'layouts/lpa-dashboard/main.njk';
+	}
+
+	res.render(VIEW.ACCESSIBILITY_STATEMENT, {
+		layoutTemplate
+	});
 };
