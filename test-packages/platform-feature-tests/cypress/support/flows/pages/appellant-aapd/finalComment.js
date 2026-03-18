6a475edc81846c13eed2bda4263042be2e0855f6
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2026-03-18 08:33:55 +0000

    test: final comments how submit added for automation tests

--- test-packages/platform-feature-tests/cypress/support/flows/pages/appellant-aapd/finalComment.js
+++ test-packages/platform-feature-tests/cypress/support/flows/pages/appellant-aapd/finalComment.js
@@ -10,40 +10,28 @@ export class FinalComment {
         if (context?.submitFinalComments?.selectAnswer) {
             cy.getByData(basePage?._selectors.answerYes).click();
             cy.advanceToNextPage();
-            // Add your Final Comments
-            cy.get('#appellantFinalCommentDetails').clear();
-            cy.get('#appellantFinalCommentDetails').type("Final comment test");
-            cy.get('#sensitiveInformationCheckbox').check({ force: true });
+            const howSubmitType = context?.howSubmitFinalComments?.type || 'text';
+            basePage.selectRadioBtn(howSubmitType);
             cy.advanceToNextPage();
-            //Upload your witnesses and their evidence
-            if (context?.additionalDocuments?.selectAnswer) {
-                cy.getByData(basePage?._selectors?.answerYes).click();
+
+            if (howSubmitType === 'text') {
+                cy.get('#appellantFinalCommentDetails').clear();
+                cy.get('#appellantFinalCommentDetails').type("Final comment test");
+                cy.get('#sensitiveInformationCheckbox').check({ force: true });
                 cy.advanceToNextPage();
-                cy.get('body').then($body => {
-                    if ($body.find('button[name="delete"]:contains("Remove")').length > 0) {
-                        cy.contains('button[name="delete"]', 'Remove').click();
-                    }
-                });
-                // cy.contains('button[name="delete"]','Remove',{timeout:0,log:false}).then($btn => {
-                //     if ($btn.length > 0) {
-                //         cy.wrap($btn).click();
-                //     }
-                // })
-                cy.uploadFileFromFixtureDirectories(context?.documents?.uploadAdditionalDocsSupportFinalComments);
-                cy.advanceToNextPage();
-            } else {
-                cy.getByData(basePage?._selectors?.answerNo).click();
+            } else if (howSubmitType === 'document') {
+                cy.uploadFileFromFixtureDirectories(context?.documents?.uploadSupportDocsFinalComments);
                 cy.advanceToNextPage();
             }
 
         } else {
             cy.getByData(basePage?._selectors.answerNo).click();
             cy.advanceToNextPage();
-        //     cy.contains('button[name="delete"]','Remove',{timeout:0,log:false}).then($btn => {
-        //         if ($btn.length > 0) {
-        //             cy.advanceToNextPage();
-        //         }
-        //     })
+            //     cy.contains('button[name="delete"]','Remove',{timeout:0,log:false}).then($btn => {
+            //         if ($btn.length > 0) {
+            //             cy.advanceToNextPage();
+            //         }
+            //     })
         }
     };
 }
