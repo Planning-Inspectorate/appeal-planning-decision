9f7425beb2871350fa7e0eb97b3b6ac6eb95407f
gmmagruder <gabrielle.magruder@gmail.com>
2026-03-24 11:58:50 +0000

    feat(web): add conditional text boxes to any significant changes page (A2-7571)

---
+++ packages/database/src/migrations/20260324095449_additional_fields_for_part_1_form/migration.sql
@@ -0,0 +1,21 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[AppellantSubmission] ADD [anySignificantChanges_courtJudgementSignificantChanges] NVARCHAR(max),
+[anySignificantChanges_localPlanSignificantChanges] NVARCHAR(max),
+[anySignificantChanges_nationalPolicySignificantChanges] NVARCHAR(max);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
