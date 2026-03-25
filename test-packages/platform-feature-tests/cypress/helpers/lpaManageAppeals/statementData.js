38ea3004af5c16abcfcdb6204150cd271a622647
Lpsatti <Lakshmi.Satti.VB@planninginspectorate.gov.uk>
2026-03-25 15:05:13 +0000

    test: statement submit changes for appellant updated automation

--- test-packages/platform-feature-tests/cypress/helpers/lpaManageAppeals/statementData.js
+++ test-packages/platform-feature-tests/cypress/helpers/lpaManageAppeals/statementData.js
@@ -1,6 +1,6 @@
-const documents = {    
+const documents = {
     uploadStatement: 'upload-statement.pdf',
-    uploadFileGreaterThan25mb:'greater-than-25-mb.docx',
+    uploadFileGreaterThan25mb: 'greater-than-25-mb.docx',
     uploadWrongFormatFile: 'wrongFormatFile.xps',
     uploadEmergingPlan: 'emerging-plan.pdf',
     uploadOtherPolicies: 'other-policies.pdf',
@@ -9,14 +9,72 @@ const documents = {
 };
 
 export const statementTestCases = [
-    {  
-        additionalDocument:{
+    {
+        appealType:"Plannig",
+        additionalDocument: {
             selectAnswer: false
         },
         documents,
     },
-    {  
-        additionalDocument:{
+    {
+        appealType:"Planning",
+        additionalDocument: {
+            selectAnswer: true
+        },
+        documents,
+    },
+    {
+        appealType:"Enforcement notice",
+        additionalDocument: {
+            selectAnswer: false
+        },
+        documents,
+    },
+    {
+        appealType:"Enforcement notice",
+        additionalDocument: {
+            selectAnswer: true
+        },
+        documents,
+    },
+    {
+        appealType:"Lawful development certificate",
+        additionalDocument: {
+            selectAnswer: false
+        },
+        documents,
+    },
+    {
+        appealType:"Lawful development certificate",
+        additionalDocument: {
+            selectAnswer: true
+        },
+        documents,
+    },
+    {
+        appealType:"Advertisement",
+        additionalDocument: {
+            selectAnswer: false
+        },
+        documents,
+    },
+    {
+        appealType:"Advertisement",
+        additionalDocument: {
+            selectAnswer: true
+        },
+        documents,
+    },    
+    {
+        appealType:"Enforcement listed building and conservation area",
+        additionalDocument: {
+            selectAnswer: false
+        },
+        documents,
+    },
+    {
+        appealType:"Enforcement listed building and conservation area",
+        additionalDocument: {
             selectAnswer: true
         },
         documents,
