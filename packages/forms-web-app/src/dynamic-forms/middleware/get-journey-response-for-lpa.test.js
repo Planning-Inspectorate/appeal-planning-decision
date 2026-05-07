fa79c2a9bffb2b71f8d5c3b59cbfbe770d8701c5
TurnerMichael <michael.turner@planninginspectorate.gov.uk>
2026-05-07 11:01:10 +0100

    feat(web): added test to check correct journey type (A2-7808)

--- packages/forms-web-app/src/dynamic-forms/middleware/get-journey-response-for-lpa.test.js
+++ packages/forms-web-app/src/dynamic-forms/middleware/get-journey-response-for-lpa.test.js
@@ -1,3 +1,6 @@
+const { isExpeditedPart1Eligible } = require('#lib/is-expedited-part1-eligible');
+jest.mock('#lib/is-expedited-part1-eligible');
+
 const getJourneyResponse = require('./get-journey-response-for-lpa');
 const { JourneyResponse } = require('@pins/dynamic-forms/src/journey-response');
 const { getUserFromSession } = require('../../services/user.service');
@@ -5,6 +8,7 @@ const { JOURNEY_TYPES } = require('@pins/common/src/dynamic-forms/journey-types'
 const { mapDBResponseToJourneyResponseFormat } = require('./utils');
 const { ApiClientError } = require('@pins/common/src/client/api-client-error.js');
 const { LPA_USER_ROLE } = require('@pins/common/src/constants');
+const { CASE_TYPES } = require('@pins/common/src/database/data-static');
 
 jest.mock('../../services/user.service');
 jest.mock('./utils');
@@ -22,6 +26,16 @@ describe('getJourneyResponse', () => {
 	const invalidLpaUser = {};
 	const mockAppeal = { appealTypeCode: 'HAS', lpaQuestionnaireDueDate: new Date() };
 	const testDBResponse = { answer1: '1', AppealCase: { LPACode: testLPACode } };
+	const appealTypeCode = CASE_TYPES.HAS.processCode;
+	const submissionId = 'sub-123';
+	const lpaCode = 'Q9999';
+	const mockSubmission = {
+		id: submissionId,
+		LPACode: lpaCode,
+		appealTypeCode,
+		lpaQuestionnaireDueDate: new Date()
+	};
+	const convertedResponse = { foo: 'bar' };
 
 	beforeEach(() => {
 		req = {
@@ -30,17 +44,23 @@ describe('getJourneyResponse', () => {
 			},
 			session: {
 				navigationHistory: ['']
+			},
+			appeal: {
+				lpaQuestionnaireDueDate: new Date()
 			}
 		};
 
 		req.appealsApiClient = {
 			getUsersAppealCase: jest.fn(),
 			getLPAQuestionnaire: jest.fn(),
-			postLPAQuestionnaire: jest.fn()
+			postLPAQuestionnaire: jest.fn(),
+			getAppellantSubmission: jest.fn()
 		};
 
 		const mockResponse = () => {
-			const res = {};
+			const res = {
+				locals: {}
+			}
 			res.status = jest.fn().mockReturnValue(res);
 			res.json = jest.fn().mockReturnValue(res);
 			res.render = jest.fn().mockReturnValue(res);
@@ -52,6 +72,9 @@ describe('getJourneyResponse', () => {
 		};
 		res = mockResponse();
 		next = jest.fn();
+		mapDBResponseToJourneyResponseFormat.mockReturnValue(convertedResponse);
+		require('../../lib/is-lpa-in-feature-flag').isLpaInFeatureFlag.mockResolvedValue(true);
+		isExpeditedPart1Eligible.mockReturnValue(false);
 		jest.clearAllMocks();
 	});
 
@@ -164,5 +187,35 @@ describe('getJourneyResponse', () => {
 
 		expect(req.appealsApiClient.getLPAQuestionnaire).not.toHaveBeenCalled();
 		expect(next).not.toHaveBeenCalled();
+	});
+
+	it('should route to JOURNEY_TYPES.S78_QUESTIONNAIRE_PART_1.id when eligible for expedited Part 1', async () => {
+		getUserFromSession.mockReturnValue(mockValidTestLpaUser);
+		const s78Appeal = {
+			...mockSubmission,
+			appealTypeCode: CASE_TYPES.S78.processCode,
+			lpaQuestionnaireDueDate: new Date(),
+			typeOfPlanningApplication: 'full',
+			applicationDate: '2024-01-01',
+			applicationDecision: 'granted',
+			caseProcedure: undefined // explicitly set to undefined
+		};
+		req.appealsApiClient.getUsersAppealCase.mockResolvedValue(s78Appeal);
+		require('../../lib/is-lpa-in-feature-flag').isLpaInFeatureFlag.mockImplementation(async () => {
+			return true;
+		});
+		isExpeditedPart1Eligible.mockReturnValue(true);
+
+		await getJourneyResponse()(req, res, next);
+
+		expect(res.locals.journeyResponse.journeyId).toBe(JOURNEY_TYPES.S78_QUESTIONNAIRE_PART_1.id);
+		expect(isExpeditedPart1Eligible).toHaveBeenCalledWith({
+			typeOfPlanningApplication: s78Appeal.typeOfPlanningApplication,
+			applicationDate: s78Appeal.applicationDate,
+			eligibility: {
+				applicationDecision: s78Appeal.applicationDecision
+			}
+		});
+		expect(next).toHaveBeenCalled();
 	});
 });
