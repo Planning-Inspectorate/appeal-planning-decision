061f89d28b322c2e1ad0aa93977a66f2be415821
Tom Pickering <tom.pickering@solirius.com>
2026-05-19 16:44:34 +0100

    feat(A2-7789): remove before you start landing page, redirect to select lpa

--- packages/forms-web-app/src/controllers/before-you-start/before-you-start.js
+++ packages/forms-web-app/src/controllers/before-you-start/before-you-start.js
@@ -1,10 +1,4 @@
-const { VIEW } = require('../../lib/views');
-
-exports.getBeforeYouStartFirstPage = async (_, res) => {
-	res.render(VIEW.BEFORE_YOU_START.FIRST_PAGE);
-};
-
-exports.postBeforeYouStartFirstPage = async (req, res) => {
+exports.getBeforeYouStartFirstPage = async (req, res) => {
 	req.session.appeal = undefined;
 	res.redirect('/before-you-start/local-planning-authority');
 };
