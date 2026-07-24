960eb324ec39a3304d4d9b2e345a07af534dbca5
Sam Laskowski <sam.laskowski@outlook.com>
2026-07-24 10:57:06 +0100

    feat(A2-8697): add sharing inquiry event documents feature flag

--- packages/common/src/feature-flags.js
+++ packages/common/src/feature-flags.js
@@ -5,7 +5,8 @@ const FLAG = {
 	LDC_APPEAL_FORM_V2: 'ldc-appeal-form-v2',
 	APPLICATION_API_LOOKUP: 'enable-application-api-lookup',
 	ADVERT_APPELLANT_STATEMENT_ENABLED: 'enable-advert-appellant-statement',
-	SHARING_HEARING_DOCUMENTS: 'sharing-hearing-documents'
+	SHARING_HEARING_DOCUMENTS: 'sharing-hearing-documents',
+	SHARING_INQUIRY_EVENT_DOCUMENTS: 'sharing-inquiry-event-documents'
 };
 
 module.exports = {
