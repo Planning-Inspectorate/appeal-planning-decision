8cd3a65b0b82481604c3d3e11dd1b2aa5d0ea309
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2026-09-22 16:47:45 +0100

    fix(a2-9332): don't login redirect to enter code to avoid loops and broken paths

--- packages/forms-web-app/src/middleware/rule-6/require-user.js
+++ packages/forms-web-app/src/middleware/rule-6/require-user.js
@@ -33,7 +33,9 @@ const requireUser = (req, res, next) => {
 			req.session = {};
 		}
 
-		req.session.loginRedirect = req.originalUrl;
+		if (!req.originalUrl.includes('enter-code')) {
+			req.session.loginRedirect = req.originalUrl;
+		}
 
 		return res.redirect(`/${EMAIL_ADDRESS}`);
 	});
